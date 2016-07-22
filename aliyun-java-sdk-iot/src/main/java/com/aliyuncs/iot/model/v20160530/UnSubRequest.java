ff50da71b469303da34ab3d68696844cdd344b14
归邪 <zhangw@alibaba-inc.com>
2016-07-22 16:20:10 +0800

    aliyun-java-sdk-iot 2.0.1

---
+++ aliyun-java-sdk-iot/src/main/java/com/aliyuncs/iot/model/v20160530/UnSubRequest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.iot.model.v20160530;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UnSubRequest extends RpcAcsRequest<UnSubResponse> {
+	
+	public UnSubRequest() {
+		super("Iot", "2016-05-30", "UnSub");
+	}
+
+	private List<String> topics;
+
+	private Long productKey;
+
+	private String topicList;
+
+	public List<String> getTopics() {
+		return this.topics;
+	}
+
+	public void setTopics(List<String> topics) {
+		this.topics = topics;	
+		for (int i = 0; i < topics.size(); i++) {
+			putQueryParameter("Topic." + (i + 1) , topics.get(i));
+		}	
+	}
+
+	public Long getProductKey() {
+		return this.productKey;
+	}
+
+	public void setProductKey(Long productKey) {
+		this.productKey = productKey;
+		putQueryParameter("ProductKey", productKey);
+	}
+
+	public String getTopicList() {
+		return this.topicList;
+	}
+
+	public void setTopicList(String topicList) {
+		this.topicList = topicList;
+		putQueryParameter("TopicList", topicList);
+	}
+
+	@Override
+	public Class<UnSubResponse> getResponseClass() {
+		return UnSubResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-iot/src/main/java/com/aliyuncs/iot/transform/v20160530/RegistDeviceResponseUnmarshaller.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.iot.transform.v20160530;
+
+import com.aliyuncs.iot.model.v20160530.RegistDeviceResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class RegistDeviceResponseUnmarshaller {
+
+	public static RegistDeviceResponse unmarshall(RegistDeviceResponse registDeviceResponse, UnmarshallerContext context) {
+		
+		registDeviceResponse.setRequestId(context.stringValue("RegistDeviceResponse.RequestId"));
+		registDeviceResponse.setSuccess(context.booleanValue("RegistDeviceResponse.Success"));
+		registDeviceResponse.setErrorMessage(context.stringValue("RegistDeviceResponse.ErrorMessage"));
+		registDeviceResponse.setDeviceId(context.stringValue("RegistDeviceResponse.DeviceId"));
+		registDeviceResponse.setDeviceName(context.stringValue("RegistDeviceResponse.DeviceName"));
+		registDeviceResponse.setDeviceSecret(context.stringValue("RegistDeviceResponse.DeviceSecret"));
+		registDeviceResponse.setDeviceStatus(context.stringValue("RegistDeviceResponse.DeviceStatus"));
+	 
+	 	return registDeviceResponse;
+	}
+}
