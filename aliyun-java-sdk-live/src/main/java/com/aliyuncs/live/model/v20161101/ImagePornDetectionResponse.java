a82cc234ca8951f2b006381ec124c8d76ebbedef
ling.wu <ling.wu@alibaba-inc.com>
2016-12-02 10:44:30 +0800

    add live sdk

---
+++ aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/model/v20161101/ImagePornDetectionResponse.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.live.model.v20161101;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.live.transform.v20161101.ImagePornDetectionResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ImagePornDetectionResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String label;
+
+	private Float rate;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Float getRate() {
+		return this.rate;
+	}
+
+	public void setRate(Float rate) {
+		this.rate = rate;
+	}
+
+	@Override
+	public ImagePornDetectionResponse getInstance(UnmarshallerContext context) {
+		return	ImagePornDetectionResponseUnmarshaller.unmarshall(this, context);
+	}
+}
