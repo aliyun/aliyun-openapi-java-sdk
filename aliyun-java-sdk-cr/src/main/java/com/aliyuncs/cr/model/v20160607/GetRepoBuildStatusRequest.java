a5fffa789935ba718ef9b90d488408e9d5bcee20
ling.wu <ling.wu@alibaba-inc.com>
2017-02-06 19:10:21 +0800

    add 容器镜像服务 sdk

---
+++ aliyun-java-sdk-cr/src/main/java/com/aliyuncs/cr/model/v20160607/GetRepoBuildStatusRequest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.cr.model.v20160607;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetRepoBuildStatusRequest extends RoaAcsRequest<GetRepoBuildStatusResponse> {
+	
+	public GetRepoBuildStatusRequest() {
+		super("cr", "2016-06-07", "GetRepoBuildStatus");
+		setUriPattern("/repos/[RepoNamespace]/[RepoName]/build/[BuildId]/status");
+		setMethod(MethodType.GET);
+	}
+
+	private String repoNamespace;
+
+	private String repoName;
+
+	private String buildId;
+
+	public String getRepoNamespace() {
+		return this.repoNamespace;
+	}
+
+	public void setRepoNamespace(String repoNamespace) {
+		this.repoNamespace = repoNamespace;
+		putPathParameter("RepoNamespace", repoNamespace);
+	}
+
+	public String getRepoName() {
+		return this.repoName;
+	}
+
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+		putPathParameter("RepoName", repoName);
+	}
+
+	public String getBuildId() {
+		return this.buildId;
+	}
+
+	public void setBuildId(String buildId) {
+		this.buildId = buildId;
+		putPathParameter("BuildId", buildId);
+	}
+
+	@Override
+	public Class<GetRepoBuildStatusResponse> getResponseClass() {
+		return GetRepoBuildStatusResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cr/src/main/java/com/aliyuncs/cr/model/v20160607/GetRepoTagsRequest.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.cr.model.v20160607;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetRepoTagsRequest extends RoaAcsRequest<GetRepoTagsResponse> {
+	
+	public GetRepoTagsRequest() {
+		super("cr", "2016-06-07", "GetRepoTags");
+		setUriPattern("/repos/[RepoNamespace]/[RepoName]/tags");
+		setMethod(MethodType.GET);
+	}
+
+	private String repoNamespace;
+
+	private String repoName;
+
+	private Integer page;
+
+	private Integer pageSize;
+
+	public String getRepoNamespace() {
+		return this.repoNamespace;
+	}
+
+	public void setRepoNamespace(String repoNamespace) {
+		this.repoNamespace = repoNamespace;
+		putPathParameter("RepoNamespace", repoNamespace);
+	}
+
+	public String getRepoName() {
+		return this.repoName;
+	}
+
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+		putPathParameter("RepoName", repoName);
+	}
+
+	public Integer getPage() {
+		return this.page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+		putQueryParameter("Page", page);
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		putQueryParameter("PageSize", pageSize);
+	}
+
+	@Override
+	public Class<GetRepoTagsResponse> getResponseClass() {
+		return GetRepoTagsResponse.class;
+	}
+
+}
