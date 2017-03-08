aeea3205a1007b3d142581401d9e363f8960a313
ling.wu <ling.wu@alibaba-inc.com>
2017-03-08 13:32:32 +0800

    cs sdk add new api

---
+++ aliyun-java-sdk-cs/src/main/java/com/aliyuncs/cs/model/v20151215/DeleteClusterNodeRequest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.cs.model.v20151215;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DeleteClusterNodeRequest extends RoaAcsRequest<DeleteClusterNodeResponse> {
+	
+	public DeleteClusterNodeRequest() {
+		super("CS", "2015-12-15", "DeleteClusterNode");
+		setUriPattern("/clusters/[ClusterId]/ip/[Ip]");
+		setMethod(MethodType.DELETE);
+	}
+
+	private String clusterId;
+
+	private String ip;
+
+	private String force;
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+		putPathParameter("ClusterId", clusterId);
+	}
+
+	public String getIp() {
+		return this.ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+		putPathParameter("Ip", ip);
+	}
+
+	public String getForce() {
+		return this.force;
+	}
+
+	public void setForce(String force) {
+		this.force = force;
+		putQueryParameter("Force", force);
+	}
+
+	@Override
+	public Class<DeleteClusterNodeResponse> getResponseClass() {
+		return DeleteClusterNodeResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cs/src/main/java/com/aliyuncs/cs/model/v20151215/GetProjectEventsRequest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.cs.model.v20151215;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetProjectEventsRequest extends RoaAcsRequest<GetProjectEventsResponse> {
+	
+	public GetProjectEventsRequest() {
+		super("CS", "2015-12-15", "GetProjectEvents");
+		setUriPattern("/clusters/[ClusterId]/projects/[ProjectId]/events");
+		setMethod(MethodType.GET);
+	}
+
+	private String clusterId;
+
+	private String projectId;
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+		putPathParameter("ClusterId", clusterId);
+	}
+
+	public String getProjectId() {
+		return this.projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+		putPathParameter("ProjectId", projectId);
+	}
+
+	@Override
+	public Class<GetProjectEventsResponse> getResponseClass() {
+		return GetProjectEventsResponse.class;
+	}
+
+}
