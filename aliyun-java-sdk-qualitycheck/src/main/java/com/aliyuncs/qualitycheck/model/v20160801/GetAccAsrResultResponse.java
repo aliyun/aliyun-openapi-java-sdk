7758d7ea60fbd33c9f7addd1774d3b21778e76db
haowei.yao <haowei.yao@alibaba-inc.com>
2017-10-25 14:01:22 +0800

    由言念发起的QUALITYCHECK SDK自动发布, BUILD_ID=127, 版本号：2.4.1 发布日志： 1, 增加输出语音转文本接口，支持输出记录号，任务状态，错误信息，文本列表等内容

---
+++ aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/model/v20160801/GetAccAsrResultResponse.java
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.qualitycheck.model.v20160801;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.qualitycheck.transform.v20160801.GetAccAsrResultResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetAccAsrResultResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean success;
+
+	private String code;
+
+	private String message;
+
+	private Integer count;
+
+	private List<AccAsrSentenceResult> data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getCount() {
+		return this.count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public List<AccAsrSentenceResult> getData() {
+		return this.data;
+	}
+
+	public void setData(List<AccAsrSentenceResult> data) {
+		this.data = data;
+	}
+
+	public static class AccAsrSentenceResult {
+
+		private String recordId;
+
+		private Integer status;
+
+		private String statusCode;
+
+		private String errorMessage;
+
+		private List<SentenceResult> results;
+
+		public String getRecordId() {
+			return this.recordId;
+		}
+
+		public void setRecordId(String recordId) {
+			this.recordId = recordId;
+		}
+
+		public Integer getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(Integer status) {
+			this.status = status;
+		}
+
+		public String getStatusCode() {
+			return this.statusCode;
+		}
+
+		public void setStatusCode(String statusCode) {
+			this.statusCode = statusCode;
+		}
+
+		public String getErrorMessage() {
+			return this.errorMessage;
+		}
+
+		public void setErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+		}
+
+		public List<SentenceResult> getResults() {
+			return this.results;
+		}
+
+		public void setResults(List<SentenceResult> results) {
+			this.results = results;
+		}
+
+		public static class SentenceResult {
+
+			private Integer beginTime;
+
+			private Integer endTime;
+
+			private Integer channelId;
+
+			private String text;
+
+			private Integer emotionValue;
+
+			private Integer silenceDuration;
+
+			private Integer speechRate;
+
+			private String speakerId;
+
+			private String agentId;
+
+			private String channelKey;
+
+			public Integer getBeginTime() {
+				return this.beginTime;
+			}
+
+			public void setBeginTime(Integer beginTime) {
+				this.beginTime = beginTime;
+			}
+
+			public Integer getEndTime() {
+				return this.endTime;
+			}
+
+			public void setEndTime(Integer endTime) {
+				this.endTime = endTime;
+			}
+
+			public Integer getChannelId() {
+				return this.channelId;
+			}
+
+			public void setChannelId(Integer channelId) {
+				this.channelId = channelId;
+			}
+
+			public String getText() {
+				return this.text;
+			}
+
+			public void setText(String text) {
+				this.text = text;
+			}
+
+			public Integer getEmotionValue() {
+				return this.emotionValue;
+			}
+
+			public void setEmotionValue(Integer emotionValue) {
+				this.emotionValue = emotionValue;
+			}
+
+			public Integer getSilenceDuration() {
+				return this.silenceDuration;
+			}
+
+			public void setSilenceDuration(Integer silenceDuration) {
+				this.silenceDuration = silenceDuration;
+			}
+
+			public Integer getSpeechRate() {
+				return this.speechRate;
+			}
+
+			public void setSpeechRate(Integer speechRate) {
+				this.speechRate = speechRate;
+			}
+
+			public String getSpeakerId() {
+				return this.speakerId;
+			}
+
+			public void setSpeakerId(String speakerId) {
+				this.speakerId = speakerId;
+			}
+
+			public String getAgentId() {
+				return this.agentId;
+			}
+
+			public void setAgentId(String agentId) {
+				this.agentId = agentId;
+			}
+
+			public String getChannelKey() {
+				return this.channelKey;
+			}
+
+			public void setChannelKey(String channelKey) {
+				this.channelKey = channelKey;
+			}
+		}
+	}
+
+	@Override
+	public GetAccAsrResultResponse getInstance(UnmarshallerContext context) {
+		return	GetAccAsrResultResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/transform/v20160801/GetAccAsrResultResponseUnmarshaller.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.qualitycheck.transform.v20160801;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.qualitycheck.model.v20160801.GetAccAsrResultResponse;
+import com.aliyuncs.qualitycheck.model.v20160801.GetAccAsrResultResponse.AccAsrSentenceResult;
+import com.aliyuncs.qualitycheck.model.v20160801.GetAccAsrResultResponse.AccAsrSentenceResult.SentenceResult;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetAccAsrResultResponseUnmarshaller {
+
+	public static GetAccAsrResultResponse unmarshall(GetAccAsrResultResponse getAccAsrResultResponse, UnmarshallerContext context) {
+		
+		getAccAsrResultResponse.setRequestId(context.stringValue("GetAccAsrResultResponse.RequestId"));
+		getAccAsrResultResponse.setSuccess(context.booleanValue("GetAccAsrResultResponse.Success"));
+		getAccAsrResultResponse.setCode(context.stringValue("GetAccAsrResultResponse.Code"));
+		getAccAsrResultResponse.setMessage(context.stringValue("GetAccAsrResultResponse.Message"));
+		getAccAsrResultResponse.setCount(context.integerValue("GetAccAsrResultResponse.Count"));
+
+		List<AccAsrSentenceResult> data = new ArrayList<AccAsrSentenceResult>();
+		for (int i = 0; i < context.lengthValue("GetAccAsrResultResponse.Data.Length"); i++) {
+			AccAsrSentenceResult accAsrSentenceResult = new AccAsrSentenceResult();
+			accAsrSentenceResult.setRecordId(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].RecordId"));
+			accAsrSentenceResult.setStatus(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Status"));
+			accAsrSentenceResult.setStatusCode(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].StatusCode"));
+			accAsrSentenceResult.setErrorMessage(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].ErrorMessage"));
+
+			List<SentenceResult> results = new ArrayList<SentenceResult>();
+			for (int j = 0; j < context.lengthValue("GetAccAsrResultResponse.Data["+ i +"].Results.Length"); j++) {
+				SentenceResult sentenceResult = new SentenceResult();
+				sentenceResult.setBeginTime(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].BeginTime"));
+				sentenceResult.setEndTime(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].EndTime"));
+				sentenceResult.setChannelId(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].ChannelId"));
+				sentenceResult.setText(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].Text"));
+				sentenceResult.setEmotionValue(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].EmotionValue"));
+				sentenceResult.setSilenceDuration(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].SilenceDuration"));
+				sentenceResult.setSpeechRate(context.integerValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].SpeechRate"));
+				sentenceResult.setSpeakerId(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].SpeakerId"));
+				sentenceResult.setAgentId(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].AgentId"));
+				sentenceResult.setChannelKey(context.stringValue("GetAccAsrResultResponse.Data["+ i +"].Results["+ j +"].ChannelKey"));
+
+				results.add(sentenceResult);
+			}
+			accAsrSentenceResult.setResults(results);
+
+			data.add(accAsrSentenceResult);
+		}
+		getAccAsrResultResponse.setData(data);
+	 
+	 	return getAccAsrResultResponse;
+	}
+}
