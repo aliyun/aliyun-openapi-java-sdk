3ddbecfbc0cf0901a8298a12c8f669f81475bb46
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-29 00:38:33 +0800

    由择仁发起的CCC SDK自动发布, BUILD_ID=316, 版本号：1.0.1 发布日志： 1, This is an example of release-log. 2, Please strictly follow this format to edit in English. 3, Format：Number + , + Space + Description

---
+++ aliyun-java-sdk-ccc/src/main/java/com/aliyuncs/ccc/model/v20170705/DownloadRecordingRequest.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.ccc.model.v20170705;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DownloadRecordingRequest extends RpcAcsRequest<DownloadRecordingResponse> {
+	
+	public DownloadRecordingRequest() {
+		super("CCC", "2017-07-05", "DownloadRecording", "ccc");
+	}
+
+	private String instanceId;
+
+	private String fileName;
+
+	private String channel;
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putQueryParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getFileName() {
+		return this.fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+		if(fileName != null){
+			putQueryParameter("FileName", fileName);
+		}
+	}
+
+	public String getChannel() {
+		return this.channel;
+	}
+
+	public void setChannel(String channel) {
+		this.channel = channel;
+		if(channel != null){
+			putQueryParameter("Channel", channel);
+		}
+	}
+
+	@Override
+	public Class<DownloadRecordingResponse> getResponseClass() {
+		return DownloadRecordingResponse.class;
+	}
+
+}
