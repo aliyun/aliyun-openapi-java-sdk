9fdbe63474f21f0442da18c2a469f109b868e77a
haowei.yao <haowei.yao@alibaba-inc.com>
2018-01-24 12:36:14 +0800

    由萨文发起的CMS SDK自动发布, BUILD_ID=276, 版本号：5.2.1 发布日志： 1, Add ActiveAlert API : ListActiveAlertRuleRequest/ListProductOfActiceAlertRequest/EnableActiceAlertRequest/DisableActiceAlertRequest

---
+++ aliyun-java-sdk-cms/src/main/java/com/aliyuncs/cms/model/v20170301/DisableActiceAlertRequest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.cms.model.v20170301;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DisableActiceAlertRequest extends RpcAcsRequest<DisableActiceAlertResponse> {
+	
+	public DisableActiceAlertRequest() {
+		super("Cms", "2017-03-01", "DisableActiceAlert", "cms");
+	}
+
+	private String product;
+
+	private String userId;
+
+	public String getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+		if(product != null){
+			putQueryParameter("Product", product);
+		}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putQueryParameter("UserId", userId);
+		}
+	}
+
+	@Override
+	public Class<DisableActiceAlertResponse> getResponseClass() {
+		return DisableActiceAlertResponse.class;
+	}
+
+}
