610cca49d9a28aa2a0a0a46e4f2256a27f35b777
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-06 16:25:18 +0800

    由言念发起的QUALITYCHECK SDK自动发布, BUILD_ID=294, 版本号：2.6.9 发布日志： 1, add new interface

--- aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/model/v20160801/DoPhysicalDeleteResourceRequest.java
+++ aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/model/v20160801/DoPhysicalDeleteResourceRequest.java
@@ -30,6 +30,188 @@ public class DoPhysicalDeleteResourceRequest extends RpcAcsRequest<DoPhysicalDel
 		super("Qualitycheck", "2016-08-01", "DoPhysicalDeleteResource");
 	}
 
+	private String country;
+
+	private Long hid;
+
+	private Long level;
+
+	private Long invoker;
+
+	private String message;
+
+	private String url;
+
+	private Boolean success;
+
+	private Boolean interrupt;
+
+	private String gmtWakeup;
+
+	private String pk;
+
+	private String bid;
+
+	private String prompt;
+
+	private String taskExtraData;
+
+	private String taskIdentifier;
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+		if(country != null){
+			putQueryParameter("Country", country);
+		}
+	}
+
+	public Long getHid() {
+		return this.hid;
+	}
+
+	public void setHid(Long hid) {
+		this.hid = hid;
+		if(hid != null){
+			putQueryParameter("Hid", hid.toString());
+		}
+	}
+
+	public Long getLevel() {
+		return this.level;
+	}
+
+	public void setLevel(Long level) {
+		this.level = level;
+		if(level != null){
+			putQueryParameter("Level", level.toString());
+		}
+	}
+
+	public Long getInvoker() {
+		return this.invoker;
+	}
+
+	public void setInvoker(Long invoker) {
+		this.invoker = invoker;
+		if(invoker != null){
+			putQueryParameter("Invoker", invoker.toString());
+		}
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+		if(message != null){
+			putQueryParameter("Message", message);
+		}
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+		if(url != null){
+			putQueryParameter("Url", url);
+		}
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+		if(success != null){
+			putQueryParameter("Success", success.toString());
+		}
+	}
+
+	public Boolean getInterrupt() {
+		return this.interrupt;
+	}
+
+	public void setInterrupt(Boolean interrupt) {
+		this.interrupt = interrupt;
+		if(interrupt != null){
+			putQueryParameter("Interrupt", interrupt.toString());
+		}
+	}
+
+	public String getGmtWakeup() {
+		return this.gmtWakeup;
+	}
+
+	public void setGmtWakeup(String gmtWakeup) {
+		this.gmtWakeup = gmtWakeup;
+		if(gmtWakeup != null){
+			putQueryParameter("GmtWakeup", gmtWakeup);
+		}
+	}
+
+	public String getPk() {
+		return this.pk;
+	}
+
+	public void setPk(String pk) {
+		this.pk = pk;
+		if(pk != null){
+			putQueryParameter("Pk", pk);
+		}
+	}
+
+	public String getBid() {
+		return this.bid;
+	}
+
+	public void setBid(String bid) {
+		this.bid = bid;
+		if(bid != null){
+			putQueryParameter("Bid", bid);
+		}
+	}
+
+	public String getPrompt() {
+		return this.prompt;
+	}
+
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
+		if(prompt != null){
+			putQueryParameter("Prompt", prompt);
+		}
+	}
+
+	public String getTaskExtraData() {
+		return this.taskExtraData;
+	}
+
+	public void setTaskExtraData(String taskExtraData) {
+		this.taskExtraData = taskExtraData;
+		if(taskExtraData != null){
+			putQueryParameter("TaskExtraData", taskExtraData);
+		}
+	}
+
+	public String getTaskIdentifier() {
+		return this.taskIdentifier;
+	}
+
+	public void setTaskIdentifier(String taskIdentifier) {
+		this.taskIdentifier = taskIdentifier;
+		if(taskIdentifier != null){
+			putQueryParameter("TaskIdentifier", taskIdentifier);
+		}
+	}
+
 	@Override
 	public Class<DoPhysicalDeleteResourceResponse> getResponseClass() {
 		return DoPhysicalDeleteResourceResponse.class;
