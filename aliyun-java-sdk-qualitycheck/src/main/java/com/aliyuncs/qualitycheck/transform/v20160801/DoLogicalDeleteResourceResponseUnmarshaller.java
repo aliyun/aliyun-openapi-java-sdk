7d8ef99595d66a1b864996c2a7027e7cedcf73b6
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-06 14:25:35 +0800

    由言念发起的QUALITYCHECK SDK自动发布, BUILD_ID=292, 版本号：2.6.8 发布日志： 1, add new interface

---
+++ aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/transform/v20160801/DoLogicalDeleteResourceResponseUnmarshaller.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.qualitycheck.transform.v20160801;
+
+import com.aliyuncs.qualitycheck.model.v20160801.DoLogicalDeleteResourceResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DoLogicalDeleteResourceResponseUnmarshaller {
+
+	public static DoLogicalDeleteResourceResponse unmarshall(DoLogicalDeleteResourceResponse doLogicalDeleteResourceResponse, UnmarshallerContext context) {
+		
+		doLogicalDeleteResourceResponse.setInterrupt(context.stringValue("DoLogicalDeleteResourceResponse.Interrupt"));
+		doLogicalDeleteResourceResponse.setInvoker(context.longValue("DoLogicalDeleteResourceResponse.Invoker"));
+		doLogicalDeleteResourceResponse.setPk(context.stringValue("DoLogicalDeleteResourceResponse.Pk"));
+		doLogicalDeleteResourceResponse.setBid(context.stringValue("DoLogicalDeleteResourceResponse.Bid"));
+		doLogicalDeleteResourceResponse.setHid(context.longValue("DoLogicalDeleteResourceResponse.Hid"));
+		doLogicalDeleteResourceResponse.setCountry(context.stringValue("DoLogicalDeleteResourceResponse.Country"));
+		doLogicalDeleteResourceResponse.setTaskIdentifier(context.stringValue("DoLogicalDeleteResourceResponse.TaskIdentifier"));
+		doLogicalDeleteResourceResponse.setTaskIdentifier1(context.stringValue("DoLogicalDeleteResourceResponse.TaskIdentifier"));
+		doLogicalDeleteResourceResponse.setGmtWakeup(context.stringValue("DoLogicalDeleteResourceResponse.GmtWakeup"));
+		doLogicalDeleteResourceResponse.setSuccess(context.booleanValue("DoLogicalDeleteResourceResponse.Success"));
+		doLogicalDeleteResourceResponse.setMessage(context.stringValue("DoLogicalDeleteResourceResponse.Message"));
+	 
+	 	return doLogicalDeleteResourceResponse;
+	}
+}
