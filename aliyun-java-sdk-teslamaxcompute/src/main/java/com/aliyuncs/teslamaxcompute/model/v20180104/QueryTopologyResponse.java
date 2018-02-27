adfda44bfe17f5b1f9782995d2756d6063b59e9a
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-27 19:09:52 +0800

    由荣旸发起的TESLAMAXCOMPUTE SDK自动发布, BUILD_ID=313, 版本号：1.2.0 发布日志： 1, Add query topology API.

---
+++ aliyun-java-sdk-teslamaxcompute/src/main/java/com/aliyuncs/teslamaxcompute/model/v20180104/QueryTopologyResponse.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.teslamaxcompute.model.v20180104;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.teslamaxcompute.transform.v20180104.QueryTopologyResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryTopologyResponse extends AcsResponse {
+
+	private Integer code;
+
+	private String message;
+
+	private String requestId;
+
+	private Result result;
+
+	public Integer getCode() {
+		return this.code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Result getResult() {
+		return this.result;
+	}
+
+	public void setResult(Result result) {
+		this.result = result;
+	}
+
+	public static class Result {
+
+		private String lastUpdate;
+
+		private Regions regions;
+
+		public String getLastUpdate() {
+			return this.lastUpdate;
+		}
+
+		public void setLastUpdate(String lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+
+		public Regions getRegions() {
+			return this.regions;
+		}
+
+		public void setRegions(Regions regions) {
+			this.regions = regions;
+		}
+
+		public static class Regions {
+
+			private String region;
+
+			private String regionEnName;
+
+			private String regionCnName;
+
+			private Clusters clusters;
+
+			public String getRegion() {
+				return this.region;
+			}
+
+			public void setRegion(String region) {
+				this.region = region;
+			}
+
+			public String getRegionEnName() {
+				return this.regionEnName;
+			}
+
+			public void setRegionEnName(String regionEnName) {
+				this.regionEnName = regionEnName;
+			}
+
+			public String getRegionCnName() {
+				return this.regionCnName;
+			}
+
+			public void setRegionCnName(String regionCnName) {
+				this.regionCnName = regionCnName;
+			}
+
+			public Clusters getClusters() {
+				return this.clusters;
+			}
+
+			public void setClusters(Clusters clusters) {
+				this.clusters = clusters;
+			}
+
+			public static class Clusters {
+
+				private String cluster;
+
+				private String productLine;
+
+				private String productClass;
+
+				private String netCode;
+
+				private String business;
+
+				private String machineRoom;
+
+				private String netArch;
+
+				public String getCluster() {
+					return this.cluster;
+				}
+
+				public void setCluster(String cluster) {
+					this.cluster = cluster;
+				}
+
+				public String getProductLine() {
+					return this.productLine;
+				}
+
+				public void setProductLine(String productLine) {
+					this.productLine = productLine;
+				}
+
+				public String getProductClass() {
+					return this.productClass;
+				}
+
+				public void setProductClass(String productClass) {
+					this.productClass = productClass;
+				}
+
+				public String getNetCode() {
+					return this.netCode;
+				}
+
+				public void setNetCode(String netCode) {
+					this.netCode = netCode;
+				}
+
+				public String getBusiness() {
+					return this.business;
+				}
+
+				public void setBusiness(String business) {
+					this.business = business;
+				}
+
+				public String getMachineRoom() {
+					return this.machineRoom;
+				}
+
+				public void setMachineRoom(String machineRoom) {
+					this.machineRoom = machineRoom;
+				}
+
+				public String getNetArch() {
+					return this.netArch;
+				}
+
+				public void setNetArch(String netArch) {
+					this.netArch = netArch;
+				}
+			}
+		}
+	}
+
+	@Override
+	public QueryTopologyResponse getInstance(UnmarshallerContext context) {
+		return	QueryTopologyResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-teslamaxcompute/src/main/java/com/aliyuncs/teslamaxcompute/transform/v20180104/QueryTopologyResponseUnmarshaller.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.aliyuncs.teslamaxcompute.transform.v20180104;
+
+import com.aliyuncs.teslamaxcompute.model.v20180104.QueryTopologyResponse;
+import com.aliyuncs.teslamaxcompute.model.v20180104.QueryTopologyResponse.Result;
+import com.aliyuncs.teslamaxcompute.model.v20180104.QueryTopologyResponse.Result.Regions;
+import com.aliyuncs.teslamaxcompute.model.v20180104.QueryTopologyResponse.Result.Regions.Clusters;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryTopologyResponseUnmarshaller {
+
+	public static QueryTopologyResponse unmarshall(QueryTopologyResponse queryTopologyResponse, UnmarshallerContext context) {
+		
+		queryTopologyResponse.setRequestId(context.stringValue("QueryTopologyResponse.RequestId"));
+		queryTopologyResponse.setCode(context.integerValue("QueryTopologyResponse.Code"));
+		queryTopologyResponse.setMessage(context.stringValue("QueryTopologyResponse.Message"));
+
+		Result result = new Result();
+		result.setLastUpdate(context.stringValue("QueryTopologyResponse.Result.LastUpdate"));
+
+		Regions regions = new Regions();
+		regions.setRegion(context.stringValue("QueryTopologyResponse.Result.Regions.Region"));
+		regions.setRegionEnName(context.stringValue("QueryTopologyResponse.Result.Regions.RegionEnName"));
+		regions.setRegionCnName(context.stringValue("QueryTopologyResponse.Result.Regions.RegionCnName"));
+
+		Clusters clusters = new Clusters();
+		clusters.setCluster(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.Cluster"));
+		clusters.setProductLine(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.ProductLine"));
+		clusters.setProductClass(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.ProductClass"));
+		clusters.setNetCode(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.NetCode"));
+		clusters.setBusiness(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.Business"));
+		clusters.setMachineRoom(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.MachineRoom"));
+		clusters.setNetArch(context.stringValue("QueryTopologyResponse.Result.Regions.Clusters.NetArch"));
+		regions.setClusters(clusters);
+		result.setRegions(regions);
+		queryTopologyResponse.setResult(result);
+	 
+	 	return queryTopologyResponse;
+	}
+}
