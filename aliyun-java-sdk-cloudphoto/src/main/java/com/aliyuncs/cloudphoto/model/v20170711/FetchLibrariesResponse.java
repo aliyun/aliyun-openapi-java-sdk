a26161501e00c3e346556fbfa1b20fe152c57867
haowei.yao <haowei.yao@alibaba-inc.com>
2018-03-27 10:23:46 +0800

    由廷诚发起的CLOUDPHOTO SDK自动发布, BUILD_ID=502, 版本号：1.1.14 发布日志： 1, add interface FetchLibraries

---
+++ aliyun-java-sdk-cloudphoto/src/main/java/com/aliyuncs/cloudphoto/model/v20170711/FetchLibrariesResponse.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudphoto.model.v20170711;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cloudphoto.transform.v20170711.FetchLibrariesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class FetchLibrariesResponse extends AcsResponse {
+
+	private String code;
+
+	private String message;
+
+	private Integer totalCount;
+
+	private String requestId;
+
+	private String action;
+
+	private List<String> library;
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(Integer totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getAction() {
+		return this.action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public List<String> getLibrary() {
+		return this.library;
+	}
+
+	public void setLibrary(List<String> library) {
+		this.library = library;
+	}
+
+	@Override
+	public FetchLibrariesResponse getInstance(UnmarshallerContext context) {
+		return	FetchLibrariesResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-cloudphoto/src/main/java/com/aliyuncs/cloudphoto/transform/v20170711/FetchLibrariesResponseUnmarshaller.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudphoto.transform.v20170711;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.cloudphoto.model.v20170711.FetchLibrariesResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class FetchLibrariesResponseUnmarshaller {
+
+	public static FetchLibrariesResponse unmarshall(FetchLibrariesResponse fetchLibrariesResponse, UnmarshallerContext context) {
+		
+		fetchLibrariesResponse.setRequestId(context.stringValue("FetchLibrariesResponse.RequestId"));
+		fetchLibrariesResponse.setCode(context.stringValue("FetchLibrariesResponse.Code"));
+		fetchLibrariesResponse.setMessage(context.stringValue("FetchLibrariesResponse.Message"));
+		fetchLibrariesResponse.setTotalCount(context.integerValue("FetchLibrariesResponse.TotalCount"));
+		fetchLibrariesResponse.setAction(context.stringValue("FetchLibrariesResponse.Action"));
+
+		List<String> library = new ArrayList<String>();
+		for (int i = 0; i < context.lengthValue("FetchLibrariesResponse.Library.Length"); i++) {
+			library.add(context.stringValue("FetchLibrariesResponse.Library["+ i +"]"));
+		}
+		fetchLibrariesResponse.setLibrary(library);
+	 
+	 	return fetchLibrariesResponse;
+	}
+}
