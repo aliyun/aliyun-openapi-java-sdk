cca74c993147435535fba80f2dd6035eea2703e3
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-24 17:35:55 +0800

    由向召元发起的CMS SDK自动发布, 版本号：6.0.3 发布日志： 1, Add api:UpdateMyGroupMembers

---
+++ aliyun-java-sdk-cms/src/main/java/com/aliyuncs/cms/model/v20180308/UpdateMyGroupMembersRequest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cms.model.v20180308;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateMyGroupMembersRequest extends RpcAcsRequest<UpdateMyGroupMembersResponse> {
+	
+	public UpdateMyGroupMembersRequest() {
+		super("Cms", "2018-03-08", "UpdateMyGroupMembers", "cms");
+	}
+
+	private Long groupId;
+
+	private String masters;
+
+	public Long getGroupId() {
+		return this.groupId;
+	}
+
+	public void setGroupId(Long groupId) {
+		this.groupId = groupId;
+		if(groupId != null){
+			putQueryParameter("GroupId", groupId.toString());
+		}
+	}
+
+	public String getMasters() {
+		return this.masters;
+	}
+
+	public void setMasters(String masters) {
+		this.masters = masters;
+		if(masters != null){
+			putQueryParameter("Masters", masters);
+		}
+	}
+
+	@Override
+	public Class<UpdateMyGroupMembersResponse> getResponseClass() {
+		return UpdateMyGroupMembersResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cms/src/main/java/com/aliyuncs/cms/transform/v20180308/UpdateMyGroupMembersResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cms.transform.v20180308;
+
+import com.aliyuncs.cms.model.v20180308.UpdateMyGroupMembersResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UpdateMyGroupMembersResponseUnmarshaller {
+
+	public static UpdateMyGroupMembersResponse unmarshall(UpdateMyGroupMembersResponse updateMyGroupMembersResponse, UnmarshallerContext context) {
+		
+		updateMyGroupMembersResponse.setRequestId(context.stringValue("UpdateMyGroupMembersResponse.RequestId"));
+		updateMyGroupMembersResponse.setSuccess(context.booleanValue("UpdateMyGroupMembersResponse.Success"));
+		updateMyGroupMembersResponse.setErrorCode(context.integerValue("UpdateMyGroupMembersResponse.ErrorCode"));
+		updateMyGroupMembersResponse.setErrorMessage(context.stringValue("UpdateMyGroupMembersResponse.ErrorMessage"));
+	 
+	 	return updateMyGroupMembersResponse;
+	}
+}
