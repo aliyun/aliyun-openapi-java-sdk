90a3a9556666c8669795c099a9cfd09a32f4b789
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-24 11:13:06 +0800

    由廷诚发起的CLOUDPHOTO SDK自动发布, 版本号：1.1.18 发布日志： 1, add interface FetchPhotos 2, add xflush log param unixTimestamp

---
+++ aliyun-java-sdk-cloudphoto/src/main/java/com/aliyuncs/cloudphoto/model/v20170711/FetchPhotosResponse.java
@@ -0,0 +1,270 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudphoto.model.v20170711;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cloudphoto.transform.v20170711.FetchPhotosResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class FetchPhotosResponse extends AcsResponse {
+
+	private String code;
+
+	private String message;
+
+	private Integer totalCount;
+
+	private String requestId;
+
+	private String action;
+
+	private List<Photo> photos;
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(Integer totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getAction() {
+		return this.action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public List<Photo> getPhotos() {
+		return this.photos;
+	}
+
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
+
+	public static class Photo {
+
+		private Long id;
+
+		private String idStr;
+
+		private String title;
+
+		private String fileId;
+
+		private String location;
+
+		private String state;
+
+		private String md5;
+
+		private Boolean isVideo;
+
+		private String remark;
+
+		private Long size;
+
+		private Long width;
+
+		private Long height;
+
+		private Long ctime;
+
+		private Long mtime;
+
+		private Long takenAt;
+
+		private Long inactiveTime;
+
+		private Long shareExpireTime;
+
+		public Long getId() {
+			return this.id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getIdStr() {
+			return this.idStr;
+		}
+
+		public void setIdStr(String idStr) {
+			this.idStr = idStr;
+		}
+
+		public String getTitle() {
+			return this.title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getFileId() {
+			return this.fileId;
+		}
+
+		public void setFileId(String fileId) {
+			this.fileId = fileId;
+		}
+
+		public String getLocation() {
+			return this.location;
+		}
+
+		public void setLocation(String location) {
+			this.location = location;
+		}
+
+		public String getState() {
+			return this.state;
+		}
+
+		public void setState(String state) {
+			this.state = state;
+		}
+
+		public String getMd5() {
+			return this.md5;
+		}
+
+		public void setMd5(String md5) {
+			this.md5 = md5;
+		}
+
+		public Boolean getIsVideo() {
+			return this.isVideo;
+		}
+
+		public void setIsVideo(Boolean isVideo) {
+			this.isVideo = isVideo;
+		}
+
+		public String getRemark() {
+			return this.remark;
+		}
+
+		public void setRemark(String remark) {
+			this.remark = remark;
+		}
+
+		public Long getSize() {
+			return this.size;
+		}
+
+		public void setSize(Long size) {
+			this.size = size;
+		}
+
+		public Long getWidth() {
+			return this.width;
+		}
+
+		public void setWidth(Long width) {
+			this.width = width;
+		}
+
+		public Long getHeight() {
+			return this.height;
+		}
+
+		public void setHeight(Long height) {
+			this.height = height;
+		}
+
+		public Long getCtime() {
+			return this.ctime;
+		}
+
+		public void setCtime(Long ctime) {
+			this.ctime = ctime;
+		}
+
+		public Long getMtime() {
+			return this.mtime;
+		}
+
+		public void setMtime(Long mtime) {
+			this.mtime = mtime;
+		}
+
+		public Long getTakenAt() {
+			return this.takenAt;
+		}
+
+		public void setTakenAt(Long takenAt) {
+			this.takenAt = takenAt;
+		}
+
+		public Long getInactiveTime() {
+			return this.inactiveTime;
+		}
+
+		public void setInactiveTime(Long inactiveTime) {
+			this.inactiveTime = inactiveTime;
+		}
+
+		public Long getShareExpireTime() {
+			return this.shareExpireTime;
+		}
+
+		public void setShareExpireTime(Long shareExpireTime) {
+			this.shareExpireTime = shareExpireTime;
+		}
+	}
+
+	@Override
+	public FetchPhotosResponse getInstance(UnmarshallerContext context) {
+		return	FetchPhotosResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-cloudphoto/src/main/java/com/aliyuncs/cloudphoto/transform/v20170711/FetchPhotosResponseUnmarshaller.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudphoto.transform.v20170711;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.cloudphoto.model.v20170711.FetchPhotosResponse;
+import com.aliyuncs.cloudphoto.model.v20170711.FetchPhotosResponse.Photo;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class FetchPhotosResponseUnmarshaller {
+
+	public static FetchPhotosResponse unmarshall(FetchPhotosResponse fetchPhotosResponse, UnmarshallerContext context) {
+		
+		fetchPhotosResponse.setRequestId(context.stringValue("FetchPhotosResponse.RequestId"));
+		fetchPhotosResponse.setCode(context.stringValue("FetchPhotosResponse.Code"));
+		fetchPhotosResponse.setMessage(context.stringValue("FetchPhotosResponse.Message"));
+		fetchPhotosResponse.setTotalCount(context.integerValue("FetchPhotosResponse.TotalCount"));
+		fetchPhotosResponse.setAction(context.stringValue("FetchPhotosResponse.Action"));
+
+		List<Photo> photos = new ArrayList<Photo>();
+		for (int i = 0; i < context.lengthValue("FetchPhotosResponse.Photos.Length"); i++) {
+			Photo photo = new Photo();
+			photo.setId(context.longValue("FetchPhotosResponse.Photos["+ i +"].Id"));
+			photo.setIdStr(context.stringValue("FetchPhotosResponse.Photos["+ i +"].IdStr"));
+			photo.setTitle(context.stringValue("FetchPhotosResponse.Photos["+ i +"].Title"));
+			photo.setFileId(context.stringValue("FetchPhotosResponse.Photos["+ i +"].FileId"));
+			photo.setLocation(context.stringValue("FetchPhotosResponse.Photos["+ i +"].Location"));
+			photo.setState(context.stringValue("FetchPhotosResponse.Photos["+ i +"].State"));
+			photo.setMd5(context.stringValue("FetchPhotosResponse.Photos["+ i +"].Md5"));
+			photo.setIsVideo(context.booleanValue("FetchPhotosResponse.Photos["+ i +"].IsVideo"));
+			photo.setRemark(context.stringValue("FetchPhotosResponse.Photos["+ i +"].Remark"));
+			photo.setSize(context.longValue("FetchPhotosResponse.Photos["+ i +"].Size"));
+			photo.setWidth(context.longValue("FetchPhotosResponse.Photos["+ i +"].Width"));
+			photo.setHeight(context.longValue("FetchPhotosResponse.Photos["+ i +"].Height"));
+			photo.setCtime(context.longValue("FetchPhotosResponse.Photos["+ i +"].Ctime"));
+			photo.setMtime(context.longValue("FetchPhotosResponse.Photos["+ i +"].Mtime"));
+			photo.setTakenAt(context.longValue("FetchPhotosResponse.Photos["+ i +"].TakenAt"));
+			photo.setInactiveTime(context.longValue("FetchPhotosResponse.Photos["+ i +"].InactiveTime"));
+			photo.setShareExpireTime(context.longValue("FetchPhotosResponse.Photos["+ i +"].ShareExpireTime"));
+
+			photos.add(photo);
+		}
+		fetchPhotosResponse.setPhotos(photos);
+	 
+	 	return fetchPhotosResponse;
+	}
+}
