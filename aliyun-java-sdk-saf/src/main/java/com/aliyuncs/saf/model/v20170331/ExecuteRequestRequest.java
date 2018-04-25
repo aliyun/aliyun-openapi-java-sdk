23225c0e0f6a74dea51d23488bd5ee2b95f03b63
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-25 16:58:33 +0800

    由连敖发起的SAF SDK自动发布, 版本号：1.0.0 发布日志： 1, sdk init .version:1.0.0 2, ExecuteRequestRequest.service .the value must be in (account_abuse,address_validation,email_risk,coupon_abuse) and the product must be paid.

---
+++ aliyun-java-sdk-saf/src/main/java/com/aliyuncs/saf/model/v20170331/ExecuteRequestRequest.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.saf.model.v20170331;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ExecuteRequestRequest extends RpcAcsRequest<ExecuteRequestResponse> {
+	
+	public ExecuteRequestRequest() {
+		super("saf", "2017-03-31", "ExecuteRequest", "saf");
+		setProtocol(ProtocolType.HTTPS);
+	}
+
+	private String serviceParameters;
+
+	private String service;
+
+	public String getServiceParameters() {
+		return this.serviceParameters;
+	}
+
+	public void setServiceParameters(String serviceParameters) {
+		this.serviceParameters = serviceParameters;
+		if(serviceParameters != null){
+			putQueryParameter("ServiceParameters", serviceParameters);
+		}
+	}
+
+	public String getService() {
+		return this.service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+		if(service != null){
+			putQueryParameter("Service", service);
+		}
+	}
+
+	@Override
+	public Class<ExecuteRequestResponse> getResponseClass() {
+		return ExecuteRequestResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-saf/src/main/java/com/aliyuncs/saf/transform/v20170331/ExecuteRequestResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.saf.transform.v20170331;
+
+import com.aliyuncs.saf.model.v20170331.ExecuteRequestResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ExecuteRequestResponseUnmarshaller {
+
+	public static ExecuteRequestResponse unmarshall(ExecuteRequestResponse executeRequestResponse, UnmarshallerContext context) {
+		
+		executeRequestResponse.setRequestId(context.stringValue("ExecuteRequestResponse.RequestId"));
+		executeRequestResponse.setCode(context.integerValue("ExecuteRequestResponse.Code"));
+		executeRequestResponse.setData(context.stringValue("ExecuteRequestResponse.Data"));
+		executeRequestResponse.setMessage(context.stringValue("ExecuteRequestResponse.Message"));
+	 
+	 	return executeRequestResponse;
+	}
+}
