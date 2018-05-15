5ebcaf6f36bc58652e0eb2b6d6967d5d7d932e86
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-15 11:13:04 +0800

    由言念发起的QUALITYCHECK SDK自动发布, 版本号：2.8.0 发布日志： 1, add new interface

---
+++ aliyun-java-sdk-qualitycheck/src/main/java/com/aliyuncs/qualitycheck/transform/v20160801/UpdateAsrVocabResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.qualitycheck.transform.v20160801;
+
+import com.aliyuncs.qualitycheck.model.v20160801.UpdateAsrVocabResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UpdateAsrVocabResponseUnmarshaller {
+
+	public static UpdateAsrVocabResponse unmarshall(UpdateAsrVocabResponse updateAsrVocabResponse, UnmarshallerContext context) {
+		
+		updateAsrVocabResponse.setRequestId(context.stringValue("UpdateAsrVocabResponse.requestId"));
+		updateAsrVocabResponse.setSuccess(context.booleanValue("UpdateAsrVocabResponse.success"));
+		updateAsrVocabResponse.setCode(context.stringValue("UpdateAsrVocabResponse.code"));
+		updateAsrVocabResponse.setMessage(context.stringValue("UpdateAsrVocabResponse.message"));
+		updateAsrVocabResponse.setData(context.stringValue("UpdateAsrVocabResponse.data"));
+	 
+	 	return updateAsrVocabResponse;
+	}
+}
