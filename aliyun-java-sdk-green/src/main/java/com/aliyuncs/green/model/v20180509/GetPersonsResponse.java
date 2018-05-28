8e5ed6d9e3e88cd9a7f3ba88a23ff84e3cdeb193
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-28 19:19:34 +0800

    GREEN SDK Auto Released By Alibaba Cloud,Version：3.2.0 发布日志： 1, Add face 1-N,1-1 scan interface.

---
+++ aliyun-java-sdk-green/src/main/java/com/aliyuncs/green/model/v20180509/GetPersonsResponse.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.green.model.v20180509;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.green.transform.v20180509.GetPersonsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetPersonsResponse extends AcsResponse {
+
+	@Override
+	public GetPersonsResponse getInstance(UnmarshallerContext context) {
+		return	GetPersonsResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-green/src/main/java/com/aliyuncs/green/transform/v20180509/FileAsyncScanResponseUnmarshaller.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.green.transform.v20180509;
+
+import com.aliyuncs.green.model.v20180509.FileAsyncScanResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class FileAsyncScanResponseUnmarshaller {
+
+	public static FileAsyncScanResponse unmarshall(FileAsyncScanResponse fileAsyncScanResponse, UnmarshallerContext context) {
+		
+	 
+	 	return fileAsyncScanResponse;
+	}
+}
