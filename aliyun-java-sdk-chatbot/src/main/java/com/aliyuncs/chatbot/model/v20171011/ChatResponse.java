ee4bb6fb6f6e10a8aaa57a584703abc6868958ce
haowei.yao <haowei.yao@alibaba-inc.com>
2018-07-05 14:25:47 +0800

    CHATBOT SDK Auto Released By yanke.yk,Version：1.1.0 发布日志： 1, A new optional parameter 'Perspectives' is introduced to the 'Chat' API. By filling this parameter when calling 'Chat', you'll get the knowledge base content within the specified perspectives.

--- aliyun-java-sdk-chatbot/src/main/java/com/aliyuncs/chatbot/model/v20171011/ChatResponse.java
+++ aliyun-java-sdk-chatbot/src/main/java/com/aliyuncs/chatbot/model/v20171011/ChatResponse.java
@@ -1,21 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.aliyuncs.chatbot.model.v20171011;
 
 import java.util.List;
@@ -129,6 +125,10 @@ public class ChatResponse extends AcsResponse {
 
 			private String answerSource;
 
+			private String summary;
+
+			private String content;
+
 			public String getKnowledgeId() {
 				return this.knowledgeId;
 			}
@@ -151,6 +151,22 @@ public class ChatResponse extends AcsResponse {
 
 			public void setAnswerSource(String answerSource) {
 				this.answerSource = answerSource;
+			}
+
+			public String getSummary() {
+				return this.summary;
+			}
+
+			public void setSummary(String summary) {
+				this.summary = summary;
+			}
+
+			public String getContent() {
+				return this.content;
+			}
+
+			public void setContent(String content) {
+				this.content = content;
 			}
 		}
 
--- aliyun-java-sdk-chatbot/src/main/java/com/aliyuncs/chatbot/transform/v20171011/ChatResponseUnmarshaller.java
+++ aliyun-java-sdk-chatbot/src/main/java/com/aliyuncs/chatbot/transform/v20171011/ChatResponseUnmarshaller.java
@@ -1,21 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.aliyuncs.chatbot.transform.v20171011;
 
 import java.util.ArrayList;
@@ -62,6 +58,8 @@ public class ChatResponseUnmarshaller {
 				recommend.setKnowledgeId(context.stringValue("ChatResponse.Messages["+ i +"].Recommends["+ j +"].KnowledgeId"));
 				recommend.setTitle(context.stringValue("ChatResponse.Messages["+ i +"].Recommends["+ j +"].Title"));
 				recommend.setAnswerSource(context.stringValue("ChatResponse.Messages["+ i +"].Recommends["+ j +"].AnswerSource"));
+				recommend.setSummary(context.stringValue("ChatResponse.Messages["+ i +"].Recommends["+ j +"].Summary"));
+				recommend.setContent(context.stringValue("ChatResponse.Messages["+ i +"].Recommends["+ j +"].Content"));
 
 				recommends.add(recommend);
 			}
