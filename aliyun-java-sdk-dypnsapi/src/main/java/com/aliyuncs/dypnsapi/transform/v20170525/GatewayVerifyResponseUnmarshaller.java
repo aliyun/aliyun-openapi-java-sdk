b41523b4f9af8bb3eae72fde4ec460ad16ae21c7
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-07-24 19:19:53 +0800

    DYPNSAPI SDK Auto Released By ganliang.gl,Version：1.0.0 发布日志： 1, This is an example of release-log. 2, Please strictly follow this format to edit in English. 3, Format：Number + , + Space + Description

---
+++ aliyun-java-sdk-dypnsapi/src/main/java/com/aliyuncs/dypnsapi/transform/v20170525/GatewayVerifyResponseUnmarshaller.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dypnsapi.transform.v20170525;
+
+import com.aliyuncs.dypnsapi.model.v20170525.GatewayVerifyResponse;
+import com.aliyuncs.dypnsapi.model.v20170525.GatewayVerifyResponse.GateVerifyResultDTO;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GatewayVerifyResponseUnmarshaller {
+
+	public static GatewayVerifyResponse unmarshall(GatewayVerifyResponse gatewayVerifyResponse, UnmarshallerContext context) {
+		
+		gatewayVerifyResponse.setRequestId(context.stringValue("GatewayVerifyResponse.RequestId"));
+		gatewayVerifyResponse.setCode(context.stringValue("GatewayVerifyResponse.Code"));
+		gatewayVerifyResponse.setMessage(context.stringValue("GatewayVerifyResponse.Message"));
+
+		GateVerifyResultDTO gateVerifyResultDTO = new GateVerifyResultDTO();
+		gateVerifyResultDTO.setVerifyId(context.stringValue("GatewayVerifyResponse.GateVerifyResultDTO.VerifyId"));
+		gateVerifyResultDTO.setVerifyResult(context.stringValue("GatewayVerifyResponse.GateVerifyResultDTO.VerifyResult"));
+		gatewayVerifyResponse.setGateVerifyResultDTO(gateVerifyResultDTO);
+	 
+	 	return gatewayVerifyResponse;
+	}
+}
