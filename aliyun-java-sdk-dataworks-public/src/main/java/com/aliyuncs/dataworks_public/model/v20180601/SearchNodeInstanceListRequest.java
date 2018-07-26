95b471bbf971944b2fdd7845072538f8e94c55fa
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-07-26 20:02:52 +0800

    DATAWORKS-PUBLIC SDK Auto Released By sam.liux,Version：1.8.0 发布日志： 1, manual biz dag api 2, get manual biz dag instance info 3, supply data api 4, get supply data instance info

---
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/model/v20180601/SearchNodeInstanceListRequest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dataworks_public.model.v20180601;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class SearchNodeInstanceListRequest extends RpcAcsRequest<SearchNodeInstanceListResponse> {
+	
+	public SearchNodeInstanceListRequest() {
+		super("dataworks-public", "2018-06-01", "SearchNodeInstanceList");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.POST);
+	}
+
+	private String projectName;
+
+	private Long dagId;
+
+	public String getProjectName() {
+		return this.projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+		if(projectName != null){
+			putQueryParameter("ProjectName", projectName);
+		}
+	}
+
+	public Long getDagId() {
+		return this.dagId;
+	}
+
+	public void setDagId(Long dagId) {
+		this.dagId = dagId;
+		if(dagId != null){
+			putQueryParameter("DagId", dagId.toString());
+		}
+	}
+
+	@Override
+	public Class<SearchNodeInstanceListResponse> getResponseClass() {
+		return SearchNodeInstanceListResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/transform/v20180601/SearchManualDagNodeInstanceResponseUnmarshaller.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dataworks_public.transform.v20180601;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.dataworks_public.model.v20180601.SearchManualDagNodeInstanceResponse;
+import com.aliyuncs.dataworks_public.model.v20180601.SearchManualDagNodeInstanceResponse.NodeInsInfo;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SearchManualDagNodeInstanceResponseUnmarshaller {
+
+	public static SearchManualDagNodeInstanceResponse unmarshall(SearchManualDagNodeInstanceResponse searchManualDagNodeInstanceResponse, UnmarshallerContext context) {
+		
+		searchManualDagNodeInstanceResponse.setRequestId(context.stringValue("SearchManualDagNodeInstanceResponse.RequestId"));
+		searchManualDagNodeInstanceResponse.setErrCode(context.stringValue("SearchManualDagNodeInstanceResponse.ErrCode"));
+		searchManualDagNodeInstanceResponse.setErrMsg(context.stringValue("SearchManualDagNodeInstanceResponse.ErrMsg"));
+		searchManualDagNodeInstanceResponse.setSuccess(context.booleanValue("SearchManualDagNodeInstanceResponse.Success"));
+
+		List<NodeInsInfo> data = new ArrayList<NodeInsInfo>();
+		for (int i = 0; i < context.lengthValue("SearchManualDagNodeInstanceResponse.Data.Length"); i++) {
+			NodeInsInfo nodeInsInfo = new NodeInsInfo();
+			nodeInsInfo.setInstanceId(context.longValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].InstanceId"));
+			nodeInsInfo.setDagId(context.longValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].DagId"));
+			nodeInsInfo.setDagType(context.integerValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].DagType"));
+			nodeInsInfo.setStatus(context.integerValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].Status"));
+			nodeInsInfo.setBizdate(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].Bizdate"));
+			nodeInsInfo.setParaValue(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].ParaValue"));
+			nodeInsInfo.setFinishTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].FinishTime"));
+			nodeInsInfo.setBeginWaitTimeTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].BeginWaitTimeTime"));
+			nodeInsInfo.setBeginWaitResTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].BeginWaitResTime"));
+			nodeInsInfo.setBeginRunningTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].BeginRunningTime"));
+			nodeInsInfo.setCreateTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].CreateTime"));
+			nodeInsInfo.setModifyTime(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].ModifyTime"));
+			nodeInsInfo.setNodeName(context.stringValue("SearchManualDagNodeInstanceResponse.Data["+ i +"].NodeName"));
+
+			data.add(nodeInsInfo);
+		}
+		searchManualDagNodeInstanceResponse.setData(data);
+	 
+	 	return searchManualDagNodeInstanceResponse;
+	}
+}
