541a8502283f381dc716b58e6ec0964bcc767f9d
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-10-19 22:15:00 +0800

    LINKEDMALL SDK Auto Released By bozhu.zb,Version：2.0.1 发布日志： 1, This is an example of release-log. 2, Please strictly follow this format to edit in English. 3, Format：Number + , + Space + Description

---
+++ aliyun-java-sdk-linkedmall/src/main/java/com/aliyuncs/linkedmall/model/v20180116/QueryGuideItemGroupResponse.java
@@ -0,0 +1,195 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkedmall.model.v20180116;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.linkedmall.transform.v20180116.QueryGuideItemGroupResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryGuideItemGroupResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String code;
+
+	private String message;
+
+	private String subCode;
+
+	private String subMessage;
+
+	private Boolean success;
+
+	private List<ItemInfo> guideItemGroup;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getSubCode() {
+		return this.subCode;
+	}
+
+	public void setSubCode(String subCode) {
+		this.subCode = subCode;
+	}
+
+	public String getSubMessage() {
+		return this.subMessage;
+	}
+
+	public void setSubMessage(String subMessage) {
+		this.subMessage = subMessage;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public List<ItemInfo> getGuideItemGroup() {
+		return this.guideItemGroup;
+	}
+
+	public void setGuideItemGroup(List<ItemInfo> guideItemGroup) {
+		this.guideItemGroup = guideItemGroup;
+	}
+
+	public static class ItemInfo {
+
+		private Long itemId;
+
+		private String itemTitle;
+
+		private String mainPicUrl;
+
+		private String itemUrl;
+
+		private Long priceCent;
+
+		private Long points;
+
+		private Long reservePrice;
+
+		private Long pointsAmount;
+
+		private Long pointPrice;
+
+		public Long getItemId() {
+			return this.itemId;
+		}
+
+		public void setItemId(Long itemId) {
+			this.itemId = itemId;
+		}
+
+		public String getItemTitle() {
+			return this.itemTitle;
+		}
+
+		public void setItemTitle(String itemTitle) {
+			this.itemTitle = itemTitle;
+		}
+
+		public String getMainPicUrl() {
+			return this.mainPicUrl;
+		}
+
+		public void setMainPicUrl(String mainPicUrl) {
+			this.mainPicUrl = mainPicUrl;
+		}
+
+		public String getItemUrl() {
+			return this.itemUrl;
+		}
+
+		public void setItemUrl(String itemUrl) {
+			this.itemUrl = itemUrl;
+		}
+
+		public Long getPriceCent() {
+			return this.priceCent;
+		}
+
+		public void setPriceCent(Long priceCent) {
+			this.priceCent = priceCent;
+		}
+
+		public Long getPoints() {
+			return this.points;
+		}
+
+		public void setPoints(Long points) {
+			this.points = points;
+		}
+
+		public Long getReservePrice() {
+			return this.reservePrice;
+		}
+
+		public void setReservePrice(Long reservePrice) {
+			this.reservePrice = reservePrice;
+		}
+
+		public Long getPointsAmount() {
+			return this.pointsAmount;
+		}
+
+		public void setPointsAmount(Long pointsAmount) {
+			this.pointsAmount = pointsAmount;
+		}
+
+		public Long getPointPrice() {
+			return this.pointPrice;
+		}
+
+		public void setPointPrice(Long pointPrice) {
+			this.pointPrice = pointPrice;
+		}
+	}
+
+	@Override
+	public QueryGuideItemGroupResponse getInstance(UnmarshallerContext context) {
+		return	QueryGuideItemGroupResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-linkedmall/src/main/java/com/aliyuncs/linkedmall/transform/v20180116/QueryGuideItemGroupResponseUnmarshaller.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkedmall.transform.v20180116;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.linkedmall.model.v20180116.QueryGuideItemGroupResponse;
+import com.aliyuncs.linkedmall.model.v20180116.QueryGuideItemGroupResponse.ItemInfo;
+import java.util.Map;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryGuideItemGroupResponseUnmarshaller {
+
+	public static QueryGuideItemGroupResponse unmarshall(QueryGuideItemGroupResponse queryGuideItemGroupResponse, UnmarshallerContext context) {
+		
+		queryGuideItemGroupResponse.setRequestId(context.stringValue("QueryGuideItemGroupResponse.RequestId"));
+		queryGuideItemGroupResponse.setCode(context.stringValue("QueryGuideItemGroupResponse.Code"));
+		queryGuideItemGroupResponse.setMessage(context.stringValue("QueryGuideItemGroupResponse.Message"));
+		queryGuideItemGroupResponse.setSubCode(context.stringValue("QueryGuideItemGroupResponse.SubCode"));
+		queryGuideItemGroupResponse.setSubMessage(context.stringValue("QueryGuideItemGroupResponse.SubMessage"));
+		queryGuideItemGroupResponse.setSuccess(context.booleanValue("QueryGuideItemGroupResponse.Success"));
+
+		List<ItemInfo> guideItemGroup = new ArrayList<ItemInfo>();
+		for (int i = 0; i < context.lengthValue("QueryGuideItemGroupResponse.GuideItemGroup.Length"); i++) {
+			ItemInfo itemInfo = new ItemInfo();
+			itemInfo.setItemId(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].ItemId"));
+			itemInfo.setItemTitle(context.stringValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].ItemTitle"));
+			itemInfo.setMainPicUrl(context.stringValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].MainPicUrl"));
+			itemInfo.setItemUrl(context.stringValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].ItemUrl"));
+			itemInfo.setPriceCent(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].PriceCent"));
+			itemInfo.setPoints(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].Points"));
+			itemInfo.setReservePrice(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].ReservePrice"));
+			itemInfo.setPointsAmount(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].PointsAmount"));
+			itemInfo.setPointPrice(context.longValue("QueryGuideItemGroupResponse.GuideItemGroup["+ i +"].PointPrice"));
+
+			guideItemGroup.add(itemInfo);
+		}
+		queryGuideItemGroupResponse.setGuideItemGroup(guideItemGroup);
+	 
+	 	return queryGuideItemGroupResponse;
+	}
+}
