4b66ca38038442ed8a5ce4ee29d0bafee8f33a9e
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-11-13 15:11:30 +0800

    PTS SDK Auto Released By zhangjin.jsf,Version：1.0.0 发布日志： 1, Added pts platinum get report api

---
+++ aliyun-java-sdk-pts/src/main/java/com/aliyuncs/pts/model/v20181111/GetReportRequest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.pts.model.v20181111;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetReportRequest extends RpcAcsRequest<GetReportResponse> {
+	
+	public GetReportRequest() {
+		super("PTS", "2018-11-11", "GetReport");
+	}
+
+	private Long reportId;
+
+	public Long getReportId() {
+		return this.reportId;
+	}
+
+	public void setReportId(Long reportId) {
+		this.reportId = reportId;
+		if(reportId != null){
+			putQueryParameter("ReportId", reportId.toString());
+		}
+	}
+
+	@Override
+	public Class<GetReportResponse> getResponseClass() {
+		return GetReportResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-pts/src/main/java/com/aliyuncs/pts/model/v20181111/GetReportResponse.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.pts.model.v20181111;
+
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.pts.transform.v20181111.GetReportResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetReportResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String code;
+
+	private String message;
+
+	private Boolean success;
+
+	private String snapshot;
+
+	private String summary;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getSnapshot() {
+		return this.snapshot;
+	}
+
+	public void setSnapshot(String snapshot) {
+		this.snapshot = snapshot;
+	}
+
+	public String getSummary() {
+		return this.summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	@Override
+	public GetReportResponse getInstance(UnmarshallerContext context) {
+		return	GetReportResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
