68c53c4d7873db0f34d2d32bd580cee1f288ccef
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-12-03 20:29:35 +0800

    SAF SDK Auto Released By xinfa.duanxf,Version：1.0.2 发布日志： 1, Format test case

--- aliyun-java-sdk-saf/src/test/java/SafPopTest.java
+++ aliyun-java-sdk-saf/src/test/java/SafPopTest.java
@@ -1,4 +1,9 @@
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.fastjson.JSONObject;
+
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
@@ -9,59 +14,52 @@ import com.aliyuncs.profile.IClientProfile;
 import com.aliyuncs.saf.model.v20180919.ExecuteRequestRequest;
 import com.aliyuncs.saf.model.v20180919.ExecuteRequestResponse;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
-public class SafaaPopTest {
+public class SafPopTest {
 
+    private static String regionId = "cn-shanghai";
+    public static final String AcesskeyId = "******";
+    public static final String secertKey = "**********";
 
-    private static String regionId="cn-shanghai";
-    public static final String AcesskeyId ="******";
-    public static final String secertKey ="**********";
-
-
-    protected static String getDomain(boolean isVpc){
+    protected static String getDomain(boolean isVpc) {
         String product = "saf";
 
-        if(isVpc){
-            product +="-vpc";
+        if (isVpc) {
+            product += "-vpc";
         }
 
-        if("cn-shanghai".equals(regionId)){
-            return product+".cn-shanghai.aliyuncs.com";
+        if ("cn-shanghai".equals(regionId)) {
+            return product + ".cn-shanghai.aliyuncs.com";
         }
 
-        if("cn-hangzhou".equals(regionId)){
-            return product+".cn-hangzhou.aliyuncs.com";
+        if ("cn-hangzhou".equals(regionId)) {
+            return product + ".cn-hangzhou.aliyuncs.com";
         }
 
-        if("cn-shenzhen".equals(regionId)){
-            return product+".cn-shenzhen.aliyuncs.com";
+        if ("cn-shenzhen".equals(regionId)) {
+            return product + ".cn-shenzhen.aliyuncs.com";
         }
 
-        if("cn-zhangjiakou".equals(regionId)){
-            return product+".cn-zhangjiakou.aliyuncs.com";
+        if ("cn-zhangjiakou".equals(regionId)) {
+            return product + ".cn-zhangjiakou.aliyuncs.com";
         }
-        if("cn-beijing".equals(regionId)){
-            return product+".cn-beijing.aliyuncs.com";
+        if ("cn-beijing".equals(regionId)) {
+            return product + ".cn-beijing.aliyuncs.com";
         }
-
 
         return "saf.cn-shanghai.aliyuncs.com";
     }
 
-
     public static void main(String[] args) throws ClientException, UnsupportedEncodingException {
         IClientProfile profile = DefaultProfile.getProfile(regionId, AcesskeyId, secertKey);
 
-        DefaultProfile.addEndpoint( regionId, "saf", getDomain(false));
+        DefaultProfile.addEndpoint(regionId, "saf", getDomain(false));
 
         IAcsClient client = new DefaultAcsClient(profile);
 
         ExecuteRequestRequest executeRequestRequest = new ExecuteRequestRequest();
         executeRequestRequest.setMethod(com.aliyuncs.http.MethodType.POST); // 指定请求方法
         //服务的产品码：address_validation/email_risk/coupon_abuse/account_abuse。
-        String service ="account_abuse_pro";
+        String service = "account_abuse_pro";
         executeRequestRequest.setService(service);
 
         // 业务详细参数，具体见文档里的业务参数部分,不需要的参数就不需要设置
@@ -77,24 +75,21 @@ public class SafaaPopTest {
         /**
          * 请务必设置超时时间
          */
-       // executeRequestRequest.setConnectTimeout(10000);
+        // executeRequestRequest.setConnectTimeout(10000);
         executeRequestRequest.setReadTimeout(12000);
-        executeRequestRequest.setHttpContent(JSONObject.toJSONString(serviceParams).getBytes("UTF-8"),"UTF-8", FormatType.JSON);
+        executeRequestRequest.setHttpContent(JSONObject.toJSONString(serviceParams).getBytes("UTF-8"), "UTF-8", FormatType.JSON);
 
         try {
             ExecuteRequestResponse httpResponse = client.getAcsResponse(executeRequestRequest);
-                System.out.println("detect not success. code:" +JSONObject.toJSONString(httpResponse));
-//            httpResponse.getData().getScore()
+            System.out.println("httpResponse:" + JSONObject.toJSONString(httpResponse));
+            //            httpResponse.getData().getScore()
         } catch (ServerException e) {
             e.printStackTrace();
         } catch (ClientException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
-
 
 }
