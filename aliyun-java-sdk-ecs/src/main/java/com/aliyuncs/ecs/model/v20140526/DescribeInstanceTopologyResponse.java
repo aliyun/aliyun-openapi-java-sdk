ae05247694ca78b7453fb2a99f6052c0e2a8ca72
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-12-06 19:58:59 +0800

    ECS SDK Auto Released By corey.ql,Version：4.15.0 发布日志： 1, Add api RedeployInstance

---
+++ aliyun-java-sdk-ecs/src/main/java/com/aliyuncs/ecs/model/v20140526/DescribeInstanceTopologyResponse.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecs.model.v20140526;
+
+import java.util.List;
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ecs.transform.v20140526.DescribeInstanceTopologyResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeInstanceTopologyResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<Topology> topologys;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<Topology> getTopologys() {
+		return this.topologys;
+	}
+
+	public void setTopologys(List<Topology> topologys) {
+		this.topologys = topologys;
+	}
+
+	public static class Topology {
+
+		private String instanceId;
+
+		private String hostId;
+
+		public String getInstanceId() {
+			return this.instanceId;
+		}
+
+		public void setInstanceId(String instanceId) {
+			this.instanceId = instanceId;
+		}
+
+		public String getHostId() {
+			return this.hostId;
+		}
+
+		public void setHostId(String hostId) {
+			this.hostId = hostId;
+		}
+	}
+
+	@Override
+	public DescribeInstanceTopologyResponse getInstance(UnmarshallerContext context) {
+		return	DescribeInstanceTopologyResponseUnmarshaller.unmarshall(this, context);
+	}
+}
