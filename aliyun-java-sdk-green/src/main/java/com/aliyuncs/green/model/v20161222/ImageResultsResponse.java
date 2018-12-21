e6c0106d34ce44b8fdae108ec8d92ea681e96ed5
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-12-21 18:49:44 +0800

    GREEN SDK Auto Released By liuhai.lh,Version：3.3.2 发布日志： 1, merge sdk 20161222 into 20180509 for compatible

---
+++ aliyun-java-sdk-green/src/main/java/com/aliyuncs/green/model/v20161222/ImageResultsResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.green.model.v20161222;
+
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.green.transform.v20161222.ImageResultsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ImageResultsResponse extends AcsResponse {
+
+	@Override
+	public ImageResultsResponse getInstance(UnmarshallerContext context) {
+		return	ImageResultsResponseUnmarshaller.unmarshall(this, context);
+	}
+}
