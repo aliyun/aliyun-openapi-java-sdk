e9744e3b3b6d6f01281c38601333aeba3508c95e
haowei.yao <haowei.yao@alibaba-inc.com>
2018-12-28 15:20:21 +0800

    UIS SDK Auto Released By hejian.hj,Version：1.1.0 发布日志： 1, Support Add/Delete Node IP 2, Support Describe Uis block ip list.

---
+++ aliyun-java-sdk-uis/src/main/java/com/aliyuncs/uis/model/v20180821/GetDroppedIpListRequest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.uis.model.v20180821;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetDroppedIpListRequest extends RpcAcsRequest<GetDroppedIpListResponse> {
+	
+	public GetDroppedIpListRequest() {
+		super("Uis", "2018-08-21", "GetDroppedIpList", "uis");
+	}
+
+	private Long resourceOwnerId;
+
+	private String resourceOwnerAccount;
+
+	private String ownerAccount;
+
+	private String uisId;
+
+	private String chartDate;
+
+	private Long ownerId;
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putQueryParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public String getUisId() {
+		return this.uisId;
+	}
+
+	public void setUisId(String uisId) {
+		this.uisId = uisId;
+		if(uisId != null){
+			putQueryParameter("UisId", uisId);
+		}
+	}
+
+	public String getChartDate() {
+		return this.chartDate;
+	}
+
+	public void setChartDate(String chartDate) {
+		this.chartDate = chartDate;
+		if(chartDate != null){
+			putQueryParameter("ChartDate", chartDate);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	@Override
+	public Class<GetDroppedIpListResponse> getResponseClass() {
+		return GetDroppedIpListResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-uis/src/main/java/com/aliyuncs/uis/transform/v20180821/GetDroppedIpListResponseUnmarshaller.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.uis.transform.v20180821;
+
+import com.aliyuncs.uis.model.v20180821.GetDroppedIpListResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetDroppedIpListResponseUnmarshaller {
+
+	public static GetDroppedIpListResponse unmarshall(GetDroppedIpListResponse getDroppedIpListResponse, UnmarshallerContext context) {
+		
+		getDroppedIpListResponse.setRequestId(context.stringValue("GetDroppedIpListResponse.RequestId"));
+		getDroppedIpListResponse.setDroppedIpListUrl(context.stringValue("GetDroppedIpListResponse.DroppedIpListUrl"));
+	 
+	 	return getDroppedIpListResponse;
+	}
+}
