87c2c4e9fe6e7d3a1e75f2928257e3e6ad61b8b9
haowei.yao <haowei.yao@alibaba-inc.com>
2018-12-29 10:14:34 +0800

    DMS-ENTERPRISE SDK Auto Released By yuyang.cjx,Version：1.1.0 发布日志： 1, Add EnableUser interface, Support admin user to enable another user. 2, Add DisableUser Interface, Support admin user to disable another user. 3, Add DeleteUser Interface, Support admin user to delete another user.

---
+++ aliyun-java-sdk-dms-enterprise/src/main/java/com/aliyuncs/dms_enterprise/transform/v20181101/DeleteUserResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dms_enterprise.transform.v20181101;
+
+import com.aliyuncs.dms_enterprise.model.v20181101.DeleteUserResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DeleteUserResponseUnmarshaller {
+
+	public static DeleteUserResponse unmarshall(DeleteUserResponse deleteUserResponse, UnmarshallerContext context) {
+		
+		deleteUserResponse.setRequestId(context.stringValue("DeleteUserResponse.RequestId"));
+		deleteUserResponse.setSuccess(context.booleanValue("DeleteUserResponse.Success"));
+		deleteUserResponse.setErrorMessage(context.stringValue("DeleteUserResponse.ErrorMessage"));
+		deleteUserResponse.setErrorCode(context.stringValue("DeleteUserResponse.ErrorCode"));
+	 
+	 	return deleteUserResponse;
+	}
+}
