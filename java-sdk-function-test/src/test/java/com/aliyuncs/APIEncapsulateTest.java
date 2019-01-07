8e384cf22921db756126c532df0d68f7221e818d
sunzhiqiang <wb-szq493230@alibaba-inc.com>
2019-01-07 17:01:26 +0800

    add java funtion test

---
+++ java-sdk-function-test/src/test/java/com/aliyuncs/APIEncapsulateTest.java
@@ -0,0 +1,242 @@
+package com.aliyuncs;
+
+import com.aliyuncs.ccc.model.v20170705.CommitContactFlowVersionModificationRequest;
+import com.aliyuncs.ccc.model.v20170705.CommitContactFlowVersionModificationResponse;
+import com.aliyuncs.cdn.model.v20180510.DescribeCdnCertificateDetailRequest;
+import com.aliyuncs.cdn.model.v20180510.DescribeCdnCertificateDetailResponse;
+import com.aliyuncs.ecs.model.v20140526.DescribeInstancesRequest;
+import com.aliyuncs.ecs.model.v20140526.DescribeInstancesResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.FormatType;
+import com.aliyuncs.ram.model.v20150501.ListAccessKeysRequest;
+import com.aliyuncs.ram.model.v20150501.ListAccessKeysResponse;
+import com.aliyuncs.rds.model.v20140815.DescribeAbnormalDBInstancesRequest;
+import com.aliyuncs.rds.model.v20140815.DescribeAbnormalDBInstancesResponse;
+import com.aliyuncs.ros.model.v20150901.*;
+import com.aliyuncs.slb.model.v20140515.DescribeAccessControlListsRequest;
+import com.aliyuncs.slb.model.v20140515.DescribeAccessControlListsResponse;
+import com.aliyuncs.vpc.model.v20160428.DescribeAccessPointsRequest;
+import com.aliyuncs.vpc.model.v20160428.DescribeAccessPointsResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class APIEncapsulateTest extends BaseTest {
+
+    @Test
+    public void ecsRequestTest() throws ClientException {
+        DescribeInstancesRequest request = new DescribeInstancesRequest();
+        DescribeInstancesResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertTrue(0 <= response.getTotalCount());
+    }
+
+    @Test
+    public void rdsRequestTest() throws ClientException {
+        DescribeAbnormalDBInstancesRequest request = new DescribeAbnormalDBInstancesRequest();
+        DescribeAbnormalDBInstancesResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertNull(response.getTotalRecordCount());
+    }
+
+    @Test
+    public void cdnRequestTest() throws ClientException {
+        DescribeCdnCertificateDetailRequest request = new DescribeCdnCertificateDetailRequest();
+        request.setCertName("cdnRequestTest");
+        DescribeCdnCertificateDetailResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getRequestId());
+    }
+
+    @Test
+    public void slbRequestTest() throws ClientException {
+        DescribeAccessControlListsRequest request = new DescribeAccessControlListsRequest();
+        DescribeAccessControlListsResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getAcls());
+    }
+
+    @Test
+    public void ramRequestTest() throws ClientException {
+        ListAccessKeysRequest request = new ListAccessKeysRequest();
+        ListAccessKeysResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getAccessKeys());
+    }
+
+    @Test
+    public void vpcRequestTest() throws ClientException {
+        DescribeAccessPointsRequest request = new DescribeAccessPointsRequest();
+        DescribeAccessPointsResponse response = this.client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getTotalCount());
+    }
+
+    @Test
+    public void JsonPutTypeTest() {
+        UpdateStackRequest request = new UpdateStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            UpdateStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("HTTPBadRequest", e.getErrCode());
+            Assert.assertEquals("No template specified", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void XmlPutTypeTest() throws ClientException {
+        // TODO：xml mode is not supported by the server
+        UpdateStackRequest request = new UpdateStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        request.setHttpContentType(FormatType.XML);
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            UpdateStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (IllegalStateException e) {
+            Assert.assertEquals("Server response has a bad format type: null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void FormPutTypeTest() {
+        UpdateStackRequest request = new UpdateStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        request.setHttpContentType(FormatType.FORM);
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            UpdateStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("ContentMD5NotMatched", e.getErrCode());
+            Assert.assertEquals("Specified content md5 is not matched with your request body.", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void JsonDeleteTypeTest() {
+        DeleteStackRequest request = new DeleteStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        // TODO: this is a bug.It should be set automatically region
+        request.putHeaderParameter("x-acs-region-id", "cn-hangzhou");
+        request.setHttpContentType(FormatType.JSON);
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            DeleteStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("StackNotFound", e.getErrCode());
+            Assert.assertEquals("The Stack (test) could not be found.", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void XmlDeleteTypeTest() throws ClientException {
+        DeleteStackRequest request = new DeleteStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putHeaderParameter("x-acs-region-id", "cn-hangzhou");
+        request.setHttpContentType(FormatType.XML);
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            DeleteStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("StackNotFound", e.getErrCode());
+            Assert.assertEquals("The Stack (test) could not be found.", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void FormDeleteTypeTest() {
+        DeleteStackRequest request = new DeleteStackRequest();
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putHeaderParameter("x-acs-region-id", "cn-hangzhou");
+        request.setHttpContentType(FormatType.FORM);
+        request.putBodyParameter("ContentMD5NotMatched", "test");
+        try {
+            DeleteStackResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("StackNotFound", e.getErrCode());
+            Assert.assertEquals("The Stack (test) could not be found.", e.getErrMsg());
+        }
+    }
+
+    /**
+     * xml method
+     */
+    @Test
+    public void xmlPostTypeTest() throws ClientException {
+        DoActionsRequest request = new DoActionsRequest();
+        request.setHttpContentType(FormatType.XML);
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putBodyParameter("test", "test");
+        request.putBodyParameter("test2", "test2");
+        try {
+            DoActionsResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (IllegalStateException e) {
+            Assert.assertEquals("Server response has a bad format type: null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void JsonPostTypeTest() {
+        DoActionsRequest request = new DoActionsRequest();
+        request.setHttpContentType(FormatType.JSON);
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putBodyParameter("test", "test");
+        try {
+            DoActionsResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("HTTPBadRequest", e.getErrCode());
+            Assert.assertEquals("Invalid action test specified", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void FormPostTypeTest() {
+        DoActionsRequest request = new DoActionsRequest();
+        request.setHttpContentType(FormatType.FORM);
+        request.setStackName("test");
+        request.setStackId("test");
+        request.putBodyParameter("test", "test");
+        try {
+            DoActionsResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("HTTPBadRequest", e.getErrCode());
+            Assert.assertEquals("No action specified", e.getErrMsg());
+        }
+    }
+
+    @Test
+    public void rpcPostTest() throws ClientException {
+        CommitContactFlowVersionModificationRequest request = new CommitContactFlowVersionModificationRequest();
+        request.setContent("test");
+        request.setCanvas("test");
+        request.setContactFlowVersionId("test");
+        request.setInstanceId("test");
+        try{
+            CommitContactFlowVersionModificationResponse response = this.client.getAcsResponse(request);
+            Assert.fail();
+        } catch (ServerException e) {
+            Assert.assertEquals("ServiceUnavailable", e.getErrCode());
+            Assert.assertEquals("The request has failed due to a temporary failure of the server.",
+                    e.getErrMsg());
+        }
+    }
+}
---
+++ java-sdk-function-test/src/test/java/com/aliyuncs/ErrorHandlingTest.java
@@ -0,0 +1,45 @@
+package com.aliyuncs;
+
+import com.aliyuncs.exceptions.ClientException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ErrorHandlingTest extends BaseTest {
+
+    @Test
+    public void parameterErrorTest() {
+        CommonRequest request = new CommonRequest();
+        request.setSysDomain("ecs-cn-hangzhou.aliyuncs.com");
+        request.setSysVersion("2014-05-26");
+        request.setSysAction("actionError");
+        CommonResponse response = null;
+        try {
+            response = this.client.getCommonResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("InvalidParameter", e.getErrCode());
+            Assert.assertEquals("The specified parameter \"Action or Version\" is not valid.",
+                    e.getErrMsg()
+            );
+        }
+    }
+
+    @Test
+    public void timeOutErrorTest() {
+        CommonRequest request = new CommonRequest();
+        request.setSysDomain("ecs-cn-hangzhou.aliyuncs.com");
+        request.setSysVersion("2014-05-26");
+        request.setSysAction("DescribeAccessPoints");
+        request.setSysReadTimeout(1);
+        CommonResponse response = null;
+        try {
+            response = this.client.getCommonResponse(request);
+            Assert.fail();
+        } catch (ClientException e) {
+            Assert.assertEquals("SDK.ServerUnreachable", e.getErrCode());
+            Assert.assertEquals("SocketTimeoutException has occurred on a socket read or accept.",
+                    e.getErrMsg()
+            );
+        }
+    }
+}
