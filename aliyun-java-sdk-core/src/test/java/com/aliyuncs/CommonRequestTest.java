42ce193f209e41d5182e6db8e55c666363f1b2a3
sunzhiqiang <wb-szq493230@alibaba-inc.com>
2019-01-11 17:49:08 +0800

    add test

--- aliyun-java-sdk-core/src/test/java/com/aliyuncs/CommonRequestTest.java
+++ aliyun-java-sdk-core/src/test/java/com/aliyuncs/CommonRequestTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CommonRequestTest {
@@ -23,6 +24,13 @@ public class CommonRequestTest {
     @Test
     public void commonRequestGetSetTest() throws UnsupportedEncodingException {
         CommonRequest request = new CommonRequest();
+        request.setHttpContent("test".getBytes("UTF-8"), "test", null);
+        request.setHttpContent(null, "test", null);
+        request.setHttpContent("test".getBytes("UTF-8"), null, FormatType.JSON);
+        Assert.assertNull(request.getSysEncoding());
+        Assert.assertNull(request.getHttpContent());
+        Assert.assertNull(request.getHttpContentType());
+
         request.setHttpContent("abc".getBytes(), "utf-8", FormatType.JSON);
         Assert.assertEquals("utf-8", request.getSysEncoding());
         Assert.assertTrue(FormatType.JSON == request.getHttpContentType());
@@ -74,6 +82,7 @@ public class CommonRequestTest {
         Assert.assertNotNull(queryMap);
         Assert.assertEquals("test", queryMap.get("test"));
 
+        request.putHeadParameter(null, null);
         request.putHeadParameter("test", "test");
         Map<String, String> headMap = request.getSysHeadParameters();
         Assert.assertNotNull(headMap);
@@ -83,10 +92,70 @@ public class CommonRequestTest {
         Map<String, String> pathMap = request.getSysPathParameters();
         Assert.assertNotNull(pathMap);
         Assert.assertEquals("test", pathMap.get("test"));
+    }
+
+    @Test
+    public void deprecatedGetSetTest() throws UnsupportedEncodingException {
+        CommonRequest request = new CommonRequest();
+
+        request.setUriPattern("uri");
+        Assert.assertEquals("uri", request.getUriPattern());
+
+        request.setProduct("ecs");
+        Assert.assertEquals("ecs", request.getProduct());
+
+        request.setReadTimeout(50000);
+        Assert.assertTrue(50000 == request.getReadTimeout());
+
+        request.putBodyParameter("test", "test");
+        Map<String, String> bodyMap = request.getBodyParameters();
+        Assert.assertNotNull(bodyMap);
+        Assert.assertEquals("test", bodyMap.get("test"));
+
+        request.setMethod(MethodType.DELETE);
+        Assert.assertTrue(MethodType.DELETE == request.getMethod());
+
+        request.setAction("action");
+        Assert.assertEquals("action", request.getAction());
+
+        request.setConnectTimeout(5000);
+        Assert.assertTrue(5000 == request.getConnectTimeout());
+
+        request.setDomain("domian");
+        Assert.assertEquals("domian", request.getDomain());
+
+        request.setEndpointType("endPoint");
+        Assert.assertEquals("endPoint", request.getEndpointType());
+
+        request.setLocationProduct("test");
+        Assert.assertEquals("test", request.getLocationProduct());
+
+        request.setProtocol(ProtocolType.HTTP);
+        Assert.assertTrue(ProtocolType.HTTP == request.getProtocol());
+
+        request.setRegionId("hangzhou");
+        Assert.assertEquals("hangzhou", request.getRegionId());
+
+        request.setVersion("4");
+        Assert.assertEquals("4", request.getVersion());
+
+        request.putQueryParameter("test", "test");
+        Map<String, String> queryMap = request.getQueryParameters();
+        Assert.assertNotNull(queryMap);
+        Assert.assertEquals("test", queryMap.get("test"));
+
+        request.putHeadParameter("test", "test");
+        Map<String, String> headMap = request.getHeadParameters();
+        Assert.assertNotNull(headMap);
+        Assert.assertEquals("test", headMap.get("test"));
+
+        request.putPathParameter("test", "test");
+        Map<String, String> pathMap = request.getPathParameters();
+        Assert.assertNotNull(pathMap);
+        Assert.assertEquals("test", pathMap.get("test"));
 
         request.setHttpContent(null, null, null);
-        request.putHeadParameter(null, null);
-
+        Assert.assertNull(request.getEncoding());
     }
 
     @Test
