67bc7c2e0becfbad54c34ee20c871312ecdc8ff5
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-13 17:41:42 +0800

    LUBANRULER SDK Auto Released By shenshi,Version：1.0.1 发布日志： 1, Update Dependency

--- aliyun-java-sdk-lubanruler/src/main/java/com/aliyuncs/lubanruler/model/v20171228/CreateLubanrulerTaskRequest.java
+++ aliyun-java-sdk-lubanruler/src/main/java/com/aliyuncs/lubanruler/model/v20171228/CreateLubanrulerTaskRequest.java
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.aliyuncs.lubanruler.model.v20171228;
 
 import com.aliyuncs.RpcAcsRequest;
@@ -23,7 +24,7 @@ import com.aliyuncs.http.MethodType;
 public class CreateLubanrulerTaskRequest extends RpcAcsRequest<CreateLubanrulerTaskResponse> {
 	
 	public CreateLubanrulerTaskRequest() {
-		super("Lubanruler", "2017-12-28", "CreateLubanrulerTask");
+		super("Lubanruler", "2017-12-28", "CreateLubanrulerTask", "lubanruler");
 		setMethod(MethodType.POST);
 	}
 
