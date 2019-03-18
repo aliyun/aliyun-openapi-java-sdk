9882210df432017c5c5df407926b9ef1c4036607
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-18 14:07:56 +0800

    ARMS4FINANCE SDK Auto Released By shenshi,Version：2.0.1 发布日志： 1, Update Dependency

--- aliyun-java-sdk-arms4finance/src/main/java/com/aliyuncs/arms4finance/transform/v20171130/ARMSQueryDataSetResponseUnmarshaller.java
+++ aliyun-java-sdk-arms4finance/src/main/java/com/aliyuncs/arms4finance/transform/v20171130/ARMSQueryDataSetResponseUnmarshaller.java
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.aliyuncs.arms4finance.transform.v20171130;
 
 import com.aliyuncs.arms4finance.model.v20171130.ARMSQueryDataSetResponse;
