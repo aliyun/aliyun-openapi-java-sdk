7a7edd22c064b1367e2766e0c1ef42f05f254642
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-17 15:15:28 +0800

    COUPONNEW SDK Auto Released By yingying,Version：1.0.0 发布日志： 1, publish first 2, add one new api name getUidsByTemplateId

---
+++ aliyun-java-sdk-couponnew/src/main/java/com/aliyuncs/couponnew/model/v20190401/GetHasCouponUidListRequest.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.couponnew.model.v20190401;
+
+import com.aliyuncs.RpcAcsRequest;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetHasCouponUidListRequest extends RpcAcsRequest<GetHasCouponUidListResponse> {
+	
+	public GetHasCouponUidListRequest() {
+		super("CouponNew", "2019-04-01", "GetHasCouponUidList");
+	}
+
+	private String fromApp;
+
+	private String param;
+
+	private String idempotentToken;
+
+	public String getFromApp() {
+		return this.fromApp;
+	}
+
+	public void setFromApp(String fromApp) {
+		this.fromApp = fromApp;
+		if(fromApp != null){
+			putQueryParameter("FromApp", fromApp);
+		}
+	}
+
+	public String getParam() {
+		return this.param;
+	}
+
+	public void setParam(String param) {
+		this.param = param;
+		if(param != null){
+			putQueryParameter("Param", param);
+		}
+	}
+
+	public String getIdempotentToken() {
+		return this.idempotentToken;
+	}
+
+	public void setIdempotentToken(String idempotentToken) {
+		this.idempotentToken = idempotentToken;
+		if(idempotentToken != null){
+			putQueryParameter("IdempotentToken", idempotentToken);
+		}
+	}
+
+	@Override
+	public Class<GetHasCouponUidListResponse> getResponseClass() {
+		return GetHasCouponUidListResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-couponnew/src/main/java/com/aliyuncs/couponnew/transform/v20190401/GetHasCouponUidListResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.couponnew.transform.v20190401;
+
+import com.aliyuncs.couponnew.model.v20190401.GetHasCouponUidListResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetHasCouponUidListResponseUnmarshaller {
+
+	public static GetHasCouponUidListResponse unmarshall(GetHasCouponUidListResponse getHasCouponUidListResponse, UnmarshallerContext context) {
+		
+		getHasCouponUidListResponse.setRequestId(context.stringValue("GetHasCouponUidListResponse.RequestId"));
+		getHasCouponUidListResponse.setSuccess(context.booleanValue("GetHasCouponUidListResponse.Success"));
+		getHasCouponUidListResponse.setCode(context.stringValue("GetHasCouponUidListResponse.Code"));
+		getHasCouponUidListResponse.setMessage(context.stringValue("GetHasCouponUidListResponse.Message"));
+		getHasCouponUidListResponse.setIsIdempotentResp(context.booleanValue("GetHasCouponUidListResponse.IsIdempotentResp"));
+		getHasCouponUidListResponse.setData(context.stringValue("GetHasCouponUidListResponse.Data"));
+	 
+	 	return getHasCouponUidListResponse;
+	}
+}
