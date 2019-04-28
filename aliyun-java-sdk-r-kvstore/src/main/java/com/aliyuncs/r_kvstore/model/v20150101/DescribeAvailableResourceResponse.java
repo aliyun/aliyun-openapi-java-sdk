f433e71d8e45b7532e9160a7d4a3e0f10278faf1
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-28 19:49:07 +0800

    R-KVSTORE SDK Auto Released By xiaoyan.yan,Version：2.0.8 发布日志： 1, Publish DescribeAvailableResource.

---
+++ aliyun-java-sdk-r-kvstore/src/main/java/com/aliyuncs/r_kvstore/model/v20150101/DescribeAvailableResourceResponse.java
@@ -0,0 +1,311 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.r_kvstore.model.v20150101;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.r_kvstore.transform.v20150101.DescribeAvailableResourceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeAvailableResourceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<AvailableZone> availableZones;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<AvailableZone> getAvailableZones() {
+		return this.availableZones;
+	}
+
+	public void setAvailableZones(List<AvailableZone> availableZones) {
+		this.availableZones = availableZones;
+	}
+
+	public static class AvailableZone {
+
+		private String regionId;
+
+		private String zoneId;
+
+		private String status;
+
+		private String networkTypes;
+
+		private List<SupportedEngine> supportedEngines;
+
+		public String getBizRegionId() {
+			return this.regionId;
+		}
+
+		public void setBizRegionId(String regionId) {
+			this.regionId = regionId;
+		}
+
+		/**
+		 * @deprecated use getBizRegionId instead of this.
+		 */
+		@Deprecated
+		public String getRegionId() {
+			return this.regionId;
+		}
+
+		/**
+		 * @deprecated use setBizRegionId instead of this.
+		 */
+		@Deprecated
+		public void setRegionId(String regionId) {
+			this.regionId = regionId;
+		}
+
+		public String getZoneId() {
+			return this.zoneId;
+		}
+
+		public void setZoneId(String zoneId) {
+			this.zoneId = zoneId;
+		}
+
+		public String getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public String getNetworkTypes() {
+			return this.networkTypes;
+		}
+
+		public void setNetworkTypes(String networkTypes) {
+			this.networkTypes = networkTypes;
+		}
+
+		public List<SupportedEngine> getSupportedEngines() {
+			return this.supportedEngines;
+		}
+
+		public void setSupportedEngines(List<SupportedEngine> supportedEngines) {
+			this.supportedEngines = supportedEngines;
+		}
+
+		public static class SupportedEngine {
+
+			private String engine;
+
+			private List<SupportedEngineVersion> supportedEngineVersions;
+
+			public String getEngine() {
+				return this.engine;
+			}
+
+			public void setEngine(String engine) {
+				this.engine = engine;
+			}
+
+			public List<SupportedEngineVersion> getSupportedEngineVersions() {
+				return this.supportedEngineVersions;
+			}
+
+			public void setSupportedEngineVersions(List<SupportedEngineVersion> supportedEngineVersions) {
+				this.supportedEngineVersions = supportedEngineVersions;
+			}
+
+			public static class SupportedEngineVersion {
+
+				private String version;
+
+				private List<SupportedArchitectureType> supportedArchitectureTypes;
+
+				public String getBizVersion() {
+					return this.version;
+				}
+
+				public void setBizVersion(String version) {
+					this.version = version;
+				}
+
+				/**
+				 * @deprecated use getBizVersion instead of this.
+				 */
+				@Deprecated
+				public String getVersion() {
+					return this.version;
+				}
+
+				/**
+				 * @deprecated use setBizVersion instead of this.
+				 */
+				@Deprecated
+				public void setVersion(String version) {
+					this.version = version;
+				}
+
+				public List<SupportedArchitectureType> getSupportedArchitectureTypes() {
+					return this.supportedArchitectureTypes;
+				}
+
+				public void setSupportedArchitectureTypes(List<SupportedArchitectureType> supportedArchitectureTypes) {
+					this.supportedArchitectureTypes = supportedArchitectureTypes;
+				}
+
+				public static class SupportedArchitectureType {
+
+					private String architecture;
+
+					private List<SupportedPerformanceType> supportedPerformanceTypes;
+
+					public String getArchitecture() {
+						return this.architecture;
+					}
+
+					public void setArchitecture(String architecture) {
+						this.architecture = architecture;
+					}
+
+					public List<SupportedPerformanceType> getSupportedPerformanceTypes() {
+						return this.supportedPerformanceTypes;
+					}
+
+					public void setSupportedPerformanceTypes(List<SupportedPerformanceType> supportedPerformanceTypes) {
+						this.supportedPerformanceTypes = supportedPerformanceTypes;
+					}
+
+					public static class SupportedPerformanceType {
+
+						private String performanceType;
+
+						private List<SupportedStorageType> supportedStorageTypes;
+
+						public String getPerformanceType() {
+							return this.performanceType;
+						}
+
+						public void setPerformanceType(String performanceType) {
+							this.performanceType = performanceType;
+						}
+
+						public List<SupportedStorageType> getSupportedStorageTypes() {
+							return this.supportedStorageTypes;
+						}
+
+						public void setSupportedStorageTypes(List<SupportedStorageType> supportedStorageTypes) {
+							this.supportedStorageTypes = supportedStorageTypes;
+						}
+
+						public static class SupportedStorageType {
+
+							private String storageType;
+
+							private List<SupportedNodeType> supportedNodeTypes;
+
+							public String getStorageType() {
+								return this.storageType;
+							}
+
+							public void setStorageType(String storageType) {
+								this.storageType = storageType;
+							}
+
+							public List<SupportedNodeType> getSupportedNodeTypes() {
+								return this.supportedNodeTypes;
+							}
+
+							public void setSupportedNodeTypes(List<SupportedNodeType> supportedNodeTypes) {
+								this.supportedNodeTypes = supportedNodeTypes;
+							}
+
+							public static class SupportedNodeType {
+
+								private String nodeType;
+
+								private List<SupportedPackageType> supportedPackageTypes;
+
+								public String getNodeType() {
+									return this.nodeType;
+								}
+
+								public void setNodeType(String nodeType) {
+									this.nodeType = nodeType;
+								}
+
+								public List<SupportedPackageType> getSupportedPackageTypes() {
+									return this.supportedPackageTypes;
+								}
+
+								public void setSupportedPackageTypes(List<SupportedPackageType> supportedPackageTypes) {
+									this.supportedPackageTypes = supportedPackageTypes;
+								}
+
+								public static class SupportedPackageType {
+
+									private String packageType;
+
+									private List<AvailableResource> availableResources;
+
+									public String getPackageType() {
+										return this.packageType;
+									}
+
+									public void setPackageType(String packageType) {
+										this.packageType = packageType;
+									}
+
+									public List<AvailableResource> getAvailableResources() {
+										return this.availableResources;
+									}
+
+									public void setAvailableResources(List<AvailableResource> availableResources) {
+										this.availableResources = availableResources;
+									}
+
+									public static class AvailableResource {
+
+										private String instanceClass;
+
+										public String getInstanceClass() {
+											return this.instanceClass;
+										}
+
+										public void setInstanceClass(String instanceClass) {
+											this.instanceClass = instanceClass;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public DescribeAvailableResourceResponse getInstance(UnmarshallerContext context) {
+		return	DescribeAvailableResourceResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-r-kvstore/src/main/java/com/aliyuncs/r_kvstore/transform/v20150101/DescribeAvailableResourceResponseUnmarshaller.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.r_kvstore.transform.v20150101;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType.SupportedPerformanceType;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType.SupportedPerformanceType.SupportedStorageType;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType.SupportedPerformanceType.SupportedStorageType.SupportedNodeType;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType.SupportedPerformanceType.SupportedStorageType.SupportedNodeType.SupportedPackageType;
+import com.aliyuncs.r_kvstore.model.v20150101.DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedArchitectureType.SupportedPerformanceType.SupportedStorageType.SupportedNodeType.SupportedPackageType.AvailableResource;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeAvailableResourceResponseUnmarshaller {
+
+	public static DescribeAvailableResourceResponse unmarshall(DescribeAvailableResourceResponse describeAvailableResourceResponse, UnmarshallerContext context) {
+		
+		describeAvailableResourceResponse.setRequestId(context.stringValue("DescribeAvailableResourceResponse.RequestId"));
+
+		List<AvailableZone> availableZones = new ArrayList<AvailableZone>();
+		for (int i = 0; i < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones.Length"); i++) {
+			AvailableZone availableZone = new AvailableZone();
+			availableZone.setRegionId(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].RegionId"));
+			availableZone.setZoneId(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].ZoneId"));
+			availableZone.setStatus(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].Status"));
+			availableZone.setNetworkTypes(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].NetworkTypes"));
+
+			List<SupportedEngine> supportedEngines = new ArrayList<SupportedEngine>();
+			for (int j = 0; j < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines.Length"); j++) {
+				SupportedEngine supportedEngine = new SupportedEngine();
+				supportedEngine.setEngine(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].Engine"));
+
+				List<SupportedEngineVersion> supportedEngineVersions = new ArrayList<SupportedEngineVersion>();
+				for (int k = 0; k < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions.Length"); k++) {
+					SupportedEngineVersion supportedEngineVersion = new SupportedEngineVersion();
+					supportedEngineVersion.setVersion(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].Version"));
+
+					List<SupportedArchitectureType> supportedArchitectureTypes = new ArrayList<SupportedArchitectureType>();
+					for (int l = 0; l < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes.Length"); l++) {
+						SupportedArchitectureType supportedArchitectureType = new SupportedArchitectureType();
+						supportedArchitectureType.setArchitecture(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].Architecture"));
+
+						List<SupportedPerformanceType> supportedPerformanceTypes = new ArrayList<SupportedPerformanceType>();
+						for (int m = 0; m < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes.Length"); m++) {
+							SupportedPerformanceType supportedPerformanceType = new SupportedPerformanceType();
+							supportedPerformanceType.setPerformanceType(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].PerformanceType"));
+
+							List<SupportedStorageType> supportedStorageTypes = new ArrayList<SupportedStorageType>();
+							for (int n = 0; n < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes.Length"); n++) {
+								SupportedStorageType supportedStorageType = new SupportedStorageType();
+								supportedStorageType.setStorageType(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].StorageType"));
+
+								List<SupportedNodeType> supportedNodeTypes = new ArrayList<SupportedNodeType>();
+								for (int o = 0; o < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes.Length"); o++) {
+									SupportedNodeType supportedNodeType = new SupportedNodeType();
+									supportedNodeType.setNodeType(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].NodeType"));
+
+									List<SupportedPackageType> supportedPackageTypes = new ArrayList<SupportedPackageType>();
+									for (int p = 0; p < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes.Length"); p++) {
+										SupportedPackageType supportedPackageType = new SupportedPackageType();
+										supportedPackageType.setPackageType(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].PackageType"));
+
+										List<AvailableResource> availableResources = new ArrayList<AvailableResource>();
+										for (int q = 0; q < context.lengthValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].AvailableResources.Length"); q++) {
+											AvailableResource availableResource = new AvailableResource();
+											availableResource.setInstanceClass(context.stringValue("DescribeAvailableResourceResponse.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].AvailableResources["+ q +"].InstanceClass"));
+
+											availableResources.add(availableResource);
+										}
+										supportedPackageType.setAvailableResources(availableResources);
+
+										supportedPackageTypes.add(supportedPackageType);
+									}
+									supportedNodeType.setSupportedPackageTypes(supportedPackageTypes);
+
+									supportedNodeTypes.add(supportedNodeType);
+								}
+								supportedStorageType.setSupportedNodeTypes(supportedNodeTypes);
+
+								supportedStorageTypes.add(supportedStorageType);
+							}
+							supportedPerformanceType.setSupportedStorageTypes(supportedStorageTypes);
+
+							supportedPerformanceTypes.add(supportedPerformanceType);
+						}
+						supportedArchitectureType.setSupportedPerformanceTypes(supportedPerformanceTypes);
+
+						supportedArchitectureTypes.add(supportedArchitectureType);
+					}
+					supportedEngineVersion.setSupportedArchitectureTypes(supportedArchitectureTypes);
+
+					supportedEngineVersions.add(supportedEngineVersion);
+				}
+				supportedEngine.setSupportedEngineVersions(supportedEngineVersions);
+
+				supportedEngines.add(supportedEngine);
+			}
+			availableZone.setSupportedEngines(supportedEngines);
+
+			availableZones.add(availableZone);
+		}
+		describeAvailableResourceResponse.setAvailableZones(availableZones);
+	 
+	 	return describeAvailableResourceResponse;
+	}
+}
