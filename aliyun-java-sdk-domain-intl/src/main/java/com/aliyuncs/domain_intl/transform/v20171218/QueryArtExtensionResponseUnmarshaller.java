986186d89148846e5cd20acc49e3246e2bcc654a
sdk-team <sdk-team@alibabacloud.com>
2019-05-13 19:13:42 +0800

    DOMAIN-INTL SDK Auto Released By honglliang_whl,Version：1.6.0 发布日志： 1, Add three new apis, SaveSingleTaskForSaveArtExtension, QueryArtExtension and CancelTask.

---
+++ aliyun-java-sdk-domain-intl/src/main/java/com/aliyuncs/domain_intl/transform/v20171218/QueryArtExtensionResponseUnmarshaller.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.domain_intl.transform.v20171218;
+
+import com.aliyuncs.domain_intl.model.v20171218.QueryArtExtensionResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryArtExtensionResponseUnmarshaller {
+
+	public static QueryArtExtensionResponse unmarshall(QueryArtExtensionResponse queryArtExtensionResponse, UnmarshallerContext context) {
+		
+		queryArtExtensionResponse.setRequestId(context.stringValue("QueryArtExtensionResponse.RequestId"));
+		queryArtExtensionResponse.setObjectType(context.stringValue("QueryArtExtensionResponse.ObjectType"));
+		queryArtExtensionResponse.setMaterialsAndTechniques(context.stringValue("QueryArtExtensionResponse.MaterialsAndTechniques"));
+		queryArtExtensionResponse.setDimensions(context.stringValue("QueryArtExtensionResponse.Dimensions"));
+		queryArtExtensionResponse.setTitle(context.stringValue("QueryArtExtensionResponse.Title"));
+		queryArtExtensionResponse.setDateOrPeriod(context.stringValue("QueryArtExtensionResponse.DateOrPeriod"));
+		queryArtExtensionResponse.setMaker(context.stringValue("QueryArtExtensionResponse.Maker"));
+		queryArtExtensionResponse.setInscriptionsAndMarkings(context.stringValue("QueryArtExtensionResponse.InscriptionsAndMarkings"));
+		queryArtExtensionResponse.setSubject(context.stringValue("QueryArtExtensionResponse.Subject"));
+		queryArtExtensionResponse.setFeatures(context.stringValue("QueryArtExtensionResponse.Features"));
+		queryArtExtensionResponse.setReference(context.stringValue("QueryArtExtensionResponse.Reference"));
+	 
+	 	return queryArtExtensionResponse;
+	}
+}
