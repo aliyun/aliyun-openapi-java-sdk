03fc9d8bf32c240fd75656d06a255e74a482d910
sdk-team <sdk-team@alibabacloud.com>
2019-05-13 12:53:59 +0800

    EDAS SDK Auto Released By owen.zw,Version：2.46.2 发布日志： 1, Release ContinuePipeline API.

---
+++ aliyun-java-sdk-edas/src/main/java/com/aliyuncs/edas/model/v20170801/ContinuePipelineRequest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.edas.model.v20170801;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ContinuePipelineRequest extends RoaAcsRequest<ContinuePipelineResponse> {
+	
+	public ContinuePipelineRequest() {
+		super("Edas", "2017-08-01", "ContinuePipeline", "edas");
+		setUriPattern("/pop/v5/changeorder/pipeline_batch_confirm");
+		setMethod(MethodType.GET);
+	}
+
+	private Boolean confirm;
+
+	private String pipelineId;
+
+	public Boolean getConfirm() {
+		return this.confirm;
+	}
+
+	public void setConfirm(Boolean confirm) {
+		this.confirm = confirm;
+		if(confirm != null){
+			putQueryParameter("Confirm", confirm.toString());
+		}
+	}
+
+	public String getPipelineId() {
+		return this.pipelineId;
+	}
+
+	public void setPipelineId(String pipelineId) {
+		this.pipelineId = pipelineId;
+		if(pipelineId != null){
+			putQueryParameter("PipelineId", pipelineId);
+		}
+	}
+
+	@Override
+	public Class<ContinuePipelineResponse> getResponseClass() {
+		return ContinuePipelineResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-edas/src/main/java/com/aliyuncs/edas/transform/v20170801/ContinuePipelineResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.edas.transform.v20170801;
+
+import com.aliyuncs.edas.model.v20170801.ContinuePipelineResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ContinuePipelineResponseUnmarshaller {
+
+	public static ContinuePipelineResponse unmarshall(ContinuePipelineResponse continuePipelineResponse, UnmarshallerContext context) {
+		
+		continuePipelineResponse.setRequestId(context.stringValue("ContinuePipelineResponse.RequestId"));
+		continuePipelineResponse.setCode(context.integerValue("ContinuePipelineResponse.Code"));
+		continuePipelineResponse.setMessage(context.stringValue("ContinuePipelineResponse.Message"));
+	 
+	 	return continuePipelineResponse;
+	}
+}
