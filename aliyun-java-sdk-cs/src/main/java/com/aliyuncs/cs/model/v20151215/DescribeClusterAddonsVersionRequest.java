6b060a59a503f6ee9cb1bdb37d9f072a31228320
sdk-team <sdk-team@alibabacloud.com>
2019-05-16 17:05:20 +0800

    CS SDK Auto Released By xiaobing.meng,Version：4.7.14 发布日志： 1, Update SDK Version 2, Change api UpgradeClusterAddons, UpgradeClusterComponents, DescribeClusterAddonsVersion to public

---
+++ aliyun-java-sdk-cs/src/main/java/com/aliyuncs/cs/model/v20151215/DescribeClusterAddonsVersionRequest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cs.model.v20151215;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeClusterAddonsVersionRequest extends RoaAcsRequest<DescribeClusterAddonsVersionResponse> {
+	
+	public DescribeClusterAddonsVersionRequest() {
+		super("CS", "2015-12-15", "DescribeClusterAddonsVersion");
+		setUriPattern("/clusters/[ClusterId]/components/version");
+		setMethod(MethodType.GET);
+	}
+
+	private String clusterId;
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+		if(clusterId != null){
+			putPathParameter("ClusterId", clusterId);
+		}
+	}
+
+	@Override
+	public Class<DescribeClusterAddonsVersionResponse> getResponseClass() {
+		return DescribeClusterAddonsVersionResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cs/src/main/java/com/aliyuncs/cs/model/v20151215/UpgradeClusterAddonsRequest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cs.model.v20151215;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpgradeClusterAddonsRequest extends RoaAcsRequest<UpgradeClusterAddonsResponse> {
+	
+	public UpgradeClusterAddonsRequest() {
+		super("CS", "2015-12-15", "UpgradeClusterAddons");
+		setUriPattern("/clusters/[ClusterId]/components/upgrade");
+		setMethod(MethodType.POST);
+	}
+
+	private String clusterId;
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+		if(clusterId != null){
+			putPathParameter("ClusterId", clusterId);
+		}
+	}
+
+	@Override
+	public Class<UpgradeClusterAddonsResponse> getResponseClass() {
+		return UpgradeClusterAddonsResponse.class;
+	}
+
+}
