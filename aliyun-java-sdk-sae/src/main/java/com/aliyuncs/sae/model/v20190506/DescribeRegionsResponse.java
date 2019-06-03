cb6359f4e46f41301b2220f56ec9480c99c750a0
sdk-team <sdk-team@alibabacloud.com>
2019-06-03 16:59:20 +0800

    发布单接口规范化

---
+++ aliyun-java-sdk-sae/src/main/java/com/aliyuncs/sae/model/v20190506/DescribeRegionsResponse.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sae.model.v20190506;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.sae.transform.v20190506.DescribeRegionsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeRegionsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Integer code;
+
+	private String message;
+
+	private List<Region> regions;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Integer getCode() {
+		return this.code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<Region> getRegions() {
+		return this.regions;
+	}
+
+	public void setRegions(List<Region> regions) {
+		this.regions = regions;
+	}
+
+	public static class Region {
+
+		private String regionId;
+
+		private String regionEndpoint;
+
+		private String localName;
+
+		public String getBizRegionId() {
+			return this.regionId;
+		}
+
+		public void setBizRegionId(String regionId) {
+			this.regionId = regionId;
+		}
+
+		/**
+		 * @deprecated use getBizRegionId instead of this.
+		 */
+		@Deprecated
+		public String getRegionId() {
+			return this.regionId;
+		}
+
+		/**
+		 * @deprecated use setBizRegionId instead of this.
+		 */
+		@Deprecated
+		public void setRegionId(String regionId) {
+			this.regionId = regionId;
+		}
+
+		public String getRegionEndpoint() {
+			return this.regionEndpoint;
+		}
+
+		public void setRegionEndpoint(String regionEndpoint) {
+			this.regionEndpoint = regionEndpoint;
+		}
+
+		public String getLocalName() {
+			return this.localName;
+		}
+
+		public void setLocalName(String localName) {
+			this.localName = localName;
+		}
+	}
+
+	@Override
+	public DescribeRegionsResponse getInstance(UnmarshallerContext context) {
+		return	DescribeRegionsResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-sae/src/main/java/com/aliyuncs/sae/transform/v20190506/DescribeRegionsResponseUnmarshaller.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sae.transform.v20190506;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.sae.model.v20190506.DescribeRegionsResponse;
+import com.aliyuncs.sae.model.v20190506.DescribeRegionsResponse.Region;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeRegionsResponseUnmarshaller {
+
+	public static DescribeRegionsResponse unmarshall(DescribeRegionsResponse describeRegionsResponse, UnmarshallerContext context) {
+		
+		describeRegionsResponse.setRequestId(context.stringValue("DescribeRegionsResponse.RequestId"));
+		describeRegionsResponse.setCode(context.integerValue("DescribeRegionsResponse.Code"));
+		describeRegionsResponse.setMessage(context.stringValue("DescribeRegionsResponse.Message"));
+
+		List<Region> regions = new ArrayList<Region>();
+		for (int i = 0; i < context.lengthValue("DescribeRegionsResponse.Regions.Length"); i++) {
+			Region region = new Region();
+			region.setRegionId(context.stringValue("DescribeRegionsResponse.Regions["+ i +"].RegionId"));
+			region.setRegionEndpoint(context.stringValue("DescribeRegionsResponse.Regions["+ i +"].RegionEndpoint"));
+			region.setLocalName(context.stringValue("DescribeRegionsResponse.Regions["+ i +"].LocalName"));
+
+			regions.add(region);
+		}
+		describeRegionsResponse.setRegions(regions);
+	 
+	 	return describeRegionsResponse;
+	}
+}
