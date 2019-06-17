8d92ee3ab363489b0ef5a8b14322018b093ff8d1
sdk-team <sdk-team@alibabacloud.com>
2019-06-17 20:43:18 +0800

    Add addRamMember api

---
+++ aliyun-java-sdk-rdc/src/main/java/com/aliyuncs/rdc/transform/v20180821/AddRamMemberResponseUnmarshaller.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.rdc.transform.v20180821;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.rdc.model.v20180821.AddRamMemberResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class AddRamMemberResponseUnmarshaller {
+
+	public static AddRamMemberResponse unmarshall(AddRamMemberResponse addRamMemberResponse, UnmarshallerContext context) {
+		
+		addRamMemberResponse.setRequestId(context.stringValue("AddRamMemberResponse.RequestId"));
+		addRamMemberResponse.setSuccess(context.booleanValue("AddRamMemberResponse.Success"));
+		addRamMemberResponse.setCode(context.integerValue("AddRamMemberResponse.Code"));
+		addRamMemberResponse.setMessage(context.stringValue("AddRamMemberResponse.Message"));
+
+		List<String> data = new ArrayList<String>();
+		for (int i = 0; i < context.lengthValue("AddRamMemberResponse.Data.Length"); i++) {
+			data.add(context.stringValue("AddRamMemberResponse.Data["+ i +"]"));
+		}
+		addRamMemberResponse.setData(data);
+	 
+	 	return addRamMemberResponse;
+	}
+}
