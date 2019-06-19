16d93fa17e591903b2f555a2b6e047bb7b502a2c
sdk-team <sdk-team@alibabacloud.com>
2019-06-19 17:00:19 +0800

    Generated 2018-01-16 for linkedmall.

---
+++ aliyun-java-sdk-linkedmall/src/main/java/com/aliyuncs/linkedmall/transform/v20180116/QueryOrderIdByPayIdResponseUnmarshaller.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkedmall.transform.v20180116;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.linkedmall.model.v20180116.QueryOrderIdByPayIdResponse;
+import com.aliyuncs.linkedmall.model.v20180116.QueryOrderIdByPayIdResponse.LmOrderIdsItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryOrderIdByPayIdResponseUnmarshaller {
+
+	public static QueryOrderIdByPayIdResponse unmarshall(QueryOrderIdByPayIdResponse queryOrderIdByPayIdResponse, UnmarshallerContext context) {
+		
+		queryOrderIdByPayIdResponse.setRequestId(context.stringValue("QueryOrderIdByPayIdResponse.RequestId"));
+		queryOrderIdByPayIdResponse.setCode(context.stringValue("QueryOrderIdByPayIdResponse.Code"));
+		queryOrderIdByPayIdResponse.setMessage(context.stringValue("QueryOrderIdByPayIdResponse.Message"));
+
+		List<LmOrderIdsItem> lmOrderIds = new ArrayList<LmOrderIdsItem>();
+		for (int i = 0; i < context.lengthValue("QueryOrderIdByPayIdResponse.LmOrderIds.Length"); i++) {
+			LmOrderIdsItem lmOrderIdsItem = new LmOrderIdsItem();
+			lmOrderIdsItem.setLmOrderId(context.longValue("QueryOrderIdByPayIdResponse.LmOrderIds["+ i +"].LmOrderId"));
+
+			lmOrderIds.add(lmOrderIdsItem);
+		}
+		queryOrderIdByPayIdResponse.setLmOrderIds(lmOrderIds);
+	 
+	 	return queryOrderIdByPayIdResponse;
+	}
+}
