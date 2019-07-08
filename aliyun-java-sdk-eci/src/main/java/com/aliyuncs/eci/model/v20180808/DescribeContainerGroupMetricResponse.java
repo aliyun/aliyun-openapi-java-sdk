39136f4e2914cdfb511ad25055c691ca14cfdfbe
lm140765 <lm140765@alibaba-inc.com>
2019-07-08 15:40:54 +0800

    eci sdk 1.0.8

---
+++ aliyun-java-sdk-eci/src/main/java/com/aliyuncs/eci/model/v20180808/DescribeContainerGroupMetricResponse.java
@@ -0,0 +1,411 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.eci.model.v20180808;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.eci.transform.v20180808.DescribeContainerGroupMetricResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeContainerGroupMetricResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String containerGroupId;
+
+	private List<Record> records;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getContainerGroupId() {
+		return this.containerGroupId;
+	}
+
+	public void setContainerGroupId(String containerGroupId) {
+		this.containerGroupId = containerGroupId;
+	}
+
+	public List<Record> getRecords() {
+		return this.records;
+	}
+
+	public void setRecords(List<Record> records) {
+		this.records = records;
+	}
+
+	public static class Record {
+
+		private String timestamp;
+
+		private List<Container> containers;
+
+		private CPU cPU;
+
+		private Memory memory;
+
+		private Network network;
+
+		public String getTimestamp() {
+			return this.timestamp;
+		}
+
+		public void setTimestamp(String timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		public List<Container> getContainers() {
+			return this.containers;
+		}
+
+		public void setContainers(List<Container> containers) {
+			this.containers = containers;
+		}
+
+		public CPU getCPU() {
+			return this.cPU;
+		}
+
+		public void setCPU(CPU cPU) {
+			this.cPU = cPU;
+		}
+
+		public Memory getMemory() {
+			return this.memory;
+		}
+
+		public void setMemory(Memory memory) {
+			this.memory = memory;
+		}
+
+		public Network getNetwork() {
+			return this.network;
+		}
+
+		public void setNetwork(Network network) {
+			this.network = network;
+		}
+
+		public static class Container {
+
+			private String name;
+
+			private CPU1 cPU1;
+
+			private Memory2 memory2;
+
+			public String getName() {
+				return this.name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public CPU1 getCPU1() {
+				return this.cPU1;
+			}
+
+			public void setCPU1(CPU1 cPU1) {
+				this.cPU1 = cPU1;
+			}
+
+			public Memory2 getMemory2() {
+				return this.memory2;
+			}
+
+			public void setMemory2(Memory2 memory2) {
+				this.memory2 = memory2;
+			}
+
+			public static class CPU1 {
+
+				private Long usageNanoCores;
+
+				private Long usageCoreNanoSeconds;
+
+				private Long load;
+
+				private Long limit;
+
+				public Long getUsageNanoCores() {
+					return this.usageNanoCores;
+				}
+
+				public void setUsageNanoCores(Long usageNanoCores) {
+					this.usageNanoCores = usageNanoCores;
+				}
+
+				public Long getUsageCoreNanoSeconds() {
+					return this.usageCoreNanoSeconds;
+				}
+
+				public void setUsageCoreNanoSeconds(Long usageCoreNanoSeconds) {
+					this.usageCoreNanoSeconds = usageCoreNanoSeconds;
+				}
+
+				public Long getLoad() {
+					return this.load;
+				}
+
+				public void setLoad(Long load) {
+					this.load = load;
+				}
+
+				public Long getLimit() {
+					return this.limit;
+				}
+
+				public void setLimit(Long limit) {
+					this.limit = limit;
+				}
+			}
+
+			public static class Memory2 {
+
+				private Long availableBytes;
+
+				private Long usageBytes;
+
+				private Long cache;
+
+				private Long workingSet;
+
+				private Long rss;
+
+				public Long getAvailableBytes() {
+					return this.availableBytes;
+				}
+
+				public void setAvailableBytes(Long availableBytes) {
+					this.availableBytes = availableBytes;
+				}
+
+				public Long getUsageBytes() {
+					return this.usageBytes;
+				}
+
+				public void setUsageBytes(Long usageBytes) {
+					this.usageBytes = usageBytes;
+				}
+
+				public Long getCache() {
+					return this.cache;
+				}
+
+				public void setCache(Long cache) {
+					this.cache = cache;
+				}
+
+				public Long getWorkingSet() {
+					return this.workingSet;
+				}
+
+				public void setWorkingSet(Long workingSet) {
+					this.workingSet = workingSet;
+				}
+
+				public Long getRss() {
+					return this.rss;
+				}
+
+				public void setRss(Long rss) {
+					this.rss = rss;
+				}
+			}
+		}
+
+		public static class CPU {
+
+			private Long usageNanoCores;
+
+			private Long usageCoreNanoSeconds;
+
+			private Long load;
+
+			private Long limit;
+
+			public Long getUsageNanoCores() {
+				return this.usageNanoCores;
+			}
+
+			public void setUsageNanoCores(Long usageNanoCores) {
+				this.usageNanoCores = usageNanoCores;
+			}
+
+			public Long getUsageCoreNanoSeconds() {
+				return this.usageCoreNanoSeconds;
+			}
+
+			public void setUsageCoreNanoSeconds(Long usageCoreNanoSeconds) {
+				this.usageCoreNanoSeconds = usageCoreNanoSeconds;
+			}
+
+			public Long getLoad() {
+				return this.load;
+			}
+
+			public void setLoad(Long load) {
+				this.load = load;
+			}
+
+			public Long getLimit() {
+				return this.limit;
+			}
+
+			public void setLimit(Long limit) {
+				this.limit = limit;
+			}
+		}
+
+		public static class Memory {
+
+			private Long availableBytes;
+
+			private Long usageBytes;
+
+			private Long cache;
+
+			private Long workingSet;
+
+			private Long rss;
+
+			public Long getAvailableBytes() {
+				return this.availableBytes;
+			}
+
+			public void setAvailableBytes(Long availableBytes) {
+				this.availableBytes = availableBytes;
+			}
+
+			public Long getUsageBytes() {
+				return this.usageBytes;
+			}
+
+			public void setUsageBytes(Long usageBytes) {
+				this.usageBytes = usageBytes;
+			}
+
+			public Long getCache() {
+				return this.cache;
+			}
+
+			public void setCache(Long cache) {
+				this.cache = cache;
+			}
+
+			public Long getWorkingSet() {
+				return this.workingSet;
+			}
+
+			public void setWorkingSet(Long workingSet) {
+				this.workingSet = workingSet;
+			}
+
+			public Long getRss() {
+				return this.rss;
+			}
+
+			public void setRss(Long rss) {
+				this.rss = rss;
+			}
+		}
+
+		public static class Network {
+
+			private List<_Interface> interfaces;
+
+			public List<_Interface> getInterfaces() {
+				return this.interfaces;
+			}
+
+			public void setInterfaces(List<_Interface> interfaces) {
+				this.interfaces = interfaces;
+			}
+
+			public static class _Interface {
+
+				private Long txBytes;
+
+				private Long rxBytes;
+
+				private Long txErrors;
+
+				private Long rxErrors;
+
+				private String name;
+
+				public Long getTxBytes() {
+					return this.txBytes;
+				}
+
+				public void setTxBytes(Long txBytes) {
+					this.txBytes = txBytes;
+				}
+
+				public Long getRxBytes() {
+					return this.rxBytes;
+				}
+
+				public void setRxBytes(Long rxBytes) {
+					this.rxBytes = rxBytes;
+				}
+
+				public Long getTxErrors() {
+					return this.txErrors;
+				}
+
+				public void setTxErrors(Long txErrors) {
+					this.txErrors = txErrors;
+				}
+
+				public Long getRxErrors() {
+					return this.rxErrors;
+				}
+
+				public void setRxErrors(Long rxErrors) {
+					this.rxErrors = rxErrors;
+				}
+
+				public String getName() {
+					return this.name;
+				}
+
+				public void setName(String name) {
+					this.name = name;
+				}
+			}
+		}
+	}
+
+	@Override
+	public DescribeContainerGroupMetricResponse getInstance(UnmarshallerContext context) {
+		return	DescribeContainerGroupMetricResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-eci/src/main/java/com/aliyuncs/eci/transform/v20180808/DescribeMultiContainerGroupMetricResponseUnmarshaller.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.eci.transform.v20180808;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.CPU;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Container;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Container.CPU1;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Container.Memory2;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Memory;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Network;
+import com.aliyuncs.eci.model.v20180808.DescribeMultiContainerGroupMetricResponse.Record.PodStat.Network._Interface;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeMultiContainerGroupMetricResponseUnmarshaller {
+
+	public static DescribeMultiContainerGroupMetricResponse unmarshall(DescribeMultiContainerGroupMetricResponse describeMultiContainerGroupMetricResponse, UnmarshallerContext context) {
+		
+		describeMultiContainerGroupMetricResponse.setRequestId(context.stringValue("DescribeMultiContainerGroupMetricResponse.RequestId"));
+
+		List<Record> monitorDatas = new ArrayList<Record>();
+		for (int i = 0; i < context.lengthValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas.Length"); i++) {
+			Record record = new Record();
+			record.setContainerGroupId(context.stringValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].ContainerGroupId"));
+
+			List<PodStat> records = new ArrayList<PodStat>();
+			for (int j = 0; j < context.lengthValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records.Length"); j++) {
+				PodStat podStat = new PodStat();
+				podStat.setTimestamp(context.stringValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Timestamp"));
+
+				CPU cPU = new CPU();
+				cPU.setUsageNanoCores(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].CPU.UsageNanoCores"));
+				cPU.setUsageCoreNanoSeconds(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].CPU.UsageCoreNanoSeconds"));
+				cPU.setLoad(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].CPU.Load"));
+				cPU.setLimit(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].CPU.Limit"));
+				podStat.setCPU(cPU);
+
+				Memory memory = new Memory();
+				memory.setAvailableBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Memory.AvailableBytes"));
+				memory.setUsageBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Memory.UsageBytes"));
+				memory.setCache(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Memory.Cache"));
+				memory.setWorkingSet(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Memory.WorkingSet"));
+				memory.setRss(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Memory.Rss"));
+				podStat.setMemory(memory);
+
+				Network network = new Network();
+
+				List<_Interface> interfaces = new ArrayList<_Interface>();
+				for (int k = 0; k < context.lengthValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces.Length"); k++) {
+					_Interface _interface = new _Interface();
+					_interface.setTxBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces["+ k +"].TxBytes"));
+					_interface.setRxBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces["+ k +"].RxBytes"));
+					_interface.setTxErrors(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces["+ k +"].TxErrors"));
+					_interface.setRxErrors(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces["+ k +"].RxErrors"));
+					_interface.setName(context.stringValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Network.Interfaces["+ k +"].Name"));
+
+					interfaces.add(_interface);
+				}
+				network.setInterfaces(interfaces);
+				podStat.setNetwork(network);
+
+				List<Container> containers = new ArrayList<Container>();
+				for (int k = 0; k < context.lengthValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers.Length"); k++) {
+					Container container = new Container();
+					container.setName(context.stringValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Name"));
+
+					CPU1 cPU1 = new CPU1();
+					cPU1.setUsageNanoCores(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].CPU.UsageNanoCores"));
+					cPU1.setUsageCoreNanoSeconds(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].CPU.UsageCoreNanoSeconds"));
+					cPU1.setLoad(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].CPU.Load"));
+					cPU1.setLimit(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].CPU.Limit"));
+					container.setCPU1(cPU1);
+
+					Memory2 memory2 = new Memory2();
+					memory2.setAvailableBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Memory.AvailableBytes"));
+					memory2.setUsageBytes(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Memory.UsageBytes"));
+					memory2.setCache(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Memory.Cache"));
+					memory2.setWorkingSet(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Memory.WorkingSet"));
+					memory2.setRss(context.longValue("DescribeMultiContainerGroupMetricResponse.MonitorDatas["+ i +"].Records["+ j +"].Containers["+ k +"].Memory.Rss"));
+					container.setMemory2(memory2);
+
+					containers.add(container);
+				}
+				podStat.setContainers(containers);
+
+				records.add(podStat);
+			}
+			record.setRecords(records);
+
+			monitorDatas.add(record);
+		}
+		describeMultiContainerGroupMetricResponse.setMonitorDatas(monitorDatas);
+	 
+	 	return describeMultiContainerGroupMetricResponse;
+	}
+}
