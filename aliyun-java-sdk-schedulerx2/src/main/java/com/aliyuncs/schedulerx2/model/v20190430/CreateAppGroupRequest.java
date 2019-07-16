9bd1306264ccbc50141a4039f83c45e89e9b14d9
sdk-team <sdk-team@alibabacloud.com>
2019-07-16 20:37:51 +0800

    Generated 2019-04-30 for schedulerx2.

---
+++ aliyun-java-sdk-schedulerx2/src/main/java/com/aliyuncs/schedulerx2/model/v20190430/CreateAppGroupRequest.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.schedulerx2.model.v20190430;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateAppGroupRequest extends RpcAcsRequest<CreateAppGroupResponse> {
+	
+	public CreateAppGroupRequest() {
+		super("schedulerx2", "2019-04-30", "CreateAppGroup");
+		setProtocol(ProtocolType.HTTPS);
+	}
+
+	private Integer maxJobs;
+
+	private String namespaceName;
+
+	private String namespaceSource;
+
+	private String appName;
+
+	private String metricsThresholdJson;
+
+	private String groupId;
+
+	private String namespace;
+
+	private String description;
+
+	private String alarmJson;
+
+	public Integer getMaxJobs() {
+		return this.maxJobs;
+	}
+
+	public void setMaxJobs(Integer maxJobs) {
+		this.maxJobs = maxJobs;
+		if(maxJobs != null){
+			putQueryParameter("MaxJobs", maxJobs.toString());
+		}
+	}
+
+	public String getNamespaceName() {
+		return this.namespaceName;
+	}
+
+	public void setNamespaceName(String namespaceName) {
+		this.namespaceName = namespaceName;
+		if(namespaceName != null){
+			putQueryParameter("NamespaceName", namespaceName);
+		}
+	}
+
+	public String getNamespaceSource() {
+		return this.namespaceSource;
+	}
+
+	public void setNamespaceSource(String namespaceSource) {
+		this.namespaceSource = namespaceSource;
+		if(namespaceSource != null){
+			putQueryParameter("NamespaceSource", namespaceSource);
+		}
+	}
+
+	public String getAppName() {
+		return this.appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+		if(appName != null){
+			putQueryParameter("AppName", appName);
+		}
+	}
+
+	public String getMetricsThresholdJson() {
+		return this.metricsThresholdJson;
+	}
+
+	public void setMetricsThresholdJson(String metricsThresholdJson) {
+		this.metricsThresholdJson = metricsThresholdJson;
+		if(metricsThresholdJson != null){
+			putQueryParameter("MetricsThresholdJson", metricsThresholdJson);
+		}
+	}
+
+	public String getGroupId() {
+		return this.groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+		if(groupId != null){
+			putQueryParameter("GroupId", groupId);
+		}
+	}
+
+	public String getNamespace() {
+		return this.namespace;
+	}
+
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+		if(namespace != null){
+			putQueryParameter("Namespace", namespace);
+		}
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+		if(description != null){
+			putQueryParameter("Description", description);
+		}
+	}
+
+	public String getAlarmJson() {
+		return this.alarmJson;
+	}
+
+	public void setAlarmJson(String alarmJson) {
+		this.alarmJson = alarmJson;
+		if(alarmJson != null){
+			putQueryParameter("AlarmJson", alarmJson);
+		}
+	}
+
+	@Override
+	public Class<CreateAppGroupResponse> getResponseClass() {
+		return CreateAppGroupResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-schedulerx2/src/main/java/com/aliyuncs/schedulerx2/transform/v20190430/CreateAppGroupResponseUnmarshaller.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.schedulerx2.transform.v20190430;
+
+import com.aliyuncs.schedulerx2.model.v20190430.CreateAppGroupResponse;
+import com.aliyuncs.schedulerx2.model.v20190430.CreateAppGroupResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class CreateAppGroupResponseUnmarshaller {
+
+	public static CreateAppGroupResponse unmarshall(CreateAppGroupResponse createAppGroupResponse, UnmarshallerContext _ctx) {
+		
+		createAppGroupResponse.setRequestId(_ctx.stringValue("CreateAppGroupResponse.RequestId"));
+		createAppGroupResponse.setCode(_ctx.integerValue("CreateAppGroupResponse.Code"));
+		createAppGroupResponse.setSuccess(_ctx.booleanValue("CreateAppGroupResponse.Success"));
+		createAppGroupResponse.setMessage(_ctx.stringValue("CreateAppGroupResponse.Message"));
+
+		Data data = new Data();
+		data.setAppGroupId(_ctx.longValue("CreateAppGroupResponse.Data.AppGroupId"));
+		createAppGroupResponse.setData(data);
+	 
+	 	return createAppGroupResponse;
+	}
+}
