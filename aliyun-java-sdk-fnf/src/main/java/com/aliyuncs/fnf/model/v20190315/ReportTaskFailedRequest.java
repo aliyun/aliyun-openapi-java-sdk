a66d613d26e9d6367b72fbe34d4b55090f3020f8
sdk-team <sdk-team@alibabacloud.com>
2019-08-15 14:42:09 +0800

    FnF public version.

---
+++ aliyun-java-sdk-fnf/src/main/java/com/aliyuncs/fnf/model/v20190315/ReportTaskFailedRequest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.fnf.model.v20190315;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ReportTaskFailedRequest extends RpcAcsRequest<ReportTaskFailedResponse> {
+	
+	public ReportTaskFailedRequest() {
+		super("fnf", "2019-03-15", "ReportTaskFailed", "fnf");
+		setMethod(MethodType.POST);
+	}
+
+	private String requestId;
+
+	private String cause;
+
+	private String taskToken;
+
+	private String error;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+		if(requestId != null){
+			putQueryParameter("RequestId", requestId);
+		}
+	}
+
+	public String getCause() {
+		return this.cause;
+	}
+
+	public void setCause(String cause) {
+		this.cause = cause;
+		if(cause != null){
+			putBodyParameter("Cause", cause);
+		}
+	}
+
+	public String getTaskToken() {
+		return this.taskToken;
+	}
+
+	public void setTaskToken(String taskToken) {
+		this.taskToken = taskToken;
+		if(taskToken != null){
+			putQueryParameter("TaskToken", taskToken);
+		}
+	}
+
+	public String getError() {
+		return this.error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+		if(error != null){
+			putBodyParameter("Error", error);
+		}
+	}
+
+	@Override
+	public Class<ReportTaskFailedResponse> getResponseClass() {
+		return ReportTaskFailedResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-fnf/src/main/java/com/aliyuncs/fnf/model/v20190315/ReportTaskSucceededRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.fnf.model.v20190315;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ReportTaskSucceededRequest extends RpcAcsRequest<ReportTaskSucceededResponse> {
+	
+	public ReportTaskSucceededRequest() {
+		super("fnf", "2019-03-15", "ReportTaskSucceeded", "fnf");
+		setMethod(MethodType.POST);
+	}
+
+	private String output;
+
+	private String requestId;
+
+	private String taskToken;
+
+	public String getOutput() {
+		return this.output;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+		if(output != null){
+			putBodyParameter("Output", output);
+		}
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+		if(requestId != null){
+			putQueryParameter("RequestId", requestId);
+		}
+	}
+
+	public String getTaskToken() {
+		return this.taskToken;
+	}
+
+	public void setTaskToken(String taskToken) {
+		this.taskToken = taskToken;
+		if(taskToken != null){
+			putQueryParameter("TaskToken", taskToken);
+		}
+	}
+
+	@Override
+	public Class<ReportTaskSucceededResponse> getResponseClass() {
+		return ReportTaskSucceededResponse.class;
+	}
+
+}
