04663c5faa38c787aa5e435cd746ff4901d91600
sdk-team <sdk-team@alibabacloud.com>
2019-08-27 13:34:26 +0800

    Fixed endpoint data init.

--- aliyun-java-sdk-appmallsservice/src/main/java/com/aliyuncs/appmallsservice/Endpoint.java
+++ aliyun-java-sdk-appmallsservice/src/main/java/com/aliyuncs/appmallsservice/Endpoint.java
@@ -19,26 +19,26 @@ import java.util.HashMap;
 public class Endpoint {
     public static HashMap<String, String> endpointMap = new HashMap<String, String>() {
         {
-            endpointMap.put("cn-shenzhen", "appms.aliyuncs.com");
-            endpointMap.put("cn-beijing", "appms.aliyuncs.com");
-            endpointMap.put("ap-south-1", "appms.aliyuncs.com");
-            endpointMap.put("eu-west-1", "appms.aliyuncs.com");
-            endpointMap.put("ap-northeast-1", "appms.aliyuncs.com");
-            endpointMap.put("me-east-1", "appms.aliyuncs.com");
-            endpointMap.put("cn-chengdu", "appms.aliyuncs.com");
-            endpointMap.put("cn-qingdao", "appms.aliyuncs.com");
-            endpointMap.put("cn-shanghai", "appms.aliyuncs.com");
-            endpointMap.put("cn-hongkong", "appms.aliyuncs.com");
-            endpointMap.put("ap-southeast-1", "appms.aliyuncs.com");
-            endpointMap.put("ap-southeast-2", "appms.aliyuncs.com");
-            endpointMap.put("ap-southeast-3", "appms.aliyuncs.com");
-            endpointMap.put("eu-central-1", "appms.aliyuncs.com");
-            endpointMap.put("cn-huhehaote", "appms.aliyuncs.com");
-            endpointMap.put("ap-southeast-5", "appms.aliyuncs.com");
-            endpointMap.put("us-east-1", "appms.aliyuncs.com");
-            endpointMap.put("cn-zhangjiakou", "appms.aliyuncs.com");
-            endpointMap.put("us-west-1", "appms.aliyuncs.com");
-            endpointMap.put("cn-hangzhou", "appms.aliyuncs.com");
+            put("cn-shenzhen", "appms.aliyuncs.com");
+            put("cn-beijing", "appms.aliyuncs.com");
+            put("ap-south-1", "appms.aliyuncs.com");
+            put("eu-west-1", "appms.aliyuncs.com");
+            put("ap-northeast-1", "appms.aliyuncs.com");
+            put("me-east-1", "appms.aliyuncs.com");
+            put("cn-chengdu", "appms.aliyuncs.com");
+            put("cn-qingdao", "appms.aliyuncs.com");
+            put("cn-shanghai", "appms.aliyuncs.com");
+            put("cn-hongkong", "appms.aliyuncs.com");
+            put("ap-southeast-1", "appms.aliyuncs.com");
+            put("ap-southeast-2", "appms.aliyuncs.com");
+            put("ap-southeast-3", "appms.aliyuncs.com");
+            put("eu-central-1", "appms.aliyuncs.com");
+            put("cn-huhehaote", "appms.aliyuncs.com");
+            put("ap-southeast-5", "appms.aliyuncs.com");
+            put("us-east-1", "appms.aliyuncs.com");
+            put("cn-zhangjiakou", "appms.aliyuncs.com");
+            put("us-west-1", "appms.aliyuncs.com");
+            put("cn-hangzhou", "appms.aliyuncs.com");
         }
     };
 
