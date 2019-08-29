ae8f4dd63d20a12411676c91b83853e4c2ebcfb9
sdk-team <sdk-team@alibabacloud.com>
2019-08-29 19:50:30 +0800

    Supported TranslateText.

---
+++ aliyun-java-sdk-hiknoengine/src/main/java/com/aliyuncs/hiknoengine/model/v20190625/TranslateTextRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.hiknoengine.model.v20190625;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class TranslateTextRequest extends RpcAcsRequest<TranslateTextResponse> {
+	
+	public TranslateTextRequest() {
+		super("hiknoengine", "2019-06-25", "TranslateText", "hiknoengine");
+		setMethod(MethodType.POST);
+	}
+
+	private String fromLang;
+
+	private String toLang;
+
+	private String text;
+
+	public String getFromLang() {
+		return this.fromLang;
+	}
+
+	public void setFromLang(String fromLang) {
+		this.fromLang = fromLang;
+		if(fromLang != null){
+			putBodyParameter("FromLang", fromLang);
+		}
+	}
+
+	public String getToLang() {
+		return this.toLang;
+	}
+
+	public void setToLang(String toLang) {
+		this.toLang = toLang;
+		if(toLang != null){
+			putBodyParameter("ToLang", toLang);
+		}
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+		if(text != null){
+			putBodyParameter("Text", text);
+		}
+	}
+
+	@Override
+	public Class<TranslateTextResponse> getResponseClass() {
+		return TranslateTextResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-hiknoengine/src/main/java/com/aliyuncs/hiknoengine/transform/v20190625/TranslateTextResponseUnmarshaller.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.hiknoengine.transform.v20190625;
+
+import com.aliyuncs.hiknoengine.model.v20190625.TranslateTextResponse;
+import com.aliyuncs.hiknoengine.model.v20190625.TranslateTextResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class TranslateTextResponseUnmarshaller {
+
+	public static TranslateTextResponse unmarshall(TranslateTextResponse translateTextResponse, UnmarshallerContext _ctx) {
+		
+		translateTextResponse.setRequestId(_ctx.stringValue("TranslateTextResponse.RequestId"));
+		translateTextResponse.setCode(_ctx.stringValue("TranslateTextResponse.Code"));
+		translateTextResponse.setMessage(_ctx.stringValue("TranslateTextResponse.Message"));
+
+		Data data = new Data();
+		data.setText(_ctx.stringValue("TranslateTextResponse.Data.Text"));
+		translateTextResponse.setData(data);
+	 
+	 	return translateTextResponse;
+	}
+}
