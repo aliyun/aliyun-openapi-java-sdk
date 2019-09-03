76b91468ea417b5ee1a16a397e77bbd83eec591a
sdk-team <sdk-team@alibabacloud.com>
2019-09-03 15:19:35 +0800

    Support get DRDS user info and instance info.

---
+++ aliyun-java-sdk-drds/src/main/java/com/aliyuncs/drds/model/v20190123/InnerDescribeDrdsInstancesRequest.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.drds.model.v20190123;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class InnerDescribeDrdsInstancesRequest extends RpcAcsRequest<InnerDescribeDrdsInstancesResponse> {
+	
+	public InnerDescribeDrdsInstancesRequest() {
+		super("Drds", "2019-01-23", "InnerDescribeDrdsInstances", "drds");
+	}
+
+	private Boolean expired;
+
+	private Integer pageSize;
+
+	private String description;
+
+	private String aliuid;
+
+	private List<Tag> tags;
+
+	private String type;
+
+	private String bid;
+
+	private Integer pageNumber;
+
+	public Boolean getExpired() {
+		return this.expired;
+	}
+
+	public void setExpired(Boolean expired) {
+		this.expired = expired;
+		if(expired != null){
+			putQueryParameter("Expired", expired.toString());
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+		if(description != null){
+			putQueryParameter("Description", description);
+		}
+	}
+
+	public String getAliuid() {
+		return this.aliuid;
+	}
+
+	public void setAliuid(String aliuid) {
+		this.aliuid = aliuid;
+		if(aliuid != null){
+			putQueryParameter("Aliuid", aliuid);
+		}
+	}
+
+	public List<Tag> getTags() {
+		return this.tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;	
+		if (tags != null) {
+			for (int depth1 = 0; depth1 < tags.size(); depth1++) {
+				putQueryParameter("Tag." + (depth1 + 1) + ".Value" , tags.get(depth1).getValue());
+				putQueryParameter("Tag." + (depth1 + 1) + ".Key" , tags.get(depth1).getKey());
+			}
+		}	
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+		if(type != null){
+			putQueryParameter("Type", type);
+		}
+	}
+
+	public String getBid() {
+		return this.bid;
+	}
+
+	public void setBid(String bid) {
+		this.bid = bid;
+		if(bid != null){
+			putQueryParameter("Bid", bid);
+		}
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+		if(pageNumber != null){
+			putQueryParameter("PageNumber", pageNumber.toString());
+		}
+	}
+
+	public static class Tag {
+
+		private String value;
+
+		private String key;
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public String getKey() {
+			return this.key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+	}
+
+	@Override
+	public Class<InnerDescribeDrdsInstancesResponse> getResponseClass() {
+		return InnerDescribeDrdsInstancesResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-drds/src/main/java/com/aliyuncs/drds/transform/v20190123/InnerGetUserListResponseUnmarshaller.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.drds.transform.v20190123;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.drds.model.v20190123.InnerGetUserListResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class InnerGetUserListResponseUnmarshaller {
+
+	public static InnerGetUserListResponse unmarshall(InnerGetUserListResponse innerGetUserListResponse, UnmarshallerContext _ctx) {
+		
+		innerGetUserListResponse.setRequestId(_ctx.stringValue("InnerGetUserListResponse.RequestId"));
+		innerGetUserListResponse.setSuccess(_ctx.booleanValue("InnerGetUserListResponse.Success"));
+
+		List<String> aliuids = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("InnerGetUserListResponse.Aliuids.Length"); i++) {
+			aliuids.add(_ctx.stringValue("InnerGetUserListResponse.Aliuids["+ i +"]"));
+		}
+		innerGetUserListResponse.setAliuids(aliuids);
+	 
+	 	return innerGetUserListResponse;
+	}
+}
