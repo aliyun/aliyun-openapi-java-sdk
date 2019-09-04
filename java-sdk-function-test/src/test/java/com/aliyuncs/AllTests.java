8d20ff6e3ec4b9e05c606f0aa59e9bd608e398eb
sonny <wb-szq493230@alibaba-inc.com>
2019-09-04 15:56:21 +0800

    fix ft test

--- java-sdk-function-test/src/test/java/com/aliyuncs/AllTests.java
+++ java-sdk-function-test/src/test/java/com/aliyuncs/AllTests.java
@@ -6,11 +6,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    ErrorHandlingTest.class, 
-    APIEncapsulateTest.class,
-    CoreTest.class, 
-    NewEndpointTest.class, 
-    EndpointResolverTest.class 
+        ErrorHandlingTest.class,
+        APIEncapsulateTest.class,
+        CoreTest.class,
+        NewEndpointTest.class,
+        EndpointResolverTest.class,
+        TimeoutTest.class,
+        ProxyTest.class,
+        CredentialsTest.class
 })
 public class AllTests {
 }
