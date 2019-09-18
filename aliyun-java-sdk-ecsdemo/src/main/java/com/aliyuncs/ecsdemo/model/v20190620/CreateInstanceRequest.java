e0175df73d63ea1668bb6ad141f4a8951de637a3
sdk-team <sdk-team@alibabacloud.com>
2019-09-18 09:59:16 +0800

    test

---
+++ aliyun-java-sdk-ecsdemo/src/main/java/com/aliyuncs/ecsdemo/model/v20190620/CreateInstanceRequest.java
@@ -0,0 +1,233 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecsdemo.model.v20190620;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateInstanceRequest extends RpcAcsRequest<CreateInstanceResponse> {
+	
+	public CreateInstanceRequest() {
+		super("EcsDemo", "2019-06-20", "CreateInstance", "EcsDemo", "innerAPI");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.POST);
+	}
+
+	private String deploymentSetId;
+
+	private Integer autoRenewPeriod;
+
+	private String imageId;
+
+	private String clientToken;
+
+	private String securityGroupId;
+
+	private Integer internetMaxBandwidthOut;
+
+	private String hostName;
+
+	private String password;
+
+	private String instanceName;
+
+	private String passwordInherit;
+
+	private Boolean autoRenew;
+
+	private String internetChargeType;
+
+	private String instanceType;
+
+	private String zoneId;
+
+	private Integer internetMaxBandwidthIn;
+
+	public String getDeploymentSetId() {
+		return this.deploymentSetId;
+	}
+
+	public void setDeploymentSetId(String deploymentSetId) {
+		this.deploymentSetId = deploymentSetId;
+		if(deploymentSetId != null){
+			putBodyParameter("DeploymentSetId", deploymentSetId);
+		}
+	}
+
+	public Integer getAutoRenewPeriod() {
+		return this.autoRenewPeriod;
+	}
+
+	public void setAutoRenewPeriod(Integer autoRenewPeriod) {
+		this.autoRenewPeriod = autoRenewPeriod;
+		if(autoRenewPeriod != null){
+			putBodyParameter("AutoRenewPeriod", autoRenewPeriod.toString());
+		}
+	}
+
+	public String getImageId() {
+		return this.imageId;
+	}
+
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+		if(imageId != null){
+			putBodyParameter("ImageId", imageId);
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putBodyParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getSecurityGroupId() {
+		return this.securityGroupId;
+	}
+
+	public void setSecurityGroupId(String securityGroupId) {
+		this.securityGroupId = securityGroupId;
+		if(securityGroupId != null){
+			putBodyParameter("SecurityGroupId", securityGroupId);
+		}
+	}
+
+	public Integer getInternetMaxBandwidthOut() {
+		return this.internetMaxBandwidthOut;
+	}
+
+	public void setInternetMaxBandwidthOut(Integer internetMaxBandwidthOut) {
+		this.internetMaxBandwidthOut = internetMaxBandwidthOut;
+		if(internetMaxBandwidthOut != null){
+			putBodyParameter("InternetMaxBandwidthOut", internetMaxBandwidthOut.toString());
+		}
+	}
+
+	public String getHostName() {
+		return this.hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+		if(hostName != null){
+			putBodyParameter("HostName", hostName);
+		}
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+		if(password != null){
+			putBodyParameter("Password", password);
+		}
+	}
+
+	public String getInstanceName() {
+		return this.instanceName;
+	}
+
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+		if(instanceName != null){
+			putBodyParameter("InstanceName", instanceName);
+		}
+	}
+
+	public String getPasswordInherit() {
+		return this.passwordInherit;
+	}
+
+	public void setPasswordInherit(String passwordInherit) {
+		this.passwordInherit = passwordInherit;
+		if(passwordInherit != null){
+			putBodyParameter("PasswordInherit", passwordInherit);
+		}
+	}
+
+	public Boolean getAutoRenew() {
+		return this.autoRenew;
+	}
+
+	public void setAutoRenew(Boolean autoRenew) {
+		this.autoRenew = autoRenew;
+		if(autoRenew != null){
+			putBodyParameter("AutoRenew", autoRenew.toString());
+		}
+	}
+
+	public String getInternetChargeType() {
+		return this.internetChargeType;
+	}
+
+	public void setInternetChargeType(String internetChargeType) {
+		this.internetChargeType = internetChargeType;
+		if(internetChargeType != null){
+			putBodyParameter("InternetChargeType", internetChargeType);
+		}
+	}
+
+	public String getInstanceType() {
+		return this.instanceType;
+	}
+
+	public void setInstanceType(String instanceType) {
+		this.instanceType = instanceType;
+		if(instanceType != null){
+			putBodyParameter("InstanceType", instanceType);
+		}
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+		if(zoneId != null){
+			putBodyParameter("ZoneId", zoneId);
+		}
+	}
+
+	public Integer getInternetMaxBandwidthIn() {
+		return this.internetMaxBandwidthIn;
+	}
+
+	public void setInternetMaxBandwidthIn(Integer internetMaxBandwidthIn) {
+		this.internetMaxBandwidthIn = internetMaxBandwidthIn;
+		if(internetMaxBandwidthIn != null){
+			putBodyParameter("InternetMaxBandwidthIn", internetMaxBandwidthIn.toString());
+		}
+	}
+
+	@Override
+	public Class<CreateInstanceResponse> getResponseClass() {
+		return CreateInstanceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-ecsdemo/src/main/java/com/aliyuncs/ecsdemo/model/v20190620/DeleteInstanceRequest.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecsdemo.model.v20190620;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DeleteInstanceRequest extends RpcAcsRequest<DeleteInstanceResponse> {
+	
+	public DeleteInstanceRequest() {
+		super("EcsDemo", "2019-06-20", "DeleteInstance", "EcsDemo", "innerAPI");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.DELETE);
+	}
+
+	private String instanceId;
+
+	private Boolean terminateSubscription;
+
+	private String ownerAccount;
+
+	private String force;
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putBodyParameter("InstanceId", instanceId);
+		}
+	}
+
+	public Boolean getTerminateSubscription() {
+		return this.terminateSubscription;
+	}
+
+	public void setTerminateSubscription(Boolean terminateSubscription) {
+		this.terminateSubscription = terminateSubscription;
+		if(terminateSubscription != null){
+			putBodyParameter("TerminateSubscription", terminateSubscription.toString());
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putBodyParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public String getForce() {
+		return this.force;
+	}
+
+	public void setForce(String force) {
+		this.force = force;
+		if(force != null){
+			putBodyParameter("Force", force);
+		}
+	}
+
+	@Override
+	public Class<DeleteInstanceResponse> getResponseClass() {
+		return DeleteInstanceResponse.class;
+	}
+
+}
