c079a5a6f357b859b1d47ceea1575ae211f54d8d
sdk-team <sdk-team@alibabacloud.com>
2019-10-12 14:40:37 +0800

    Supported Managed Conditions Query and Verification Interface.

---
+++ aliyun-java-sdk-efc-service/src/main/java/com/aliyuncs/efc_service/model/v20190318/VerifyRdOperationResponse.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.efc_service.model.v20190318;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.efc_service.transform.v20190318.VerifyRdOperationResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class VerifyRdOperationResponse extends AcsResponse {
+
+	private String requestId;
+
+	private RdOprationCheckResult rdOprationCheckResult;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public RdOprationCheckResult getRdOprationCheckResult() {
+		return this.rdOprationCheckResult;
+	}
+
+	public void setRdOprationCheckResult(RdOprationCheckResult rdOprationCheckResult) {
+		this.rdOprationCheckResult = rdOprationCheckResult;
+	}
+
+	public static class RdOprationCheckResult {
+
+		private Boolean success;
+
+		private String bizType;
+
+		private String failCode;
+
+		private String failMessage;
+
+		private String extraData;
+
+		public Boolean getSuccess() {
+			return this.success;
+		}
+
+		public void setSuccess(Boolean success) {
+			this.success = success;
+		}
+
+		public String getBizType() {
+			return this.bizType;
+		}
+
+		public void setBizType(String bizType) {
+			this.bizType = bizType;
+		}
+
+		public String getFailCode() {
+			return this.failCode;
+		}
+
+		public void setFailCode(String failCode) {
+			this.failCode = failCode;
+		}
+
+		public String getFailMessage() {
+			return this.failMessage;
+		}
+
+		public void setFailMessage(String failMessage) {
+			this.failMessage = failMessage;
+		}
+
+		public String getExtraData() {
+			return this.extraData;
+		}
+
+		public void setExtraData(String extraData) {
+			this.extraData = extraData;
+		}
+	}
+
+	@Override
+	public VerifyRdOperationResponse getInstance(UnmarshallerContext context) {
+		return	VerifyRdOperationResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-efc-service/src/main/java/com/aliyuncs/efc_service/transform/v20190318/VerifyRdOperationResponseUnmarshaller.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.efc_service.transform.v20190318;
+
+import com.aliyuncs.efc_service.model.v20190318.VerifyRdOperationResponse;
+import com.aliyuncs.efc_service.model.v20190318.VerifyRdOperationResponse.RdOprationCheckResult;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class VerifyRdOperationResponseUnmarshaller {
+
+	public static VerifyRdOperationResponse unmarshall(VerifyRdOperationResponse verifyRdOperationResponse, UnmarshallerContext _ctx) {
+		
+		verifyRdOperationResponse.setRequestId(_ctx.stringValue("VerifyRdOperationResponse.RequestId"));
+
+		RdOprationCheckResult rdOprationCheckResult = new RdOprationCheckResult();
+		rdOprationCheckResult.setSuccess(_ctx.booleanValue("VerifyRdOperationResponse.RdOprationCheckResult.Success"));
+		rdOprationCheckResult.setBizType(_ctx.stringValue("VerifyRdOperationResponse.RdOprationCheckResult.BizType"));
+		rdOprationCheckResult.setFailCode(_ctx.stringValue("VerifyRdOperationResponse.RdOprationCheckResult.FailCode"));
+		rdOprationCheckResult.setFailMessage(_ctx.stringValue("VerifyRdOperationResponse.RdOprationCheckResult.FailMessage"));
+		rdOprationCheckResult.setExtraData(_ctx.stringValue("VerifyRdOperationResponse.RdOprationCheckResult.ExtraData"));
+		verifyRdOperationResponse.setRdOprationCheckResult(rdOprationCheckResult);
+	 
+	 	return verifyRdOperationResponse;
+	}
+}
