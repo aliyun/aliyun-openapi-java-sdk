6175adb3a69560ef90e05b75611e3f817cdee8e5
sdk-team <sdk-team@alibabacloud.com>
2019-10-21 13:45:52 +0800

    Supported For ListRunnableScens.

---
+++ aliyun-java-sdk-pts/src/main/java/com/aliyuncs/pts/model/v20181111/ListRunnableScenesResponse.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.pts.model.v20181111;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.pts.transform.v20181111.ListRunnableScenesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListRunnableScenesResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String code;
+
+	private String message;
+
+	private Boolean success;
+
+	private Integer pageNumber;
+
+	private Integer pageSize;
+
+	private Long totalCount;
+
+	private List<Scene> scenes;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public Long getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(Long totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public List<Scene> getScenes() {
+		return this.scenes;
+	}
+
+	public void setScenes(List<Scene> scenes) {
+		this.scenes = scenes;
+	}
+
+	public static class Scene {
+
+		private Long sceneId;
+
+		private String sceneName;
+
+		private Integer duration;
+
+		private Long modifiedTime;
+
+		public Long getSceneId() {
+			return this.sceneId;
+		}
+
+		public void setSceneId(Long sceneId) {
+			this.sceneId = sceneId;
+		}
+
+		public String getSceneName() {
+			return this.sceneName;
+		}
+
+		public void setSceneName(String sceneName) {
+			this.sceneName = sceneName;
+		}
+
+		public Integer getDuration() {
+			return this.duration;
+		}
+
+		public void setDuration(Integer duration) {
+			this.duration = duration;
+		}
+
+		public Long getModifiedTime() {
+			return this.modifiedTime;
+		}
+
+		public void setModifiedTime(Long modifiedTime) {
+			this.modifiedTime = modifiedTime;
+		}
+	}
+
+	@Override
+	public ListRunnableScenesResponse getInstance(UnmarshallerContext context) {
+		return	ListRunnableScenesResponseUnmarshaller.unmarshall(this, context);
+	}
+}
