e591d81d1993f84b3daf32f3a8887e775c4d850b
sdk-team <sdk-team@alibabacloud.com>
2019-11-05 19:16:31 +0800

    Add OperateBlackNo.

---
+++ aliyun-java-sdk-dyplsapi/src/main/java/com/aliyuncs/dyplsapi/model/v20170525/OperateBlackNoRequest.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dyplsapi.model.v20170525;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.dyplsapi.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class OperateBlackNoRequest extends RpcAcsRequest<OperateBlackNoResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String tips;
+
+	private String resourceOwnerAccount;
+
+	private Long ownerId;
+
+	private String poolKey;
+
+	private String blackNo;
+
+	private String operateType;
+	public OperateBlackNoRequest() {
+		super("Dyplsapi", "2017-05-25", "OperateBlackNo", "dypls");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getTips() {
+		return this.tips;
+	}
+
+	public void setTips(String tips) {
+		this.tips = tips;
+		if(tips != null){
+			putQueryParameter("Tips", tips);
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getPoolKey() {
+		return this.poolKey;
+	}
+
+	public void setPoolKey(String poolKey) {
+		this.poolKey = poolKey;
+		if(poolKey != null){
+			putQueryParameter("PoolKey", poolKey);
+		}
+	}
+
+	public String getBlackNo() {
+		return this.blackNo;
+	}
+
+	public void setBlackNo(String blackNo) {
+		this.blackNo = blackNo;
+		if(blackNo != null){
+			putQueryParameter("BlackNo", blackNo);
+		}
+	}
+
+	public String getOperateType() {
+		return this.operateType;
+	}
+
+	public void setOperateType(String operateType) {
+		this.operateType = operateType;
+		if(operateType != null){
+			putQueryParameter("OperateType", operateType);
+		}
+	}
+
+	@Override
+	public Class<OperateBlackNoResponse> getResponseClass() {
+		return OperateBlackNoResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-dyplsapi/src/main/java/com/aliyuncs/dyplsapi/transform/v20170525/OperateBlackNoResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dyplsapi.transform.v20170525;
+
+import com.aliyuncs.dyplsapi.model.v20170525.OperateBlackNoResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class OperateBlackNoResponseUnmarshaller {
+
+	public static OperateBlackNoResponse unmarshall(OperateBlackNoResponse operateBlackNoResponse, UnmarshallerContext _ctx) {
+		
+		operateBlackNoResponse.setRequestId(_ctx.stringValue("OperateBlackNoResponse.RequestId"));
+		operateBlackNoResponse.setCode(_ctx.stringValue("OperateBlackNoResponse.Code"));
+		operateBlackNoResponse.setMessage(_ctx.stringValue("OperateBlackNoResponse.Message"));
+	 
+	 	return operateBlackNoResponse;
+	}
+}
