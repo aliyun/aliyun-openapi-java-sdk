845c128d2795f40510805e08267ed691f77a8286
sdk-team <sdk-team@alibabacloud.com>
2019-11-13 20:41:38 +0800

    Supported Stream analyse.

---
+++ aliyun-java-sdk-imm/src/main/java/com/aliyuncs/imm/model/v20170906/CreateStreamAnalyseTaskRequest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imm.model.v20170906;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateStreamAnalyseTaskRequest extends RpcAcsRequest<CreateStreamAnalyseTaskResponse> {
+	   
+
+	private String project;
+
+	private String notifyEndpoint;
+
+	private String streamUri;
+
+	private String notifyTopicName;
+
+	private String tgtUri;
+	public CreateStreamAnalyseTaskRequest() {
+		super("imm", "2017-09-06", "CreateStreamAnalyseTask", "imm");
+		setMethod(MethodType.POST);
+	}
+
+	public String getProject() {
+		return this.project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+		if(project != null){
+			putQueryParameter("Project", project);
+		}
+	}
+
+	public String getNotifyEndpoint() {
+		return this.notifyEndpoint;
+	}
+
+	public void setNotifyEndpoint(String notifyEndpoint) {
+		this.notifyEndpoint = notifyEndpoint;
+		if(notifyEndpoint != null){
+			putQueryParameter("NotifyEndpoint", notifyEndpoint);
+		}
+	}
+
+	public String getStreamUri() {
+		return this.streamUri;
+	}
+
+	public void setStreamUri(String streamUri) {
+		this.streamUri = streamUri;
+		if(streamUri != null){
+			putQueryParameter("StreamUri", streamUri);
+		}
+	}
+
+	public String getNotifyTopicName() {
+		return this.notifyTopicName;
+	}
+
+	public void setNotifyTopicName(String notifyTopicName) {
+		this.notifyTopicName = notifyTopicName;
+		if(notifyTopicName != null){
+			putQueryParameter("NotifyTopicName", notifyTopicName);
+		}
+	}
+
+	public String getTgtUri() {
+		return this.tgtUri;
+	}
+
+	public void setTgtUri(String tgtUri) {
+		this.tgtUri = tgtUri;
+		if(tgtUri != null){
+			putQueryParameter("TgtUri", tgtUri);
+		}
+	}
+
+	@Override
+	public Class<CreateStreamAnalyseTaskResponse> getResponseClass() {
+		return CreateStreamAnalyseTaskResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-imm/src/main/java/com/aliyuncs/imm/transform/v20170906/CreateStreamAnalyseTaskResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imm.transform.v20170906;
+
+import com.aliyuncs.imm.model.v20170906.CreateStreamAnalyseTaskResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class CreateStreamAnalyseTaskResponseUnmarshaller {
+
+	public static CreateStreamAnalyseTaskResponse unmarshall(CreateStreamAnalyseTaskResponse createStreamAnalyseTaskResponse, UnmarshallerContext _ctx) {
+		
+		createStreamAnalyseTaskResponse.setRequestId(_ctx.stringValue("CreateStreamAnalyseTaskResponse.RequestId"));
+		createStreamAnalyseTaskResponse.setTaskId(_ctx.stringValue("CreateStreamAnalyseTaskResponse.TaskId"));
+		createStreamAnalyseTaskResponse.setTaskType(_ctx.stringValue("CreateStreamAnalyseTaskResponse.TaskType"));
+	 
+	 	return createStreamAnalyseTaskResponse;
+	}
+}
