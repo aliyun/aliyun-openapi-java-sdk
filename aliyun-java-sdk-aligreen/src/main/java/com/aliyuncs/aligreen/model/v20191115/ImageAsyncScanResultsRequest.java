a8e5a2662f6fbb69b75ed43ef2a474dcc020d0be
sdk-team <sdk-team@alibabacloud.com>
2019-12-03 17:27:02 +0800

    Supported image scan.

---
+++ aliyun-java-sdk-aligreen/src/main/java/com/aliyuncs/aligreen/model/v20191115/ImageAsyncScanResultsRequest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.aligreen.model.v20191115;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.aligreen.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ImageAsyncScanResultsRequest extends RoaAcsRequest<ImageAsyncScanResultsResponse> {
+	   
+
+	private String clientInfo;
+	public ImageAsyncScanResultsRequest() {
+		super("aligreen", "2019-11-15", "ImageAsyncScanResults");
+		setUriPattern("/green/image/results");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getClientInfo() {
+		return this.clientInfo;
+	}
+
+	public void setClientInfo(String clientInfo) {
+		this.clientInfo = clientInfo;
+		if(clientInfo != null){
+			putQueryParameter("ClientInfo", clientInfo);
+		}
+	}
+
+	@Override
+	public Class<ImageAsyncScanResultsResponse> getResponseClass() {
+		return ImageAsyncScanResultsResponse.class;
+	}
+
+}
