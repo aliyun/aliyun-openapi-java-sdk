c33460e3c5f1210ce0a2a5112b6af889c7246924
sdk-team <sdk-team@alibabacloud.com>
2019-12-18 11:20:22 +0800

    First sdk version.

---
+++ aliyun-java-sdk-facebody/src/main/java/com/aliyuncs/facebody/model/v20191230/DetectFaceResponse.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.facebody.model.v20191230;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.facebody.transform.v20191230.DetectFaceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DetectFaceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Integer faceCount;
+
+		private Integer landmarkCount;
+
+		private List<Integer> faceRectangles;
+
+		private List<Float> faceProbabilityList;
+
+		private List<Float> poseList;
+
+		private List<Float> landmarks;
+
+		private List<Float> pupils;
+
+		public Integer getFaceCount() {
+			return this.faceCount;
+		}
+
+		public void setFaceCount(Integer faceCount) {
+			this.faceCount = faceCount;
+		}
+
+		public Integer getLandmarkCount() {
+			return this.landmarkCount;
+		}
+
+		public void setLandmarkCount(Integer landmarkCount) {
+			this.landmarkCount = landmarkCount;
+		}
+
+		public List<Integer> getFaceRectangles() {
+			return this.faceRectangles;
+		}
+
+		public void setFaceRectangles(List<Integer> faceRectangles) {
+			this.faceRectangles = faceRectangles;
+		}
+
+		public List<Float> getFaceProbabilityList() {
+			return this.faceProbabilityList;
+		}
+
+		public void setFaceProbabilityList(List<Float> faceProbabilityList) {
+			this.faceProbabilityList = faceProbabilityList;
+		}
+
+		public List<Float> getPoseList() {
+			return this.poseList;
+		}
+
+		public void setPoseList(List<Float> poseList) {
+			this.poseList = poseList;
+		}
+
+		public List<Float> getLandmarks() {
+			return this.landmarks;
+		}
+
+		public void setLandmarks(List<Float> landmarks) {
+			this.landmarks = landmarks;
+		}
+
+		public List<Float> getPupils() {
+			return this.pupils;
+		}
+
+		public void setPupils(List<Float> pupils) {
+			this.pupils = pupils;
+		}
+	}
+
+	@Override
+	public DetectFaceResponse getInstance(UnmarshallerContext context) {
+		return	DetectFaceResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-facebody/src/main/java/com/aliyuncs/facebody/transform/v20191230/DetectFaceResponseUnmarshaller.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.facebody.transform.v20191230;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.facebody.model.v20191230.DetectFaceResponse;
+import com.aliyuncs.facebody.model.v20191230.DetectFaceResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DetectFaceResponseUnmarshaller {
+
+	public static DetectFaceResponse unmarshall(DetectFaceResponse detectFaceResponse, UnmarshallerContext _ctx) {
+		
+		detectFaceResponse.setRequestId(_ctx.stringValue("DetectFaceResponse.RequestId"));
+
+		Data data = new Data();
+		data.setFaceCount(_ctx.integerValue("DetectFaceResponse.Data.FaceCount"));
+		data.setLandmarkCount(_ctx.integerValue("DetectFaceResponse.Data.LandmarkCount"));
+
+		List<Integer> faceRectangles = new ArrayList<Integer>();
+		for (int i = 0; i < _ctx.lengthValue("DetectFaceResponse.Data.FaceRectangles.Length"); i++) {
+			faceRectangles.add(_ctx.integerValue("DetectFaceResponse.Data.FaceRectangles["+ i +"]"));
+		}
+		data.setFaceRectangles(faceRectangles);
+
+		List<Float> faceProbabilityList = new ArrayList<Float>();
+		for (int i = 0; i < _ctx.lengthValue("DetectFaceResponse.Data.FaceProbabilityList.Length"); i++) {
+			faceProbabilityList.add(_ctx.floatValue("DetectFaceResponse.Data.FaceProbabilityList["+ i +"]"));
+		}
+		data.setFaceProbabilityList(faceProbabilityList);
+
+		List<Float> poseList = new ArrayList<Float>();
+		for (int i = 0; i < _ctx.lengthValue("DetectFaceResponse.Data.PoseList.Length"); i++) {
+			poseList.add(_ctx.floatValue("DetectFaceResponse.Data.PoseList["+ i +"]"));
+		}
+		data.setPoseList(poseList);
+
+		List<Float> landmarks = new ArrayList<Float>();
+		for (int i = 0; i < _ctx.lengthValue("DetectFaceResponse.Data.Landmarks.Length"); i++) {
+			landmarks.add(_ctx.floatValue("DetectFaceResponse.Data.Landmarks["+ i +"]"));
+		}
+		data.setLandmarks(landmarks);
+
+		List<Float> pupils = new ArrayList<Float>();
+		for (int i = 0; i < _ctx.lengthValue("DetectFaceResponse.Data.Pupils.Length"); i++) {
+			pupils.add(_ctx.floatValue("DetectFaceResponse.Data.Pupils["+ i +"]"));
+		}
+		data.setPupils(pupils);
+		detectFaceResponse.setData(data);
+	 
+	 	return detectFaceResponse;
+	}
+}
