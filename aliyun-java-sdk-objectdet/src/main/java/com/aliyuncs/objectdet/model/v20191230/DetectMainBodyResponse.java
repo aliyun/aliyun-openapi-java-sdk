1b84d9abdbedffcff92d05d68d01832a3f2f9391
sdk-team <sdk-team@alibabacloud.com>
2019-12-18 11:18:11 +0800

    First sdk version.

---
+++ aliyun-java-sdk-objectdet/src/main/java/com/aliyuncs/objectdet/model/v20191230/DetectMainBodyResponse.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.objectdet.model.v20191230;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.objectdet.transform.v20191230.DetectMainBodyResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DetectMainBodyResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Location location;
+
+		public Location getLocation() {
+			return this.location;
+		}
+
+		public void setLocation(Location location) {
+			this.location = location;
+		}
+
+		public static class Location {
+
+			private Integer x;
+
+			private Integer y;
+
+			private Integer width;
+
+			private Integer height;
+
+			public Integer getX() {
+				return this.x;
+			}
+
+			public void setX(Integer x) {
+				this.x = x;
+			}
+
+			public Integer getY() {
+				return this.y;
+			}
+
+			public void setY(Integer y) {
+				this.y = y;
+			}
+
+			public Integer getWidth() {
+				return this.width;
+			}
+
+			public void setWidth(Integer width) {
+				this.width = width;
+			}
+
+			public Integer getHeight() {
+				return this.height;
+			}
+
+			public void setHeight(Integer height) {
+				this.height = height;
+			}
+		}
+	}
+
+	@Override
+	public DetectMainBodyResponse getInstance(UnmarshallerContext context) {
+		return	DetectMainBodyResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-objectdet/src/main/java/com/aliyuncs/objectdet/transform/v20191230/DetectMainBodyResponseUnmarshaller.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.objectdet.transform.v20191230;
+
+import com.aliyuncs.objectdet.model.v20191230.DetectMainBodyResponse;
+import com.aliyuncs.objectdet.model.v20191230.DetectMainBodyResponse.Data;
+import com.aliyuncs.objectdet.model.v20191230.DetectMainBodyResponse.Data.Location;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DetectMainBodyResponseUnmarshaller {
+
+	public static DetectMainBodyResponse unmarshall(DetectMainBodyResponse detectMainBodyResponse, UnmarshallerContext _ctx) {
+		
+		detectMainBodyResponse.setRequestId(_ctx.stringValue("DetectMainBodyResponse.RequestId"));
+
+		Data data = new Data();
+
+		Location location = new Location();
+		location.setX(_ctx.integerValue("DetectMainBodyResponse.Data.Location.X"));
+		location.setY(_ctx.integerValue("DetectMainBodyResponse.Data.Location.Y"));
+		location.setWidth(_ctx.integerValue("DetectMainBodyResponse.Data.Location.Width"));
+		location.setHeight(_ctx.integerValue("DetectMainBodyResponse.Data.Location.Height"));
+		data.setLocation(location);
+		detectMainBodyResponse.setData(data);
+	 
+	 	return detectMainBodyResponse;
+	}
+}
