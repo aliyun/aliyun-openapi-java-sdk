e23eb074b0f1640a6bef6008a4b543a9b2d92291
sdk-team <sdk-team@alibabacloud.com>
2019-12-18 11:16:07 +0800

    First sdk version.

---
+++ aliyun-java-sdk-goodstech/src/main/java/com/aliyuncs/goodstech/model/v20191230/ClassifyCommodityResponse.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.goodstech.model.v20191230;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.goodstech.transform.v20191230.ClassifyCommodityResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ClassifyCommodityResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private List<Category> categories;
+
+		public List<Category> getCategories() {
+			return this.categories;
+		}
+
+		public void setCategories(List<Category> categories) {
+			this.categories = categories;
+		}
+
+		public static class Category {
+
+			private Float score;
+
+			private String categoryName;
+
+			private String categoryId;
+
+			public Float getScore() {
+				return this.score;
+			}
+
+			public void setScore(Float score) {
+				this.score = score;
+			}
+
+			public String getCategoryName() {
+				return this.categoryName;
+			}
+
+			public void setCategoryName(String categoryName) {
+				this.categoryName = categoryName;
+			}
+
+			public String getCategoryId() {
+				return this.categoryId;
+			}
+
+			public void setCategoryId(String categoryId) {
+				this.categoryId = categoryId;
+			}
+		}
+	}
+
+	@Override
+	public ClassifyCommodityResponse getInstance(UnmarshallerContext context) {
+		return	ClassifyCommodityResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-goodstech/src/main/java/com/aliyuncs/goodstech/transform/v20191230/ClassifyCommodityResponseUnmarshaller.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.goodstech.transform.v20191230;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.goodstech.model.v20191230.ClassifyCommodityResponse;
+import com.aliyuncs.goodstech.model.v20191230.ClassifyCommodityResponse.Data;
+import com.aliyuncs.goodstech.model.v20191230.ClassifyCommodityResponse.Data.Category;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ClassifyCommodityResponseUnmarshaller {
+
+	public static ClassifyCommodityResponse unmarshall(ClassifyCommodityResponse classifyCommodityResponse, UnmarshallerContext _ctx) {
+		
+		classifyCommodityResponse.setRequestId(_ctx.stringValue("ClassifyCommodityResponse.RequestId"));
+
+		Data data = new Data();
+
+		List<Category> categories = new ArrayList<Category>();
+		for (int i = 0; i < _ctx.lengthValue("ClassifyCommodityResponse.Data.Categories.Length"); i++) {
+			Category category = new Category();
+			category.setScore(_ctx.floatValue("ClassifyCommodityResponse.Data.Categories["+ i +"].Score"));
+			category.setCategoryName(_ctx.stringValue("ClassifyCommodityResponse.Data.Categories["+ i +"].CategoryName"));
+			category.setCategoryId(_ctx.stringValue("ClassifyCommodityResponse.Data.Categories["+ i +"].CategoryId"));
+
+			categories.add(category);
+		}
+		data.setCategories(categories);
+		classifyCommodityResponse.setData(data);
+	 
+	 	return classifyCommodityResponse;
+	}
+}
