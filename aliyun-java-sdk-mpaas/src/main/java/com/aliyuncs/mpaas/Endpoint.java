6b9379018fd44897c053b58f7cd03b154f24c856
sdk-team <sdk-team@alibabacloud.com>
2019-12-27 17:57:35 +0800

    Sdk for mPaaS public Api.

---
+++ aliyun-java-sdk-mpaas/src/main/java/com/aliyuncs/mpaas/Endpoint.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.mpaas;
+
+import java.util.HashMap;
+
+public class Endpoint {
+    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {
+        {
+            put("cn-shanghai-internal-test-1", "mpaas.aliyuncs.com");
+            put("cn-shenzhen-su18-b01", "mpaas.aliyuncs.com");
+            put("cn-beijing", "mpaas.aliyuncs.com");
+            put("cn-shanghai-inner", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-internal-prod-1", "mpaas.aliyuncs.com");
+            put("cn-north-2-gov-1", "mpaas.aliyuncs.com");
+            put("cn-yushanfang", "mpaas.aliyuncs.com");
+            put("cn-qingdao-nebula", "mpaas.aliyuncs.com");
+            put("cn-beijing-finance-pop", "mpaas.aliyuncs.com");
+            put("cn-wuhan", "mpaas.aliyuncs.com");
+            put("cn-zhangjiakou", "mpaas.aliyuncs.com");
+            put("us-west-1", "mpaas.aliyuncs.com");
+            put("rus-west-1-pop", "mpaas.aliyuncs.com");
+            put("cn-shanghai-et15-b01", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-bj-b01", "mpaas.aliyuncs.com");
+            put("cn-zhangbei-na61-b01", "mpaas.aliyuncs.com");
+            put("ap-northeast-1", "mpaas.aliyuncs.com");
+            put("cn-shanghai-et2-b01", "mpaas.aliyuncs.com");
+            put("ap-southeast-1", "mpaas.aliyuncs.com");
+            put("ap-southeast-2", "mpaas.aliyuncs.com");
+            put("ap-southeast-3", "mpaas.aliyuncs.com");
+            put("ap-southeast-5", "mpaas.aliyuncs.com");
+            put("us-east-1", "mpaas.aliyuncs.com");
+            put("cn-shenzhen-inner", "mpaas.aliyuncs.com");
+            put("cn-zhangjiakou-na62-a01", "mpaas.aliyuncs.com");
+            put("cn-beijing-gov-1", "mpaas.aliyuncs.com");
+            put("ap-south-1", "mpaas.aliyuncs.com");
+            put("cn-shenzhen-st4-d01", "mpaas.aliyuncs.com");
+            put("cn-haidian-cm12-c01", "mpaas.aliyuncs.com");
+            put("cn-qingdao", "mpaas.aliyuncs.com");
+            put("cn-hongkong-finance-pop", "mpaas.aliyuncs.com");
+            put("cn-shanghai", "mpaas.aliyuncs.com");
+            put("cn-shanghai-finance-1", "mpaas.aliyuncs.com");
+            put("cn-hongkong", "mpaas.aliyuncs.com");
+            put("eu-central-1", "mpaas.aliyuncs.com");
+            put("cn-shenzhen", "mpaas.aliyuncs.com");
+            put("cn-zhengzhou-nebula-1", "mpaas.aliyuncs.com");
+            put("eu-west-1", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-1", "mpaas.aliyuncs.com");
+            put("eu-west-1-oxs", "mpaas.aliyuncs.com");
+            put("cn-beijing-finance-1", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-3", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-2", "mpaas.aliyuncs.com");
+            put("cn-shenzhen-finance-1", "mpaas.aliyuncs.com");
+            put("me-east-1", "mpaas.aliyuncs.com");
+            put("cn-chengdu", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-test-306", "mpaas.aliyuncs.com");
+            put("cn-hangzhou-finance", "mpaas.aliyuncs.com");
+            put("cn-beijing-nu16-b01", "mpaas.aliyuncs.com");
+            put("cn-edge-1", "mpaas.aliyuncs.com");
+            put("cn-huhehaote", "mpaas.aliyuncs.com");
+            put("cn-fujian", "mpaas.aliyuncs.com");
+            put("ap-northeast-2-pop", "mpaas.aliyuncs.com");
+            put("cn-hangzhou", "mpaas.aliyuncs.com");
+        }
+    };
+
+    public static String endpointRegionalType = "regional";
+}
---
+++ aliyun-java-sdk-mpaas/src/main/java/com/aliyuncs/mpaas/transform/v20190821/PushBroadcastResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.mpaas.transform.v20190821;
+
+import com.aliyuncs.mpaas.model.v20190821.PushBroadcastResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class PushBroadcastResponseUnmarshaller {
+
+	public static PushBroadcastResponse unmarshall(PushBroadcastResponse pushBroadcastResponse, UnmarshallerContext _ctx) {
+		
+		pushBroadcastResponse.setRequestId(_ctx.stringValue("PushBroadcastResponse.RequestId"));
+		pushBroadcastResponse.setResultCode(_ctx.stringValue("PushBroadcastResponse.ResultCode"));
+		pushBroadcastResponse.setResultMessage(_ctx.stringValue("PushBroadcastResponse.ResultMessage"));
+	 
+	 	return pushBroadcastResponse;
+	}
+}
