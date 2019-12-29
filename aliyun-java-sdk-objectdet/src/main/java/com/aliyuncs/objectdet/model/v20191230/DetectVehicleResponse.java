50b06a58a723f1c8e1974cd98872878e01fdae02
sdk-team <sdk-team@alibabacloud.com>
2019-12-29 19:14:59 +0800

    Add new api.

---
+++ aliyun-java-sdk-objectdet/src/main/java/com/aliyuncs/objectdet/model/v20191230/DetectVehicleResponse.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.objectdet.model.v20191230;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.objectdet.transform.v20191230.DetectVehicleResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DetectVehicleResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Integer width;
+
+		private Integer height;
+
+		private List<DetectObjectInfo> detectObjectInfoList;
+
+		public Integer getWidth() {
+			return this.width;
+		}
+
+		public void setWidth(Integer width) {
+			this.width = width;
+		}
+
+		public Integer getHeight() {
+			return this.height;
+		}
+
+		public void setHeight(Integer height) {
+			this.height = height;
+		}
+
+		public List<DetectObjectInfo> getDetectObjectInfoList() {
+			return this.detectObjectInfoList;
+		}
+
+		public void setDetectObjectInfoList(List<DetectObjectInfo> detectObjectInfoList) {
+			this.detectObjectInfoList = detectObjectInfoList;
+		}
+
+		public static class DetectObjectInfo {
+
+			private Float score;
+
+			private String type;
+
+			private Integer id;
+
+			private List<Integer> boxes;
+
+			public Float getScore() {
+				return this.score;
+			}
+
+			public void setScore(Float score) {
+				this.score = score;
+			}
+
+			public String getType() {
+				return this.type;
+			}
+
+			public void setType(String type) {
+				this.type = type;
+			}
+
+			public Integer getId() {
+				return this.id;
+			}
+
+			public void setId(Integer id) {
+				this.id = id;
+			}
+
+			public List<Integer> getBoxes() {
+				return this.boxes;
+			}
+
+			public void setBoxes(List<Integer> boxes) {
+				this.boxes = boxes;
+			}
+		}
+	}
+
+	@Override
+	public DetectVehicleResponse getInstance(UnmarshallerContext context) {
+		return	DetectVehicleResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-objectdet/src/main/java/com/aliyuncs/objectdet/transform/v20191230/DetectVehicleResponseUnmarshaller.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.objectdet.transform.v20191230;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.objectdet.model.v20191230.DetectVehicleResponse;
+import com.aliyuncs.objectdet.model.v20191230.DetectVehicleResponse.Data;
+import com.aliyuncs.objectdet.model.v20191230.DetectVehicleResponse.Data.DetectObjectInfo;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DetectVehicleResponseUnmarshaller {
+
+	public static DetectVehicleResponse unmarshall(DetectVehicleResponse detectVehicleResponse, UnmarshallerContext _ctx) {
+		
+		detectVehicleResponse.setRequestId(_ctx.stringValue("DetectVehicleResponse.RequestId"));
+
+		Data data = new Data();
+		data.setWidth(_ctx.integerValue("DetectVehicleResponse.Data.Width"));
+		data.setHeight(_ctx.integerValue("DetectVehicleResponse.Data.Height"));
+
+		List<DetectObjectInfo> detectObjectInfoList = new ArrayList<DetectObjectInfo>();
+		for (int i = 0; i < _ctx.lengthValue("DetectVehicleResponse.Data.DetectObjectInfoList.Length"); i++) {
+			DetectObjectInfo detectObjectInfo = new DetectObjectInfo();
+			detectObjectInfo.setScore(_ctx.floatValue("DetectVehicleResponse.Data.DetectObjectInfoList["+ i +"].Score"));
+			detectObjectInfo.setType(_ctx.stringValue("DetectVehicleResponse.Data.DetectObjectInfoList["+ i +"].Type"));
+			detectObjectInfo.setId(_ctx.integerValue("DetectVehicleResponse.Data.DetectObjectInfoList["+ i +"].Id"));
+
+			List<Integer> boxes = new ArrayList<Integer>();
+			for (int j = 0; j < _ctx.lengthValue("DetectVehicleResponse.Data.DetectObjectInfoList["+ i +"].Boxes.Length"); j++) {
+				boxes.add(_ctx.integerValue("DetectVehicleResponse.Data.DetectObjectInfoList["+ i +"].Boxes["+ j +"]"));
+			}
+			detectObjectInfo.setBoxes(boxes);
+
+			detectObjectInfoList.add(detectObjectInfo);
+		}
+		data.setDetectObjectInfoList(detectObjectInfoList);
+		detectVehicleResponse.setData(data);
+	 
+	 	return detectVehicleResponse;
+	}
+}
