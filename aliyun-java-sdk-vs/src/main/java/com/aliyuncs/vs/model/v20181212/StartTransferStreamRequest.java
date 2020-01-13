5b816b34faf4cf91a3f9aa4ff038f2e0e1bf0754
sdk-team <sdk-team@alibabacloud.com>
2020-01-13 15:16:56 +0800

    Supported ondemand start transfer stream.

---
+++ aliyun-java-sdk-vs/src/main/java/com/aliyuncs/vs/model/v20181212/StartTransferStreamRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vs.model.v20181212;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.vs.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class StartTransferStreamRequest extends RpcAcsRequest<StartTransferStreamResponse> {
+	   
+
+	private String id;
+
+	private String transcode;
+
+	private Long ownerId;
+
+	private String url;
+	public StartTransferStreamRequest() {
+		super("vs", "2018-12-12", "StartTransferStream", "vs");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+		if(id != null){
+			putQueryParameter("Id", id);
+		}
+	}
+
+	public String getTranscode() {
+		return this.transcode;
+	}
+
+	public void setTranscode(String transcode) {
+		this.transcode = transcode;
+		if(transcode != null){
+			putQueryParameter("Transcode", transcode);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+		if(url != null){
+			putQueryParameter("Url", url);
+		}
+	}
+
+	@Override
+	public Class<StartTransferStreamResponse> getResponseClass() {
+		return StartTransferStreamResponse.class;
+	}
+
+}
