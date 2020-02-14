a2bea1b02fba4576d2a8d2dfcfe63dc2a50773fa
sdk-team <sdk-team@alibabacloud.com>
2020-02-14 09:42:48 +0800

    Update default endpoints.

--- aliyun-java-sdk-pvtz/src/main/java/com/aliyuncs/pvtz/Endpoint.java
+++ aliyun-java-sdk-pvtz/src/main/java/com/aliyuncs/pvtz/Endpoint.java
@@ -17,21 +17,7 @@ package com.aliyuncs.pvtz;
 import java.util.HashMap;
 
 public class Endpoint {
-    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {
-        {
-            put("cn-shenzhen", "pvtz.cn-shenzhen.aliyuncs.com");
-            put("cn-beijing", "pvtz.cn-beijing.aliyuncs.com");
-            put("cn-shanghai", "pvtz.cn-shanghai.aliyuncs.com");
-            put("cn-hongkong", "pvtz.cn-hongkong.aliyuncs.com");
-            put("ap-southeast-1", "pvtz.ap-southeast-1.aliyuncs.com");
-            put("ap-southeast-3", "pvtz.ap-southeast-3.aliyuncs.com");
-            put("eu-central-1", "pvtz.eu-central-1.aliyuncs.com");
-            put("cn-huhehaote", "pvtz.cn-huhehaote.aliyuncs.com");
-            put("ap-southeast-5", "pvtz.ap-southeast-5.aliyuncs.com");
-            put("cn-zhangjiakou", "pvtz.cn-zhangjiakou.aliyuncs.com");
-            put("cn-chengdu", "pvtz.cn-chengdu.aliyuncs.com");
-        }
-    };
+    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {};
 
     public static String endpointRegionalType = "central";
 }
