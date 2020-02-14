29b6357ca8e01d860bf2b50a488a16cc42939cb2
sdk-team <sdk-team@alibabacloud.com>
2020-02-14 11:12:25 +0800

    Update default endpoints.

--- aliyun-java-sdk-alidns/src/main/java/com/aliyuncs/alidns/Endpoint.java
+++ aliyun-java-sdk-alidns/src/main/java/com/aliyuncs/alidns/Endpoint.java
@@ -17,63 +17,7 @@ package com.aliyuncs.alidns;
 import java.util.HashMap;
 
 public class Endpoint {
-    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {
-        {
-            put("cn-shanghai-internal-test-1", "dns.aliyuncs.com");
-            put("cn-shenzhen-su18-b01", "dns.aliyuncs.com");
-            put("cn-beijing", "alidns.cn-beijing.aliyuncs.com");
-            put("cn-shanghai-inner", "dns.aliyuncs.com");
-            put("cn-hangzhou-internal-prod-1", "dns.aliyuncs.com");
-            put("cn-north-2-gov-1", "dns.aliyuncs.com");
-            put("cn-yushanfang", "dns.aliyuncs.com");
-            put("cn-qingdao-nebula", "dns.aliyuncs.com");
-            put("cn-beijing-finance-pop", "dns.aliyuncs.com");
-            put("cn-wuhan", "dns.aliyuncs.com");
-            put("cn-zhangjiakou", "alidns.cn-zhangjiakou.aliyuncs.com");
-            put("us-west-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("rus-west-1-pop", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-shanghai-et15-b01", "dns.aliyuncs.com");
-            put("cn-hangzhou-bj-b01", "dns.aliyuncs.com");
-            put("cn-zhangbei-na61-b01", "dns.aliyuncs.com");
-            put("ap-northeast-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-shanghai-et2-b01", "dns.aliyuncs.com");
-            put("ap-southeast-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("ap-southeast-2", "alidns.ap-southeast-1.aliyuncs.com");
-            put("ap-southeast-3", "alidns.ap-southeast-3.aliyuncs.com");
-            put("ap-southeast-5", "alidns.ap-southeast-5.aliyuncs.com");
-            put("us-east-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-shenzhen-inner", "dns.aliyuncs.com");
-            put("cn-zhangjiakou-na62-a01", "dns.aliyuncs.com");
-            put("cn-beijing-gov-1", "dns.aliyuncs.com");
-            put("ap-south-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-shenzhen-st4-d01", "dns.aliyuncs.com");
-            put("cn-haidian-cm12-c01", "dns.aliyuncs.com");
-            put("cn-qingdao", "dns.aliyuncs.com");
-            put("cn-hongkong-finance-pop", "dns.aliyuncs.com");
-            put("cn-shanghai", "alidns.cn-shanghai.aliyuncs.com");
-            put("cn-shanghai-finance-1", "dns.aliyuncs.com");
-            put("cn-hongkong", "alidns.cn-hongkong.aliyuncs.com");
-            put("eu-central-1", "alidns.eu-central-1.aliyuncs.com");
-            put("cn-shenzhen", "alidns.cn-shenzhen.aliyuncs.com");
-            put("cn-zhengzhou-nebula-1", "dns.aliyuncs.com");
-            put("eu-west-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-hangzhou-internal-test-1", "dns.aliyuncs.com");
-            put("eu-west-1-oxs", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-beijing-finance-1", "dns.aliyuncs.com");
-            put("cn-hangzhou-internal-test-3", "dns.aliyuncs.com");
-            put("cn-hangzhou-internal-test-2", "dns.aliyuncs.com");
-            put("cn-shenzhen-finance-1", "dns.aliyuncs.com");
-            put("me-east-1", "alidns.ap-southeast-1.aliyuncs.com");
-            put("cn-chengdu", "alidns.cn-chengdu.aliyuncs.com");
-            put("cn-hangzhou-test-306", "dns.aliyuncs.com");
-            put("cn-hangzhou-finance", "dns.aliyuncs.com");
-            put("cn-beijing-nu16-b01", "dns.aliyuncs.com");
-            put("cn-edge-1", "dns.aliyuncs.com");
-            put("cn-huhehaote", "alidns.cn-huhehaote.aliyuncs.com");
-            put("cn-fujian", "dns.aliyuncs.com");
-            put("ap-northeast-2-pop", "alidns.ap-southeast-1.aliyuncs.com");
-        }
-    };
+    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {};
 
     public static String endpointRegionalType = "central";
 }
