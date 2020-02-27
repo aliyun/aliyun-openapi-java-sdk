5ca82e7eca149ead3092b69b0a9df96dd98d931d
sdk-team <sdk-team@alibabacloud.com>
2020-02-27 21:15:12 +0800

    Sixth sdk version.

---
+++ aliyun-java-sdk-imageseg/src/main/java/com/aliyuncs/imageseg/model/v20191230/ParseFaceResponse.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageseg.model.v20191230;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.imageseg.transform.v20191230.ParseFaceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ParseFaceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private String originImageURL;
+
+		private List<Element> elements;
+
+		public String getOriginImageURL() {
+			return this.originImageURL;
+		}
+
+		public void setOriginImageURL(String originImageURL) {
+			this.originImageURL = originImageURL;
+		}
+
+		public List<Element> getElements() {
+			return this.elements;
+		}
+
+		public void setElements(List<Element> elements) {
+			this.elements = elements;
+		}
+
+		public static class Element {
+
+			private String name;
+
+			private String imageURL;
+
+			public String getName() {
+				return this.name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public String getImageURL() {
+				return this.imageURL;
+			}
+
+			public void setImageURL(String imageURL) {
+				this.imageURL = imageURL;
+			}
+		}
+	}
+
+	@Override
+	public ParseFaceResponse getInstance(UnmarshallerContext context) {
+		return	ParseFaceResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
