2acca74c5ffc31a12e7290e612b601bc831b31ad
sdk-team <sdk-team@alibabacloud.com>
2020-03-06 18:58:56 +0800

    Support light up led lamp for ESL.

---
+++ aliyun-java-sdk-cloudesl/src/main/java/com/aliyuncs/cloudesl/model/v20190801/UpdateEslDeviceLightRequest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudesl.model.v20190801;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.cloudesl.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateEslDeviceLightRequest extends RpcAcsRequest<UpdateEslDeviceLightResponse> {
+	   
+
+	private Integer lightUpTime;
+
+	private String storeId;
+
+	private String frequency;
+
+	private String eslBarCode;
+
+	private String ledColor;
+
+	private String itemBarCode;
+	public UpdateEslDeviceLightRequest() {
+		super("cloudesl", "2019-08-01", "UpdateEslDeviceLight", "cloudesl");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Integer getLightUpTime() {
+		return this.lightUpTime;
+	}
+
+	public void setLightUpTime(Integer lightUpTime) {
+		this.lightUpTime = lightUpTime;
+		if(lightUpTime != null){
+			putBodyParameter("LightUpTime", lightUpTime.toString());
+		}
+	}
+
+	public String getStoreId() {
+		return this.storeId;
+	}
+
+	public void setStoreId(String storeId) {
+		this.storeId = storeId;
+		if(storeId != null){
+			putBodyParameter("StoreId", storeId);
+		}
+	}
+
+	public String getFrequency() {
+		return this.frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+		if(frequency != null){
+			putBodyParameter("Frequency", frequency);
+		}
+	}
+
+	public String getEslBarCode() {
+		return this.eslBarCode;
+	}
+
+	public void setEslBarCode(String eslBarCode) {
+		this.eslBarCode = eslBarCode;
+		if(eslBarCode != null){
+			putBodyParameter("EslBarCode", eslBarCode);
+		}
+	}
+
+	public String getLedColor() {
+		return this.ledColor;
+	}
+
+	public void setLedColor(String ledColor) {
+		this.ledColor = ledColor;
+		if(ledColor != null){
+			putBodyParameter("LedColor", ledColor);
+		}
+	}
+
+	public String getItemBarCode() {
+		return this.itemBarCode;
+	}
+
+	public void setItemBarCode(String itemBarCode) {
+		this.itemBarCode = itemBarCode;
+		if(itemBarCode != null){
+			putBodyParameter("ItemBarCode", itemBarCode);
+		}
+	}
+
+	@Override
+	public Class<UpdateEslDeviceLightResponse> getResponseClass() {
+		return UpdateEslDeviceLightResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cloudesl/src/main/java/com/aliyuncs/cloudesl/transform/v20190801/UpdateEslDeviceLightResponseUnmarshaller.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudesl.transform.v20190801;
+
+import com.aliyuncs.cloudesl.model.v20190801.UpdateEslDeviceLightResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UpdateEslDeviceLightResponseUnmarshaller {
+
+	public static UpdateEslDeviceLightResponse unmarshall(UpdateEslDeviceLightResponse updateEslDeviceLightResponse, UnmarshallerContext _ctx) {
+		
+		updateEslDeviceLightResponse.setRequestId(_ctx.stringValue("UpdateEslDeviceLightResponse.RequestId"));
+		updateEslDeviceLightResponse.setErrorMessage(_ctx.stringValue("UpdateEslDeviceLightResponse.ErrorMessage"));
+		updateEslDeviceLightResponse.setErrorCode(_ctx.stringValue("UpdateEslDeviceLightResponse.ErrorCode"));
+		updateEslDeviceLightResponse.setFailEslBarCodes(_ctx.stringValue("UpdateEslDeviceLightResponse.FailEslBarCodes"));
+		updateEslDeviceLightResponse.setMessage(_ctx.stringValue("UpdateEslDeviceLightResponse.Message"));
+		updateEslDeviceLightResponse.setSuccessCount(_ctx.longValue("UpdateEslDeviceLightResponse.SuccessCount"));
+		updateEslDeviceLightResponse.setFailCount(_ctx.longValue("UpdateEslDeviceLightResponse.FailCount"));
+		updateEslDeviceLightResponse.setDynamicCode(_ctx.stringValue("UpdateEslDeviceLightResponse.DynamicCode"));
+		updateEslDeviceLightResponse.setCode(_ctx.stringValue("UpdateEslDeviceLightResponse.Code"));
+		updateEslDeviceLightResponse.setDynamicMessage(_ctx.stringValue("UpdateEslDeviceLightResponse.DynamicMessage"));
+		updateEslDeviceLightResponse.setSuccess(_ctx.booleanValue("UpdateEslDeviceLightResponse.Success"));
+	 
+	 	return updateEslDeviceLightResponse;
+	}
+}
