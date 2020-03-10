fe519280bd3db8fcfb75df5f9fc9ddfc8440465d
sdk-team <sdk-team@alibabacloud.com>
2020-03-10 17:08:01 +0800

    Deploy GetCustomerList Service.

---
+++ aliyun-java-sdk-resellertrade/src/main/java/com/aliyuncs/resellertrade/model/v20191227/GetCustomerListRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.resellertrade.model.v20191227;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.resellertrade.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetCustomerListRequest extends RpcAcsRequest<GetCustomerListResponse> {
+	   
+
+	private Integer pageNo;
+
+	private Integer pageSize;
+	public GetCustomerListRequest() {
+		super("ResellerTrade", "2019-12-27", "GetCustomerList");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Integer getPageNo() {
+		return this.pageNo;
+	}
+
+	public void setPageNo(Integer pageNo) {
+		this.pageNo = pageNo;
+		if(pageNo != null){
+			putQueryParameter("PageNo", pageNo.toString());
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	@Override
+	public Class<GetCustomerListResponse> getResponseClass() {
+		return GetCustomerListResponse.class;
+	}
+
+}
