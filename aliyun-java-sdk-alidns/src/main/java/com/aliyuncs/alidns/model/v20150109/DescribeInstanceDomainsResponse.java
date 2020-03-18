bde523d5d144154f55b8354e215d227262fb9913
sdk-team <sdk-team@alibabacloud.com>
2020-03-18 17:41:49 +0800

    Supported API for DescribeInstanceDomains.

---
+++ aliyun-java-sdk-alidns/src/main/java/com/aliyuncs/alidns/model/v20150109/DescribeInstanceDomainsResponse.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alidns.model.v20150109;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.alidns.transform.v20150109.DescribeInstanceDomainsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeInstanceDomainsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Integer totalItems;
+
+	private Integer pageNumber;
+
+	private Integer pageSize;
+
+	private Integer totalPages;
+
+	private List<InstanceDomain> instanceDomains;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Integer getTotalItems() {
+		return this.totalItems;
+	}
+
+	public void setTotalItems(Integer totalItems) {
+		this.totalItems = totalItems;
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public Integer getTotalPages() {
+		return this.totalPages;
+	}
+
+	public void setTotalPages(Integer totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public List<InstanceDomain> getInstanceDomains() {
+		return this.instanceDomains;
+	}
+
+	public void setInstanceDomains(List<InstanceDomain> instanceDomains) {
+		this.instanceDomains = instanceDomains;
+	}
+
+	public static class InstanceDomain {
+
+		private String domainName;
+
+		private String createTime;
+
+		private Long createTimestamp;
+
+		public String getDomainName() {
+			return this.domainName;
+		}
+
+		public void setDomainName(String domainName) {
+			this.domainName = domainName;
+		}
+
+		public String getCreateTime() {
+			return this.createTime;
+		}
+
+		public void setCreateTime(String createTime) {
+			this.createTime = createTime;
+		}
+
+		public Long getCreateTimestamp() {
+			return this.createTimestamp;
+		}
+
+		public void setCreateTimestamp(Long createTimestamp) {
+			this.createTimestamp = createTimestamp;
+		}
+	}
+
+	@Override
+	public DescribeInstanceDomainsResponse getInstance(UnmarshallerContext context) {
+		return	DescribeInstanceDomainsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
