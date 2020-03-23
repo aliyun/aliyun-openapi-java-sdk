eec9b079dd80d0d55e07aef9830eb3354fb80ef5
sdk-team <sdk-team@alibabacloud.com>
2020-03-23 10:30:47 +0800

    SDK version 107.

---
+++ aliyun-java-sdk-imagerecog/src/main/java/com/aliyuncs/imagerecog/model/v20190930/RecognizeVehicleTypeResponse.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imagerecog.model.v20190930;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.imagerecog.transform.v20190930.RecognizeVehicleTypeResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class RecognizeVehicleTypeResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Float threshold;
+
+		private List<Element> elements;
+
+		public Float getThreshold() {
+			return this.threshold;
+		}
+
+		public void setThreshold(Float threshold) {
+			this.threshold = threshold;
+		}
+
+		public List<Element> getElements() {
+			return this.elements;
+		}
+
+		public void setElements(List<Element> elements) {
+			this.elements = elements;
+		}
+
+		public static class Element {
+
+			private String name;
+
+			private Float score;
+
+			public String getName() {
+				return this.name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public Float getScore() {
+				return this.score;
+			}
+
+			public void setScore(Float score) {
+				this.score = score;
+			}
+		}
+	}
+
+	@Override
+	public RecognizeVehicleTypeResponse getInstance(UnmarshallerContext context) {
+		return	RecognizeVehicleTypeResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-imagerecog/src/main/java/com/aliyuncs/imagerecog/transform/v20190930/RecognizeVehicleTypeResponseUnmarshaller.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imagerecog.transform.v20190930;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.imagerecog.model.v20190930.RecognizeVehicleTypeResponse;
+import com.aliyuncs.imagerecog.model.v20190930.RecognizeVehicleTypeResponse.Data;
+import com.aliyuncs.imagerecog.model.v20190930.RecognizeVehicleTypeResponse.Data.Element;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class RecognizeVehicleTypeResponseUnmarshaller {
+
+	public static RecognizeVehicleTypeResponse unmarshall(RecognizeVehicleTypeResponse recognizeVehicleTypeResponse, UnmarshallerContext _ctx) {
+		
+		recognizeVehicleTypeResponse.setRequestId(_ctx.stringValue("RecognizeVehicleTypeResponse.RequestId"));
+
+		Data data = new Data();
+		data.setThreshold(_ctx.floatValue("RecognizeVehicleTypeResponse.Data.Threshold"));
+
+		List<Element> elements = new ArrayList<Element>();
+		for (int i = 0; i < _ctx.lengthValue("RecognizeVehicleTypeResponse.Data.Elements.Length"); i++) {
+			Element element = new Element();
+			element.setName(_ctx.stringValue("RecognizeVehicleTypeResponse.Data.Elements["+ i +"].Name"));
+			element.setScore(_ctx.floatValue("RecognizeVehicleTypeResponse.Data.Elements["+ i +"].Score"));
+
+			elements.add(element);
+		}
+		data.setElements(elements);
+		recognizeVehicleTypeResponse.setData(data);
+	 
+	 	return recognizeVehicleTypeResponse;
+	}
+}
