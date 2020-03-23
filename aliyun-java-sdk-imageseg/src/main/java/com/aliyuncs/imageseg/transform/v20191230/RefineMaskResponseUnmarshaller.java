cfd239b5577a56a84d899866b879d41d6edbf9b2
sdk-team <sdk-team@alibabacloud.com>
2020-03-23 10:31:38 +0800

    SDK version 107.

---
+++ aliyun-java-sdk-imageseg/src/main/java/com/aliyuncs/imageseg/transform/v20191230/RefineMaskResponseUnmarshaller.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageseg.transform.v20191230;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.imageseg.model.v20191230.RefineMaskResponse;
+import com.aliyuncs.imageseg.model.v20191230.RefineMaskResponse.Data;
+import com.aliyuncs.imageseg.model.v20191230.RefineMaskResponse.Data.Element;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class RefineMaskResponseUnmarshaller {
+
+	public static RefineMaskResponse unmarshall(RefineMaskResponse refineMaskResponse, UnmarshallerContext _ctx) {
+		
+		refineMaskResponse.setRequestId(_ctx.stringValue("RefineMaskResponse.RequestId"));
+
+		Data data = new Data();
+
+		List<Element> elements = new ArrayList<Element>();
+		for (int i = 0; i < _ctx.lengthValue("RefineMaskResponse.Data.Elements.Length"); i++) {
+			Element element = new Element();
+			element.setImageURL(_ctx.stringValue("RefineMaskResponse.Data.Elements["+ i +"].ImageURL"));
+
+			elements.add(element);
+		}
+		data.setElements(elements);
+		refineMaskResponse.setData(data);
+	 
+	 	return refineMaskResponse;
+	}
+}
---
+++ aliyun-java-sdk-imageseg/src/main/java/com/aliyuncs/imageseg/transform/v20191230/SegmentFurnitureResponseUnmarshaller.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageseg.transform.v20191230;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.imageseg.model.v20191230.SegmentFurnitureResponse;
+import com.aliyuncs.imageseg.model.v20191230.SegmentFurnitureResponse.Data;
+import com.aliyuncs.imageseg.model.v20191230.SegmentFurnitureResponse.Data.Element;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SegmentFurnitureResponseUnmarshaller {
+
+	public static SegmentFurnitureResponse unmarshall(SegmentFurnitureResponse segmentFurnitureResponse, UnmarshallerContext _ctx) {
+		
+		segmentFurnitureResponse.setRequestId(_ctx.stringValue("SegmentFurnitureResponse.RequestId"));
+
+		Data data = new Data();
+
+		List<Element> elements = new ArrayList<Element>();
+		for (int i = 0; i < _ctx.lengthValue("SegmentFurnitureResponse.Data.Elements.Length"); i++) {
+			Element element = new Element();
+			element.setImageURL(_ctx.stringValue("SegmentFurnitureResponse.Data.Elements["+ i +"].ImageURL"));
+
+			elements.add(element);
+		}
+		data.setElements(elements);
+		segmentFurnitureResponse.setData(data);
+	 
+	 	return segmentFurnitureResponse;
+	}
+}
