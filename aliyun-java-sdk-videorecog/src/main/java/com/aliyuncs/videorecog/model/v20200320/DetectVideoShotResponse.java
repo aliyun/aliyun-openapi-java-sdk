08d42f2e51b2ef23aae006f16760f8882c680161
sdk-team <sdk-team@alibabacloud.com>
2020-03-23 10:34:15 +0800

    SDK version 100.

---
+++ aliyun-java-sdk-videorecog/src/main/java/com/aliyuncs/videorecog/model/v20200320/DetectVideoShotResponse.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videorecog.model.v20200320;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.videorecog.transform.v20200320.DetectVideoShotResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DetectVideoShotResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private List<Integer> shotFrameIds;
+
+		public List<Integer> getShotFrameIds() {
+			return this.shotFrameIds;
+		}
+
+		public void setShotFrameIds(List<Integer> shotFrameIds) {
+			this.shotFrameIds = shotFrameIds;
+		}
+	}
+
+	@Override
+	public DetectVideoShotResponse getInstance(UnmarshallerContext context) {
+		return	DetectVideoShotResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-videorecog/src/main/java/com/aliyuncs/videorecog/transform/v20200320/GenerateVideoCoverResponseUnmarshaller.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videorecog.transform.v20200320;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.videorecog.model.v20200320.GenerateVideoCoverResponse;
+import com.aliyuncs.videorecog.model.v20200320.GenerateVideoCoverResponse.Data;
+import com.aliyuncs.videorecog.model.v20200320.GenerateVideoCoverResponse.Data.Output;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GenerateVideoCoverResponseUnmarshaller {
+
+	public static GenerateVideoCoverResponse unmarshall(GenerateVideoCoverResponse generateVideoCoverResponse, UnmarshallerContext _ctx) {
+		
+		generateVideoCoverResponse.setRequestId(_ctx.stringValue("GenerateVideoCoverResponse.RequestId"));
+
+		Data data = new Data();
+
+		List<Output> outputs = new ArrayList<Output>();
+		for (int i = 0; i < _ctx.lengthValue("GenerateVideoCoverResponse.Data.Outputs.Length"); i++) {
+			Output output = new Output();
+			output.setImageURL(_ctx.stringValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].ImageURL"));
+			output.setConfidence(_ctx.floatValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].Confidence"));
+			output.setTime(_ctx.floatValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].Time"));
+			output.setFaceCount(_ctx.floatValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].FaceCount"));
+			output.setStartTime(_ctx.floatValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].StartTime"));
+			output.setEndTime(_ctx.floatValue("GenerateVideoCoverResponse.Data.Outputs["+ i +"].EndTime"));
+
+			outputs.add(output);
+		}
+		data.setOutputs(outputs);
+		generateVideoCoverResponse.setData(data);
+	 
+	 	return generateVideoCoverResponse;
+	}
+}
