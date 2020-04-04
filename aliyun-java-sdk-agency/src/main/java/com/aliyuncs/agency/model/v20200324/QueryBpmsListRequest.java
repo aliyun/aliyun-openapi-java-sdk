5d91e2d9375621585f0da256f23e086c2c5a50f8
sdk-team <sdk-team@alibabacloud.com>
2020-04-04 22:55:02 +0800

    Add new method.

---
+++ aliyun-java-sdk-agency/src/main/java/com/aliyuncs/agency/model/v20200324/QueryBpmsListRequest.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.agency.model.v20200324;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryBpmsListRequest extends RpcAcsRequest<QueryBpmsListResponse> {
+	   
+
+	private String bpmsType;
+
+	private Long partnerId;
+
+	private String customerName;
+
+	private String auditStatus;
+
+	private String timeStart;
+
+	private String timeEnd;
+
+	private Integer pageNo;
+
+	private Integer pageSize;
+	public QueryBpmsListRequest() {
+		super("Agency", "2020-03-24", "QueryBpmsList");
+		setMethod(MethodType.POST);
+	}
+
+	public String getBpmsType() {
+		return this.bpmsType;
+	}
+
+	public void setBpmsType(String bpmsType) {
+		this.bpmsType = bpmsType;
+		if(bpmsType != null){
+			putQueryParameter("BpmsType", bpmsType);
+		}
+	}
+
+	public Long getPartnerId() {
+		return this.partnerId;
+	}
+
+	public void setPartnerId(Long partnerId) {
+		this.partnerId = partnerId;
+		if(partnerId != null){
+			putQueryParameter("PartnerId", partnerId.toString());
+		}
+	}
+
+	public String getCustomerName() {
+		return this.customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+		if(customerName != null){
+			putQueryParameter("CustomerName", customerName);
+		}
+	}
+
+	public String getAuditStatus() {
+		return this.auditStatus;
+	}
+
+	public void setAuditStatus(String auditStatus) {
+		this.auditStatus = auditStatus;
+		if(auditStatus != null){
+			putQueryParameter("AuditStatus", auditStatus);
+		}
+	}
+
+	public String getTimeStart() {
+		return this.timeStart;
+	}
+
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+		if(timeStart != null){
+			putQueryParameter("TimeStart", timeStart);
+		}
+	}
+
+	public String getTimeEnd() {
+		return this.timeEnd;
+	}
+
+	public void setTimeEnd(String timeEnd) {
+		this.timeEnd = timeEnd;
+		if(timeEnd != null){
+			putQueryParameter("TimeEnd", timeEnd);
+		}
+	}
+
+	public Integer getPageNo() {
+		return this.pageNo;
+	}
+
+	public void setPageNo(Integer pageNo) {
+		this.pageNo = pageNo;
+		if(pageNo != null){
+			putQueryParameter("PageNo", pageNo.toString());
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	@Override
+	public Class<QueryBpmsListResponse> getResponseClass() {
+		return QueryBpmsListResponse.class;
+	}
+
+}
