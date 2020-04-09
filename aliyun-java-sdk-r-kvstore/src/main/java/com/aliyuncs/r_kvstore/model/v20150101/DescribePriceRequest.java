59a24ca518c85bd66e663380ce493ada5a66bee0
sdk-team <sdk-team@alibabacloud.com>
2020-04-09 09:45:10 +0800

    Generated 2015-01-01 for R-kvstore.

---
+++ aliyun-java-sdk-r-kvstore/src/main/java/com/aliyuncs/r_kvstore/model/v20150101/DescribePriceRequest.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.r_kvstore.model.v20150101;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.r_kvstore.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribePriceRequest extends RpcAcsRequest<DescribePriceResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String nodeType;
+
+	private String instances;
+
+	private String couponNo;
+
+	private String instanceClass;
+
+	private Long capacity;
+
+	private String securityToken;
+
+	private String businessInfo;
+
+	private Long period;
+
+	private Long quantity;
+
+	private String resourceOwnerAccount;
+
+	private String ownerAccount;
+
+	private String orderParamOut;
+
+	private Long ownerId;
+
+	private String instanceId;
+
+	private String zoneId;
+
+	private String chargeType;
+
+	private Boolean forceUpgrade;
+
+	private String orderType;
+	public DescribePriceRequest() {
+		super("R-kvstore", "2015-01-01", "DescribePrice", "redisa");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getNodeType() {
+		return this.nodeType;
+	}
+
+	public void setNodeType(String nodeType) {
+		this.nodeType = nodeType;
+		if(nodeType != null){
+			putQueryParameter("NodeType", nodeType);
+		}
+	}
+
+	public String getInstances() {
+		return this.instances;
+	}
+
+	public void setInstances(String instances) {
+		this.instances = instances;
+		if(instances != null){
+			putQueryParameter("Instances", instances);
+		}
+	}
+
+	public String getCouponNo() {
+		return this.couponNo;
+	}
+
+	public void setCouponNo(String couponNo) {
+		this.couponNo = couponNo;
+		if(couponNo != null){
+			putQueryParameter("CouponNo", couponNo);
+		}
+	}
+
+	public String getInstanceClass() {
+		return this.instanceClass;
+	}
+
+	public void setInstanceClass(String instanceClass) {
+		this.instanceClass = instanceClass;
+		if(instanceClass != null){
+			putQueryParameter("InstanceClass", instanceClass);
+		}
+	}
+
+	public Long getCapacity() {
+		return this.capacity;
+	}
+
+	public void setCapacity(Long capacity) {
+		this.capacity = capacity;
+		if(capacity != null){
+			putQueryParameter("Capacity", capacity.toString());
+		}
+	}
+
+	public String getSecurityToken() {
+		return this.securityToken;
+	}
+
+	public void setSecurityToken(String securityToken) {
+		this.securityToken = securityToken;
+		if(securityToken != null){
+			putQueryParameter("SecurityToken", securityToken);
+		}
+	}
+
+	public String getBusinessInfo() {
+		return this.businessInfo;
+	}
+
+	public void setBusinessInfo(String businessInfo) {
+		this.businessInfo = businessInfo;
+		if(businessInfo != null){
+			putQueryParameter("BusinessInfo", businessInfo);
+		}
+	}
+
+	public Long getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(Long period) {
+		this.period = period;
+		if(period != null){
+			putQueryParameter("Period", period.toString());
+		}
+	}
+
+	public Long getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+		if(quantity != null){
+			putQueryParameter("Quantity", quantity.toString());
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putQueryParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public String getOrderParamOut() {
+		return this.orderParamOut;
+	}
+
+	public void setOrderParamOut(String orderParamOut) {
+		this.orderParamOut = orderParamOut;
+		if(orderParamOut != null){
+			putQueryParameter("OrderParamOut", orderParamOut);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putQueryParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+		if(zoneId != null){
+			putQueryParameter("ZoneId", zoneId);
+		}
+	}
+
+	public String getChargeType() {
+		return this.chargeType;
+	}
+
+	public void setChargeType(String chargeType) {
+		this.chargeType = chargeType;
+		if(chargeType != null){
+			putQueryParameter("ChargeType", chargeType);
+		}
+	}
+
+	public Boolean getForceUpgrade() {
+		return this.forceUpgrade;
+	}
+
+	public void setForceUpgrade(Boolean forceUpgrade) {
+		this.forceUpgrade = forceUpgrade;
+		if(forceUpgrade != null){
+			putQueryParameter("ForceUpgrade", forceUpgrade.toString());
+		}
+	}
+
+	public String getOrderType() {
+		return this.orderType;
+	}
+
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+		if(orderType != null){
+			putQueryParameter("OrderType", orderType);
+		}
+	}
+
+	@Override
+	public Class<DescribePriceResponse> getResponseClass() {
+		return DescribePriceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-r-kvstore/src/main/java/com/aliyuncs/r_kvstore/model/v20150101/DescribePriceResponse.java
@@ -0,0 +1,274 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.r_kvstore.model.v20150101;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.r_kvstore.transform.v20150101.DescribePriceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribePriceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String orderParams;
+
+	private List<Rule> rules;
+
+	private List<SubOrder> subOrders;
+
+	private Order order;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getOrderParams() {
+		return this.orderParams;
+	}
+
+	public void setOrderParams(String orderParams) {
+		this.orderParams = orderParams;
+	}
+
+	public List<Rule> getRules() {
+		return this.rules;
+	}
+
+	public void setRules(List<Rule> rules) {
+		this.rules = rules;
+	}
+
+	public List<SubOrder> getSubOrders() {
+		return this.subOrders;
+	}
+
+	public void setSubOrders(List<SubOrder> subOrders) {
+		this.subOrders = subOrders;
+	}
+
+	public Order getOrder() {
+		return this.order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public static class Rule {
+
+		private Long ruleDescId;
+
+		private String name;
+
+		private String title;
+
+		public Long getRuleDescId() {
+			return this.ruleDescId;
+		}
+
+		public void setRuleDescId(Long ruleDescId) {
+			this.ruleDescId = ruleDescId;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getTitle() {
+			return this.title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+
+	public static class SubOrder {
+
+		private Float originalAmount;
+
+		private Float tradeAmount;
+
+		private Float discountAmount;
+
+		private String instanceId;
+
+		private List<String> ruleIds;
+
+		public Float getOriginalAmount() {
+			return this.originalAmount;
+		}
+
+		public void setOriginalAmount(Float originalAmount) {
+			this.originalAmount = originalAmount;
+		}
+
+		public Float getTradeAmount() {
+			return this.tradeAmount;
+		}
+
+		public void setTradeAmount(Float tradeAmount) {
+			this.tradeAmount = tradeAmount;
+		}
+
+		public Float getDiscountAmount() {
+			return this.discountAmount;
+		}
+
+		public void setDiscountAmount(Float discountAmount) {
+			this.discountAmount = discountAmount;
+		}
+
+		public String getInstanceId() {
+			return this.instanceId;
+		}
+
+		public void setInstanceId(String instanceId) {
+			this.instanceId = instanceId;
+		}
+
+		public List<String> getRuleIds() {
+			return this.ruleIds;
+		}
+
+		public void setRuleIds(List<String> ruleIds) {
+			this.ruleIds = ruleIds;
+		}
+	}
+
+	public static class Order {
+
+		private Float originalAmount;
+
+		private Float tradeAmount;
+
+		private Float discountAmount;
+
+		private String currency;
+
+		private List<Coupon> coupons;
+
+		private List<String> ruleIds1;
+
+		public Float getOriginalAmount() {
+			return this.originalAmount;
+		}
+
+		public void setOriginalAmount(Float originalAmount) {
+			this.originalAmount = originalAmount;
+		}
+
+		public Float getTradeAmount() {
+			return this.tradeAmount;
+		}
+
+		public void setTradeAmount(Float tradeAmount) {
+			this.tradeAmount = tradeAmount;
+		}
+
+		public Float getDiscountAmount() {
+			return this.discountAmount;
+		}
+
+		public void setDiscountAmount(Float discountAmount) {
+			this.discountAmount = discountAmount;
+		}
+
+		public String getCurrency() {
+			return this.currency;
+		}
+
+		public void setCurrency(String currency) {
+			this.currency = currency;
+		}
+
+		public List<Coupon> getCoupons() {
+			return this.coupons;
+		}
+
+		public void setCoupons(List<Coupon> coupons) {
+			this.coupons = coupons;
+		}
+
+		public List<String> getRuleIds1() {
+			return this.ruleIds1;
+		}
+
+		public void setRuleIds1(List<String> ruleIds1) {
+			this.ruleIds1 = ruleIds1;
+		}
+
+		public static class Coupon {
+
+			private String couponNo;
+
+			private String name;
+
+			private String description;
+
+			private String isSelected;
+
+			public String getCouponNo() {
+				return this.couponNo;
+			}
+
+			public void setCouponNo(String couponNo) {
+				this.couponNo = couponNo;
+			}
+
+			public String getName() {
+				return this.name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public String getDescription() {
+				return this.description;
+			}
+
+			public void setDescription(String description) {
+				this.description = description;
+			}
+
+			public String getIsSelected() {
+				return this.isSelected;
+			}
+
+			public void setIsSelected(String isSelected) {
+				this.isSelected = isSelected;
+			}
+		}
+	}
+
+	@Override
+	public DescribePriceResponse getInstance(UnmarshallerContext context) {
+		return	DescribePriceResponseUnmarshaller.unmarshall(this, context);
+	}
+}
