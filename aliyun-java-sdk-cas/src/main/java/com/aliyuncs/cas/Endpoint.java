7a5e816d25083aa0153a8967573890b24b36f880
sdk-team <sdk-team@alibabacloud.com>
2020-04-15 15:28:28 +0800

    Publish SDK For Java python.

---
+++ aliyun-java-sdk-cas/src/main/java/com/aliyuncs/cas/Endpoint.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cas;
+
+import java.util.HashMap;
+
+public class Endpoint {
+    public static HashMap<String, String> endpointMap = new HashMap<String, String>() {
+        {
+            put("cn-shanghai-internal-test-1", "cas.aliyuncs.com");
+            put("cn-shenzhen-su18-b01", "cas.aliyuncs.com");
+            put("cn-beijing", "cas.aliyuncs.com");
+            put("cn-shanghai-inner", "cas.aliyuncs.com");
+            put("cn-hangzhou-internal-prod-1", "cas.aliyuncs.com");
+            put("cn-north-2-gov-1", "cas.aliyuncs.com");
+            put("cn-yushanfang", "cas.aliyuncs.com");
+            put("cn-qingdao-nebula", "cas.aliyuncs.com");
+            put("cn-beijing-finance-pop", "cas.aliyuncs.com");
+            put("cn-wuhan", "cas.aliyuncs.com");
+            put("cn-zhangjiakou", "cas.aliyuncs.com");
+            put("us-west-1", "cas.aliyuncs.com");
+            put("rus-west-1-pop", "cas.aliyuncs.com");
+            put("cn-shanghai-et15-b01", "cas.aliyuncs.com");
+            put("cn-hangzhou-bj-b01", "cas.aliyuncs.com");
+            put("cn-zhangbei-na61-b01", "cas.aliyuncs.com");
+            put("cn-shanghai-et2-b01", "cas.aliyuncs.com");
+            put("ap-southeast-1", "cas.aliyuncs.com");
+            put("ap-southeast-3", "cas.aliyuncs.com");
+            put("ap-southeast-5", "cas.aliyuncs.com");
+            put("us-east-1", "cas.aliyuncs.com");
+            put("cn-shenzhen-inner", "cas.aliyuncs.com");
+            put("cn-zhangjiakou-na62-a01", "cas.aliyuncs.com");
+            put("cn-beijing-gov-1", "cas.aliyuncs.com");
+            put("cn-shenzhen-st4-d01", "cas.aliyuncs.com");
+            put("cn-haidian-cm12-c01", "cas.aliyuncs.com");
+            put("cn-qingdao", "cas.aliyuncs.com");
+            put("cn-hongkong-finance-pop", "cas.aliyuncs.com");
+            put("cn-shanghai", "cas.aliyuncs.com");
+            put("cn-shanghai-finance-1", "cas.aliyuncs.com");
+            put("cn-hongkong", "cas.aliyuncs.com");
+            put("cn-shenzhen", "cas.aliyuncs.com");
+            put("cn-zhengzhou-nebula-1", "cas.aliyuncs.com");
+            put("eu-west-1", "cas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-1", "cas.aliyuncs.com");
+            put("eu-west-1-oxs", "cas.aliyuncs.com");
+            put("cn-beijing-finance-1", "cas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-3", "cas.aliyuncs.com");
+            put("cn-hangzhou-internal-test-2", "cas.aliyuncs.com");
+            put("cn-shenzhen-finance-1", "cas.aliyuncs.com");
+            put("cn-chengdu", "cas.aliyuncs.com");
+            put("cn-hangzhou-test-306", "cas.aliyuncs.com");
+            put("cn-hangzhou-finance", "cas.aliyuncs.com");
+            put("cn-beijing-nu16-b01", "cas.aliyuncs.com");
+            put("cn-edge-1", "cas.aliyuncs.com");
+            put("cn-huhehaote", "cas.aliyuncs.com");
+            put("cn-fujian", "cas.aliyuncs.com");
+            put("ap-northeast-2-pop", "cas.aliyuncs.com");
+            put("cn-hangzhou", "cas.aliyuncs.com");
+        }
+    };
+
+    public static String endpointRegionalType = "regional";
+}
---
+++ aliyun-java-sdk-cas/src/main/java/com/aliyuncs/cas/model/v20200407/DescribeCertificateStateResponse.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cas.model.v20200407;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cas.transform.v20200407.DescribeCertificateStateResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeCertificateStateResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String type;
+
+	private String certificate;
+
+	private String privateKey;
+
+	private String validateType;
+
+	private String domain;
+
+	private String uri;
+
+	private String content;
+
+	private String recordValue;
+
+	private String recordType;
+
+	private String recordDomain;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCertificate() {
+		return this.certificate;
+	}
+
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+
+	public String getPrivateKey() {
+		return this.privateKey;
+	}
+
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public String getValidateType() {
+		return this.validateType;
+	}
+
+	public void setValidateType(String validateType) {
+		this.validateType = validateType;
+	}
+
+	public String getDomain() {
+		return this.domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getUri() {
+		return this.uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRecordValue() {
+		return this.recordValue;
+	}
+
+	public void setRecordValue(String recordValue) {
+		this.recordValue = recordValue;
+	}
+
+	public String getRecordType() {
+		return this.recordType;
+	}
+
+	public void setRecordType(String recordType) {
+		this.recordType = recordType;
+	}
+
+	public String getRecordDomain() {
+		return this.recordDomain;
+	}
+
+	public void setRecordDomain(String recordDomain) {
+		this.recordDomain = recordDomain;
+	}
+
+	@Override
+	public DescribeCertificateStateResponse getInstance(UnmarshallerContext context) {
+		return	DescribeCertificateStateResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
