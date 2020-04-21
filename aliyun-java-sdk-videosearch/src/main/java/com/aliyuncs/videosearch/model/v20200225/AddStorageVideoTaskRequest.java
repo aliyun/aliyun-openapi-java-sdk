f8960ad2869fec9f1aa1bbe840e38381dbeb377b
sdk-team <sdk-team@alibabacloud.com>
2020-04-21 14:02:46 +0800

    VideoSearch deploy SDK.

---
+++ aliyun-java-sdk-videosearch/src/main/java/com/aliyuncs/videosearch/model/v20200225/AddStorageVideoTaskRequest.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videosearch.model.v20200225;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class AddStorageVideoTaskRequest extends RpcAcsRequest<AddStorageVideoTaskResponse> {
+	   
+
+	private String clientToken;
+
+	private String videoTags;
+
+	private String videoId;
+
+	private String instanceId;
+
+	private String videoUrl;
+
+	private String callbackUrl;
+	public AddStorageVideoTaskRequest() {
+		super("videosearch", "2020-02-25", "AddStorageVideoTask");
+		setMethod(MethodType.POST);
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getVideoTags() {
+		return this.videoTags;
+	}
+
+	public void setVideoTags(String videoTags) {
+		this.videoTags = videoTags;
+		if(videoTags != null){
+			putBodyParameter("VideoTags", videoTags);
+		}
+	}
+
+	public String getVideoId() {
+		return this.videoId;
+	}
+
+	public void setVideoId(String videoId) {
+		this.videoId = videoId;
+		if(videoId != null){
+			putBodyParameter("VideoId", videoId);
+		}
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putBodyParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getVideoUrl() {
+		return this.videoUrl;
+	}
+
+	public void setVideoUrl(String videoUrl) {
+		this.videoUrl = videoUrl;
+		if(videoUrl != null){
+			putBodyParameter("VideoUrl", videoUrl);
+		}
+	}
+
+	public String getCallbackUrl() {
+		return this.callbackUrl;
+	}
+
+	public void setCallbackUrl(String callbackUrl) {
+		this.callbackUrl = callbackUrl;
+		if(callbackUrl != null){
+			putBodyParameter("CallbackUrl", callbackUrl);
+		}
+	}
+
+	@Override
+	public Class<AddStorageVideoTaskResponse> getResponseClass() {
+		return AddStorageVideoTaskResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-videosearch/src/main/java/com/aliyuncs/videosearch/transform/v20200225/AddDeletionVideoTaskResponseUnmarshaller.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videosearch.transform.v20200225;
+
+import com.aliyuncs.videosearch.model.v20200225.AddDeletionVideoTaskResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class AddDeletionVideoTaskResponseUnmarshaller {
+
+	public static AddDeletionVideoTaskResponse unmarshall(AddDeletionVideoTaskResponse addDeletionVideoTaskResponse, UnmarshallerContext _ctx) {
+		
+		addDeletionVideoTaskResponse.setRequestId(_ctx.stringValue("AddDeletionVideoTaskResponse.RequestId"));
+		addDeletionVideoTaskResponse.setData(_ctx.booleanValue("AddDeletionVideoTaskResponse.Data"));
+	 
+	 	return addDeletionVideoTaskResponse;
+	}
+}
