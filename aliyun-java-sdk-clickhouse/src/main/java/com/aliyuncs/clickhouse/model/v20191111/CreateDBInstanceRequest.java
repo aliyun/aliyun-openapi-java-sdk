4b18c88dfffaeab70793d165c7258919c6025e6d
sdk-team <sdk-team@alibabacloud.com>
2020-04-24 10:00:59 +0800

    Support nmd.

---
+++ aliyun-java-sdk-clickhouse/src/main/java/com/aliyuncs/clickhouse/model/v20191111/CreateDBInstanceRequest.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.clickhouse.model.v20191111;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.clickhouse.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateDBInstanceRequest extends RpcAcsRequest<CreateDBInstanceResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String dBClusterDescription;
+
+	private String clientToken;
+
+	private String dbNodeStorageType;
+
+	private String dBClusterCategory;
+
+	private String dBClusterNetworkType;
+
+	private String period;
+
+	private String resourceOwnerAccount;
+
+	private String ownerAccount;
+
+	private String dBClusterVersion;
+
+	private String dBClusterClass;
+
+	private Long ownerId;
+
+	private String dBNodeGroupCount;
+
+	private String usedTime;
+
+	private String vSwitchId;
+
+	private String dBNodeStorage;
+
+	private String vPCId;
+
+	private String zoneId;
+
+	private String payType;
+	public CreateDBInstanceRequest() {
+		super("clickhouse", "2019-11-11", "CreateDBInstance", "clickhouse");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getDBClusterDescription() {
+		return this.dBClusterDescription;
+	}
+
+	public void setDBClusterDescription(String dBClusterDescription) {
+		this.dBClusterDescription = dBClusterDescription;
+		if(dBClusterDescription != null){
+			putQueryParameter("DBClusterDescription", dBClusterDescription);
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getDbNodeStorageType() {
+		return this.dbNodeStorageType;
+	}
+
+	public void setDbNodeStorageType(String dbNodeStorageType) {
+		this.dbNodeStorageType = dbNodeStorageType;
+		if(dbNodeStorageType != null){
+			putQueryParameter("DbNodeStorageType", dbNodeStorageType);
+		}
+	}
+
+	public String getDBClusterCategory() {
+		return this.dBClusterCategory;
+	}
+
+	public void setDBClusterCategory(String dBClusterCategory) {
+		this.dBClusterCategory = dBClusterCategory;
+		if(dBClusterCategory != null){
+			putQueryParameter("DBClusterCategory", dBClusterCategory);
+		}
+	}
+
+	public String getDBClusterNetworkType() {
+		return this.dBClusterNetworkType;
+	}
+
+	public void setDBClusterNetworkType(String dBClusterNetworkType) {
+		this.dBClusterNetworkType = dBClusterNetworkType;
+		if(dBClusterNetworkType != null){
+			putQueryParameter("DBClusterNetworkType", dBClusterNetworkType);
+		}
+	}
+
+	public String getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+		if(period != null){
+			putQueryParameter("Period", period);
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putQueryParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public String getDBClusterVersion() {
+		return this.dBClusterVersion;
+	}
+
+	public void setDBClusterVersion(String dBClusterVersion) {
+		this.dBClusterVersion = dBClusterVersion;
+		if(dBClusterVersion != null){
+			putQueryParameter("DBClusterVersion", dBClusterVersion);
+		}
+	}
+
+	public String getDBClusterClass() {
+		return this.dBClusterClass;
+	}
+
+	public void setDBClusterClass(String dBClusterClass) {
+		this.dBClusterClass = dBClusterClass;
+		if(dBClusterClass != null){
+			putQueryParameter("DBClusterClass", dBClusterClass);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getDBNodeGroupCount() {
+		return this.dBNodeGroupCount;
+	}
+
+	public void setDBNodeGroupCount(String dBNodeGroupCount) {
+		this.dBNodeGroupCount = dBNodeGroupCount;
+		if(dBNodeGroupCount != null){
+			putQueryParameter("DBNodeGroupCount", dBNodeGroupCount);
+		}
+	}
+
+	public String getUsedTime() {
+		return this.usedTime;
+	}
+
+	public void setUsedTime(String usedTime) {
+		this.usedTime = usedTime;
+		if(usedTime != null){
+			putQueryParameter("UsedTime", usedTime);
+		}
+	}
+
+	public String getVSwitchId() {
+		return this.vSwitchId;
+	}
+
+	public void setVSwitchId(String vSwitchId) {
+		this.vSwitchId = vSwitchId;
+		if(vSwitchId != null){
+			putQueryParameter("VSwitchId", vSwitchId);
+		}
+	}
+
+	public String getDBNodeStorage() {
+		return this.dBNodeStorage;
+	}
+
+	public void setDBNodeStorage(String dBNodeStorage) {
+		this.dBNodeStorage = dBNodeStorage;
+		if(dBNodeStorage != null){
+			putQueryParameter("DBNodeStorage", dBNodeStorage);
+		}
+	}
+
+	public String getVPCId() {
+		return this.vPCId;
+	}
+
+	public void setVPCId(String vPCId) {
+		this.vPCId = vPCId;
+		if(vPCId != null){
+			putQueryParameter("VPCId", vPCId);
+		}
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+		if(zoneId != null){
+			putQueryParameter("ZoneId", zoneId);
+		}
+	}
+
+	public String getPayType() {
+		return this.payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+		if(payType != null){
+			putQueryParameter("PayType", payType);
+		}
+	}
+
+	@Override
+	public Class<CreateDBInstanceResponse> getResponseClass() {
+		return CreateDBInstanceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-clickhouse/src/main/java/com/aliyuncs/clickhouse/model/v20191111/DescribeAvailableResourceResponse.java
@@ -0,0 +1,253 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.clickhouse.model.v20191111;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.clickhouse.transform.v20191111.DescribeAvailableResourceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeAvailableResourceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String regionId;
+
+	private List<AvailableZone> availableZoneList;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getRegionId() {
+		return this.regionId;
+	}
+
+	public void setRegionId(String regionId) {
+		this.regionId = regionId;
+	}
+
+	public List<AvailableZone> getAvailableZoneList() {
+		return this.availableZoneList;
+	}
+
+	public void setAvailableZoneList(List<AvailableZone> availableZoneList) {
+		this.availableZoneList = availableZoneList;
+	}
+
+	public static class AvailableZone {
+
+		private String zoneId;
+
+		private List<SupportedSerial> supportedSerialList;
+
+		public String getZoneId() {
+			return this.zoneId;
+		}
+
+		public void setZoneId(String zoneId) {
+			this.zoneId = zoneId;
+		}
+
+		public List<SupportedSerial> getSupportedSerialList() {
+			return this.supportedSerialList;
+		}
+
+		public void setSupportedSerialList(List<SupportedSerial> supportedSerialList) {
+			this.supportedSerialList = supportedSerialList;
+		}
+
+		public static class SupportedSerial {
+
+			private String serial;
+
+			private List<SupportedInstanceClass> supportedInstanceClassList;
+
+			public String getSerial() {
+				return this.serial;
+			}
+
+			public void setSerial(String serial) {
+				this.serial = serial;
+			}
+
+			public List<SupportedInstanceClass> getSupportedInstanceClassList() {
+				return this.supportedInstanceClassList;
+			}
+
+			public void setSupportedInstanceClassList(List<SupportedInstanceClass> supportedInstanceClassList) {
+				this.supportedInstanceClassList = supportedInstanceClassList;
+			}
+
+			public static class SupportedInstanceClass {
+
+				private String instanceClass;
+
+				private String tips;
+
+				private List<SupportedNodeCount> supportedNodeCountList;
+
+				private List<SupportedExecutor> supportedExecutorList;
+
+				public String getInstanceClass() {
+					return this.instanceClass;
+				}
+
+				public void setInstanceClass(String instanceClass) {
+					this.instanceClass = instanceClass;
+				}
+
+				public String getTips() {
+					return this.tips;
+				}
+
+				public void setTips(String tips) {
+					this.tips = tips;
+				}
+
+				public List<SupportedNodeCount> getSupportedNodeCountList() {
+					return this.supportedNodeCountList;
+				}
+
+				public void setSupportedNodeCountList(List<SupportedNodeCount> supportedNodeCountList) {
+					this.supportedNodeCountList = supportedNodeCountList;
+				}
+
+				public List<SupportedExecutor> getSupportedExecutorList() {
+					return this.supportedExecutorList;
+				}
+
+				public void setSupportedExecutorList(List<SupportedExecutor> supportedExecutorList) {
+					this.supportedExecutorList = supportedExecutorList;
+				}
+
+				public static class SupportedNodeCount {
+
+					private List<String> storageSize;
+
+					private NodeCount nodeCount;
+
+					public List<String> getStorageSize() {
+						return this.storageSize;
+					}
+
+					public void setStorageSize(List<String> storageSize) {
+						this.storageSize = storageSize;
+					}
+
+					public NodeCount getNodeCount() {
+						return this.nodeCount;
+					}
+
+					public void setNodeCount(NodeCount nodeCount) {
+						this.nodeCount = nodeCount;
+					}
+
+					public static class NodeCount {
+
+						private String minCount;
+
+						private String maxCount;
+
+						private String step;
+
+						public String getMinCount() {
+							return this.minCount;
+						}
+
+						public void setMinCount(String minCount) {
+							this.minCount = minCount;
+						}
+
+						public String getMaxCount() {
+							return this.maxCount;
+						}
+
+						public void setMaxCount(String maxCount) {
+							this.maxCount = maxCount;
+						}
+
+						public String getStep() {
+							return this.step;
+						}
+
+						public void setStep(String step) {
+							this.step = step;
+						}
+					}
+				}
+
+				public static class SupportedExecutor {
+
+					private NodeCount1 nodeCount1;
+
+					public NodeCount1 getNodeCount1() {
+						return this.nodeCount1;
+					}
+
+					public void setNodeCount1(NodeCount1 nodeCount1) {
+						this.nodeCount1 = nodeCount1;
+					}
+
+					public static class NodeCount1 {
+
+						private String minCount;
+
+						private String maxCount;
+
+						private String step;
+
+						public String getMinCount() {
+							return this.minCount;
+						}
+
+						public void setMinCount(String minCount) {
+							this.minCount = minCount;
+						}
+
+						public String getMaxCount() {
+							return this.maxCount;
+						}
+
+						public void setMaxCount(String maxCount) {
+							this.maxCount = maxCount;
+						}
+
+						public String getStep() {
+							return this.step;
+						}
+
+						public void setStep(String step) {
+							this.step = step;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public DescribeAvailableResourceResponse getInstance(UnmarshallerContext context) {
+		return	DescribeAvailableResourceResponseUnmarshaller.unmarshall(this, context);
+	}
+}
