b0f9f34dcd5cdbb816692c1606e830eb026bbe41
sdk-team <sdk-team@alibabacloud.com>
2020-04-29 23:42:22 +0800

    Build 20190808 release sdk.

---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/model/v20190808/ListDashboardsResponse.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.model.v20190808;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.arms.transform.v20190808.ListDashboardsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListDashboardsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<DashboardVosItem> dashboardVos;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<DashboardVosItem> getDashboardVos() {
+		return this.dashboardVos;
+	}
+
+	public void setDashboardVos(List<DashboardVosItem> dashboardVos) {
+		this.dashboardVos = dashboardVos;
+	}
+
+	public static class DashboardVosItem {
+
+		private String id;
+
+		private String uid;
+
+		private String title;
+
+		private String time;
+
+		private String url;
+
+		private String type;
+
+		private String exporter;
+
+		private Boolean isArmsExporter;
+
+		private List<String> tags;
+
+		public String getId() {
+			return this.id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getUid() {
+			return this.uid;
+		}
+
+		public void setUid(String uid) {
+			this.uid = uid;
+		}
+
+		public String getTitle() {
+			return this.title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getTime() {
+			return this.time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+
+		public String getUrl() {
+			return this.url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public String getType() {
+			return this.type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public String getExporter() {
+			return this.exporter;
+		}
+
+		public void setExporter(String exporter) {
+			this.exporter = exporter;
+		}
+
+		public Boolean getIsArmsExporter() {
+			return this.isArmsExporter;
+		}
+
+		public void setIsArmsExporter(Boolean isArmsExporter) {
+			this.isArmsExporter = isArmsExporter;
+		}
+
+		public List<String> getTags() {
+			return this.tags;
+		}
+
+		public void setTags(List<String> tags) {
+			this.tags = tags;
+		}
+	}
+
+	@Override
+	public ListDashboardsResponse getInstance(UnmarshallerContext context) {
+		return	ListDashboardsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/transform/v20190808/ListDashboardsResponseUnmarshaller.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.transform.v20190808;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.arms.model.v20190808.ListDashboardsResponse;
+import com.aliyuncs.arms.model.v20190808.ListDashboardsResponse.DashboardVosItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListDashboardsResponseUnmarshaller {
+
+	public static ListDashboardsResponse unmarshall(ListDashboardsResponse listDashboardsResponse, UnmarshallerContext _ctx) {
+		
+		listDashboardsResponse.setRequestId(_ctx.stringValue("ListDashboardsResponse.RequestId"));
+
+		List<DashboardVosItem> dashboardVos = new ArrayList<DashboardVosItem>();
+		for (int i = 0; i < _ctx.lengthValue("ListDashboardsResponse.DashboardVos.Length"); i++) {
+			DashboardVosItem dashboardVosItem = new DashboardVosItem();
+			dashboardVosItem.setId(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Id"));
+			dashboardVosItem.setUid(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Uid"));
+			dashboardVosItem.setTitle(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Title"));
+			dashboardVosItem.setTime(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Time"));
+			dashboardVosItem.setUrl(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Url"));
+			dashboardVosItem.setType(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Type"));
+			dashboardVosItem.setExporter(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Exporter"));
+			dashboardVosItem.setIsArmsExporter(_ctx.booleanValue("ListDashboardsResponse.DashboardVos["+ i +"].IsArmsExporter"));
+
+			List<String> tags = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("ListDashboardsResponse.DashboardVos["+ i +"].Tags.Length"); j++) {
+				tags.add(_ctx.stringValue("ListDashboardsResponse.DashboardVos["+ i +"].Tags["+ j +"]"));
+			}
+			dashboardVosItem.setTags(tags);
+
+			dashboardVos.add(dashboardVosItem);
+		}
+		listDashboardsResponse.setDashboardVos(dashboardVos);
+	 
+	 	return listDashboardsResponse;
+	}
+}
