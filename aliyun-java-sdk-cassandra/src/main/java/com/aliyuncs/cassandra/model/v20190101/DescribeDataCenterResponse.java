14b77a7b121180b887bcd7a5e6e4dfd6d542128c
sdk-team <sdk-team@alibabacloud.com>
2020-05-09 22:54:34 +0800

    Generated 2019-01-01 for Cassandra.

---
+++ aliyun-java-sdk-cassandra/src/main/java/com/aliyuncs/cassandra/model/v20190101/DescribeDataCenterResponse.java
@@ -0,0 +1,211 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cassandra.model.v20190101;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cassandra.transform.v20190101.DescribeDataCenterResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeDataCenterResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String dataCenterId;
+
+	private String regionId;
+
+	private String zoneId;
+
+	private String clusterId;
+
+	private String dataCenterName;
+
+	private String status;
+
+	private String createdTime;
+
+	private String instanceType;
+
+	private Integer nodeCount;
+
+	private String diskType;
+
+	private Integer diskSize;
+
+	private String vpcId;
+
+	private String vswitchId;
+
+	private String payType;
+
+	private String commodityInstance;
+
+	private String expireTime;
+
+	private String lockMode;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getDataCenterId() {
+		return this.dataCenterId;
+	}
+
+	public void setDataCenterId(String dataCenterId) {
+		this.dataCenterId = dataCenterId;
+	}
+
+	public String getRegionId() {
+		return this.regionId;
+	}
+
+	public void setRegionId(String regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+	}
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+	}
+
+	public String getDataCenterName() {
+		return this.dataCenterName;
+	}
+
+	public void setDataCenterName(String dataCenterName) {
+		this.dataCenterName = dataCenterName;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCreatedTime() {
+		return this.createdTime;
+	}
+
+	public void setCreatedTime(String createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public String getInstanceType() {
+		return this.instanceType;
+	}
+
+	public void setInstanceType(String instanceType) {
+		this.instanceType = instanceType;
+	}
+
+	public Integer getNodeCount() {
+		return this.nodeCount;
+	}
+
+	public void setNodeCount(Integer nodeCount) {
+		this.nodeCount = nodeCount;
+	}
+
+	public String getDiskType() {
+		return this.diskType;
+	}
+
+	public void setDiskType(String diskType) {
+		this.diskType = diskType;
+	}
+
+	public Integer getDiskSize() {
+		return this.diskSize;
+	}
+
+	public void setDiskSize(Integer diskSize) {
+		this.diskSize = diskSize;
+	}
+
+	public String getVpcId() {
+		return this.vpcId;
+	}
+
+	public void setVpcId(String vpcId) {
+		this.vpcId = vpcId;
+	}
+
+	public String getVswitchId() {
+		return this.vswitchId;
+	}
+
+	public void setVswitchId(String vswitchId) {
+		this.vswitchId = vswitchId;
+	}
+
+	public String getPayType() {
+		return this.payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+	}
+
+	public String getCommodityInstance() {
+		return this.commodityInstance;
+	}
+
+	public void setCommodityInstance(String commodityInstance) {
+		this.commodityInstance = commodityInstance;
+	}
+
+	public String getExpireTime() {
+		return this.expireTime;
+	}
+
+	public void setExpireTime(String expireTime) {
+		this.expireTime = expireTime;
+	}
+
+	public String getLockMode() {
+		return this.lockMode;
+	}
+
+	public void setLockMode(String lockMode) {
+		this.lockMode = lockMode;
+	}
+
+	@Override
+	public DescribeDataCenterResponse getInstance(UnmarshallerContext context) {
+		return	DescribeDataCenterResponseUnmarshaller.unmarshall(this, context);
+	}
+}
