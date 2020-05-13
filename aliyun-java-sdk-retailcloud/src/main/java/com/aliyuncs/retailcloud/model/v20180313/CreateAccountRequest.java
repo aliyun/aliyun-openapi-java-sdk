193049e9afda8dc7bb9ccc51dca1b2bb2aea0de7
sdk-team <sdk-team@alibabacloud.com>
2020-05-13 14:43:24 +0800

    Supported AAA for BBB.

---
+++ aliyun-java-sdk-retailcloud/src/main/java/com/aliyuncs/retailcloud/model/v20180313/CreateAccountRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.retailcloud.model.v20180313;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.retailcloud.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateAccountRequest extends RpcAcsRequest<CreateAccountResponse> {
+	   
+
+	private String accountPassword;
+
+	private String accountName;
+
+	private String accountType;
+
+	private String dbInstanceId;
+	public CreateAccountRequest() {
+		super("retailcloud", "2018-03-13", "CreateAccount", "retailcloud");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getAccountPassword() {
+		return this.accountPassword;
+	}
+
+	public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+		if(accountPassword != null){
+			putBodyParameter("AccountPassword", accountPassword);
+		}
+	}
+
+	public String getAccountName() {
+		return this.accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+		if(accountName != null){
+			putBodyParameter("AccountName", accountName);
+		}
+	}
+
+	public String getAccountType() {
+		return this.accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+		if(accountType != null){
+			putBodyParameter("AccountType", accountType);
+		}
+	}
+
+	public String getDbInstanceId() {
+		return this.dbInstanceId;
+	}
+
+	public void setDbInstanceId(String dbInstanceId) {
+		this.dbInstanceId = dbInstanceId;
+		if(dbInstanceId != null){
+			putBodyParameter("DbInstanceId", dbInstanceId);
+		}
+	}
+
+	@Override
+	public Class<CreateAccountResponse> getResponseClass() {
+		return CreateAccountResponse.class;
+	}
+
+}
