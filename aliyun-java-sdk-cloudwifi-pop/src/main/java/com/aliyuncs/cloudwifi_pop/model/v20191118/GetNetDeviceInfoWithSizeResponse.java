f7fb80e0042def37a19eda327ba618ab92c56e7b
sdk-team <sdk-team@alibabacloud.com>
2020-05-13 22:40:44 +0800

    Add two apis.

---
+++ aliyun-java-sdk-cloudwifi-pop/src/main/java/com/aliyuncs/cloudwifi_pop/model/v20191118/GetNetDeviceInfoWithSizeResponse.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudwifi_pop.model.v20191118;
+
+import java.util.List;
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cloudwifi_pop.transform.v20191118.GetNetDeviceInfoWithSizeResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetNetDeviceInfoWithSizeResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean isSuccess;
+
+	private Integer errorCode;
+
+	private String errorMessage;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getIsSuccess() {
+		return this.isSuccess;
+	}
+
+	public void setIsSuccess(Boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+
+	public Integer getErrorCode() {
+		return this.errorCode;
+	}
+
+	public void setErrorCode(Integer errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return this.errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Long count;
+
+		private List<Map<Object,Object>> data1;
+
+		public Long getCount() {
+			return this.count;
+		}
+
+		public void setCount(Long count) {
+			this.count = count;
+		}
+
+		public List<Map<Object,Object>> getData1() {
+			return this.data1;
+		}
+
+		public void setData1(List<Map<Object,Object>> data1) {
+			this.data1 = data1;
+		}
+	}
+
+	@Override
+	public GetNetDeviceInfoWithSizeResponse getInstance(UnmarshallerContext context) {
+		return	GetNetDeviceInfoWithSizeResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-cloudwifi-pop/src/main/java/com/aliyuncs/cloudwifi_pop/model/v20191118/ListJobOrdersWithSizeRequest.java
@@ -0,0 +1,236 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudwifi_pop.model.v20191118;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.cloudwifi_pop.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListJobOrdersWithSizeRequest extends RpcAcsRequest<ListJobOrdersWithSizeResponse> {
+	   
+
+	private Long cursor;
+
+	private String handler;
+
+	private String clientSystem;
+
+	private String orderStatus;
+
+	private String endTime;
+
+	private String startTime;
+
+	private String title;
+
+	private String clientUniqueId;
+
+	private String changingType;
+
+	private String appName;
+
+	private String requestId;
+
+	private Integer pageSize;
+
+	private String appCode;
+
+	private String id;
+
+	private String status;
+	public ListJobOrdersWithSizeRequest() {
+		super("cloudwifi-pop", "2019-11-18", "ListJobOrdersWithSize", "cloudap");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getCursor() {
+		return this.cursor;
+	}
+
+	public void setCursor(Long cursor) {
+		this.cursor = cursor;
+		if(cursor != null){
+			putQueryParameter("Cursor", cursor.toString());
+		}
+	}
+
+	public String getHandler() {
+		return this.handler;
+	}
+
+	public void setHandler(String handler) {
+		this.handler = handler;
+		if(handler != null){
+			putQueryParameter("Handler", handler);
+		}
+	}
+
+	public String getClientSystem() {
+		return this.clientSystem;
+	}
+
+	public void setClientSystem(String clientSystem) {
+		this.clientSystem = clientSystem;
+		if(clientSystem != null){
+			putQueryParameter("ClientSystem", clientSystem);
+		}
+	}
+
+	public String getOrderStatus() {
+		return this.orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+		if(orderStatus != null){
+			putQueryParameter("OrderStatus", orderStatus);
+		}
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+		if(endTime != null){
+			putQueryParameter("EndTime", endTime);
+		}
+	}
+
+	public String getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+		if(startTime != null){
+			putQueryParameter("StartTime", startTime);
+		}
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+		if(title != null){
+			putQueryParameter("Title", title);
+		}
+	}
+
+	public String getClientUniqueId() {
+		return this.clientUniqueId;
+	}
+
+	public void setClientUniqueId(String clientUniqueId) {
+		this.clientUniqueId = clientUniqueId;
+		if(clientUniqueId != null){
+			putQueryParameter("ClientUniqueId", clientUniqueId);
+		}
+	}
+
+	public String getChangingType() {
+		return this.changingType;
+	}
+
+	public void setChangingType(String changingType) {
+		this.changingType = changingType;
+		if(changingType != null){
+			putQueryParameter("ChangingType", changingType);
+		}
+	}
+
+	public String getAppName() {
+		return this.appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+		if(appName != null){
+			putQueryParameter("AppName", appName);
+		}
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+		if(requestId != null){
+			putQueryParameter("RequestId", requestId);
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public String getAppCode() {
+		return this.appCode;
+	}
+
+	public void setAppCode(String appCode) {
+		this.appCode = appCode;
+		if(appCode != null){
+			putQueryParameter("AppCode", appCode);
+		}
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+		if(id != null){
+			putQueryParameter("Id", id);
+		}
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+		if(status != null){
+			putQueryParameter("Status", status);
+		}
+	}
+
+	@Override
+	public Class<ListJobOrdersWithSizeResponse> getResponseClass() {
+		return ListJobOrdersWithSizeResponse.class;
+	}
+
+}
