4942f2327d4d55dad74ea0460c67e844155cbaa2
sdk-team <sdk-team@alibabacloud.com>
2020-05-15 11:27:08 +0800

    Generated 2016-01-02 for Ft.

---
+++ aliyun-java-sdk-ft/src/main/java/com/aliyuncs/ft/transform/v20160102/TestGateWayByRoaResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ft.transform.v20160102;
+
+import com.aliyuncs.ft.model.v20160102.TestGateWayByRoaResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class TestGateWayByRoaResponseUnmarshaller {
+
+	public static TestGateWayByRoaResponse unmarshall(TestGateWayByRoaResponse testGateWayByRoaResponse, UnmarshallerContext _ctx) {
+		
+		testGateWayByRoaResponse.setRequestId(_ctx.stringValue("TestGateWayByRoaResponse.RequestId"));
+		testGateWayByRoaResponse.setSuccess(_ctx.booleanValue("TestGateWayByRoaResponse.Success"));
+		testGateWayByRoaResponse.setCode(_ctx.stringValue("TestGateWayByRoaResponse.Code"));
+	 
+	 	return testGateWayByRoaResponse;
+	}
+}
---
+++ aliyun-java-sdk-ft/src/main/java/com/aliyuncs/ft/transform/v20160102/TestRoaGlobalApiResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ft.transform.v20160102;
+
+import com.aliyuncs.ft.model.v20160102.TestRoaGlobalApiResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class TestRoaGlobalApiResponseUnmarshaller {
+
+	public static TestRoaGlobalApiResponse unmarshall(TestRoaGlobalApiResponse testRoaGlobalApiResponse, UnmarshallerContext _ctx) {
+		
+		testRoaGlobalApiResponse.setHeaders(_ctx.stringValue("TestRoaGlobalApiResponse.Headers"));
+		testRoaGlobalApiResponse.setBody(_ctx.stringValue("TestRoaGlobalApiResponse.Body"));
+		testRoaGlobalApiResponse.setParams(_ctx.stringValue("TestRoaGlobalApiResponse.Params"));
+		testRoaGlobalApiResponse.setRemoteAddr(_ctx.stringValue("TestRoaGlobalApiResponse.RemoteAddr"));
+		testRoaGlobalApiResponse.setQueryString(_ctx.stringValue("TestRoaGlobalApiResponse.QueryString"));
+		testRoaGlobalApiResponse.setRequestURL(_ctx.stringValue("TestRoaGlobalApiResponse.RequestURL"));
+	 
+	 	return testRoaGlobalApiResponse;
+	}
+}
