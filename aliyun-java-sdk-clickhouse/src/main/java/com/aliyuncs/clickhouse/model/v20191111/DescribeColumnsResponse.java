021479a641b1bb3c6efb1c3cd88ca804104fc260
sdk-team <sdk-team@alibabacloud.com>
2020-05-19 20:00:41 +0800

    Update DescribeColumns and DescribeTables.

---
+++ aliyun-java-sdk-clickhouse/src/main/java/com/aliyuncs/clickhouse/model/v20191111/DescribeColumnsResponse.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.clickhouse.model.v20191111;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.clickhouse.transform.v20191111.DescribeColumnsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeColumnsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<Column> items;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<Column> getItems() {
+		return this.items;
+	}
+
+	public void setItems(List<Column> items) {
+		this.items = items;
+	}
+
+	public static class Column {
+
+		private String dBClusterId;
+
+		private String schemaName;
+
+		private String tableName;
+
+		private String columnName;
+
+		private String type;
+
+		private Boolean primaryKey;
+
+		private Boolean autoIncrementColumn;
+
+		public String getDBClusterId() {
+			return this.dBClusterId;
+		}
+
+		public void setDBClusterId(String dBClusterId) {
+			this.dBClusterId = dBClusterId;
+		}
+
+		public String getSchemaName() {
+			return this.schemaName;
+		}
+
+		public void setSchemaName(String schemaName) {
+			this.schemaName = schemaName;
+		}
+
+		public String getTableName() {
+			return this.tableName;
+		}
+
+		public void setTableName(String tableName) {
+			this.tableName = tableName;
+		}
+
+		public String getColumnName() {
+			return this.columnName;
+		}
+
+		public void setColumnName(String columnName) {
+			this.columnName = columnName;
+		}
+
+		public String getType() {
+			return this.type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public Boolean getPrimaryKey() {
+			return this.primaryKey;
+		}
+
+		public void setPrimaryKey(Boolean primaryKey) {
+			this.primaryKey = primaryKey;
+		}
+
+		public Boolean getAutoIncrementColumn() {
+			return this.autoIncrementColumn;
+		}
+
+		public void setAutoIncrementColumn(Boolean autoIncrementColumn) {
+			this.autoIncrementColumn = autoIncrementColumn;
+		}
+	}
+
+	@Override
+	public DescribeColumnsResponse getInstance(UnmarshallerContext context) {
+		return	DescribeColumnsResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-clickhouse/src/main/java/com/aliyuncs/clickhouse/transform/v20191111/DescribeAllDataSourceResponseUnmarshaller.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.clickhouse.transform.v20191111;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.clickhouse.model.v20191111.DescribeAllDataSourceResponse;
+import com.aliyuncs.clickhouse.model.v20191111.DescribeAllDataSourceResponse.Column;
+import com.aliyuncs.clickhouse.model.v20191111.DescribeAllDataSourceResponse.Schema;
+import com.aliyuncs.clickhouse.model.v20191111.DescribeAllDataSourceResponse.Table;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeAllDataSourceResponseUnmarshaller {
+
+	public static DescribeAllDataSourceResponse unmarshall(DescribeAllDataSourceResponse describeAllDataSourceResponse, UnmarshallerContext _ctx) {
+		
+		describeAllDataSourceResponse.setRequestId(_ctx.stringValue("DescribeAllDataSourceResponse.RequestId"));
+
+		List<Schema> schemas = new ArrayList<Schema>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeAllDataSourceResponse.Schemas.Length"); i++) {
+			Schema schema = new Schema();
+			schema.setDBClusterId(_ctx.stringValue("DescribeAllDataSourceResponse.Schemas["+ i +"].DBClusterId"));
+			schema.setSchemaName(_ctx.stringValue("DescribeAllDataSourceResponse.Schemas["+ i +"].SchemaName"));
+
+			schemas.add(schema);
+		}
+		describeAllDataSourceResponse.setSchemas(schemas);
+
+		List<Table> tables = new ArrayList<Table>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeAllDataSourceResponse.Tables.Length"); i++) {
+			Table table = new Table();
+			table.setDBClusterId(_ctx.stringValue("DescribeAllDataSourceResponse.Tables["+ i +"].DBClusterId"));
+			table.setSchemaName(_ctx.stringValue("DescribeAllDataSourceResponse.Tables["+ i +"].SchemaName"));
+			table.setTableName(_ctx.stringValue("DescribeAllDataSourceResponse.Tables["+ i +"].TableName"));
+
+			tables.add(table);
+		}
+		describeAllDataSourceResponse.setTables(tables);
+
+		List<Column> columns = new ArrayList<Column>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeAllDataSourceResponse.Columns.Length"); i++) {
+			Column column = new Column();
+			column.setDBClusterId(_ctx.stringValue("DescribeAllDataSourceResponse.Columns["+ i +"].DBClusterId"));
+			column.setSchemaName(_ctx.stringValue("DescribeAllDataSourceResponse.Columns["+ i +"].SchemaName"));
+			column.setTableName(_ctx.stringValue("DescribeAllDataSourceResponse.Columns["+ i +"].TableName"));
+			column.setColumnName(_ctx.stringValue("DescribeAllDataSourceResponse.Columns["+ i +"].ColumnName"));
+			column.setType(_ctx.stringValue("DescribeAllDataSourceResponse.Columns["+ i +"].Type"));
+			column.setPrimaryKey(_ctx.booleanValue("DescribeAllDataSourceResponse.Columns["+ i +"].PrimaryKey"));
+			column.setAutoIncrementColumn(_ctx.booleanValue("DescribeAllDataSourceResponse.Columns["+ i +"].AutoIncrementColumn"));
+
+			columns.add(column);
+		}
+		describeAllDataSourceResponse.setColumns(columns);
+	 
+	 	return describeAllDataSourceResponse;
+	}
+}
