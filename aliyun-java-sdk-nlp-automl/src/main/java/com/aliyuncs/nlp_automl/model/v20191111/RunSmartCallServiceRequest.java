cc4d885d5760f0b184a89785ffc399ba07de5d8e
sdk-team <sdk-team@alibabacloud.com>
2020-05-26 17:42:27 +0800

    Add smartcall service api.

---
+++ aliyun-java-sdk-nlp-automl/src/main/java/com/aliyuncs/nlp_automl/model/v20191111/RunSmartCallServiceRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.nlp_automl.model.v20191111;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.nlp_automl.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class RunSmartCallServiceRequest extends RpcAcsRequest<RunSmartCallServiceResponse> {
+	   
+
+	private String sessionId;
+
+	private Long robotId;
+
+	private String paramContent;
+
+	private String serviceName;
+	public RunSmartCallServiceRequest() {
+		super("nlp-automl", "2019-11-11", "RunSmartCallService", "nlpautoml");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getSessionId() {
+		return this.sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+		if(sessionId != null){
+			putBodyParameter("SessionId", sessionId);
+		}
+	}
+
+	public Long getRobotId() {
+		return this.robotId;
+	}
+
+	public void setRobotId(Long robotId) {
+		this.robotId = robotId;
+		if(robotId != null){
+			putBodyParameter("RobotId", robotId.toString());
+		}
+	}
+
+	public String getParamContent() {
+		return this.paramContent;
+	}
+
+	public void setParamContent(String paramContent) {
+		this.paramContent = paramContent;
+		if(paramContent != null){
+			putBodyParameter("ParamContent", paramContent);
+		}
+	}
+
+	public String getServiceName() {
+		return this.serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+		if(serviceName != null){
+			putBodyParameter("ServiceName", serviceName);
+		}
+	}
+
+	@Override
+	public Class<RunSmartCallServiceResponse> getResponseClass() {
+		return RunSmartCallServiceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-nlp-automl/src/main/java/com/aliyuncs/nlp_automl/transform/v20191111/RunSmartCallServiceResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.nlp_automl.transform.v20191111;
+
+import com.aliyuncs.nlp_automl.model.v20191111.RunSmartCallServiceResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class RunSmartCallServiceResponseUnmarshaller {
+
+	public static RunSmartCallServiceResponse unmarshall(RunSmartCallServiceResponse runSmartCallServiceResponse, UnmarshallerContext _ctx) {
+		
+		runSmartCallServiceResponse.setRequestId(_ctx.stringValue("RunSmartCallServiceResponse.RequestId"));
+		runSmartCallServiceResponse.setCode(_ctx.integerValue("RunSmartCallServiceResponse.Code"));
+		runSmartCallServiceResponse.setMessage(_ctx.stringValue("RunSmartCallServiceResponse.Message"));
+		runSmartCallServiceResponse.setData(_ctx.stringValue("RunSmartCallServiceResponse.Data"));
+	 
+	 	return runSmartCallServiceResponse;
+	}
+}
