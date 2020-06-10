d677019f3b53529a91a553da2186ebf9c7c79d8c
sdk-team <sdk-team@alibabacloud.com>
2020-06-10 22:15:02 +0800

    Public beta version.

---
+++ aliyun-java-sdk-aliyuncvc/src/main/java/com/aliyuncs/aliyuncvc/model/v20191030/UpdateGonggeLayoutRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.aliyuncvc.model.v20191030;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.aliyuncvc.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateGonggeLayoutRequest extends RpcAcsRequest<UpdateGonggeLayoutResponse> {
+	   
+
+	private String meetingUUID;
+
+	private String videoCount;
+
+	private String value;
+	public UpdateGonggeLayoutRequest() {
+		super("aliyuncvc", "2019-10-30", "UpdateGonggeLayout", "aliyuncvc");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getMeetingUUID() {
+		return this.meetingUUID;
+	}
+
+	public void setMeetingUUID(String meetingUUID) {
+		this.meetingUUID = meetingUUID;
+		if(meetingUUID != null){
+			putBodyParameter("MeetingUUID", meetingUUID);
+		}
+	}
+
+	public String getVideoCount() {
+		return this.videoCount;
+	}
+
+	public void setVideoCount(String videoCount) {
+		this.videoCount = videoCount;
+		if(videoCount != null){
+			putBodyParameter("VideoCount", videoCount);
+		}
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+		if(value != null){
+			putBodyParameter("Value", value);
+		}
+	}
+
+	@Override
+	public Class<UpdateGonggeLayoutResponse> getResponseClass() {
+		return UpdateGonggeLayoutResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-aliyuncvc/src/main/java/com/aliyuncs/aliyuncvc/transform/v20191030/UpdateGonggeLayoutResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.aliyuncvc.transform.v20191030;
+
+import com.aliyuncs.aliyuncvc.model.v20191030.UpdateGonggeLayoutResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UpdateGonggeLayoutResponseUnmarshaller {
+
+	public static UpdateGonggeLayoutResponse unmarshall(UpdateGonggeLayoutResponse updateGonggeLayoutResponse, UnmarshallerContext _ctx) {
+		
+		updateGonggeLayoutResponse.setRequestId(_ctx.stringValue("UpdateGonggeLayoutResponse.RequestId"));
+		updateGonggeLayoutResponse.setErrorCode(_ctx.integerValue("UpdateGonggeLayoutResponse.ErrorCode"));
+		updateGonggeLayoutResponse.setMessage(_ctx.stringValue("UpdateGonggeLayoutResponse.Message"));
+		updateGonggeLayoutResponse.setSuccess(_ctx.booleanValue("UpdateGonggeLayoutResponse.Success"));
+	 
+	 	return updateGonggeLayoutResponse;
+	}
+}
