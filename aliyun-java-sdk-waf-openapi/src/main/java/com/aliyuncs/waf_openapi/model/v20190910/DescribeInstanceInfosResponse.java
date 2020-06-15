40d9de0e0bc3acb9728e04ae0de671376d929013
sdk-team <sdk-team@alibabacloud.com>
2020-06-15 12:25:19 +0800

    Generated 2019-09-10 for waf-openapi.

---
+++ aliyun-java-sdk-waf-openapi/src/main/java/com/aliyuncs/waf_openapi/model/v20190910/DescribeInstanceInfosResponse.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.waf_openapi.model.v20190910;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.waf_openapi.transform.v20190910.DescribeInstanceInfosResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeInstanceInfosResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<InstanceInfo> instanceInfos;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<InstanceInfo> getInstanceInfos() {
+		return this.instanceInfos;
+	}
+
+	public void setInstanceInfos(List<InstanceInfo> instanceInfos) {
+		this.instanceInfos = instanceInfos;
+	}
+
+	public static class InstanceInfo {
+
+		private Integer inDebt;
+
+		private String instanceId;
+
+		private Integer payType;
+
+		private Long endDate;
+
+		private Integer remainDay;
+
+		private String region;
+
+		private Integer trial;
+
+		private Integer status;
+
+		private String subscriptionType;
+
+		public Integer getInDebt() {
+			return this.inDebt;
+		}
+
+		public void setInDebt(Integer inDebt) {
+			this.inDebt = inDebt;
+		}
+
+		public String getInstanceId() {
+			return this.instanceId;
+		}
+
+		public void setInstanceId(String instanceId) {
+			this.instanceId = instanceId;
+		}
+
+		public Integer getPayType() {
+			return this.payType;
+		}
+
+		public void setPayType(Integer payType) {
+			this.payType = payType;
+		}
+
+		public Long getEndDate() {
+			return this.endDate;
+		}
+
+		public void setEndDate(Long endDate) {
+			this.endDate = endDate;
+		}
+
+		public Integer getRemainDay() {
+			return this.remainDay;
+		}
+
+		public void setRemainDay(Integer remainDay) {
+			this.remainDay = remainDay;
+		}
+
+		public String getRegion() {
+			return this.region;
+		}
+
+		public void setRegion(String region) {
+			this.region = region;
+		}
+
+		public Integer getTrial() {
+			return this.trial;
+		}
+
+		public void setTrial(Integer trial) {
+			this.trial = trial;
+		}
+
+		public Integer getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(Integer status) {
+			this.status = status;
+		}
+
+		public String getSubscriptionType() {
+			return this.subscriptionType;
+		}
+
+		public void setSubscriptionType(String subscriptionType) {
+			this.subscriptionType = subscriptionType;
+		}
+	}
+
+	@Override
+	public DescribeInstanceInfosResponse getInstance(UnmarshallerContext context) {
+		return	DescribeInstanceInfosResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-waf-openapi/src/main/java/com/aliyuncs/waf_openapi/transform/v20190910/DescribeInstanceInfosResponseUnmarshaller.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.waf_openapi.transform.v20190910;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.waf_openapi.model.v20190910.DescribeInstanceInfosResponse;
+import com.aliyuncs.waf_openapi.model.v20190910.DescribeInstanceInfosResponse.InstanceInfo;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeInstanceInfosResponseUnmarshaller {
+
+	public static DescribeInstanceInfosResponse unmarshall(DescribeInstanceInfosResponse describeInstanceInfosResponse, UnmarshallerContext _ctx) {
+		
+		describeInstanceInfosResponse.setRequestId(_ctx.stringValue("DescribeInstanceInfosResponse.RequestId"));
+
+		List<InstanceInfo> instanceInfos = new ArrayList<InstanceInfo>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeInstanceInfosResponse.InstanceInfos.Length"); i++) {
+			InstanceInfo instanceInfo = new InstanceInfo();
+			instanceInfo.setInDebt(_ctx.integerValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].InDebt"));
+			instanceInfo.setInstanceId(_ctx.stringValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].InstanceId"));
+			instanceInfo.setPayType(_ctx.integerValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].PayType"));
+			instanceInfo.setEndDate(_ctx.longValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].EndDate"));
+			instanceInfo.setRemainDay(_ctx.integerValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].RemainDay"));
+			instanceInfo.setRegion(_ctx.stringValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].Region"));
+			instanceInfo.setTrial(_ctx.integerValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].Trial"));
+			instanceInfo.setStatus(_ctx.integerValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].Status"));
+			instanceInfo.setSubscriptionType(_ctx.stringValue("DescribeInstanceInfosResponse.InstanceInfos["+ i +"].SubscriptionType"));
+
+			instanceInfos.add(instanceInfo);
+		}
+		describeInstanceInfosResponse.setInstanceInfos(instanceInfos);
+	 
+	 	return describeInstanceInfosResponse;
+	}
+}
