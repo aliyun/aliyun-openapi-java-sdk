e5b856c4202003336f44009011a6272e54934f1e
sdk-team <sdk-team@alibabacloud.com>
2020-07-02 21:33:45 +0800

    Support CreateAgent API.

---
+++ aliyun-java-sdk-scsp/src/main/java/com/aliyuncs/scsp/model/v20200702/GetAgentResponse.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.scsp.model.v20200702;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.scsp.transform.v20200702.GetAgentResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetAgentResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean success;
+
+	private String code;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Long tenantId;
+
+		private Long agentId;
+
+		private String accountName;
+
+		private String displayName;
+
+		private Integer status;
+
+		public Long getTenantId() {
+			return this.tenantId;
+		}
+
+		public void setTenantId(Long tenantId) {
+			this.tenantId = tenantId;
+		}
+
+		public Long getAgentId() {
+			return this.agentId;
+		}
+
+		public void setAgentId(Long agentId) {
+			this.agentId = agentId;
+		}
+
+		public String getAccountName() {
+			return this.accountName;
+		}
+
+		public void setAccountName(String accountName) {
+			this.accountName = accountName;
+		}
+
+		public String getDisplayName() {
+			return this.displayName;
+		}
+
+		public void setDisplayName(String displayName) {
+			this.displayName = displayName;
+		}
+
+		public Integer getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(Integer status) {
+			this.status = status;
+		}
+	}
+
+	@Override
+	public GetAgentResponse getInstance(UnmarshallerContext context) {
+		return	GetAgentResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-scsp/src/main/java/com/aliyuncs/scsp/transform/v20200702/GetAgentResponseUnmarshaller.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.scsp.transform.v20200702;
+
+import com.aliyuncs.scsp.model.v20200702.GetAgentResponse;
+import com.aliyuncs.scsp.model.v20200702.GetAgentResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetAgentResponseUnmarshaller {
+
+	public static GetAgentResponse unmarshall(GetAgentResponse getAgentResponse, UnmarshallerContext _ctx) {
+		
+		getAgentResponse.setRequestId(_ctx.stringValue("GetAgentResponse.RequestId"));
+		getAgentResponse.setSuccess(_ctx.booleanValue("GetAgentResponse.Success"));
+		getAgentResponse.setCode(_ctx.stringValue("GetAgentResponse.Code"));
+
+		Data data = new Data();
+		data.setTenantId(_ctx.longValue("GetAgentResponse.Data.TenantId"));
+		data.setAgentId(_ctx.longValue("GetAgentResponse.Data.AgentId"));
+		data.setAccountName(_ctx.stringValue("GetAgentResponse.Data.AccountName"));
+		data.setDisplayName(_ctx.stringValue("GetAgentResponse.Data.DisplayName"));
+		data.setStatus(_ctx.integerValue("GetAgentResponse.Data.Status"));
+		getAgentResponse.setData(data);
+	 
+	 	return getAgentResponse;
+	}
+}
