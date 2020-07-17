b96d599bc1a21c1c92f17410232a90f56648d18d
sdk-team <sdk-team@alibabacloud.com>
2020-07-17 18:47:42 +0800

    Add DescribeRegions.

---
+++ aliyun-java-sdk-dbs/src/main/java/com/aliyuncs/dbs/model/v20190306/DescribeRegionsResponse.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dbs.model.v20190306;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.dbs.transform.v20190306.DescribeRegionsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeRegionsResponse extends AcsResponse {
+
+	private Boolean success;
+
+	private String errCode;
+
+	private String errMessage;
+
+	private Integer httpStatusCode;
+
+	private String requestId;
+
+	private List<String> regions;
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getErrCode() {
+		return this.errCode;
+	}
+
+	public void setErrCode(String errCode) {
+		this.errCode = errCode;
+	}
+
+	public String getErrMessage() {
+		return this.errMessage;
+	}
+
+	public void setErrMessage(String errMessage) {
+		this.errMessage = errMessage;
+	}
+
+	public Integer getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(Integer httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<String> getRegions() {
+		return this.regions;
+	}
+
+	public void setRegions(List<String> regions) {
+		this.regions = regions;
+	}
+
+	@Override
+	public DescribeRegionsResponse getInstance(UnmarshallerContext context) {
+		return	DescribeRegionsResponseUnmarshaller.unmarshall(this, context);
+	}
+}
