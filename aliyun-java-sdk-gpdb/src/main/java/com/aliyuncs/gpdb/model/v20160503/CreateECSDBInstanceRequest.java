79250742a9433447e4b90a495d41ba63bf8120af
sdk-team <sdk-team@alibabacloud.com>
2020-07-31 12:15:02 +0800

    Support create ecs instance.

---
+++ aliyun-java-sdk-gpdb/src/main/java/com/aliyuncs/gpdb/model/v20160503/CreateECSDBInstanceRequest.java
@@ -0,0 +1,275 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.gpdb.model.v20160503;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.gpdb.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateECSDBInstanceRequest extends RpcAcsRequest<CreateECSDBInstanceResponse> {
+	   
+
+	private String clientToken;
+
+	private String instanceSpec;
+
+	private Integer storageSize;
+
+	private String segStorageType;
+
+	private String engineVersion;
+
+	private Integer segNodeNum;
+
+	private String engine;
+
+	private String dBInstanceDescription;
+
+	private String period;
+
+	private Long ownerId;
+
+	private String usedTime;
+
+	private String securityIPList;
+
+	private String vSwitchId;
+
+	private String privateIpAddress;
+
+	private String vPCId;
+
+	private String zoneId;
+
+	private String payType;
+
+	private String instanceNetworkType;
+	public CreateECSDBInstanceRequest() {
+		super("gpdb", "2016-05-03", "CreateECSDBInstance", "gpdb");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getInstanceSpec() {
+		return this.instanceSpec;
+	}
+
+	public void setInstanceSpec(String instanceSpec) {
+		this.instanceSpec = instanceSpec;
+		if(instanceSpec != null){
+			putQueryParameter("InstanceSpec", instanceSpec);
+		}
+	}
+
+	public Integer getStorageSize() {
+		return this.storageSize;
+	}
+
+	public void setStorageSize(Integer storageSize) {
+		this.storageSize = storageSize;
+		if(storageSize != null){
+			putQueryParameter("StorageSize", storageSize.toString());
+		}
+	}
+
+	public String getSegStorageType() {
+		return this.segStorageType;
+	}
+
+	public void setSegStorageType(String segStorageType) {
+		this.segStorageType = segStorageType;
+		if(segStorageType != null){
+			putQueryParameter("SegStorageType", segStorageType);
+		}
+	}
+
+	public String getEngineVersion() {
+		return this.engineVersion;
+	}
+
+	public void setEngineVersion(String engineVersion) {
+		this.engineVersion = engineVersion;
+		if(engineVersion != null){
+			putQueryParameter("EngineVersion", engineVersion);
+		}
+	}
+
+	public Integer getSegNodeNum() {
+		return this.segNodeNum;
+	}
+
+	public void setSegNodeNum(Integer segNodeNum) {
+		this.segNodeNum = segNodeNum;
+		if(segNodeNum != null){
+			putQueryParameter("SegNodeNum", segNodeNum.toString());
+		}
+	}
+
+	public String getEngine() {
+		return this.engine;
+	}
+
+	public void setEngine(String engine) {
+		this.engine = engine;
+		if(engine != null){
+			putQueryParameter("Engine", engine);
+		}
+	}
+
+	public String getDBInstanceDescription() {
+		return this.dBInstanceDescription;
+	}
+
+	public void setDBInstanceDescription(String dBInstanceDescription) {
+		this.dBInstanceDescription = dBInstanceDescription;
+		if(dBInstanceDescription != null){
+			putQueryParameter("DBInstanceDescription", dBInstanceDescription);
+		}
+	}
+
+	public String getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+		if(period != null){
+			putQueryParameter("Period", period);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getUsedTime() {
+		return this.usedTime;
+	}
+
+	public void setUsedTime(String usedTime) {
+		this.usedTime = usedTime;
+		if(usedTime != null){
+			putQueryParameter("UsedTime", usedTime);
+		}
+	}
+
+	public String getSecurityIPList() {
+		return this.securityIPList;
+	}
+
+	public void setSecurityIPList(String securityIPList) {
+		this.securityIPList = securityIPList;
+		if(securityIPList != null){
+			putQueryParameter("SecurityIPList", securityIPList);
+		}
+	}
+
+	public String getVSwitchId() {
+		return this.vSwitchId;
+	}
+
+	public void setVSwitchId(String vSwitchId) {
+		this.vSwitchId = vSwitchId;
+		if(vSwitchId != null){
+			putQueryParameter("VSwitchId", vSwitchId);
+		}
+	}
+
+	public String getPrivateIpAddress() {
+		return this.privateIpAddress;
+	}
+
+	public void setPrivateIpAddress(String privateIpAddress) {
+		this.privateIpAddress = privateIpAddress;
+		if(privateIpAddress != null){
+			putQueryParameter("PrivateIpAddress", privateIpAddress);
+		}
+	}
+
+	public String getVPCId() {
+		return this.vPCId;
+	}
+
+	public void setVPCId(String vPCId) {
+		this.vPCId = vPCId;
+		if(vPCId != null){
+			putQueryParameter("VPCId", vPCId);
+		}
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+		if(zoneId != null){
+			putQueryParameter("ZoneId", zoneId);
+		}
+	}
+
+	public String getPayType() {
+		return this.payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+		if(payType != null){
+			putQueryParameter("PayType", payType);
+		}
+	}
+
+	public String getInstanceNetworkType() {
+		return this.instanceNetworkType;
+	}
+
+	public void setInstanceNetworkType(String instanceNetworkType) {
+		this.instanceNetworkType = instanceNetworkType;
+		if(instanceNetworkType != null){
+			putQueryParameter("InstanceNetworkType", instanceNetworkType);
+		}
+	}
+
+	@Override
+	public Class<CreateECSDBInstanceResponse> getResponseClass() {
+		return CreateECSDBInstanceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-gpdb/src/main/java/com/aliyuncs/gpdb/model/v20160503/CreateECSDBInstanceResponse.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.gpdb.model.v20160503;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.gpdb.transform.v20160503.CreateECSDBInstanceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateECSDBInstanceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String dBInstanceId;
+
+	private String orderId;
+
+	private String connectionString;
+
+	private String port;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getDBInstanceId() {
+		return this.dBInstanceId;
+	}
+
+	public void setDBInstanceId(String dBInstanceId) {
+		this.dBInstanceId = dBInstanceId;
+	}
+
+	public String getOrderId() {
+		return this.orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getConnectionString() {
+		return this.connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+
+	public String getPort() {
+		return this.port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	@Override
+	public CreateECSDBInstanceResponse getInstance(UnmarshallerContext context) {
+		return	CreateECSDBInstanceResponseUnmarshaller.unmarshall(this, context);
+	}
+}
