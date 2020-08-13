73f69e60a42a6eda5f36d31a0044bd1a94fa169a
sdk-team <sdk-team@alibabacloud.com>
2020-08-13 18:33:35 +0800

    Support ListHotlineRecord.

---
+++ aliyun-java-sdk-scsp/src/main/java/com/aliyuncs/scsp/model/v20200702/ListHotlineRecordRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.scsp.model.v20200702;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListHotlineRecordRequest extends RpcAcsRequest<ListHotlineRecordResponse> {
+	   
+
+	private String clientToken;
+
+	private String instanceId;
+
+	private String callId;
+	public ListHotlineRecordRequest() {
+		super("scsp", "2020-07-02", "ListHotlineRecord", "scsp");
+		setMethod(MethodType.GET);
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putQueryParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getCallId() {
+		return this.callId;
+	}
+
+	public void setCallId(String callId) {
+		this.callId = callId;
+		if(callId != null){
+			putQueryParameter("CallId", callId);
+		}
+	}
+
+	@Override
+	public Class<ListHotlineRecordResponse> getResponseClass() {
+		return ListHotlineRecordResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-scsp/src/main/java/com/aliyuncs/scsp/model/v20200702/ListHotlineRecordResponse.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.scsp.model.v20200702;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.scsp.transform.v20200702.ListHotlineRecordResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListHotlineRecordResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String success;
+
+	private String code;
+
+	private String message;
+
+	private List<DataItem> data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<DataItem> getData() {
+		return this.data;
+	}
+
+	public void setData(List<DataItem> data) {
+		this.data = data;
+	}
+
+	public static class DataItem {
+
+		private String callId;
+
+		private String connectionId;
+
+		private String url;
+
+		private String startTime;
+
+		private String endTime;
+
+		public String getCallId() {
+			return this.callId;
+		}
+
+		public void setCallId(String callId) {
+			this.callId = callId;
+		}
+
+		public String getConnectionId() {
+			return this.connectionId;
+		}
+
+		public void setConnectionId(String connectionId) {
+			this.connectionId = connectionId;
+		}
+
+		public String getUrl() {
+			return this.url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public String getStartTime() {
+			return this.startTime;
+		}
+
+		public void setStartTime(String startTime) {
+			this.startTime = startTime;
+		}
+
+		public String getEndTime() {
+			return this.endTime;
+		}
+
+		public void setEndTime(String endTime) {
+			this.endTime = endTime;
+		}
+	}
+
+	@Override
+	public ListHotlineRecordResponse getInstance(UnmarshallerContext context) {
+		return	ListHotlineRecordResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
