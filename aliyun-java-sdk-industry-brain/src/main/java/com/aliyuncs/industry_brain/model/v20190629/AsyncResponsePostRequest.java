75db506c2a2e95603edb7641979e4a7913e58fc1
sdk-team <sdk-team@alibabacloud.com>
2020-08-28 12:01:38 +0800

    Supported Add GetAlgorithmHistoryResult.

---
+++ aliyun-java-sdk-industry-brain/src/main/java/com/aliyuncs/industry_brain/model/v20190629/AsyncResponsePostRequest.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.industry_brain.model.v20190629;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class AsyncResponsePostRequest extends RpcAcsRequest<AsyncResponsePostResponse> {
+	   
+
+	private String data;
+
+	private String context;
+
+	private Float progress;
+
+	private String message;
+
+	private String taskId;
+
+	private String status;
+	public AsyncResponsePostRequest() {
+		super("industry-brain", "2019-06-29", "AsyncResponsePost");
+		setMethod(MethodType.POST);
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+		if(data != null){
+			putQueryParameter("Data", data);
+		}
+	}
+
+	public String getContext() {
+		return this.context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+		if(context != null){
+			putQueryParameter("Context", context);
+		}
+	}
+
+	public Float getProgress() {
+		return this.progress;
+	}
+
+	public void setProgress(Float progress) {
+		this.progress = progress;
+		if(progress != null){
+			putQueryParameter("Progress", progress.toString());
+		}
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+		if(message != null){
+			putQueryParameter("Message", message);
+		}
+	}
+
+	public String getTaskId() {
+		return this.taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+		if(taskId != null){
+			putQueryParameter("TaskId", taskId);
+		}
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+		if(status != null){
+			putQueryParameter("Status", status);
+		}
+	}
+
+	@Override
+	public Class<AsyncResponsePostResponse> getResponseClass() {
+		return AsyncResponsePostResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-industry-brain/src/main/java/com/aliyuncs/industry_brain/transform/v20190629/AsyncResponsePostResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.industry_brain.transform.v20190629;
+
+import com.aliyuncs.industry_brain.model.v20190629.AsyncResponsePostResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class AsyncResponsePostResponseUnmarshaller {
+
+	public static AsyncResponsePostResponse unmarshall(AsyncResponsePostResponse asyncResponsePostResponse, UnmarshallerContext _ctx) {
+		
+		asyncResponsePostResponse.setRequestId(_ctx.stringValue("AsyncResponsePostResponse.RequestId"));
+		asyncResponsePostResponse.setData(_ctx.stringValue("AsyncResponsePostResponse.Data"));
+		asyncResponsePostResponse.setStatus(_ctx.stringValue("AsyncResponsePostResponse.Status"));
+		asyncResponsePostResponse.setCode(_ctx.stringValue("AsyncResponsePostResponse.Code"));
+		asyncResponsePostResponse.setMessage(_ctx.stringValue("AsyncResponsePostResponse.Message"));
+	 
+	 	return asyncResponsePostResponse;
+	}
+}
