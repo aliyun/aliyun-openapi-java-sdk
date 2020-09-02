0eabe0436a6f68540b4ba339f208d3b41d5d2e32
sdk-team <sdk-team@alibabacloud.com>
2020-09-02 15:46:57 +0800

    Generated 2017-11-11 for Dyiotapi.

---
+++ aliyun-java-sdk-dyiotapi/src/main/java/com/aliyuncs/dyiotapi/model/v20171111/QueryCardsInfoResponse.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dyiotapi.model.v20171111;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.dyiotapi.transform.v20171111.QueryCardsInfoResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryCardsInfoResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String code;
+
+	private String message;
+
+	private List<String> cardsInfo;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getCardsInfo() {
+		return this.cardsInfo;
+	}
+
+	public void setCardsInfo(List<String> cardsInfo) {
+		this.cardsInfo = cardsInfo;
+	}
+
+	@Override
+	public QueryCardsInfoResponse getInstance(UnmarshallerContext context) {
+		return	QueryCardsInfoResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-dyiotapi/src/main/java/com/aliyuncs/dyiotapi/transform/v20171111/QueryCardsInfoResponseUnmarshaller.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dyiotapi.transform.v20171111;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.dyiotapi.model.v20171111.QueryCardsInfoResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryCardsInfoResponseUnmarshaller {
+
+	public static QueryCardsInfoResponse unmarshall(QueryCardsInfoResponse queryCardsInfoResponse, UnmarshallerContext _ctx) {
+		
+		queryCardsInfoResponse.setRequestId(_ctx.stringValue("QueryCardsInfoResponse.RequestId"));
+		queryCardsInfoResponse.setCode(_ctx.stringValue("QueryCardsInfoResponse.Code"));
+		queryCardsInfoResponse.setMessage(_ctx.stringValue("QueryCardsInfoResponse.Message"));
+
+		List<String> cardsInfo = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("QueryCardsInfoResponse.CardsInfo.Length"); i++) {
+			cardsInfo.add(_ctx.stringValue("QueryCardsInfoResponse.CardsInfo["+ i +"]"));
+		}
+		queryCardsInfoResponse.setCardsInfo(cardsInfo);
+	 
+	 	return queryCardsInfoResponse;
+	}
+}
