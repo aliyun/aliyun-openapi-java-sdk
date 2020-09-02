70a0f6bb89b0ed21ec32215c97a57c28f8d19224
sdk-team <sdk-team@alibabacloud.com>
2020-09-02 10:12:42 +0800

    Supported AndroidNotificationHuaweiChannel for Push and MassPush.

---
+++ aliyun-java-sdk-push/src/main/java/com/aliyuncs/push/model/v20160801/QueryDeviceCountRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.push.model.v20160801;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.push.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryDeviceCountRequest extends RpcAcsRequest<QueryDeviceCountResponse> {
+	   
+
+	private String target;
+
+	private Long appKey;
+
+	private String targetValue;
+	public QueryDeviceCountRequest() {
+		super("Push", "2016-08-01", "QueryDeviceCount");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getTarget() {
+		return this.target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+		if(target != null){
+			putQueryParameter("Target", target);
+		}
+	}
+
+	public Long getAppKey() {
+		return this.appKey;
+	}
+
+	public void setAppKey(Long appKey) {
+		this.appKey = appKey;
+		if(appKey != null){
+			putQueryParameter("AppKey", appKey.toString());
+		}
+	}
+
+	public String getTargetValue() {
+		return this.targetValue;
+	}
+
+	public void setTargetValue(String targetValue) {
+		this.targetValue = targetValue;
+		if(targetValue != null){
+			putQueryParameter("TargetValue", targetValue);
+		}
+	}
+
+	@Override
+	public Class<QueryDeviceCountResponse> getResponseClass() {
+		return QueryDeviceCountResponse.class;
+	}
+
+}
