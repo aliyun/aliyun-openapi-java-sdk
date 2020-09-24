e22ea7e420de78b268fb18eeeb86a6a94b3d9820
sdk-team <sdk-team@alibabacloud.com>
2020-09-24 13:42:07 +0000

    Support create for serverlesshbase.

---
+++ aliyun-java-sdk-hbase/src/main/java/com/aliyuncs/hbase/model/v20190101/CreateServerlessClusterRequest.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.hbase.model.v20190101;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.hbase.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateServerlessClusterRequest extends RpcAcsRequest<CreateServerlessClusterResponse> {
+	   
+
+	private String clusterName;
+
+	private String clientToken;
+
+	private String engineVersion;
+
+	private String engine;
+
+	private Integer autoRenewPeriod;
+
+	private Integer period;
+
+	private String vSwitchId;
+
+	private String periodUnit;
+
+	private Integer serverlessCapability;
+
+	private String vpcId;
+
+	private String zoneId;
+
+	private Integer serverlessStorage;
+
+	private String payType;
+
+	private String serverlessSpec;
+	public CreateServerlessClusterRequest() {
+		super("HBase", "2019-01-01", "CreateServerlessCluster", "hbase");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getClusterName() {
+		return this.clusterName;
+	}
+
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+		if(clusterName != null){
+			putQueryParameter("ClusterName", clusterName);
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getEngineVersion() {
+		return this.engineVersion;
+	}
+
+	public void setEngineVersion(String engineVersion) {
+		this.engineVersion = engineVersion;
+		if(engineVersion != null){
+			putQueryParameter("EngineVersion", engineVersion);
+		}
+	}
+
+	public String getEngine() {
+		return this.engine;
+	}
+
+	public void setEngine(String engine) {
+		this.engine = engine;
+		if(engine != null){
+			putQueryParameter("Engine", engine);
+		}
+	}
+
+	public Integer getAutoRenewPeriod() {
+		return this.autoRenewPeriod;
+	}
+
+	public void setAutoRenewPeriod(Integer autoRenewPeriod) {
+		this.autoRenewPeriod = autoRenewPeriod;
+		if(autoRenewPeriod != null){
+			putQueryParameter("AutoRenewPeriod", autoRenewPeriod.toString());
+		}
+	}
+
+	public Integer getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(Integer period) {
+		this.period = period;
+		if(period != null){
+			putQueryParameter("Period", period.toString());
+		}
+	}
+
+	public String getVSwitchId() {
+		return this.vSwitchId;
+	}
+
+	public void setVSwitchId(String vSwitchId) {
+		this.vSwitchId = vSwitchId;
+		if(vSwitchId != null){
+			putQueryParameter("VSwitchId", vSwitchId);
+		}
+	}
+
+	public String getPeriodUnit() {
+		return this.periodUnit;
+	}
+
+	public void setPeriodUnit(String periodUnit) {
+		this.periodUnit = periodUnit;
+		if(periodUnit != null){
+			putQueryParameter("PeriodUnit", periodUnit);
+		}
+	}
+
+	public Integer getServerlessCapability() {
+		return this.serverlessCapability;
+	}
+
+	public void setServerlessCapability(Integer serverlessCapability) {
+		this.serverlessCapability = serverlessCapability;
+		if(serverlessCapability != null){
+			putQueryParameter("ServerlessCapability", serverlessCapability.toString());
+		}
+	}
+
+	public String getVpcId() {
+		return this.vpcId;
+	}
+
+	public void setVpcId(String vpcId) {
+		this.vpcId = vpcId;
+		if(vpcId != null){
+			putQueryParameter("VpcId", vpcId);
+		}
+	}
+
+	public String getZoneId() {
+		return this.zoneId;
+	}
+
+	public void setZoneId(String zoneId) {
+		this.zoneId = zoneId;
+		if(zoneId != null){
+			putQueryParameter("ZoneId", zoneId);
+		}
+	}
+
+	public Integer getServerlessStorage() {
+		return this.serverlessStorage;
+	}
+
+	public void setServerlessStorage(Integer serverlessStorage) {
+		this.serverlessStorage = serverlessStorage;
+		if(serverlessStorage != null){
+			putQueryParameter("ServerlessStorage", serverlessStorage.toString());
+		}
+	}
+
+	public String getPayType() {
+		return this.payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+		if(payType != null){
+			putQueryParameter("PayType", payType);
+		}
+	}
+
+	public String getServerlessSpec() {
+		return this.serverlessSpec;
+	}
+
+	public void setServerlessSpec(String serverlessSpec) {
+		this.serverlessSpec = serverlessSpec;
+		if(serverlessSpec != null){
+			putQueryParameter("ServerlessSpec", serverlessSpec);
+		}
+	}
+
+	@Override
+	public Class<CreateServerlessClusterResponse> getResponseClass() {
+		return CreateServerlessClusterResponse.class;
+	}
+
+}
