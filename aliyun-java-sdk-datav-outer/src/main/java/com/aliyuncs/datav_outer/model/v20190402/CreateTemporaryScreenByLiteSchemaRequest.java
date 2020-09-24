0e075b0645c614fd31f48f0dbe2001da1f26a33d
sdk-team <sdk-team@alibabacloud.com>
2020-09-24 13:30:00 +0000

    Generated 2019-04-02 for datav-outer.

---
+++ aliyun-java-sdk-datav-outer/src/main/java/com/aliyuncs/datav_outer/model/v20190402/CreateTemporaryScreenByLiteSchemaRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.datav_outer.model.v20190402;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.datav_outer.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateTemporaryScreenByLiteSchemaRequest extends RpcAcsRequest<CreateTemporaryScreenByLiteSchemaResponse> {
+	   
+
+	private String schema;
+
+	private String product;
+
+	private String domain;
+
+	private String version;
+	public CreateTemporaryScreenByLiteSchemaRequest() {
+		super("datav-outer", "2019-04-02", "CreateTemporaryScreenByLiteSchema", "datav");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getSchema() {
+		return this.schema;
+	}
+
+	public void setSchema(String schema) {
+		this.schema = schema;
+		if(schema != null){
+			putBodyParameter("Schema", schema);
+		}
+	}
+
+	public String getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+		if(product != null){
+			putBodyParameter("Product", product);
+		}
+	}
+
+	public String getDomain() {
+		return this.domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+		if(domain != null){
+			putBodyParameter("Domain", domain);
+		}
+	}
+
+	public String getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+		if(version != null){
+			putBodyParameter("Version", version);
+		}
+	}
+
+	@Override
+	public Class<CreateTemporaryScreenByLiteSchemaResponse> getResponseClass() {
+		return CreateTemporaryScreenByLiteSchemaResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-datav-outer/src/main/java/com/aliyuncs/datav_outer/model/v20190402/CreateTemporaryScreenByLiteSchemaResponse.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.datav_outer.model.v20190402;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.datav_outer.transform.v20190402.CreateTemporaryScreenByLiteSchemaResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateTemporaryScreenByLiteSchemaResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String url;
+
+	private String cssSelector;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getCssSelector() {
+		return this.cssSelector;
+	}
+
+	public void setCssSelector(String cssSelector) {
+		this.cssSelector = cssSelector;
+	}
+
+	@Override
+	public CreateTemporaryScreenByLiteSchemaResponse getInstance(UnmarshallerContext context) {
+		return	CreateTemporaryScreenByLiteSchemaResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
