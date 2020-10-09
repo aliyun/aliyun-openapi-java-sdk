3e1f94107ce8673c06868944da4d798953004789
sdk-team <sdk-team@alibabacloud.com>
2020-10-09 06:48:33 +0000

    Update DescribeCdnUserBillHistory API.

---
+++ aliyun-java-sdk-cdn/src/main/java/com/aliyuncs/cdn/model/v20180510/DescribeCdnUserBillHistoryResponse.java
@@ -0,0 +1,148 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cdn.model.v20180510;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.cdn.transform.v20180510.DescribeCdnUserBillHistoryResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeCdnUserBillHistoryResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<BillHistoryDataItem> billHistoryData;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<BillHistoryDataItem> getBillHistoryData() {
+		return this.billHistoryData;
+	}
+
+	public void setBillHistoryData(List<BillHistoryDataItem> billHistoryData) {
+		this.billHistoryData = billHistoryData;
+	}
+
+	public static class BillHistoryDataItem {
+
+		private String dimension;
+
+		private String billType;
+
+		private String billTime;
+
+		private List<BillingDataItem> billingData;
+
+		public String getDimension() {
+			return this.dimension;
+		}
+
+		public void setDimension(String dimension) {
+			this.dimension = dimension;
+		}
+
+		public String getBillType() {
+			return this.billType;
+		}
+
+		public void setBillType(String billType) {
+			this.billType = billType;
+		}
+
+		public String getBillTime() {
+			return this.billTime;
+		}
+
+		public void setBillTime(String billTime) {
+			this.billTime = billTime;
+		}
+
+		public List<BillingDataItem> getBillingData() {
+			return this.billingData;
+		}
+
+		public void setBillingData(List<BillingDataItem> billingData) {
+			this.billingData = billingData;
+		}
+
+		public static class BillingDataItem {
+
+			private String chargeType;
+
+			private String cdnRegion;
+
+			private Float bandwidth;
+
+			private Float flow;
+
+			private Float count;
+
+			public String getChargeType() {
+				return this.chargeType;
+			}
+
+			public void setChargeType(String chargeType) {
+				this.chargeType = chargeType;
+			}
+
+			public String getCdnRegion() {
+				return this.cdnRegion;
+			}
+
+			public void setCdnRegion(String cdnRegion) {
+				this.cdnRegion = cdnRegion;
+			}
+
+			public Float getBandwidth() {
+				return this.bandwidth;
+			}
+
+			public void setBandwidth(Float bandwidth) {
+				this.bandwidth = bandwidth;
+			}
+
+			public Float getFlow() {
+				return this.flow;
+			}
+
+			public void setFlow(Float flow) {
+				this.flow = flow;
+			}
+
+			public Float getCount() {
+				return this.count;
+			}
+
+			public void setCount(Float count) {
+				this.count = count;
+			}
+		}
+	}
+
+	@Override
+	public DescribeCdnUserBillHistoryResponse getInstance(UnmarshallerContext context) {
+		return	DescribeCdnUserBillHistoryResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-cdn/src/main/java/com/aliyuncs/cdn/transform/v20180510/DescribeCdnUserBillHistoryResponseUnmarshaller.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cdn.transform.v20180510;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.cdn.model.v20180510.DescribeCdnUserBillHistoryResponse;
+import com.aliyuncs.cdn.model.v20180510.DescribeCdnUserBillHistoryResponse.BillHistoryDataItem;
+import com.aliyuncs.cdn.model.v20180510.DescribeCdnUserBillHistoryResponse.BillHistoryDataItem.BillingDataItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeCdnUserBillHistoryResponseUnmarshaller {
+
+	public static DescribeCdnUserBillHistoryResponse unmarshall(DescribeCdnUserBillHistoryResponse describeCdnUserBillHistoryResponse, UnmarshallerContext _ctx) {
+		
+		describeCdnUserBillHistoryResponse.setRequestId(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.RequestId"));
+
+		List<BillHistoryDataItem> billHistoryData = new ArrayList<BillHistoryDataItem>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeCdnUserBillHistoryResponse.BillHistoryData.Length"); i++) {
+			BillHistoryDataItem billHistoryDataItem = new BillHistoryDataItem();
+			billHistoryDataItem.setDimension(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].Dimension"));
+			billHistoryDataItem.setBillType(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillType"));
+			billHistoryDataItem.setBillTime(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillTime"));
+
+			List<BillingDataItem> billingData = new ArrayList<BillingDataItem>();
+			for (int j = 0; j < _ctx.lengthValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData.Length"); j++) {
+				BillingDataItem billingDataItem = new BillingDataItem();
+				billingDataItem.setChargeType(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData["+ j +"].ChargeType"));
+				billingDataItem.setCdnRegion(_ctx.stringValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData["+ j +"].CdnRegion"));
+				billingDataItem.setBandwidth(_ctx.floatValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData["+ j +"].Bandwidth"));
+				billingDataItem.setFlow(_ctx.floatValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData["+ j +"].Flow"));
+				billingDataItem.setCount(_ctx.floatValue("DescribeCdnUserBillHistoryResponse.BillHistoryData["+ i +"].BillingData["+ j +"].Count"));
+
+				billingData.add(billingDataItem);
+			}
+			billHistoryDataItem.setBillingData(billingData);
+
+			billHistoryData.add(billHistoryDataItem);
+		}
+		describeCdnUserBillHistoryResponse.setBillHistoryData(billHistoryData);
+	 
+	 	return describeCdnUserBillHistoryResponse;
+	}
+}
