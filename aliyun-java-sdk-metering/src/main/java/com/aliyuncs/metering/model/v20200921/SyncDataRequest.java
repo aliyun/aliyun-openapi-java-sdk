c7cc819b64d42d804e04d0dc046bdb8c02a32e12
sdk-team <sdk-team@alibabacloud.com>
2020-10-12 02:41:11 +0000

    Init.

---
+++ aliyun-java-sdk-metering/src/main/java/com/aliyuncs/metering/model/v20200921/SyncDataRequest.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.metering.model.v20200921;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class SyncDataRequest extends RoaAcsRequest<SyncDataResponse> {
+	   
+
+	private String instanceId;
+
+	private String data;
+
+	private String dataType;
+
+	private String endTime;
+
+	private String startTime;
+
+	private String commodityCode;
+
+	private String region;
+
+	private String userId;
+	public SyncDataRequest() {
+		super("Metering", "2020-09-21", "SyncData", "pai");
+		setUriPattern("/api/dataSync");
+		setMethod(MethodType.POST);
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putQueryParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+		if(data != null){
+			putQueryParameter("Data", data);
+		}
+	}
+
+	public String getDataType() {
+		return this.dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+		if(dataType != null){
+			putQueryParameter("DataType", dataType);
+		}
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+		if(endTime != null){
+			putQueryParameter("EndTime", endTime);
+		}
+	}
+
+	public String getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+		if(startTime != null){
+			putQueryParameter("StartTime", startTime);
+		}
+	}
+
+	public String getCommodityCode() {
+		return this.commodityCode;
+	}
+
+	public void setCommodityCode(String commodityCode) {
+		this.commodityCode = commodityCode;
+		if(commodityCode != null){
+			putQueryParameter("CommodityCode", commodityCode);
+		}
+	}
+
+	public String getRegion() {
+		return this.region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+		if(region != null){
+			putQueryParameter("Region", region);
+		}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putQueryParameter("UserId", userId);
+		}
+	}
+
+	@Override
+	public Class<SyncDataResponse> getResponseClass() {
+		return SyncDataResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-metering/src/main/java/com/aliyuncs/metering/transform/v20200921/SyncDataResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.metering.transform.v20200921;
+
+import com.aliyuncs.metering.model.v20200921.SyncDataResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SyncDataResponseUnmarshaller {
+
+	public static SyncDataResponse unmarshall(SyncDataResponse syncDataResponse, UnmarshallerContext _ctx) {
+		
+		syncDataResponse.setRequestId(_ctx.stringValue("SyncDataResponse.RequestId"));
+		syncDataResponse.setCode(_ctx.integerValue("SyncDataResponse.Code"));
+		syncDataResponse.setSuccess(_ctx.booleanValue("SyncDataResponse.Success"));
+		syncDataResponse.setData(_ctx.booleanValue("SyncDataResponse.Data"));
+		syncDataResponse.setErrCode(_ctx.integerValue("SyncDataResponse.ErrCode"));
+		syncDataResponse.setErrMessage(_ctx.stringValue("SyncDataResponse.ErrMessage"));
+	 
+	 	return syncDataResponse;
+	}
+}
