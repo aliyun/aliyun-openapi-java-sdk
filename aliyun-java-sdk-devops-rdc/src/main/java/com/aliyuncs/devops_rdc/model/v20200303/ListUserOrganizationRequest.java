320d1143a552d200015d39f844a8d6dceb599f42
sdk-team <sdk-team@alibabacloud.com>
2020-10-12 05:18:43 +0000

    Add list Organizations.

---
+++ aliyun-java-sdk-devops-rdc/src/main/java/com/aliyuncs/devops_rdc/model/v20200303/ListUserOrganizationRequest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.devops_rdc.model.v20200303;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListUserOrganizationRequest extends RpcAcsRequest<ListUserOrganizationResponse> {
+	   
+
+	private String realPk;
+	public ListUserOrganizationRequest() {
+		super("devops-rdc", "2020-03-03", "ListUserOrganization");
+		setMethod(MethodType.POST);
+	}
+
+	public String getRealPk() {
+		return this.realPk;
+	}
+
+	public void setRealPk(String realPk) {
+		this.realPk = realPk;
+		if(realPk != null){
+			putBodyParameter("RealPk", realPk);
+		}
+	}
+
+	@Override
+	public Class<ListUserOrganizationResponse> getResponseClass() {
+		return ListUserOrganizationResponse.class;
+	}
+
+}
