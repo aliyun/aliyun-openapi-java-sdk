b991b75cf5094157df26811fd8f1b4e31a23d333
sdk-team <sdk-team@alibabacloud.com>
2020-10-13 02:46:12 +0000

    Release GenerateDynamicImage.

---
+++ aliyun-java-sdk-imageenhan/src/main/java/com/aliyuncs/imageenhan/model/v20190930/GenerateDynamicImageRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageenhan.model.v20190930;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.imageenhan.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GenerateDynamicImageRequest extends RpcAcsRequest<GenerateDynamicImageResponse> {
+	   
+
+	private String url;
+
+	private String operation;
+	public GenerateDynamicImageRequest() {
+		super("imageenhan", "2019-09-30", "GenerateDynamicImage", "imageenhan");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+		if(url != null){
+			putBodyParameter("Url", url);
+		}
+	}
+
+	public String getOperation() {
+		return this.operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+		if(operation != null){
+			putBodyParameter("Operation", operation);
+		}
+	}
+
+	@Override
+	public Class<GenerateDynamicImageResponse> getResponseClass() {
+		return GenerateDynamicImageResponse.class;
+	}
+
+}
