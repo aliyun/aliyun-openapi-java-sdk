2a3e10c54d0cff28b43c460824ab7767b1c32bea
sdk-team <sdk-team@alibabacloud.com>
2020-10-16 14:25:18 +0000

    Metering.

---
+++ aliyun-java-sdk-metering/src/main/java/com/aliyuncs/metering/model/v20200921/PostDataRequest.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.metering.model.v20200921;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class PostDataRequest extends RoaAcsRequest<PostDataResponse> {
+	   
+
+	private String instanceId;
+
+	private String data;
+
+	private String dataType;
+
+	private String endTime;
+
+	private String startTime;
+
+	private String commodityCode;
+
+	private String region;
+
+	private String userId;
+	public PostDataRequest() {
+		super("Metering", "2020-09-21", "PostData", "pai");
+		setUriPattern("/api/dataPost");
+		setMethod(MethodType.POST);
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putBodyParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+		if(data != null){
+			putBodyParameter("Data", data);
+		}
+	}
+
+	public String getDataType() {
+		return this.dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+		if(dataType != null){
+			putBodyParameter("DataType", dataType);
+		}
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+		if(endTime != null){
+			putBodyParameter("EndTime", endTime);
+		}
+	}
+
+	public String getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+		if(startTime != null){
+			putBodyParameter("StartTime", startTime);
+		}
+	}
+
+	public String getCommodityCode() {
+		return this.commodityCode;
+	}
+
+	public void setCommodityCode(String commodityCode) {
+		this.commodityCode = commodityCode;
+		if(commodityCode != null){
+			putBodyParameter("CommodityCode", commodityCode);
+		}
+	}
+
+	public String getRegion() {
+		return this.region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+		if(region != null){
+			putBodyParameter("Region", region);
+		}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putBodyParameter("UserId", userId);
+		}
+	}
+
+	@Override
+	public Class<PostDataResponse> getResponseClass() {
+		return PostDataResponse.class;
+	}
+
+}
