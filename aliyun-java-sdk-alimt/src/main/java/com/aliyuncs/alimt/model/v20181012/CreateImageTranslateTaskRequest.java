ce435e5b62af3ee1ded93e8d2ad648b30ca7a0f2
sdk-team <sdk-team@alibabacloud.com>
2020-10-19 12:14:30 +0000

    Add a new interface for asynchronous image translation.

---
+++ aliyun-java-sdk-alimt/src/main/java/com/aliyuncs/alimt/model/v20181012/CreateImageTranslateTaskRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alimt.model.v20181012;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.alimt.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateImageTranslateTaskRequest extends RpcAcsRequest<CreateImageTranslateTaskResponse> {
+	   
+
+	private String sourceLanguage;
+
+	private String clientToken;
+
+	private String urlList;
+
+	private String extra;
+
+	private String targetLanguage;
+	public CreateImageTranslateTaskRequest() {
+		super("alimt", "2018-10-12", "CreateImageTranslateTask", "alimt");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getSourceLanguage() {
+		return this.sourceLanguage;
+	}
+
+	public void setSourceLanguage(String sourceLanguage) {
+		this.sourceLanguage = sourceLanguage;
+		if(sourceLanguage != null){
+			putBodyParameter("SourceLanguage", sourceLanguage);
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putBodyParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getUrlList() {
+		return this.urlList;
+	}
+
+	public void setUrlList(String urlList) {
+		this.urlList = urlList;
+		if(urlList != null){
+			putBodyParameter("UrlList", urlList);
+		}
+	}
+
+	public String getExtra() {
+		return this.extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+		if(extra != null){
+			putBodyParameter("Extra", extra);
+		}
+	}
+
+	public String getTargetLanguage() {
+		return this.targetLanguage;
+	}
+
+	public void setTargetLanguage(String targetLanguage) {
+		this.targetLanguage = targetLanguage;
+		if(targetLanguage != null){
+			putBodyParameter("TargetLanguage", targetLanguage);
+		}
+	}
+
+	@Override
+	public Class<CreateImageTranslateTaskResponse> getResponseClass() {
+		return CreateImageTranslateTaskResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-alimt/src/main/java/com/aliyuncs/alimt/transform/v20181012/GetImageTranslateTaskResponseUnmarshaller.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alimt.transform.v20181012;
+
+import com.aliyuncs.alimt.model.v20181012.GetImageTranslateTaskResponse;
+import com.aliyuncs.alimt.model.v20181012.GetImageTranslateTaskResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetImageTranslateTaskResponseUnmarshaller {
+
+	public static GetImageTranslateTaskResponse unmarshall(GetImageTranslateTaskResponse getImageTranslateTaskResponse, UnmarshallerContext _ctx) {
+		
+		getImageTranslateTaskResponse.setRequestId(_ctx.stringValue("GetImageTranslateTaskResponse.RequestId"));
+		getImageTranslateTaskResponse.setCode(_ctx.integerValue("GetImageTranslateTaskResponse.Code"));
+		getImageTranslateTaskResponse.setMessage(_ctx.stringValue("GetImageTranslateTaskResponse.Message"));
+
+		Data data = new Data();
+		data.setImageData(_ctx.stringValue("GetImageTranslateTaskResponse.Data.ImageData"));
+		getImageTranslateTaskResponse.setData(data);
+	 
+	 	return getImageTranslateTaskResponse;
+	}
+}
