095327e60c7338246354c9b36f533c504cf6cd58
sdk-team <sdk-team@alibabacloud.com>
2020-10-20 09:49:05 +0000

    Update Configuration.

---
+++ aliyun-java-sdk-vcs/src/main/java/com/aliyuncs/vcs/model/v20200515/ListPersonTraceDetailsRequest.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vcs.model.v20200515;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.vcs.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListPersonTraceDetailsRequest extends RpcAcsRequest<ListPersonTraceDetailsResponse> {
+	   
+
+	private String corpId;
+
+	private String endTime;
+
+	private String startTime;
+
+	private Long pageNumber;
+
+	private String subId;
+
+	private Long pageSize;
+
+	private String personId;
+
+	private String isvId;
+	public ListPersonTraceDetailsRequest() {
+		super("Vcs", "2020-05-15", "ListPersonTraceDetails");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getCorpId() {
+		return this.corpId;
+	}
+
+	public void setCorpId(String corpId) {
+		this.corpId = corpId;
+		if(corpId != null){
+			putBodyParameter("CorpId", corpId);
+		}
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+		if(endTime != null){
+			putBodyParameter("EndTime", endTime);
+		}
+	}
+
+	public String getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+		if(startTime != null){
+			putBodyParameter("StartTime", startTime);
+		}
+	}
+
+	public Long getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Long pageNumber) {
+		this.pageNumber = pageNumber;
+		if(pageNumber != null){
+			putBodyParameter("PageNumber", pageNumber.toString());
+		}
+	}
+
+	public String getSubId() {
+		return this.subId;
+	}
+
+	public void setSubId(String subId) {
+		this.subId = subId;
+		if(subId != null){
+			putBodyParameter("SubId", subId);
+		}
+	}
+
+	public Long getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Long pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putBodyParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public String getPersonId() {
+		return this.personId;
+	}
+
+	public void setPersonId(String personId) {
+		this.personId = personId;
+		if(personId != null){
+			putBodyParameter("PersonId", personId);
+		}
+	}
+
+	public String getIsvId() {
+		return this.isvId;
+	}
+
+	public void setIsvId(String isvId) {
+		this.isvId = isvId;
+		if(isvId != null){
+			putBodyParameter("IsvId", isvId);
+		}
+	}
+
+	@Override
+	public Class<ListPersonTraceDetailsResponse> getResponseClass() {
+		return ListPersonTraceDetailsResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-vcs/src/main/java/com/aliyuncs/vcs/transform/v20200515/ListPersonTraceDetailsResponseUnmarshaller.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vcs.transform.v20200515;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.vcs.model.v20200515.ListPersonTraceDetailsResponse;
+import com.aliyuncs.vcs.model.v20200515.ListPersonTraceDetailsResponse.Datas;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListPersonTraceDetailsResponseUnmarshaller {
+
+	public static ListPersonTraceDetailsResponse unmarshall(ListPersonTraceDetailsResponse listPersonTraceDetailsResponse, UnmarshallerContext _ctx) {
+		
+		listPersonTraceDetailsResponse.setRequestId(_ctx.stringValue("ListPersonTraceDetailsResponse.RequestId"));
+		listPersonTraceDetailsResponse.setCode(_ctx.stringValue("ListPersonTraceDetailsResponse.Code"));
+		listPersonTraceDetailsResponse.setMessage(_ctx.stringValue("ListPersonTraceDetailsResponse.Message"));
+
+		List<Datas> data = new ArrayList<Datas>();
+		for (int i = 0; i < _ctx.lengthValue("ListPersonTraceDetailsResponse.Data.Length"); i++) {
+			Datas datas = new Datas();
+			datas.setPageNumber(_ctx.longValue("ListPersonTraceDetailsResponse.Data["+ i +"].PageNumber"));
+			datas.setPageSize(_ctx.longValue("ListPersonTraceDetailsResponse.Data["+ i +"].PageSize"));
+			datas.setTotalCount(_ctx.longValue("ListPersonTraceDetailsResponse.Data["+ i +"].TotalCount"));
+			datas.setTotalPage(_ctx.longValue("ListPersonTraceDetailsResponse.Data["+ i +"].TotalPage"));
+			datas.setTargetPicUrlPath(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].TargetPicUrlPath"));
+			datas.setDataSourceId(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].DataSourceId"));
+			datas.setPersonId(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].PersonId"));
+			datas.setPicUrlPath(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].PicUrlPath"));
+			datas.setIsvId(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].IsvId"));
+			datas.setRightBottomY(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].RightBottomY"));
+			datas.setRightBottomX(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].RightBottomX"));
+			datas.setShotTime(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].ShotTime"));
+			datas.setCorpId(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].CorpId"));
+			datas.setSubId(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].SubId"));
+			datas.setLeftTopY(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].LeftTopY"));
+			datas.setLeftTopX(_ctx.stringValue("ListPersonTraceDetailsResponse.Data["+ i +"].LeftTopX"));
+
+			data.add(datas);
+		}
+		listPersonTraceDetailsResponse.setData(data);
+	 
+	 	return listPersonTraceDetailsResponse;
+	}
+}
