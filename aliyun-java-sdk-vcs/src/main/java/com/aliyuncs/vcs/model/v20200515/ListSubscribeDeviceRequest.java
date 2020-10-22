b4dd2e6652ce54661510a4fd09e2430d7a8b2622
sdk-team <sdk-team@alibabacloud.com>
2020-10-22 16:26:42 +0000

    Support push service api.

---
+++ aliyun-java-sdk-vcs/src/main/java/com/aliyuncs/vcs/model/v20200515/ListSubscribeDeviceRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vcs.model.v20200515;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.vcs.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListSubscribeDeviceRequest extends RpcAcsRequest<ListSubscribeDeviceResponse> {
+	   
+
+	private Integer pageSize;
+
+	private Integer pageNum;
+	public ListSubscribeDeviceRequest() {
+		super("Vcs", "2020-05-15", "ListSubscribeDevice");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public Integer getPageNum() {
+		return this.pageNum;
+	}
+
+	public void setPageNum(Integer pageNum) {
+		this.pageNum = pageNum;
+		if(pageNum != null){
+			putQueryParameter("PageNum", pageNum.toString());
+		}
+	}
+
+	@Override
+	public Class<ListSubscribeDeviceResponse> getResponseClass() {
+		return ListSubscribeDeviceResponse.class;
+	}
+
+}
