581f1160349af4a95587f175ba440318e48029e5
Jackson Tian <shyvo1987@gmail.com>
2020-11-09 11:00:30 +0800

    remove the useless RegionId

--- aliyun-java-sdk-core/src/main/java/com/aliyuncs/RpcAcsRequest.java
+++ aliyun-java-sdk-core/src/main/java/com/aliyuncs/RpcAcsRequest.java
@@ -132,8 +132,6 @@ public abstract class RpcAcsRequest<T extends AcsResponse> extends AcsRequest<T>
                 }
             }
             imutableMap = this.composer.refreshSignParameters(this.getSysQueryParameters(), signer, accessKeyId, format);
-            imutableMap.put("RegionId", getSysRegionId());
-
             Map<String, String> paramsToSign = new HashMap<String, String>(imutableMap);
             Map<String, String> bodyParams = this.getSysBodyParameters();
             if (bodyParams != null && !bodyParams.isEmpty()) {
