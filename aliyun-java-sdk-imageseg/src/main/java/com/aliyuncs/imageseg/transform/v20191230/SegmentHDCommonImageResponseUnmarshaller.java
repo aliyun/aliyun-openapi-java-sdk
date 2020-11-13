b14cfe04b0b745fea0b873effa0797c83745e303
sdk-team <sdk-team@alibabacloud.com>
2020-11-13 06:12:07 +0000

    Release SegmentHDSky SegmentHDCommonImage.

---
+++ aliyun-java-sdk-imageseg/src/main/java/com/aliyuncs/imageseg/transform/v20191230/SegmentHDCommonImageResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageseg.transform.v20191230;
+
+import com.aliyuncs.imageseg.model.v20191230.SegmentHDCommonImageResponse;
+import com.aliyuncs.imageseg.model.v20191230.SegmentHDCommonImageResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SegmentHDCommonImageResponseUnmarshaller {
+
+	public static SegmentHDCommonImageResponse unmarshall(SegmentHDCommonImageResponse segmentHDCommonImageResponse, UnmarshallerContext _ctx) {
+		
+		segmentHDCommonImageResponse.setRequestId(_ctx.stringValue("SegmentHDCommonImageResponse.RequestId"));
+
+		Data data = new Data();
+		data.setImageUrl(_ctx.stringValue("SegmentHDCommonImageResponse.Data.ImageUrl"));
+		segmentHDCommonImageResponse.setData(data);
+	 
+	 	return segmentHDCommonImageResponse;
+	}
+}
---
+++ aliyun-java-sdk-imageseg/src/main/java/com/aliyuncs/imageseg/transform/v20191230/SegmentHDSkyResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imageseg.transform.v20191230;
+
+import com.aliyuncs.imageseg.model.v20191230.SegmentHDSkyResponse;
+import com.aliyuncs.imageseg.model.v20191230.SegmentHDSkyResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SegmentHDSkyResponseUnmarshaller {
+
+	public static SegmentHDSkyResponse unmarshall(SegmentHDSkyResponse segmentHDSkyResponse, UnmarshallerContext _ctx) {
+		
+		segmentHDSkyResponse.setRequestId(_ctx.stringValue("SegmentHDSkyResponse.RequestId"));
+
+		Data data = new Data();
+		data.setImageURL(_ctx.stringValue("SegmentHDSkyResponse.Data.ImageURL"));
+		segmentHDSkyResponse.setData(data);
+	 
+	 	return segmentHDSkyResponse;
+	}
+}
