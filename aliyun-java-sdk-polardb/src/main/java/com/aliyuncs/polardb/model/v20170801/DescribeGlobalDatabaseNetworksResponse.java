2d93d6976e2b0bec574bbf576017fb5011438ce5
sdk-team <sdk-team@alibabacloud.com>
2020-11-17 09:15:31 +0000

    Support ModifyDBClusterPrimaryZone.

---
+++ aliyun-java-sdk-polardb/src/main/java/com/aliyuncs/polardb/model/v20170801/DescribeGlobalDatabaseNetworksResponse.java
@@ -0,0 +1,193 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.polardb.model.v20170801;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.polardb.transform.v20170801.DescribeGlobalDatabaseNetworksResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeGlobalDatabaseNetworksResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Integer pageNumber;
+
+	private Integer totalRecordCount;
+
+	private Integer pageRecordCount;
+
+	private List<GlobalDatabaseNetwork> items;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public Integer getTotalRecordCount() {
+		return this.totalRecordCount;
+	}
+
+	public void setTotalRecordCount(Integer totalRecordCount) {
+		this.totalRecordCount = totalRecordCount;
+	}
+
+	public Integer getPageRecordCount() {
+		return this.pageRecordCount;
+	}
+
+	public void setPageRecordCount(Integer pageRecordCount) {
+		this.pageRecordCount = pageRecordCount;
+	}
+
+	public List<GlobalDatabaseNetwork> getItems() {
+		return this.items;
+	}
+
+	public void setItems(List<GlobalDatabaseNetwork> items) {
+		this.items = items;
+	}
+
+	public static class GlobalDatabaseNetwork {
+
+		private String gDNId;
+
+		private String gDNStatus;
+
+		private String gDNDescription;
+
+		private String dBType;
+
+		private String dBVersion;
+
+		private String createTime;
+
+		private List<DBCluster> dBClusters;
+
+		public String getGDNId() {
+			return this.gDNId;
+		}
+
+		public void setGDNId(String gDNId) {
+			this.gDNId = gDNId;
+		}
+
+		public String getGDNStatus() {
+			return this.gDNStatus;
+		}
+
+		public void setGDNStatus(String gDNStatus) {
+			this.gDNStatus = gDNStatus;
+		}
+
+		public String getGDNDescription() {
+			return this.gDNDescription;
+		}
+
+		public void setGDNDescription(String gDNDescription) {
+			this.gDNDescription = gDNDescription;
+		}
+
+		public String getDBType() {
+			return this.dBType;
+		}
+
+		public void setDBType(String dBType) {
+			this.dBType = dBType;
+		}
+
+		public String getDBVersion() {
+			return this.dBVersion;
+		}
+
+		public void setDBVersion(String dBVersion) {
+			this.dBVersion = dBVersion;
+		}
+
+		public String getCreateTime() {
+			return this.createTime;
+		}
+
+		public void setCreateTime(String createTime) {
+			this.createTime = createTime;
+		}
+
+		public List<DBCluster> getDBClusters() {
+			return this.dBClusters;
+		}
+
+		public void setDBClusters(List<DBCluster> dBClusters) {
+			this.dBClusters = dBClusters;
+		}
+
+		public static class DBCluster {
+
+			private String dBClusterId;
+
+			private String regionId;
+
+			private String role;
+
+			public String getDBClusterId() {
+				return this.dBClusterId;
+			}
+
+			public void setDBClusterId(String dBClusterId) {
+				this.dBClusterId = dBClusterId;
+			}
+
+			public String getRegionId() {
+				return this.regionId;
+			}
+
+			public void setRegionId(String regionId) {
+				this.regionId = regionId;
+			}
+
+			public String getRole() {
+				return this.role;
+			}
+
+			public void setRole(String role) {
+				this.role = role;
+			}
+		}
+	}
+
+	@Override
+	public DescribeGlobalDatabaseNetworksResponse getInstance(UnmarshallerContext context) {
+		return	DescribeGlobalDatabaseNetworksResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-polardb/src/main/java/com/aliyuncs/polardb/transform/v20170801/DescribeGlobalDatabaseNetworksResponseUnmarshaller.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.polardb.transform.v20170801;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.polardb.model.v20170801.DescribeGlobalDatabaseNetworksResponse;
+import com.aliyuncs.polardb.model.v20170801.DescribeGlobalDatabaseNetworksResponse.GlobalDatabaseNetwork;
+import com.aliyuncs.polardb.model.v20170801.DescribeGlobalDatabaseNetworksResponse.GlobalDatabaseNetwork.DBCluster;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeGlobalDatabaseNetworksResponseUnmarshaller {
+
+	public static DescribeGlobalDatabaseNetworksResponse unmarshall(DescribeGlobalDatabaseNetworksResponse describeGlobalDatabaseNetworksResponse, UnmarshallerContext _ctx) {
+		
+		describeGlobalDatabaseNetworksResponse.setRequestId(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.RequestId"));
+		describeGlobalDatabaseNetworksResponse.setPageNumber(_ctx.integerValue("DescribeGlobalDatabaseNetworksResponse.PageNumber"));
+		describeGlobalDatabaseNetworksResponse.setTotalRecordCount(_ctx.integerValue("DescribeGlobalDatabaseNetworksResponse.TotalRecordCount"));
+		describeGlobalDatabaseNetworksResponse.setPageRecordCount(_ctx.integerValue("DescribeGlobalDatabaseNetworksResponse.PageRecordCount"));
+
+		List<GlobalDatabaseNetwork> items = new ArrayList<GlobalDatabaseNetwork>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeGlobalDatabaseNetworksResponse.Items.Length"); i++) {
+			GlobalDatabaseNetwork globalDatabaseNetwork = new GlobalDatabaseNetwork();
+			globalDatabaseNetwork.setGDNId(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].GDNId"));
+			globalDatabaseNetwork.setGDNStatus(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].GDNStatus"));
+			globalDatabaseNetwork.setGDNDescription(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].GDNDescription"));
+			globalDatabaseNetwork.setDBType(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBType"));
+			globalDatabaseNetwork.setDBVersion(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBVersion"));
+			globalDatabaseNetwork.setCreateTime(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].CreateTime"));
+
+			List<DBCluster> dBClusters = new ArrayList<DBCluster>();
+			for (int j = 0; j < _ctx.lengthValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBClusters.Length"); j++) {
+				DBCluster dBCluster = new DBCluster();
+				dBCluster.setDBClusterId(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBClusters["+ j +"].DBClusterId"));
+				dBCluster.setRegionId(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBClusters["+ j +"].RegionId"));
+				dBCluster.setRole(_ctx.stringValue("DescribeGlobalDatabaseNetworksResponse.Items["+ i +"].DBClusters["+ j +"].Role"));
+
+				dBClusters.add(dBCluster);
+			}
+			globalDatabaseNetwork.setDBClusters(dBClusters);
+
+			items.add(globalDatabaseNetwork);
+		}
+		describeGlobalDatabaseNetworksResponse.setItems(items);
+	 
+	 	return describeGlobalDatabaseNetworksResponse;
+	}
+}
