41374a672207b025e4d34471d4f4957d25681c40
sdk-team <sdk-team@alibabacloud.com>
2020-11-27 02:42:18 +0000

    Support taskList.

---
+++ aliyun-java-sdk-devops-rdc/src/main/java/com/aliyuncs/devops_rdc/model/v20200303/ListDevopsProjectTaskListResponse.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.devops_rdc.model.v20200303;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.devops_rdc.transform.v20200303.ListDevopsProjectTaskListResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListDevopsProjectTaskListResponse extends AcsResponse {
+
+	private Boolean successful;
+
+	private String errorCode;
+
+	private String errorMsg;
+
+	private String requestId;
+
+	private Object object;
+
+	public Boolean getSuccessful() {
+		return this.successful;
+	}
+
+	public void setSuccessful(Boolean successful) {
+		this.successful = successful;
+	}
+
+	public String getErrorCode() {
+		return this.errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Object getObject() {
+		return this.object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public static class Object {
+
+		private List<ResultItem> result;
+
+		public List<ResultItem> getResult() {
+			return this.result;
+		}
+
+		public void setResult(List<ResultItem> result) {
+			this.result = result;
+		}
+
+		public static class ResultItem {
+
+			private String id;
+
+			public String getId() {
+				return this.id;
+			}
+
+			public void setId(String id) {
+				this.id = id;
+			}
+		}
+	}
+
+	@Override
+	public ListDevopsProjectTaskListResponse getInstance(UnmarshallerContext context) {
+		return	ListDevopsProjectTaskListResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-devops-rdc/src/main/java/com/aliyuncs/devops_rdc/transform/v20200303/ListDevopsProjectTaskListResponseUnmarshaller.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.devops_rdc.transform.v20200303;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.devops_rdc.model.v20200303.ListDevopsProjectTaskListResponse;
+import com.aliyuncs.devops_rdc.model.v20200303.ListDevopsProjectTaskListResponse.Object;
+import com.aliyuncs.devops_rdc.model.v20200303.ListDevopsProjectTaskListResponse.Object.ResultItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListDevopsProjectTaskListResponseUnmarshaller {
+
+	public static ListDevopsProjectTaskListResponse unmarshall(ListDevopsProjectTaskListResponse listDevopsProjectTaskListResponse, UnmarshallerContext _ctx) {
+		
+		listDevopsProjectTaskListResponse.setRequestId(_ctx.stringValue("ListDevopsProjectTaskListResponse.RequestId"));
+		listDevopsProjectTaskListResponse.setSuccessful(_ctx.booleanValue("ListDevopsProjectTaskListResponse.Successful"));
+		listDevopsProjectTaskListResponse.setErrorCode(_ctx.stringValue("ListDevopsProjectTaskListResponse.ErrorCode"));
+		listDevopsProjectTaskListResponse.setErrorMsg(_ctx.stringValue("ListDevopsProjectTaskListResponse.ErrorMsg"));
+
+		Object object = new Object();
+
+		List<ResultItem> result = new ArrayList<ResultItem>();
+		for (int i = 0; i < _ctx.lengthValue("ListDevopsProjectTaskListResponse.Object.Result.Length"); i++) {
+			ResultItem resultItem = new ResultItem();
+			resultItem.setId(_ctx.stringValue("ListDevopsProjectTaskListResponse.Object.Result["+ i +"].Id"));
+
+			result.add(resultItem);
+		}
+		object.setResult(result);
+		listDevopsProjectTaskListResponse.setObject(object);
+	 
+	 	return listDevopsProjectTaskListResponse;
+	}
+}
