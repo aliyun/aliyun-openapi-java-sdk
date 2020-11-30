3150e1762046dcfcb62f09881f55a0e07c04febb
sdk-team <sdk-team@alibabacloud.com>
2020-11-30 07:25:21 +0000

    Update OpenDcdnService.

---
+++ aliyun-java-sdk-dcdn/src/main/java/com/aliyuncs/dcdn/model/v20180115/OpenDcdnServiceRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dcdn.model.v20180115;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.dcdn.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class OpenDcdnServiceRequest extends RpcAcsRequest<OpenDcdnServiceResponse> {
+	   
+
+	private String websocketBillType;
+
+	private String billType;
+
+	private String securityToken;
+
+	private Long ownerId;
+	public OpenDcdnServiceRequest() {
+		super("dcdn", "2018-01-15", "OpenDcdnService");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getWebsocketBillType() {
+		return this.websocketBillType;
+	}
+
+	public void setWebsocketBillType(String websocketBillType) {
+		this.websocketBillType = websocketBillType;
+		if(websocketBillType != null){
+			putQueryParameter("WebsocketBillType", websocketBillType);
+		}
+	}
+
+	public String getBillType() {
+		return this.billType;
+	}
+
+	public void setBillType(String billType) {
+		this.billType = billType;
+		if(billType != null){
+			putQueryParameter("BillType", billType);
+		}
+	}
+
+	public String getSecurityToken() {
+		return this.securityToken;
+	}
+
+	public void setSecurityToken(String securityToken) {
+		this.securityToken = securityToken;
+		if(securityToken != null){
+			putQueryParameter("SecurityToken", securityToken);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	@Override
+	public Class<OpenDcdnServiceResponse> getResponseClass() {
+		return OpenDcdnServiceResponse.class;
+	}
+
+}
