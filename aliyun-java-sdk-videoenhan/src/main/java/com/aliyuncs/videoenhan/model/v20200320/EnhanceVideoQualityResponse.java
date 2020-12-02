5d174a2fde580eb73468397538361202f2f4adfb
sdk-team <sdk-team@alibabacloud.com>
2020-12-02 02:30:07 +0000

    Release MergeVideoFace EnhanceVideoQuality.

---
+++ aliyun-java-sdk-videoenhan/src/main/java/com/aliyuncs/videoenhan/model/v20200320/EnhanceVideoQualityResponse.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videoenhan.model.v20200320;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.videoenhan.transform.v20200320.EnhanceVideoQualityResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class EnhanceVideoQualityResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private String videoURL;
+
+		public String getVideoURL() {
+			return this.videoURL;
+		}
+
+		public void setVideoURL(String videoURL) {
+			this.videoURL = videoURL;
+		}
+	}
+
+	@Override
+	public EnhanceVideoQualityResponse getInstance(UnmarshallerContext context) {
+		return	EnhanceVideoQualityResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-videoenhan/src/main/java/com/aliyuncs/videoenhan/model/v20200320/MergeVideoFaceRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.videoenhan.model.v20200320;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.videoenhan.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class MergeVideoFaceRequest extends RpcAcsRequest<MergeVideoFaceResponse> {
+	   
+
+	private String postURL;
+
+	private String referenceURL;
+
+	private String videoURL;
+	public MergeVideoFaceRequest() {
+		super("videoenhan", "2020-03-20", "MergeVideoFace", "videoenhan");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getPostURL() {
+		return this.postURL;
+	}
+
+	public void setPostURL(String postURL) {
+		this.postURL = postURL;
+		if(postURL != null){
+			putBodyParameter("PostURL", postURL);
+		}
+	}
+
+	public String getReferenceURL() {
+		return this.referenceURL;
+	}
+
+	public void setReferenceURL(String referenceURL) {
+		this.referenceURL = referenceURL;
+		if(referenceURL != null){
+			putBodyParameter("ReferenceURL", referenceURL);
+		}
+	}
+
+	public String getVideoURL() {
+		return this.videoURL;
+	}
+
+	public void setVideoURL(String videoURL) {
+		this.videoURL = videoURL;
+		if(videoURL != null){
+			putBodyParameter("VideoURL", videoURL);
+		}
+	}
+
+	@Override
+	public Class<MergeVideoFaceResponse> getResponseClass() {
+		return MergeVideoFaceResponse.class;
+	}
+
+}
