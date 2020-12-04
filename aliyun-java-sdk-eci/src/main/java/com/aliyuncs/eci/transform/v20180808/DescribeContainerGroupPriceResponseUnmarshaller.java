b9982c39fd57bcd9e0e55a491c536456027966b0
wb-hxh549296 <wb-hxh549296@alibaba-inc.com>
2020-12-04 18:25:29 +0800

    describePrice support spot

--- aliyun-java-sdk-eci/src/main/java/com/aliyuncs/eci/transform/v20180808/DescribeContainerGroupPriceResponseUnmarshaller.java
+++ aliyun-java-sdk-eci/src/main/java/com/aliyuncs/eci/transform/v20180808/DescribeContainerGroupPriceResponseUnmarshaller.java
@@ -18,62 +18,79 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse;
-import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse.PriceInfo;
-import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse.PriceInfo.Price;
-import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfo;
-import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfo.Rule2;
-import com.aliyuncs.eci.model.v20180808.DescribeContainerGroupPriceResponse.PriceInfo.Rule;
 import com.aliyuncs.transform.UnmarshallerContext;
 
 
 public class DescribeContainerGroupPriceResponseUnmarshaller {
 
-	public static DescribeContainerGroupPriceResponse unmarshall(DescribeContainerGroupPriceResponse describeContainerGroupPriceResponse, UnmarshallerContext context) {
-		
-		describeContainerGroupPriceResponse.setRequestId(context.stringValue("DescribeContainerGroupPriceResponse.RequestId"));
+    public static DescribeContainerGroupPriceResponse unmarshall(DescribeContainerGroupPriceResponse describeContainerGroupPriceResponse, UnmarshallerContext context) {
 
-		PriceInfo priceInfo = new PriceInfo();
+        describeContainerGroupPriceResponse.setRequestId(context.stringValue("DescribeContainerGroupPriceResponse.RequestId"));
 
-		Price price = new Price();
-		price.setOriginalPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.OriginalPrice"));
-		price.setDiscountPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DiscountPrice"));
-		price.setTradePrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.TradePrice"));
-		price.setCurrency(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.Currency"));
+        DescribeContainerGroupPriceResponse.PriceInfo priceInfo = new DescribeContainerGroupPriceResponse.PriceInfo();
+        DescribeContainerGroupPriceResponse.PriceInfo.Price price = new DescribeContainerGroupPriceResponse.PriceInfo.Price();
 
-		List<DetailInfo> detailInfos = new ArrayList<DetailInfo>();
-		for (int i = 0; i < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.Length"); i++) {
-			DetailInfo detailInfo = new DetailInfo();
-			detailInfo.setResource(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].Resource"));
-			detailInfo.setOriginalPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].OriginalPrice"));
-			detailInfo.setDiscountPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].DiscountPrice"));
-			detailInfo.setTradePrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].TradePrice"));
+        price.setOriginalPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.OriginalPrice"));
+        price.setDiscountPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DiscountPrice"));
+        price.setTradePrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.TradePrice"));
+        price.setCurrency(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.Currency"));
 
-			List<Rule2> rules1 = new ArrayList<Rule2>();
-			for (int j = 0; j < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].Rules.Length"); j++) {
-				Rule2 rule2 = new Rule2();
-				rule2.setRuleId(context.longValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].Rules["+ j +"].RuleId"));
-				rule2.setDescription(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos["+ i +"].Rules["+ j +"].Description"));
 
-				rules1.add(rule2);
-			}
-			detailInfo.setRules1(rules1);
+        DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos detailInfos = new DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos();
 
-			detailInfos.add(detailInfo);
-		}
-		price.setDetailInfos(detailInfos);
-		priceInfo.setPrice(price);
+        List<DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo> detailInfo = new ArrayList<DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo>();
+        for (int i = 0; i < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.Length"); i++) {
+            DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo info = new DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo();
+            info.setResource(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].Resource"));
+            info.setOriginalPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].OriginalPrice"));
+            info.setDiscountPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].DiscountPrice"));
+            info.setTradePrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].TradePrice"));
 
-		List<Rule> rules = new ArrayList<Rule>();
-		for (int i = 0; i < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules.Length"); i++) {
-			Rule rule = new Rule();
-			rule.setRuleId(context.longValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules["+ i +"].RuleId"));
-			rule.setDescription(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules["+ i +"].Description"));
+            DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules rules1 = new DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules();
 
-			rules.add(rule);
-		}
-		priceInfo.setRules(rules);
-		describeContainerGroupPriceResponse.setPriceInfo(priceInfo);
-	 
-	 	return describeContainerGroupPriceResponse;
-	}
+            List<DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules.Rule> rule = new ArrayList<DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules.Rule>();
+            for (int j = 0; j < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].Rules.Length"); j++) {
+                DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules.Rule rule1 = new DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos.DetailInfo.Rules.Rule();
+                rule1.setRuleId(context.longValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].Rules[" + j + "].RuleId"));
+                rule1.setDescription(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Price.DetailInfos[" + i + "].Rules[" + j + "].Description"));
+                rule.add(rule1);
+            }
+            rules1.setRule(rule);
+            info.setRules(rules1);
+            detailInfo.add(info);
+        }
+        detailInfos.setDetailInfo(detailInfo);
+        price.setDetailInfos(detailInfos);
+        priceInfo.setPrice(price);
+
+        DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices spotPrices = new DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices();
+
+        List<DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices.SpotPrice> spotPrice = new ArrayList<DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices.SpotPrice>();
+        for (int i = 0; i < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices.Length"); i++) {
+            DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices.SpotPrice sp = new DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices.SpotPrice();
+            sp.setSpotPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices[" + i + "].SpotPrice"));
+            sp.setOriginPrice(context.floatValue("DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices[" + i + "].OriginPrice"));
+            sp.setZoneId(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices[" + i + "].ZoneId"));
+            sp.setInstanceType(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.SpotPrices[" + i + "].InstanceType"));
+            spotPrice.add(sp);
+        }
+        spotPrices.setSpotPrice(spotPrice);
+        priceInfo.setSpotPrices(spotPrices);
+
+        DescribeContainerGroupPriceResponse.PriceInfo.Rules rules2 = new DescribeContainerGroupPriceResponse.PriceInfo.Rules();
+
+        List<DescribeContainerGroupPriceResponse.PriceInfo.Rules.Rule> rules = new ArrayList<DescribeContainerGroupPriceResponse.PriceInfo.Rules.Rule>();
+        for (int i = 0; i < context.lengthValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules.Length"); i++) {
+            DescribeContainerGroupPriceResponse.PriceInfo.Rules.Rule rule = new DescribeContainerGroupPriceResponse.PriceInfo.Rules.Rule();
+            rule.setRuleId(context.longValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules[" + i + "].RuleId"));
+            rule.setDescription(context.stringValue("DescribeContainerGroupPriceResponse.PriceInfo.Rules[" + i + "].Description"));
+
+            rules.add(rule);
+        }
+        rules2.setRule(rules);
+        priceInfo.setRules(rules2);
+        describeContainerGroupPriceResponse.setPriceInfo(priceInfo);
+
+        return describeContainerGroupPriceResponse;
+    }
 }
