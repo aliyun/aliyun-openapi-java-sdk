6e04da66be3507e1d399eaa770e407a14ce02e27
sdk-team <sdk-team@alibabacloud.com>
2020-12-18 06:16:40 +0000

    Release EvaluateCertificateQuality.

---
+++ aliyun-java-sdk-imagerecog/src/main/java/com/aliyuncs/imagerecog/model/v20190930/EvaluateCertificateQualityResponse.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imagerecog.model.v20190930;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.imagerecog.transform.v20190930.EvaluateCertificateQualityResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class EvaluateCertificateQualityResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Data data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private List<Element> elements;
+
+		public List<Element> getElements() {
+			return this.elements;
+		}
+
+		public void setElements(List<Element> elements) {
+			this.elements = elements;
+		}
+
+		public static class Element {
+
+			private String value;
+
+			private String pass;
+
+			private String score;
+
+			public String getValue() {
+				return this.value;
+			}
+
+			public void setValue(String value) {
+				this.value = value;
+			}
+
+			public String getPass() {
+				return this.pass;
+			}
+
+			public void setPass(String pass) {
+				this.pass = pass;
+			}
+
+			public String getScore() {
+				return this.score;
+			}
+
+			public void setScore(String score) {
+				this.score = score;
+			}
+		}
+	}
+
+	@Override
+	public EvaluateCertificateQualityResponse getInstance(UnmarshallerContext context) {
+		return	EvaluateCertificateQualityResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-imagerecog/src/main/java/com/aliyuncs/imagerecog/transform/v20190930/EvaluateCertificateQualityResponseUnmarshaller.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.imagerecog.transform.v20190930;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.imagerecog.model.v20190930.EvaluateCertificateQualityResponse;
+import com.aliyuncs.imagerecog.model.v20190930.EvaluateCertificateQualityResponse.Data;
+import com.aliyuncs.imagerecog.model.v20190930.EvaluateCertificateQualityResponse.Data.Element;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class EvaluateCertificateQualityResponseUnmarshaller {
+
+	public static EvaluateCertificateQualityResponse unmarshall(EvaluateCertificateQualityResponse evaluateCertificateQualityResponse, UnmarshallerContext _ctx) {
+		
+		evaluateCertificateQualityResponse.setRequestId(_ctx.stringValue("EvaluateCertificateQualityResponse.RequestId"));
+
+		Data data = new Data();
+
+		List<Element> elements = new ArrayList<Element>();
+		for (int i = 0; i < _ctx.lengthValue("EvaluateCertificateQualityResponse.Data.Elements.Length"); i++) {
+			Element element = new Element();
+			element.setValue(_ctx.stringValue("EvaluateCertificateQualityResponse.Data.Elements["+ i +"].Value"));
+			element.setPass(_ctx.stringValue("EvaluateCertificateQualityResponse.Data.Elements["+ i +"].Pass"));
+			element.setScore(_ctx.stringValue("EvaluateCertificateQualityResponse.Data.Elements["+ i +"].Score"));
+
+			elements.add(element);
+		}
+		data.setElements(elements);
+		evaluateCertificateQualityResponse.setData(data);
+	 
+	 	return evaluateCertificateQualityResponse;
+	}
+}
