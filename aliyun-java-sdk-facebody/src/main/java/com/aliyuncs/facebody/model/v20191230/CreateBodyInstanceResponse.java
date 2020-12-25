98bd7ba93da1a5c7a835b18ca08310dd0b255fe1
sdk-team <sdk-team@alibabacloud.com>
2020-12-25 10:12:02 +0000

    Release GenRealPersonVerificationToken GetRealPersonVerificationResult.

---
+++ aliyun-java-sdk-facebody/src/main/java/com/aliyuncs/facebody/model/v20191230/CreateBodyInstanceResponse.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.facebody.model.v20191230;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.facebody.transform.v20191230.CreateBodyInstanceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateBodyInstanceResponse extends AcsResponse {
+
+	private String message;
+
+	private String requestId;
+
+	private String code;
+
+	private Data data;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Long id;
+
+		public Long getId() {
+			return this.id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	@Override
+	public CreateBodyInstanceResponse getInstance(UnmarshallerContext context) {
+		return	CreateBodyInstanceResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
