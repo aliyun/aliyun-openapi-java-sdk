975236057b60d495822fc9f31ae3f5f9ad16d191
sdk-team <sdk-team@alibabacloud.com>
2021-01-08 02:38:40 +0000

    Add new API for configuration and report, generate SDK again.

---
+++ aliyun-java-sdk-ccc/src/main/java/com/aliyuncs/ccc/model/v20200701/ListRealtimeAgentStatesResponse.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ccc.model.v20200701;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ccc.transform.v20200701.ListRealtimeAgentStatesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListRealtimeAgentStatesResponse extends AcsResponse {
+
+	private String code;
+
+	private Integer httpStatusCode;
+
+	private String message;
+
+	private String requestId;
+
+	private Data data;
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Integer getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(Integer httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Integer pageNumber;
+
+		private Integer pageSize;
+
+		private Integer totalCount;
+
+		private List<AgentState> list;
+
+		public Integer getPageNumber() {
+			return this.pageNumber;
+		}
+
+		public void setPageNumber(Integer pageNumber) {
+			this.pageNumber = pageNumber;
+		}
+
+		public Integer getPageSize() {
+			return this.pageSize;
+		}
+
+		public void setPageSize(Integer pageSize) {
+			this.pageSize = pageSize;
+		}
+
+		public Integer getTotalCount() {
+			return this.totalCount;
+		}
+
+		public void setTotalCount(Integer totalCount) {
+			this.totalCount = totalCount;
+		}
+
+		public List<AgentState> getList() {
+			return this.list;
+		}
+
+		public void setList(List<AgentState> list) {
+			this.list = list;
+		}
+
+		public static class AgentState {
+
+			private String agentId;
+
+			private String agentName;
+
+			private String counterParty;
+
+			private String extension;
+
+			private String instanceId;
+
+			private String state;
+
+			private String stateCode;
+
+			private Long stateTime;
+
+			private List<String> skillGroupIdList;
+
+			public String getAgentId() {
+				return this.agentId;
+			}
+
+			public void setAgentId(String agentId) {
+				this.agentId = agentId;
+			}
+
+			public String getAgentName() {
+				return this.agentName;
+			}
+
+			public void setAgentName(String agentName) {
+				this.agentName = agentName;
+			}
+
+			public String getCounterParty() {
+				return this.counterParty;
+			}
+
+			public void setCounterParty(String counterParty) {
+				this.counterParty = counterParty;
+			}
+
+			public String getExtension() {
+				return this.extension;
+			}
+
+			public void setExtension(String extension) {
+				this.extension = extension;
+			}
+
+			public String getInstanceId() {
+				return this.instanceId;
+			}
+
+			public void setInstanceId(String instanceId) {
+				this.instanceId = instanceId;
+			}
+
+			public String getState() {
+				return this.state;
+			}
+
+			public void setState(String state) {
+				this.state = state;
+			}
+
+			public String getStateCode() {
+				return this.stateCode;
+			}
+
+			public void setStateCode(String stateCode) {
+				this.stateCode = stateCode;
+			}
+
+			public Long getStateTime() {
+				return this.stateTime;
+			}
+
+			public void setStateTime(Long stateTime) {
+				this.stateTime = stateTime;
+			}
+
+			public List<String> getSkillGroupIdList() {
+				return this.skillGroupIdList;
+			}
+
+			public void setSkillGroupIdList(List<String> skillGroupIdList) {
+				this.skillGroupIdList = skillGroupIdList;
+			}
+		}
+	}
+
+	@Override
+	public ListRealtimeAgentStatesResponse getInstance(UnmarshallerContext context) {
+		return	ListRealtimeAgentStatesResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-ccc/src/main/java/com/aliyuncs/ccc/model/v20200701/ListRecentCallDetailRecordsResponse.java
@@ -0,0 +1,243 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ccc.model.v20200701;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ccc.transform.v20200701.ListRecentCallDetailRecordsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListRecentCallDetailRecordsResponse extends AcsResponse {
+
+	private String code;
+
+	private Integer httpStatusCode;
+
+	private String message;
+
+	private String requestId;
+
+	private Data data;
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Integer getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(Integer httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private Integer pageNumber;
+
+		private Integer pageSize;
+
+		private Integer totalCount;
+
+		private List<CallDetailRecord> list;
+
+		public Integer getPageNumber() {
+			return this.pageNumber;
+		}
+
+		public void setPageNumber(Integer pageNumber) {
+			this.pageNumber = pageNumber;
+		}
+
+		public Integer getPageSize() {
+			return this.pageSize;
+		}
+
+		public void setPageSize(Integer pageSize) {
+			this.pageSize = pageSize;
+		}
+
+		public Integer getTotalCount() {
+			return this.totalCount;
+		}
+
+		public void setTotalCount(Integer totalCount) {
+			this.totalCount = totalCount;
+		}
+
+		public List<CallDetailRecord> getList() {
+			return this.list;
+		}
+
+		public void setList(List<CallDetailRecord> list) {
+			this.list = list;
+		}
+
+		public static class CallDetailRecord {
+
+			private String agentIds;
+
+			private String callDuration;
+
+			private String calledNumber;
+
+			private String callingNumber;
+
+			private String contactDisposition;
+
+			private String contactId;
+
+			private String contactType;
+
+			private Long duration;
+
+			private String instanceId;
+
+			private String skillGroupIds;
+
+			private Long startTime;
+
+			public String getAgentIds() {
+				return this.agentIds;
+			}
+
+			public void setAgentIds(String agentIds) {
+				this.agentIds = agentIds;
+			}
+
+			public String getCallDuration() {
+				return this.callDuration;
+			}
+
+			public void setCallDuration(String callDuration) {
+				this.callDuration = callDuration;
+			}
+
+			public String getCalledNumber() {
+				return this.calledNumber;
+			}
+
+			public void setCalledNumber(String calledNumber) {
+				this.calledNumber = calledNumber;
+			}
+
+			public String getCallingNumber() {
+				return this.callingNumber;
+			}
+
+			public void setCallingNumber(String callingNumber) {
+				this.callingNumber = callingNumber;
+			}
+
+			public String getContactDisposition() {
+				return this.contactDisposition;
+			}
+
+			public void setContactDisposition(String contactDisposition) {
+				this.contactDisposition = contactDisposition;
+			}
+
+			public String getContactId() {
+				return this.contactId;
+			}
+
+			public void setContactId(String contactId) {
+				this.contactId = contactId;
+			}
+
+			public String getContactType() {
+				return this.contactType;
+			}
+
+			public void setContactType(String contactType) {
+				this.contactType = contactType;
+			}
+
+			public Long getDuration() {
+				return this.duration;
+			}
+
+			public void setDuration(Long duration) {
+				this.duration = duration;
+			}
+
+			public String getInstanceId() {
+				return this.instanceId;
+			}
+
+			public void setInstanceId(String instanceId) {
+				this.instanceId = instanceId;
+			}
+
+			public String getSkillGroupIds() {
+				return this.skillGroupIds;
+			}
+
+			public void setSkillGroupIds(String skillGroupIds) {
+				this.skillGroupIds = skillGroupIds;
+			}
+
+			public Long getStartTime() {
+				return this.startTime;
+			}
+
+			public void setStartTime(Long startTime) {
+				this.startTime = startTime;
+			}
+		}
+	}
+
+	@Override
+	public ListRecentCallDetailRecordsResponse getInstance(UnmarshallerContext context) {
+		return	ListRecentCallDetailRecordsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
