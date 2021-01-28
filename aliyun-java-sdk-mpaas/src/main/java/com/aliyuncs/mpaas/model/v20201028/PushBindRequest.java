fd03e42440003ac225a2034942f8fb856972f1a5
sdk-team <sdk-team@alibabacloud.com>
2021-01-28 12:37:24 +0000

    Push add bind unbind report api.

---
+++ aliyun-java-sdk-mpaas/src/main/java/com/aliyuncs/mpaas/model/v20201028/PushBindRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.mpaas.model.v20201028;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.mpaas.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class PushBindRequest extends RpcAcsRequest<PushBindResponse> {
+	   
+
+	private String userId;
+
+	private String deliveryToken;
+
+	private Integer osType;
+
+	private String appId;
+
+	private String workspaceId;
+	public PushBindRequest() {
+		super("mPaaS", "2020-10-28", "PushBind");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putBodyParameter("UserId", userId);
+		}
+	}
+
+	public String getDeliveryToken() {
+		return this.deliveryToken;
+	}
+
+	public void setDeliveryToken(String deliveryToken) {
+		this.deliveryToken = deliveryToken;
+		if(deliveryToken != null){
+			putBodyParameter("DeliveryToken", deliveryToken);
+		}
+	}
+
+	public Integer getOsType() {
+		return this.osType;
+	}
+
+	public void setOsType(Integer osType) {
+		this.osType = osType;
+		if(osType != null){
+			putBodyParameter("OsType", osType.toString());
+		}
+	}
+
+	public String getAppId() {
+		return this.appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+		if(appId != null){
+			putBodyParameter("AppId", appId);
+		}
+	}
+
+	public String getWorkspaceId() {
+		return this.workspaceId;
+	}
+
+	public void setWorkspaceId(String workspaceId) {
+		this.workspaceId = workspaceId;
+		if(workspaceId != null){
+			putBodyParameter("WorkspaceId", workspaceId);
+		}
+	}
+
+	@Override
+	public Class<PushBindResponse> getResponseClass() {
+		return PushBindResponse.class;
+	}
+
+}
