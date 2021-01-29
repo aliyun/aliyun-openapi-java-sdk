c690dcb39568bf6a781cbb84b6df9409b2c7363a
sdk-team <sdk-team@alibabacloud.com>
2021-01-29 02:21:56 +0000

    Generated 2020-05-18 for dataworks-public.

--- aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/model/v20200518/RunTriggerNodeRequest.java
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/model/v20200518/RunTriggerNodeRequest.java
@@ -34,7 +34,7 @@ public class RunTriggerNodeRequest extends RpcAcsRequest<RunTriggerNodeResponse>
 	private Long nodeId;
 	public RunTriggerNodeRequest() {
 		super("dataworks-public", "2020-05-18", "RunTriggerNode");
-		setMethod(MethodType.PUT);
+		setMethod(MethodType.POST);
 		try {
 			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
 			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
