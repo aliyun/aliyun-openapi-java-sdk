3e738563974b162960fedaa0625577439894b48a
sdk-team <sdk-team@alibabacloud.com>
2021-02-01 03:14:15 +0000

    Release GenerateHumanSketchStyle MergeImageFace AddFaceImageTemplate QueryFaceImageTemplate DeleteFaceImageTemplate.

---
+++ aliyun-java-sdk-facebody/src/main/java/com/aliyuncs/facebody/model/v20200910/ExecuteServerSideVerificationResponse.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.facebody.model.v20200910;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.facebody.transform.v20200910.ExecuteServerSideVerificationResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ExecuteServerSideVerificationResponse extends AcsResponse {
+
+	private String message;
+
+	private String requestId;
+
+	private String code;
+
+	private Data data;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Data getData() {
+		return this.data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data {
+
+		private String verificationToken;
+
+		private Boolean pass;
+
+		private String reason;
+
+		public String getVerificationToken() {
+			return this.verificationToken;
+		}
+
+		public void setVerificationToken(String verificationToken) {
+			this.verificationToken = verificationToken;
+		}
+
+		public Boolean getPass() {
+			return this.pass;
+		}
+
+		public void setPass(Boolean pass) {
+			this.pass = pass;
+		}
+
+		public String getReason() {
+			return this.reason;
+		}
+
+		public void setReason(String reason) {
+			this.reason = reason;
+		}
+	}
+
+	@Override
+	public ExecuteServerSideVerificationResponse getInstance(UnmarshallerContext context) {
+		return	ExecuteServerSideVerificationResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-facebody/src/main/java/com/aliyuncs/facebody/transform/v20200910/ExecuteServerSideVerificationResponseUnmarshaller.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.facebody.transform.v20200910;
+
+import com.aliyuncs.facebody.model.v20200910.ExecuteServerSideVerificationResponse;
+import com.aliyuncs.facebody.model.v20200910.ExecuteServerSideVerificationResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ExecuteServerSideVerificationResponseUnmarshaller {
+
+	public static ExecuteServerSideVerificationResponse unmarshall(ExecuteServerSideVerificationResponse executeServerSideVerificationResponse, UnmarshallerContext _ctx) {
+		
+		executeServerSideVerificationResponse.setRequestId(_ctx.stringValue("ExecuteServerSideVerificationResponse.RequestId"));
+		executeServerSideVerificationResponse.setMessage(_ctx.stringValue("ExecuteServerSideVerificationResponse.Message"));
+		executeServerSideVerificationResponse.setCode(_ctx.stringValue("ExecuteServerSideVerificationResponse.Code"));
+
+		Data data = new Data();
+		data.setVerificationToken(_ctx.stringValue("ExecuteServerSideVerificationResponse.Data.VerificationToken"));
+		data.setPass(_ctx.booleanValue("ExecuteServerSideVerificationResponse.Data.Pass"));
+		data.setReason(_ctx.stringValue("ExecuteServerSideVerificationResponse.Data.Reason"));
+		executeServerSideVerificationResponse.setData(data);
+	 
+	 	return executeServerSideVerificationResponse;
+	}
+}
