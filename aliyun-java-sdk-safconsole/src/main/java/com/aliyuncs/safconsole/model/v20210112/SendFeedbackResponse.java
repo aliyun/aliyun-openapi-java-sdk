afc95be9a663b86a670a07a86c1ff63837d5bf8f
sdk-team <sdk-team@alibabacloud.com>
2021-02-04 06:21:34 +0000

    Supported console.

---
+++ aliyun-java-sdk-safconsole/src/main/java/com/aliyuncs/safconsole/model/v20210112/SendFeedbackResponse.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.safconsole.model.v20210112;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.safconsole.transform.v20210112.SendFeedbackResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class SendFeedbackResponse extends AcsResponse {
+
+	private Integer code;
+
+	private String message;
+
+	private String requestId;
+
+	public Integer getCode() {
+		return this.code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	@Override
+	public SendFeedbackResponse getInstance(UnmarshallerContext context) {
+		return	SendFeedbackResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
