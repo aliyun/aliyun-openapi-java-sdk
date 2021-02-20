40fe5d7392a3c9670af1d69645d7ca574e83b49e
sdk-team <sdk-team@alibabacloud.com>
2021-02-20 15:28:59 +0000

    Add CheckServiceStatus.

---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/model/v20190808/ListDispatchRuleResponse.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.model.v20190808;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.arms.transform.v20190808.ListDispatchRuleResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListDispatchRuleResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<DispatchRule> dispatchRules;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<DispatchRule> getDispatchRules() {
+		return this.dispatchRules;
+	}
+
+	public void setDispatchRules(List<DispatchRule> dispatchRules) {
+		this.dispatchRules = dispatchRules;
+	}
+
+	public static class DispatchRule {
+
+		private Long ruleId;
+
+		private String name;
+
+		private String state;
+
+		public Long getRuleId() {
+			return this.ruleId;
+		}
+
+		public void setRuleId(Long ruleId) {
+			this.ruleId = ruleId;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getState() {
+			return this.state;
+		}
+
+		public void setState(String state) {
+			this.state = state;
+		}
+	}
+
+	@Override
+	public ListDispatchRuleResponse getInstance(UnmarshallerContext context) {
+		return	ListDispatchRuleResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/transform/v20190808/ListDispatchRuleResponseUnmarshaller.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.transform.v20190808;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.arms.model.v20190808.ListDispatchRuleResponse;
+import com.aliyuncs.arms.model.v20190808.ListDispatchRuleResponse.DispatchRule;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListDispatchRuleResponseUnmarshaller {
+
+	public static ListDispatchRuleResponse unmarshall(ListDispatchRuleResponse listDispatchRuleResponse, UnmarshallerContext _ctx) {
+		
+		listDispatchRuleResponse.setRequestId(_ctx.stringValue("ListDispatchRuleResponse.RequestId"));
+
+		List<DispatchRule> dispatchRules = new ArrayList<DispatchRule>();
+		for (int i = 0; i < _ctx.lengthValue("ListDispatchRuleResponse.DispatchRules.Length"); i++) {
+			DispatchRule dispatchRule = new DispatchRule();
+			dispatchRule.setRuleId(_ctx.longValue("ListDispatchRuleResponse.DispatchRules["+ i +"].RuleId"));
+			dispatchRule.setName(_ctx.stringValue("ListDispatchRuleResponse.DispatchRules["+ i +"].Name"));
+			dispatchRule.setState(_ctx.stringValue("ListDispatchRuleResponse.DispatchRules["+ i +"].State"));
+
+			dispatchRules.add(dispatchRule);
+		}
+		listDispatchRuleResponse.setDispatchRules(dispatchRules);
+	 
+	 	return listDispatchRuleResponse;
+	}
+}
