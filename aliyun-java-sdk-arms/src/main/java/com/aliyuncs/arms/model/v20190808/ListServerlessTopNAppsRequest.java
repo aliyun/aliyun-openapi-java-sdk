98668ea6c1772d814c483f32c7a6d8a1eb191ffe
sdk-team <sdk-team@alibabacloud.com>
2021-02-26 06:36:06 +0000

    Add ListServerlessTopNApps.

---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/model/v20190808/ListServerlessTopNAppsRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.model.v20190808;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.arms.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListServerlessTopNAppsRequest extends RpcAcsRequest<ListServerlessTopNAppsResponse> {
+	   
+
+	private Integer limit;
+
+	private String orderBy;
+	public ListServerlessTopNAppsRequest() {
+		super("ARMS", "2019-08-08", "ListServerlessTopNApps", "arms");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Integer getLimit() {
+		return this.limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+		if(limit != null){
+			putQueryParameter("Limit", limit.toString());
+		}
+	}
+
+	public String getOrderBy() {
+		return this.orderBy;
+	}
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+		if(orderBy != null){
+			putQueryParameter("OrderBy", orderBy);
+		}
+	}
+
+	@Override
+	public Class<ListServerlessTopNAppsResponse> getResponseClass() {
+		return ListServerlessTopNAppsResponse.class;
+	}
+
+}
