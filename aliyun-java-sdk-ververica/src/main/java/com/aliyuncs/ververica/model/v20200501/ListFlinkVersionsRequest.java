8676cc2b4b344de7d56157e05c9ebba1b8f91f3b
sdk-team <sdk-team@alibabacloud.com>
2021-03-24 02:22:50 +0000

    Dataphin OpenApi.

---
+++ aliyun-java-sdk-ververica/src/main/java/com/aliyuncs/ververica/model/v20200501/ListFlinkVersionsRequest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ververica.model.v20200501;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.ververica.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListFlinkVersionsRequest extends RoaAcsRequest<ListFlinkVersionsResponse> {
+	   
+
+	private String workspace;
+	public ListFlinkVersionsRequest() {
+		super("ververica", "2020-05-01", "ListFlinkVersions", "ververica");
+		setUriPattern("/pop/workspaces/[workspace]/api/v1/flink-version-meta.json");
+		setMethod(MethodType.GET);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getWorkspace() {
+		return this.workspace;
+	}
+
+	public void setWorkspace(String workspace) {
+		this.workspace = workspace;
+		if(workspace != null){
+			putPathParameter("workspace", workspace);
+		}
+	}
+
+	@Override
+	public Class<ListFlinkVersionsResponse> getResponseClass() {
+		return ListFlinkVersionsResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-ververica/src/main/java/com/aliyuncs/ververica/transform/v20200501/GetUdfArtifactResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ververica.transform.v20200501;
+
+import com.aliyuncs.ververica.model.v20200501.GetUdfArtifactResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetUdfArtifactResponseUnmarshaller {
+
+	public static GetUdfArtifactResponse unmarshall(GetUdfArtifactResponse getUdfArtifactResponse, UnmarshallerContext _ctx) {
+		
+		getUdfArtifactResponse.setData(_ctx.stringValue("GetUdfArtifactResponse.data"));
+		getUdfArtifactResponse.setRequestId(_ctx.stringValue("GetUdfArtifactResponse.requestId"));
+		getUdfArtifactResponse.setSuccess(_ctx.booleanValue("GetUdfArtifactResponse.success"));
+	 
+	 	return getUdfArtifactResponse;
+	}
+}
