a01f120a503c7f2ade92b4a05ed875615b1e7a1a
sdk-team <sdk-team@alibabacloud.com>
2021-04-16 08:34:48 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/model/v20200518/ImportDISyncTasksRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dataworks_public.model.v20200518;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.dataworks_public.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ImportDISyncTasksRequest extends RpcAcsRequest<ImportDISyncTasksResponse> {
+	   
+
+	private String taskType;
+
+	private String taskContent;
+
+	private String taskParam;
+
+	private Long projectId;
+	public ImportDISyncTasksRequest() {
+		super("dataworks-public", "2020-05-18", "ImportDISyncTasks");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getTaskType() {
+		return this.taskType;
+	}
+
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+		if(taskType != null){
+			putQueryParameter("TaskType", taskType);
+		}
+	}
+
+	public String getTaskContent() {
+		return this.taskContent;
+	}
+
+	public void setTaskContent(String taskContent) {
+		this.taskContent = taskContent;
+		if(taskContent != null){
+			putQueryParameter("TaskContent", taskContent);
+		}
+	}
+
+	public String getTaskParam() {
+		return this.taskParam;
+	}
+
+	public void setTaskParam(String taskParam) {
+		this.taskParam = taskParam;
+		if(taskParam != null){
+			putQueryParameter("TaskParam", taskParam);
+		}
+	}
+
+	public Long getProjectId() {
+		return this.projectId;
+	}
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+		if(projectId != null){
+			putQueryParameter("ProjectId", projectId.toString());
+		}
+	}
+
+	@Override
+	public Class<ImportDISyncTasksResponse> getResponseClass() {
+		return ImportDISyncTasksResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/transform/v20200518/ListDISyncTasksResponseUnmarshaller.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dataworks_public.transform.v20200518;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.dataworks_public.model.v20200518.ListDISyncTasksResponse;
+import com.aliyuncs.dataworks_public.model.v20200518.ListDISyncTasksResponse.TaskList;
+import com.aliyuncs.dataworks_public.model.v20200518.ListDISyncTasksResponse.TaskList.RealTimeSolutionListItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListDISyncTasksResponseUnmarshaller {
+
+	public static ListDISyncTasksResponse unmarshall(ListDISyncTasksResponse listDISyncTasksResponse, UnmarshallerContext _ctx) {
+		
+		listDISyncTasksResponse.setRequestId(_ctx.stringValue("ListDISyncTasksResponse.RequestId"));
+		listDISyncTasksResponse.setSuccess(_ctx.booleanValue("ListDISyncTasksResponse.Success"));
+
+		TaskList taskList = new TaskList();
+		taskList.setPageNumber(_ctx.integerValue("ListDISyncTasksResponse.TaskList.PageNumber"));
+		taskList.setPageSize(_ctx.integerValue("ListDISyncTasksResponse.TaskList.PageSize"));
+		taskList.setTotalCount(_ctx.integerValue("ListDISyncTasksResponse.TaskList.TotalCount"));
+
+		List<RealTimeSolutionListItem> realTimeSolutionList = new ArrayList<RealTimeSolutionListItem>();
+		for (int i = 0; i < _ctx.lengthValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList.Length"); i++) {
+			RealTimeSolutionListItem realTimeSolutionListItem = new RealTimeSolutionListItem();
+			realTimeSolutionListItem.setProcessId(_ctx.longValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList["+ i +"].ProcessId"));
+			realTimeSolutionListItem.setProcessName(_ctx.stringValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList["+ i +"].ProcessName"));
+			realTimeSolutionListItem.setTaskStatus(_ctx.stringValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList["+ i +"].TaskStatus"));
+			realTimeSolutionListItem.setTaskType(_ctx.stringValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList["+ i +"].TaskType"));
+			realTimeSolutionListItem.setCreateTime(_ctx.longValue("ListDISyncTasksResponse.TaskList.RealTimeSolutionList["+ i +"].CreateTime"));
+
+			realTimeSolutionList.add(realTimeSolutionListItem);
+		}
+		taskList.setRealTimeSolutionList(realTimeSolutionList);
+		listDISyncTasksResponse.setTaskList(taskList);
+	 
+	 	return listDISyncTasksResponse;
+	}
+}
