c4d52313d14b0ea9017b52d9370367ea5ae03f93
sdk-team <sdk-team@alibabacloud.com>
2021-05-08 07:43:41 +0000

    Support SLB DescribeLoadBalancerListeners.

---
+++ aliyun-java-sdk-slb/src/main/java/com/aliyuncs/slb/model/v20140515/DescribeLoadBalancerListenersRequest.java
@@ -0,0 +1,148 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.slb.model.v20140515;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.slb.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeLoadBalancerListenersRequest extends RpcAcsRequest<DescribeLoadBalancerListenersResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String nextToken;
+
+	private String resourceOwnerAccount;
+
+	private String ownerAccount;
+
+	private Long ownerId;
+
+	private String listenerProtocol;
+
+	private List<String> loadBalancerIds;
+
+	private Integer maxResults;
+	public DescribeLoadBalancerListenersRequest() {
+		super("Slb", "2014-05-15", "DescribeLoadBalancerListeners", "slb");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getNextToken() {
+		return this.nextToken;
+	}
+
+	public void setNextToken(String nextToken) {
+		this.nextToken = nextToken;
+		if(nextToken != null){
+			putQueryParameter("NextToken", nextToken);
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putQueryParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getListenerProtocol() {
+		return this.listenerProtocol;
+	}
+
+	public void setListenerProtocol(String listenerProtocol) {
+		this.listenerProtocol = listenerProtocol;
+		if(listenerProtocol != null){
+			putQueryParameter("ListenerProtocol", listenerProtocol);
+		}
+	}
+
+	public List<String> getLoadBalancerIds() {
+		return this.loadBalancerIds;
+	}
+
+	public void setLoadBalancerIds(List<String> loadBalancerIds) {
+		this.loadBalancerIds = loadBalancerIds;	
+		if (loadBalancerIds != null) {
+			for (int i = 0; i < loadBalancerIds.size(); i++) {
+				putQueryParameter("LoadBalancerId." + (i + 1) , loadBalancerIds.get(i));
+			}
+		}	
+	}
+
+	public Integer getMaxResults() {
+		return this.maxResults;
+	}
+
+	public void setMaxResults(Integer maxResults) {
+		this.maxResults = maxResults;
+		if(maxResults != null){
+			putQueryParameter("MaxResults", maxResults.toString());
+		}
+	}
+
+	@Override
+	public Class<DescribeLoadBalancerListenersResponse> getResponseClass() {
+		return DescribeLoadBalancerListenersResponse.class;
+	}
+
+}
