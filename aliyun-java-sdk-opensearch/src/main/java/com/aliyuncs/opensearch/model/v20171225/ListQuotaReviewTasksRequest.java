d348521daeee474d674b45e0ba961c07c025e965
sdk-team <sdk-team@alibabacloud.com>
2021-05-17 09:45:14 +0000

    Add API ListQuotaReviewTasks.

---
+++ aliyun-java-sdk-opensearch/src/main/java/com/aliyuncs/opensearch/model/v20171225/ListQuotaReviewTasksRequest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.opensearch.model.v20171225;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.opensearch.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListQuotaReviewTasksRequest extends RoaAcsRequest<ListQuotaReviewTasksResponse> {
+	   
+
+	private Integer pageSize;
+
+	private String appGroupIdentity;
+
+	private Integer pageNumber;
+	public ListQuotaReviewTasksRequest() {
+		super("OpenSearch", "2017-12-25", "ListQuotaReviewTasks", "opensearch");
+		setUriPattern("/v4/openapi/app-groups/[appGroupIdentity]/quota-review-tasks");
+		setMethod(MethodType.GET);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("pageSize", pageSize.toString());
+		}
+	}
+
+	public String getAppGroupIdentity() {
+		return this.appGroupIdentity;
+	}
+
+	public void setAppGroupIdentity(String appGroupIdentity) {
+		this.appGroupIdentity = appGroupIdentity;
+		if(appGroupIdentity != null){
+			putPathParameter("appGroupIdentity", appGroupIdentity);
+		}
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+		if(pageNumber != null){
+			putQueryParameter("pageNumber", pageNumber.toString());
+		}
+	}
+
+	@Override
+	public Class<ListQuotaReviewTasksResponse> getResponseClass() {
+		return ListQuotaReviewTasksResponse.class;
+	}
+
+}
