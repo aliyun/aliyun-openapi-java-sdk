3ba5cdb9feea03d92c11b3763ff68aa23f2113a1
sdk-team <sdk-team@alibabacloud.com>
2021-05-23 10:20:40 +0000

    New API- GetDrdsDbRdsRelationInfo, which can get private RDS list under certain DB out of DRDS instance ID and DB name.

---
+++ aliyun-java-sdk-drds/src/main/java/com/aliyuncs/drds/model/v20190123/GetDrdsDbRdsRelationInfoResponse.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.drds.model.v20190123;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.drds.transform.v20190123.GetDrdsDbRdsRelationInfoResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetDrdsDbRdsRelationInfoResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean success;
+
+	private List<DataItem> data;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public List<DataItem> getData() {
+		return this.data;
+	}
+
+	public void setData(List<DataItem> data) {
+		this.data = data;
+	}
+
+	public static class DataItem {
+
+		private String rdsInstanceId;
+
+		private String usedInstanceId;
+
+		private String usedInstanceType;
+
+		private List<String> readOnlyInstanceInfo;
+
+		public String getRdsInstanceId() {
+			return this.rdsInstanceId;
+		}
+
+		public void setRdsInstanceId(String rdsInstanceId) {
+			this.rdsInstanceId = rdsInstanceId;
+		}
+
+		public String getUsedInstanceId() {
+			return this.usedInstanceId;
+		}
+
+		public void setUsedInstanceId(String usedInstanceId) {
+			this.usedInstanceId = usedInstanceId;
+		}
+
+		public String getUsedInstanceType() {
+			return this.usedInstanceType;
+		}
+
+		public void setUsedInstanceType(String usedInstanceType) {
+			this.usedInstanceType = usedInstanceType;
+		}
+
+		public List<String> getReadOnlyInstanceInfo() {
+			return this.readOnlyInstanceInfo;
+		}
+
+		public void setReadOnlyInstanceInfo(List<String> readOnlyInstanceInfo) {
+			this.readOnlyInstanceInfo = readOnlyInstanceInfo;
+		}
+	}
+
+	@Override
+	public GetDrdsDbRdsRelationInfoResponse getInstance(UnmarshallerContext context) {
+		return	GetDrdsDbRdsRelationInfoResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-drds/src/main/java/com/aliyuncs/drds/transform/v20190123/GetDrdsDbRdsRelationInfoResponseUnmarshaller.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.drds.transform.v20190123;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.drds.model.v20190123.GetDrdsDbRdsRelationInfoResponse;
+import com.aliyuncs.drds.model.v20190123.GetDrdsDbRdsRelationInfoResponse.DataItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetDrdsDbRdsRelationInfoResponseUnmarshaller {
+
+	public static GetDrdsDbRdsRelationInfoResponse unmarshall(GetDrdsDbRdsRelationInfoResponse getDrdsDbRdsRelationInfoResponse, UnmarshallerContext _ctx) {
+		
+		getDrdsDbRdsRelationInfoResponse.setRequestId(_ctx.stringValue("GetDrdsDbRdsRelationInfoResponse.RequestId"));
+		getDrdsDbRdsRelationInfoResponse.setSuccess(_ctx.booleanValue("GetDrdsDbRdsRelationInfoResponse.Success"));
+
+		List<DataItem> data = new ArrayList<DataItem>();
+		for (int i = 0; i < _ctx.lengthValue("GetDrdsDbRdsRelationInfoResponse.Data.Length"); i++) {
+			DataItem dataItem = new DataItem();
+			dataItem.setRdsInstanceId(_ctx.stringValue("GetDrdsDbRdsRelationInfoResponse.Data["+ i +"].RdsInstanceId"));
+			dataItem.setUsedInstanceId(_ctx.stringValue("GetDrdsDbRdsRelationInfoResponse.Data["+ i +"].UsedInstanceId"));
+			dataItem.setUsedInstanceType(_ctx.stringValue("GetDrdsDbRdsRelationInfoResponse.Data["+ i +"].UsedInstanceType"));
+
+			List<String> readOnlyInstanceInfo = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("GetDrdsDbRdsRelationInfoResponse.Data["+ i +"].ReadOnlyInstanceInfo.Length"); j++) {
+				readOnlyInstanceInfo.add(_ctx.stringValue("GetDrdsDbRdsRelationInfoResponse.Data["+ i +"].ReadOnlyInstanceInfo["+ j +"]"));
+			}
+			dataItem.setReadOnlyInstanceInfo(readOnlyInstanceInfo);
+
+			data.add(dataItem);
+		}
+		getDrdsDbRdsRelationInfoResponse.setData(data);
+	 
+	 	return getDrdsDbRdsRelationInfoResponse;
+	}
+}
