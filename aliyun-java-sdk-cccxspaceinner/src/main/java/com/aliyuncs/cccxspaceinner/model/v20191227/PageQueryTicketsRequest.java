137354f43a22d3809ab430db3b0151213da3fa0b
sdk-team <sdk-team@alibabacloud.com>
2021-05-26 11:59:31 +0000

    Add QueryServicerByPage api.

---
+++ aliyun-java-sdk-cccxspaceinner/src/main/java/com/aliyuncs/cccxspaceinner/model/v20191227/PageQueryTicketsRequest.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cccxspaceinner.model.v20191227;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class PageQueryTicketsRequest extends RpcAcsRequest<PageQueryTicketsResponse> {
+	   
+
+	private Long buId;
+
+	private Integer pageNo;
+
+	private Integer pageSize;
+
+	private Integer ticketType;
+
+	private Long ticketId;
+
+	private Long memberId;
+	public PageQueryTicketsRequest() {
+		super("CCCXSpaceInner", "2019-12-27", "PageQueryTickets");
+		setMethod(MethodType.GET);
+	}
+
+	public Long getBuId() {
+		return this.buId;
+	}
+
+	public void setBuId(Long buId) {
+		this.buId = buId;
+		if(buId != null){
+			putQueryParameter("BuId", buId.toString());
+		}
+	}
+
+	public Integer getPageNo() {
+		return this.pageNo;
+	}
+
+	public void setPageNo(Integer pageNo) {
+		this.pageNo = pageNo;
+		if(pageNo != null){
+			putQueryParameter("PageNo", pageNo.toString());
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public Integer getTicketType() {
+		return this.ticketType;
+	}
+
+	public void setTicketType(Integer ticketType) {
+		this.ticketType = ticketType;
+		if(ticketType != null){
+			putQueryParameter("TicketType", ticketType.toString());
+		}
+	}
+
+	public Long getTicketId() {
+		return this.ticketId;
+	}
+
+	public void setTicketId(Long ticketId) {
+		this.ticketId = ticketId;
+		if(ticketId != null){
+			putQueryParameter("TicketId", ticketId.toString());
+		}
+	}
+
+	public Long getMemberId() {
+		return this.memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+		if(memberId != null){
+			putQueryParameter("MemberId", memberId.toString());
+		}
+	}
+
+	@Override
+	public Class<PageQueryTicketsResponse> getResponseClass() {
+		return PageQueryTicketsResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cccxspaceinner/src/main/java/com/aliyuncs/cccxspaceinner/transform/v20191227/PageQueryTicketsResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cccxspaceinner.transform.v20191227;
+
+import com.aliyuncs.cccxspaceinner.model.v20191227.PageQueryTicketsResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class PageQueryTicketsResponseUnmarshaller {
+
+	public static PageQueryTicketsResponse unmarshall(PageQueryTicketsResponse pageQueryTicketsResponse, UnmarshallerContext _ctx) {
+		
+		pageQueryTicketsResponse.setMessage(_ctx.stringValue("PageQueryTicketsResponse.Message"));
+		pageQueryTicketsResponse.setData(_ctx.stringValue("PageQueryTicketsResponse.Data"));
+		pageQueryTicketsResponse.setCode(_ctx.stringValue("PageQueryTicketsResponse.Code"));
+		pageQueryTicketsResponse.setSuccess(_ctx.booleanValue("PageQueryTicketsResponse.Success"));
+	 
+	 	return pageQueryTicketsResponse;
+	}
+}
