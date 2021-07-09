6a524ac5866608b8a8b76c02e61a991df55c8536
sdk-team <sdk-team@alibabacloud.com>
2021-07-09 02:59:16 +0000

    Add GetUserCommercialStatus.

---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/model/v20190808/GetUserCommercialStatusRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.model.v20190808;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.arms.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetUserCommercialStatusRequest extends RpcAcsRequest<GetUserCommercialStatusResponse> {
+	   
+
+	private String userId;
+
+	private String parentId;
+
+	private String targetUserId;
+	public GetUserCommercialStatusRequest() {
+		super("ARMS", "2019-08-08", "GetUserCommercialStatus", "arms");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putQueryParameter("UserId", userId);
+		}
+	}
+
+	public String getParentId() {
+		return this.parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+		if(parentId != null){
+			putQueryParameter("ParentId", parentId);
+		}
+	}
+
+	public String getTargetUserId() {
+		return this.targetUserId;
+	}
+
+	public void setTargetUserId(String targetUserId) {
+		this.targetUserId = targetUserId;
+		if(targetUserId != null){
+			putQueryParameter("TargetUserId", targetUserId);
+		}
+	}
+
+	@Override
+	public Class<GetUserCommercialStatusResponse> getResponseClass() {
+		return GetUserCommercialStatusResponse.class;
+	}
+
+}
