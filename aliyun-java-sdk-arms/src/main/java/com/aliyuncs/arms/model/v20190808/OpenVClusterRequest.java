986ab2f1b94cdec6cd6baa897f3e4b6713d7bc57
sdk-team <sdk-team@alibabacloud.com>
2021-07-27 06:36:04 +0000

    Update OpenVCluster.

---
+++ aliyun-java-sdk-arms/src/main/java/com/aliyuncs/arms/model/v20190808/OpenVClusterRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.arms.model.v20190808;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.arms.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class OpenVClusterRequest extends RpcAcsRequest<OpenVClusterResponse> {
+	   
+
+	private String clusterType;
+
+	private String product;
+
+	private Boolean recreateSwitch;
+
+	private Integer length;
+	public OpenVClusterRequest() {
+		super("ARMS", "2019-08-08", "OpenVCluster", "arms");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getClusterType() {
+		return this.clusterType;
+	}
+
+	public void setClusterType(String clusterType) {
+		this.clusterType = clusterType;
+		if(clusterType != null){
+			putQueryParameter("ClusterType", clusterType);
+		}
+	}
+
+	public String getProduct() {
+		return this.product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+		if(product != null){
+			putQueryParameter("Product", product);
+		}
+	}
+
+	public Boolean getRecreateSwitch() {
+		return this.recreateSwitch;
+	}
+
+	public void setRecreateSwitch(Boolean recreateSwitch) {
+		this.recreateSwitch = recreateSwitch;
+		if(recreateSwitch != null){
+			putQueryParameter("RecreateSwitch", recreateSwitch.toString());
+		}
+	}
+
+	public Integer getLength() {
+		return this.length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+		if(length != null){
+			putQueryParameter("Length", length.toString());
+		}
+	}
+
+	@Override
+	public Class<OpenVClusterResponse> getResponseClass() {
+		return OpenVClusterResponse.class;
+	}
+
+}
