1523692db7edbf74e694c7ec78b5f905fb9a59bb
sdk-team <sdk-team@alibabacloud.com>
2021-07-30 06:36:27 +0000

    Support Msc Common Query.

---
+++ aliyun-java-sdk-msccommonquery/src/main/java/com/aliyuncs/msccommonquery/model/v20210407/ListMessagesRequest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.msccommonquery.model.v20210407;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListMessagesRequest extends RpcAcsRequest<ListMessagesResponse> {
+	   
+
+	private Long startTimestamp;
+
+	private String locale;
+
+	private Long endTimestamp;
+
+	private String channelType;
+	public ListMessagesRequest() {
+		super("MscCommonQuery", "2021-04-07", "ListMessages");
+		setMethod(MethodType.POST);
+	}
+
+	public Long getStartTimestamp() {
+		return this.startTimestamp;
+	}
+
+	public void setStartTimestamp(Long startTimestamp) {
+		this.startTimestamp = startTimestamp;
+		if(startTimestamp != null){
+			putBodyParameter("StartTimestamp", startTimestamp.toString());
+		}
+	}
+
+	public String getLocale() {
+		return this.locale;
+	}
+
+	public void setLocale(String locale) {
+		this.locale = locale;
+		if(locale != null){
+			putQueryParameter("Locale", locale);
+		}
+	}
+
+	public Long getEndTimestamp() {
+		return this.endTimestamp;
+	}
+
+	public void setEndTimestamp(Long endTimestamp) {
+		this.endTimestamp = endTimestamp;
+		if(endTimestamp != null){
+			putBodyParameter("EndTimestamp", endTimestamp.toString());
+		}
+	}
+
+	public String getChannelType() {
+		return this.channelType;
+	}
+
+	public void setChannelType(String channelType) {
+		this.channelType = channelType;
+		if(channelType != null){
+			putBodyParameter("ChannelType", channelType);
+		}
+	}
+
+	@Override
+	public Class<ListMessagesResponse> getResponseClass() {
+		return ListMessagesResponse.class;
+	}
+
+}
