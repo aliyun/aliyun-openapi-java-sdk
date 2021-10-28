2e9ce2572a79941864db96819942712988d7af16
sdk-team <sdk-team@alibabacloud.com>
2021-10-28 06:46:21 +0000

    Add API UploadStreamByURL.

---
+++ aliyun-java-sdk-vod/src/main/java/com/aliyuncs/vod/model/v20170321/UploadStreamByURLRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vod.model.v20170321;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.vod.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UploadStreamByURLRequest extends RpcAcsRequest<UploadStreamByURLResponse> {
+	   
+
+	private String fileExtension;
+
+	private String mediaId;
+
+	private String userData;
+
+	private String definition;
+
+	private String streamURL;
+	public UploadStreamByURLRequest() {
+		super("vod", "2017-03-21", "UploadStreamByURL", "vod");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getFileExtension() {
+		return this.fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+		if(fileExtension != null){
+			putQueryParameter("FileExtension", fileExtension);
+		}
+	}
+
+	public String getMediaId() {
+		return this.mediaId;
+	}
+
+	public void setMediaId(String mediaId) {
+		this.mediaId = mediaId;
+		if(mediaId != null){
+			putQueryParameter("MediaId", mediaId);
+		}
+	}
+
+	public String getUserData() {
+		return this.userData;
+	}
+
+	public void setUserData(String userData) {
+		this.userData = userData;
+		if(userData != null){
+			putQueryParameter("UserData", userData);
+		}
+	}
+
+	public String getDefinition() {
+		return this.definition;
+	}
+
+	public void setDefinition(String definition) {
+		this.definition = definition;
+		if(definition != null){
+			putQueryParameter("Definition", definition);
+		}
+	}
+
+	public String getStreamURL() {
+		return this.streamURL;
+	}
+
+	public void setStreamURL(String streamURL) {
+		this.streamURL = streamURL;
+		if(streamURL != null){
+			putQueryParameter("StreamURL", streamURL);
+		}
+	}
+
+	@Override
+	public Class<UploadStreamByURLResponse> getResponseClass() {
+		return UploadStreamByURLResponse.class;
+	}
+
+}
