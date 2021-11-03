d6abc3822e40429fda5126cbe77f297ac5715cdb
yndu13 <yndu13@gmail.com>
2021-11-03 11:34:26 +0800

    fix test cases

--- aliyun-java-sdk-core/src/test/java/com/aliyuncs/auth/SignerTest.java
+++ aliyun-java-sdk-core/src/test/java/com/aliyuncs/auth/SignerTest.java
@@ -11,21 +11,21 @@ public class SignerTest {
     public void getSignerReturnSha256WithRSASigner() {
         AlibabaCloudCredentials credentials = mock(KeyPairCredentials.class);
         Signer signer = Signer.getSigner(credentials, SignatureVersion.V1, null);
-        Assert.assertEquals(true, SHA256withRSASigner.class.isAssignableFrom(signer.getClass()));
+        Assert.assertEquals(true, com.aliyuncs.auth.signers.SHA256withRSASigner.class.isAssignableFrom(signer.getClass()));
     }
 
     @Test
     public void getSignerReturnBearerTokenSigner() {
         AlibabaCloudCredentials credentials = mock(BearerTokenCredentials.class);
         Signer signer = Signer.getSigner(credentials, SignatureVersion.V1, null);
-        Assert.assertEquals(true, BearerTokenSigner.class.isAssignableFrom(signer.getClass()));
+        Assert.assertEquals(true, com.aliyuncs.auth.signers.BearerTokenSigner.class.isAssignableFrom(signer.getClass()));
     }
 
     @Test
     public void getSignerReturnHmacSHA1Signer() {
         AlibabaCloudCredentials credentials = mock(AlibabaCloudCredentials.class);
         Signer signer = Signer.getSigner(credentials, SignatureVersion.V1, null);
-        Assert.assertEquals(true, HmacSHA1Signer.class.isAssignableFrom(signer.getClass()));
+        Assert.assertEquals(true, com.aliyuncs.auth.signers.HmacSHA1Signer.class.isAssignableFrom(signer.getClass()));
     }
 
     @Test
