b119af76343b145e3f7ef54e5dde5c852ea94539
sdk-team <sdk-team@alibabacloud.com>
2021-12-09 08:53:13 +0000

    Init.

---
+++ aliyun-java-sdk-unimkt-dunshan/src/main/java/com/aliyuncs/unimkt_dunshan/model/v20211201/SdkCallApiServiceRequest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.unimkt_dunshan.model.v20211201;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class SdkCallApiServiceRequest extends RpcAcsRequest<SdkCallApiServiceResponse> {
+	   
+
+	@SerializedName("body")
+	private Map<String,String> body;
+	public SdkCallApiServiceRequest() {
+		super("UniMkt-dunshan", "2021-12-01", "SdkCallApiService");
+		setMethod(MethodType.POST);
+	}
+
+	public Map<String,String> getBody() {
+		return this.body;
+	}
+
+	public void setBody(Map<String,String> body) {
+		this.body = body;	
+		if (body != null) {
+			putBodyParameter("body" , new Gson().toJson(body));
+		}	
+	}
+
+	@Override
+	public Class<SdkCallApiServiceResponse> getResponseClass() {
+		return SdkCallApiServiceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-unimkt-dunshan/src/main/java/com/aliyuncs/unimkt_dunshan/transform/v20211201/SdkCallApiServiceResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.unimkt_dunshan.transform.v20211201;
+
+import com.aliyuncs.unimkt_dunshan.model.v20211201.SdkCallApiServiceResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SdkCallApiServiceResponseUnmarshaller {
+
+	public static SdkCallApiServiceResponse unmarshall(SdkCallApiServiceResponse sdkCallApiServiceResponse, UnmarshallerContext _ctx) {
+		
+		sdkCallApiServiceResponse.setRequestId(_ctx.stringValue("SdkCallApiServiceResponse.RequestId"));
+		sdkCallApiServiceResponse.setResult(_ctx.stringValue("SdkCallApiServiceResponse.Result"));
+		sdkCallApiServiceResponse.setSuccess(_ctx.booleanValue("SdkCallApiServiceResponse.Success"));
+		sdkCallApiServiceResponse.setErrorCode(_ctx.stringValue("SdkCallApiServiceResponse.ErrorCode"));
+		sdkCallApiServiceResponse.setMessage(_ctx.stringValue("SdkCallApiServiceResponse.Message"));
+	 
+	 	return sdkCallApiServiceResponse;
+	}
+}
