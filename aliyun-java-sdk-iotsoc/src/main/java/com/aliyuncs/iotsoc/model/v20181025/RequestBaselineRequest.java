b518dc58a0871bd2923218d7d6271f44cf4bffe0
sdk-team <sdk-team@alibabacloud.com>
2021-12-17 07:47:43 +0000

    Support vulnerability fix.

---
+++ aliyun-java-sdk-iotsoc/src/main/java/com/aliyuncs/iotsoc/model/v20181025/RequestBaselineRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.iotsoc.model.v20181025;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.iotsoc.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class RequestBaselineRequest extends RpcAcsRequest<RequestBaselineResponse> {
+	   
+
+	private String productKey;
+
+	private String firmwareVersion;
+	public RequestBaselineRequest() {
+		super("Iotsoc", "2018-10-25", "RequestBaseline");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getProductKey() {
+		return this.productKey;
+	}
+
+	public void setProductKey(String productKey) {
+		this.productKey = productKey;
+		if(productKey != null){
+			putBodyParameter("ProductKey", productKey);
+		}
+	}
+
+	public String getFirmwareVersion() {
+		return this.firmwareVersion;
+	}
+
+	public void setFirmwareVersion(String firmwareVersion) {
+		this.firmwareVersion = firmwareVersion;
+		if(firmwareVersion != null){
+			putBodyParameter("FirmwareVersion", firmwareVersion);
+		}
+	}
+
+	@Override
+	public Class<RequestBaselineResponse> getResponseClass() {
+		return RequestBaselineResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-iotsoc/src/main/java/com/aliyuncs/iotsoc/transform/v20181025/QueryPatchInfoListResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.iotsoc.transform.v20181025;
+
+import com.aliyuncs.iotsoc.model.v20181025.QueryPatchInfoListResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryPatchInfoListResponseUnmarshaller {
+
+	public static QueryPatchInfoListResponse unmarshall(QueryPatchInfoListResponse queryPatchInfoListResponse, UnmarshallerContext _ctx) {
+		
+		queryPatchInfoListResponse.setRequestId(_ctx.stringValue("QueryPatchInfoListResponse.RequestId"));
+		queryPatchInfoListResponse.setCode(_ctx.stringValue("QueryPatchInfoListResponse.Code"));
+		queryPatchInfoListResponse.setData(_ctx.stringValue("QueryPatchInfoListResponse.Data"));
+	 
+	 	return queryPatchInfoListResponse;
+	}
+}
