e36881b5a622cc7ce3f12094b4d367903ec4b2dd
sdk-team <sdk-team@alibabacloud.com>
2021-12-22 09:18:47 +0000

    IPMA.

---
+++ aliyun-java-sdk-cciotgw/src/main/java/com/aliyuncs/cciotgw/model/v20210721/DeleteCloudConnectorGatewayPrivilegeRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cciotgw.model.v20210721;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DeleteCloudConnectorGatewayPrivilegeRequest extends RpcAcsRequest<DeleteCloudConnectorGatewayPrivilegeResponse> {
+	   
+
+	private String userAliUid;
+
+	private String type;
+
+	private String ioTCloudConnectorGatewayId;
+	public DeleteCloudConnectorGatewayPrivilegeRequest() {
+		super("CCIoTGW", "2021-07-21", "DeleteCloudConnectorGatewayPrivilege");
+		setMethod(MethodType.POST);
+	}
+
+	public String getUserAliUid() {
+		return this.userAliUid;
+	}
+
+	public void setUserAliUid(String userAliUid) {
+		this.userAliUid = userAliUid;
+		if(userAliUid != null){
+			putQueryParameter("UserAliUid", userAliUid);
+		}
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+		if(type != null){
+			putQueryParameter("Type", type);
+		}
+	}
+
+	public String getIoTCloudConnectorGatewayId() {
+		return this.ioTCloudConnectorGatewayId;
+	}
+
+	public void setIoTCloudConnectorGatewayId(String ioTCloudConnectorGatewayId) {
+		this.ioTCloudConnectorGatewayId = ioTCloudConnectorGatewayId;
+		if(ioTCloudConnectorGatewayId != null){
+			putQueryParameter("IoTCloudConnectorGatewayId", ioTCloudConnectorGatewayId);
+		}
+	}
+
+	@Override
+	public Class<DeleteCloudConnectorGatewayPrivilegeResponse> getResponseClass() {
+		return DeleteCloudConnectorGatewayPrivilegeResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cciotgw/src/main/java/com/aliyuncs/cciotgw/transform/v20210721/DeleteCloudConnectorGatewayPrivilegeResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cciotgw.transform.v20210721;
+
+import com.aliyuncs.cciotgw.model.v20210721.DeleteCloudConnectorGatewayPrivilegeResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DeleteCloudConnectorGatewayPrivilegeResponseUnmarshaller {
+
+	public static DeleteCloudConnectorGatewayPrivilegeResponse unmarshall(DeleteCloudConnectorGatewayPrivilegeResponse deleteCloudConnectorGatewayPrivilegeResponse, UnmarshallerContext _ctx) {
+		
+		deleteCloudConnectorGatewayPrivilegeResponse.setRequestId(_ctx.stringValue("DeleteCloudConnectorGatewayPrivilegeResponse.RequestId"));
+		deleteCloudConnectorGatewayPrivilegeResponse.setCode(_ctx.stringValue("DeleteCloudConnectorGatewayPrivilegeResponse.Code"));
+		deleteCloudConnectorGatewayPrivilegeResponse.setMessage(_ctx.stringValue("DeleteCloudConnectorGatewayPrivilegeResponse.Message"));
+		deleteCloudConnectorGatewayPrivilegeResponse.setSuccess(_ctx.stringValue("DeleteCloudConnectorGatewayPrivilegeResponse.Success"));
+	 
+	 	return deleteCloudConnectorGatewayPrivilegeResponse;
+	}
+}
