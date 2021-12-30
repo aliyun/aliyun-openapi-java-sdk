398248f4907a98a848a22f12b8190bd782494652
sdk-team <sdk-team@alibabacloud.com>
2021-12-30 03:55:53 +0000

    Add new interface.

---
+++ aliyun-java-sdk-ccc/src/main/java/com/aliyuncs/ccc/model/v20170705/GetPredictiveJobGroupResponse.java
@@ -0,0 +1,382 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ccc.model.v20170705;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ccc.transform.v20170705.GetPredictiveJobGroupResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetPredictiveJobGroupResponse extends AcsResponse {
+
+	private Integer httpStatusCode;
+
+	private String code;
+
+	private String message;
+
+	private String requestId;
+
+	private Boolean success;
+
+	private JobGroup jobGroup;
+
+	public Integer getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(Integer httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public JobGroup getJobGroup() {
+		return this.jobGroup;
+	}
+
+	public void setJobGroup(JobGroup jobGroup) {
+		this.jobGroup = jobGroup;
+	}
+
+	public static class JobGroup {
+
+		private Long creationTime;
+
+		private String jobGroupId;
+
+		private String description;
+
+		private String skillGroupId;
+
+		private String skillGroupName;
+
+		private String instanceId;
+
+		private String name;
+
+		private Long ratio;
+
+		private Progress progress;
+
+		private Strategy strategy;
+
+		public Long getCreationTime() {
+			return this.creationTime;
+		}
+
+		public void setCreationTime(Long creationTime) {
+			this.creationTime = creationTime;
+		}
+
+		public String getJobGroupId() {
+			return this.jobGroupId;
+		}
+
+		public void setJobGroupId(String jobGroupId) {
+			this.jobGroupId = jobGroupId;
+		}
+
+		public String getDescription() {
+			return this.description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getSkillGroupId() {
+			return this.skillGroupId;
+		}
+
+		public void setSkillGroupId(String skillGroupId) {
+			this.skillGroupId = skillGroupId;
+		}
+
+		public String getSkillGroupName() {
+			return this.skillGroupName;
+		}
+
+		public void setSkillGroupName(String skillGroupName) {
+			this.skillGroupName = skillGroupName;
+		}
+
+		public String getInstanceId() {
+			return this.instanceId;
+		}
+
+		public void setInstanceId(String instanceId) {
+			this.instanceId = instanceId;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Long getRatio() {
+			return this.ratio;
+		}
+
+		public void setRatio(Long ratio) {
+			this.ratio = ratio;
+		}
+
+		public Progress getProgress() {
+			return this.progress;
+		}
+
+		public void setProgress(Progress progress) {
+			this.progress = progress;
+		}
+
+		public Strategy getStrategy() {
+			return this.strategy;
+		}
+
+		public void setStrategy(Strategy strategy) {
+			this.strategy = strategy;
+		}
+
+		public static class Progress {
+
+			private String status;
+
+			private Integer totalNotAnswered;
+
+			private Long startTime;
+
+			private Integer totalJobs;
+
+			private Integer totalCompleted;
+
+			private Integer duration;
+
+			private List<KeyValuePair> categories;
+
+			public String getStatus() {
+				return this.status;
+			}
+
+			public void setStatus(String status) {
+				this.status = status;
+			}
+
+			public Integer getTotalNotAnswered() {
+				return this.totalNotAnswered;
+			}
+
+			public void setTotalNotAnswered(Integer totalNotAnswered) {
+				this.totalNotAnswered = totalNotAnswered;
+			}
+
+			public Long getStartTime() {
+				return this.startTime;
+			}
+
+			public void setStartTime(Long startTime) {
+				this.startTime = startTime;
+			}
+
+			public Integer getTotalJobs() {
+				return this.totalJobs;
+			}
+
+			public void setTotalJobs(Integer totalJobs) {
+				this.totalJobs = totalJobs;
+			}
+
+			public Integer getTotalCompleted() {
+				return this.totalCompleted;
+			}
+
+			public void setTotalCompleted(Integer totalCompleted) {
+				this.totalCompleted = totalCompleted;
+			}
+
+			public Integer getDuration() {
+				return this.duration;
+			}
+
+			public void setDuration(Integer duration) {
+				this.duration = duration;
+			}
+
+			public List<KeyValuePair> getCategories() {
+				return this.categories;
+			}
+
+			public void setCategories(List<KeyValuePair> categories) {
+				this.categories = categories;
+			}
+
+			public static class KeyValuePair {
+
+				private String key;
+
+				private String value;
+
+				public String getKey() {
+					return this.key;
+				}
+
+				public void setKey(String key) {
+					this.key = key;
+				}
+
+				public String getValue() {
+					return this.value;
+				}
+
+				public void setValue(String value) {
+					this.value = value;
+				}
+			}
+		}
+
+		public static class Strategy {
+
+			private Integer minAttemptInterval;
+
+			private Long endTime;
+
+			private Long startTime;
+
+			private String strategyId;
+
+			private Integer maxAttemptsPerDay;
+
+			private List<TimeFrame> workingTime;
+
+			public Integer getMinAttemptInterval() {
+				return this.minAttemptInterval;
+			}
+
+			public void setMinAttemptInterval(Integer minAttemptInterval) {
+				this.minAttemptInterval = minAttemptInterval;
+			}
+
+			public Long getEndTime() {
+				return this.endTime;
+			}
+
+			public void setEndTime(Long endTime) {
+				this.endTime = endTime;
+			}
+
+			public Long getStartTime() {
+				return this.startTime;
+			}
+
+			public void setStartTime(Long startTime) {
+				this.startTime = startTime;
+			}
+
+			public String getStrategyId() {
+				return this.strategyId;
+			}
+
+			public void setStrategyId(String strategyId) {
+				this.strategyId = strategyId;
+			}
+
+			public Integer getMaxAttemptsPerDay() {
+				return this.maxAttemptsPerDay;
+			}
+
+			public void setMaxAttemptsPerDay(Integer maxAttemptsPerDay) {
+				this.maxAttemptsPerDay = maxAttemptsPerDay;
+			}
+
+			public List<TimeFrame> getWorkingTime() {
+				return this.workingTime;
+			}
+
+			public void setWorkingTime(List<TimeFrame> workingTime) {
+				this.workingTime = workingTime;
+			}
+
+			public static class TimeFrame {
+
+				private String endTime;
+
+				private String beginTime;
+
+				public String getEndTime() {
+					return this.endTime;
+				}
+
+				public void setEndTime(String endTime) {
+					this.endTime = endTime;
+				}
+
+				public String getBeginTime() {
+					return this.beginTime;
+				}
+
+				public void setBeginTime(String beginTime) {
+					this.beginTime = beginTime;
+				}
+			}
+		}
+	}
+
+	@Override
+	public GetPredictiveJobGroupResponse getInstance(UnmarshallerContext context) {
+		return	GetPredictiveJobGroupResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
