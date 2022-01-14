1e42e3206a63a742a0c69e43867ede9862b91280
sdk-team <sdk-team@alibabacloud.com>
2022-01-14 06:41:25 +0000

    Support More Enhance Options.

---
+++ aliyun-java-sdk-mpaas/src/main/java/com/aliyuncs/mpaas/model/v20201028/QueryMdsUpgradeTaskDetailRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.mpaas.model.v20201028;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.mpaas.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryMdsUpgradeTaskDetailRequest extends RpcAcsRequest<QueryMdsUpgradeTaskDetailResponse> {
+	   
+
+	private String tenantId;
+
+	private Long taskId;
+
+	private String appId;
+
+	private String workspaceId;
+	public QueryMdsUpgradeTaskDetailRequest() {
+		super("mPaaS", "2020-10-28", "QueryMdsUpgradeTaskDetail");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+		if(tenantId != null){
+			putBodyParameter("TenantId", tenantId);
+		}
+	}
+
+	public Long getTaskId() {
+		return this.taskId;
+	}
+
+	public void setTaskId(Long taskId) {
+		this.taskId = taskId;
+		if(taskId != null){
+			putBodyParameter("TaskId", taskId.toString());
+		}
+	}
+
+	public String getAppId() {
+		return this.appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+		if(appId != null){
+			putBodyParameter("AppId", appId);
+		}
+	}
+
+	public String getWorkspaceId() {
+		return this.workspaceId;
+	}
+
+	public void setWorkspaceId(String workspaceId) {
+		this.workspaceId = workspaceId;
+		if(workspaceId != null){
+			putBodyParameter("WorkspaceId", workspaceId);
+		}
+	}
+
+	@Override
+	public Class<QueryMdsUpgradeTaskDetailResponse> getResponseClass() {
+		return QueryMdsUpgradeTaskDetailResponse.class;
+	}
+
+}
