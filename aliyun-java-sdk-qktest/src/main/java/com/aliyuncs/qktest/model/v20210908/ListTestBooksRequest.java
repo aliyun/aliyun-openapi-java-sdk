6255caedad378112c8dfe97ac5375e3c007d7522
sdk-team <sdk-team@alibabacloud.com>
2022-02-28 08:00:58 +0000

    Fix.

---
+++ aliyun-java-sdk-qktest/src/main/java/com/aliyuncs/qktest/model/v20210908/ListTestBooksRequest.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.qktest.model.v20210908;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListTestBooksRequest extends RpcAcsRequest<ListTestBooksResponse> {
+	   
+
+	private String filter;
+
+	private String nextToken;
+
+	private List<String> testBookIdss;
+
+	private Integer maxResults;
+	public ListTestBooksRequest() {
+		super("QKTest", "2021-09-08", "ListTestBooks");
+		setMethod(MethodType.GET);
+	}
+
+	public String getFilter() {
+		return this.filter;
+	}
+
+	public void setFilter(String filter) {
+		this.filter = filter;
+		if(filter != null){
+			putQueryParameter("Filter", filter);
+		}
+	}
+
+	public String getNextToken() {
+		return this.nextToken;
+	}
+
+	public void setNextToken(String nextToken) {
+		this.nextToken = nextToken;
+		if(nextToken != null){
+			putQueryParameter("NextToken", nextToken);
+		}
+	}
+
+	public List<String> getTestBookIdss() {
+		return this.testBookIdss;
+	}
+
+	public void setTestBookIdss(List<String> testBookIdss) {
+		this.testBookIdss = testBookIdss;	
+		if (testBookIdss != null) {
+			for (int i = 0; i < testBookIdss.size(); i++) {
+				putQueryParameter("TestBookIds." + (i + 1) , testBookIdss.get(i));
+			}
+		}	
+	}
+
+	public Integer getMaxResults() {
+		return this.maxResults;
+	}
+
+	public void setMaxResults(Integer maxResults) {
+		this.maxResults = maxResults;
+		if(maxResults != null){
+			putQueryParameter("MaxResults", maxResults.toString());
+		}
+	}
+
+	@Override
+	public Class<ListTestBooksResponse> getResponseClass() {
+		return ListTestBooksResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-qktest/src/main/java/com/aliyuncs/qktest/transform/v20210908/ListTestBooksResponseUnmarshaller.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.qktest.transform.v20210908;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.qktest.model.v20210908.ListTestBooksResponse;
+import com.aliyuncs.qktest.model.v20210908.ListTestBooksResponse.TestBook;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListTestBooksResponseUnmarshaller {
+
+	public static ListTestBooksResponse unmarshall(ListTestBooksResponse listTestBooksResponse, UnmarshallerContext _ctx) {
+		
+		listTestBooksResponse.setRequestId(_ctx.stringValue("ListTestBooksResponse.RequestId"));
+		listTestBooksResponse.setTotalCount(_ctx.integerValue("ListTestBooksResponse.TotalCount"));
+		listTestBooksResponse.setNextToken(_ctx.integerValue("ListTestBooksResponse.NextToken"));
+
+		List<TestBook> testBooks = new ArrayList<TestBook>();
+		for (int i = 0; i < _ctx.lengthValue("ListTestBooksResponse.TestBooks.Length"); i++) {
+			TestBook testBook = new TestBook();
+			testBook.setResourceGroupId(_ctx.stringValue("ListTestBooksResponse.TestBooks["+ i +"].ResourceGroupId"));
+			testBook.setCreateTime(_ctx.stringValue("ListTestBooksResponse.TestBooks["+ i +"].CreateTime"));
+			testBook.setTestBookId(_ctx.stringValue("ListTestBooksResponse.TestBooks["+ i +"].TestBookId"));
+			testBook.setTestBookName(_ctx.stringValue("ListTestBooksResponse.TestBooks["+ i +"].TestBookName"));
+
+			testBooks.add(testBook);
+		}
+		listTestBooksResponse.setTestBooks(testBooks);
+	 
+	 	return listTestBooksResponse;
+	}
+}
