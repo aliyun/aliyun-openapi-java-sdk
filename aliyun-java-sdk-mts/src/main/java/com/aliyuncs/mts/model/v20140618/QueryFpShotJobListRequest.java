b4247bff94ec67a366c14951c2217489512aae8c
sdk-team <sdk-team@alibabacloud.com>
2022-03-10 07:17:42 +0000

    Add SubmitSyncFpShotJob.

--- aliyun-java-sdk-mts/src/main/java/com/aliyuncs/mts/model/v20140618/QueryFpShotJobListRequest.java
+++ aliyun-java-sdk-mts/src/main/java/com/aliyuncs/mts/model/v20140618/QueryFpShotJobListRequest.java
@@ -27,27 +27,11 @@ public class QueryFpShotJobListRequest extends RpcAcsRequest<QueryFpShotJobListR
 
 	private Long resourceOwnerId;
 
-	private String nextPageToken;
-
-	private String startOfJobCreatedTimeRange;
-
-	private String userData;
-
-	private String state;
-
-	private String endOfJobCreatedTimeRange;
-
 	private String resourceOwnerAccount;
 
 	private String ownerAccount;
 
-	private Long maximumPageSize;
-
 	private Long ownerId;
-
-	private String pipelineId;
-
-	private String primaryKeyList;
 
 	private String jobIds;
 	public QueryFpShotJobListRequest() {
@@ -67,61 +51,6 @@ public class QueryFpShotJobListRequest extends RpcAcsRequest<QueryFpShotJobListR
 		this.resourceOwnerId = resourceOwnerId;
 		if(resourceOwnerId != null){
 			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
-		}
-	}
-
-	public String getNextPageToken() {
-		return this.nextPageToken;
-	}
-
-	public void setNextPageToken(String nextPageToken) {
-		this.nextPageToken = nextPageToken;
-		if(nextPageToken != null){
-			putQueryParameter("NextPageToken", nextPageToken);
-		}
-	}
-
-	public String getStartOfJobCreatedTimeRange() {
-		return this.startOfJobCreatedTimeRange;
-	}
-
-	public void setStartOfJobCreatedTimeRange(String startOfJobCreatedTimeRange) {
-		this.startOfJobCreatedTimeRange = startOfJobCreatedTimeRange;
-		if(startOfJobCreatedTimeRange != null){
-			putQueryParameter("StartOfJobCreatedTimeRange", startOfJobCreatedTimeRange);
-		}
-	}
-
-	public String getUserData() {
-		return this.userData;
-	}
-
-	public void setUserData(String userData) {
-		this.userData = userData;
-		if(userData != null){
-			putQueryParameter("UserData", userData);
-		}
-	}
-
-	public String getState() {
-		return this.state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-		if(state != null){
-			putQueryParameter("State", state);
-		}
-	}
-
-	public String getEndOfJobCreatedTimeRange() {
-		return this.endOfJobCreatedTimeRange;
-	}
-
-	public void setEndOfJobCreatedTimeRange(String endOfJobCreatedTimeRange) {
-		this.endOfJobCreatedTimeRange = endOfJobCreatedTimeRange;
-		if(endOfJobCreatedTimeRange != null){
-			putQueryParameter("EndOfJobCreatedTimeRange", endOfJobCreatedTimeRange);
 		}
 	}
 
@@ -147,17 +76,6 @@ public class QueryFpShotJobListRequest extends RpcAcsRequest<QueryFpShotJobListR
 		}
 	}
 
-	public Long getMaximumPageSize() {
-		return this.maximumPageSize;
-	}
-
-	public void setMaximumPageSize(Long maximumPageSize) {
-		this.maximumPageSize = maximumPageSize;
-		if(maximumPageSize != null){
-			putQueryParameter("MaximumPageSize", maximumPageSize.toString());
-		}
-	}
-
 	public Long getOwnerId() {
 		return this.ownerId;
 	}
@@ -166,28 +84,6 @@ public class QueryFpShotJobListRequest extends RpcAcsRequest<QueryFpShotJobListR
 		this.ownerId = ownerId;
 		if(ownerId != null){
 			putQueryParameter("OwnerId", ownerId.toString());
-		}
-	}
-
-	public String getPipelineId() {
-		return this.pipelineId;
-	}
-
-	public void setPipelineId(String pipelineId) {
-		this.pipelineId = pipelineId;
-		if(pipelineId != null){
-			putQueryParameter("PipelineId", pipelineId);
-		}
-	}
-
-	public String getPrimaryKeyList() {
-		return this.primaryKeyList;
-	}
-
-	public void setPrimaryKeyList(String primaryKeyList) {
-		this.primaryKeyList = primaryKeyList;
-		if(primaryKeyList != null){
-			putQueryParameter("PrimaryKeyList", primaryKeyList);
 		}
 	}
 
