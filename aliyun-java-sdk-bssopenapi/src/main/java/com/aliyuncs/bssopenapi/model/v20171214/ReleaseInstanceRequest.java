78a0a6583797030693514e6eabde8b725b6439ec
sdk-team <sdk-team@alibabacloud.com>
2022-05-11 02:32:20 +0000

    Add release api and publish.

---
+++ aliyun-java-sdk-bssopenapi/src/main/java/com/aliyuncs/bssopenapi/model/v20171214/ReleaseInstanceRequest.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.bssopenapi.model.v20171214;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.bssopenapi.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ReleaseInstanceRequest extends RpcAcsRequest<ReleaseInstanceResponse> {
+	   
+
+	private String productCode;
+
+	private String subscriptionType;
+
+	private Long ownerId;
+
+	private String productType;
+
+	private String instanceIds;
+
+	private String region;
+
+	private String renewStatus;
+	public ReleaseInstanceRequest() {
+		super("BssOpenApi", "2017-12-14", "ReleaseInstance");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getProductCode() {
+		return this.productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+		if(productCode != null){
+			putQueryParameter("ProductCode", productCode);
+		}
+	}
+
+	public String getSubscriptionType() {
+		return this.subscriptionType;
+	}
+
+	public void setSubscriptionType(String subscriptionType) {
+		this.subscriptionType = subscriptionType;
+		if(subscriptionType != null){
+			putQueryParameter("SubscriptionType", subscriptionType);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getProductType() {
+		return this.productType;
+	}
+
+	public void setProductType(String productType) {
+		this.productType = productType;
+		if(productType != null){
+			putQueryParameter("ProductType", productType);
+		}
+	}
+
+	public String getInstanceIds() {
+		return this.instanceIds;
+	}
+
+	public void setInstanceIds(String instanceIds) {
+		this.instanceIds = instanceIds;
+		if(instanceIds != null){
+			putQueryParameter("InstanceIds", instanceIds);
+		}
+	}
+
+	public String getRegion() {
+		return this.region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+		if(region != null){
+			putQueryParameter("Region", region);
+		}
+	}
+
+	public String getRenewStatus() {
+		return this.renewStatus;
+	}
+
+	public void setRenewStatus(String renewStatus) {
+		this.renewStatus = renewStatus;
+		if(renewStatus != null){
+			putQueryParameter("RenewStatus", renewStatus);
+		}
+	}
+
+	@Override
+	public Class<ReleaseInstanceResponse> getResponseClass() {
+		return ReleaseInstanceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-bssopenapi/src/main/java/com/aliyuncs/bssopenapi/transform/v20171214/ReleaseInstanceResponseUnmarshaller.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.bssopenapi.transform.v20171214;
+
+import com.aliyuncs.bssopenapi.model.v20171214.ReleaseInstanceResponse;
+import com.aliyuncs.bssopenapi.model.v20171214.ReleaseInstanceResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ReleaseInstanceResponseUnmarshaller {
+
+	public static ReleaseInstanceResponse unmarshall(ReleaseInstanceResponse releaseInstanceResponse, UnmarshallerContext _ctx) {
+		
+		releaseInstanceResponse.setRequestId(_ctx.stringValue("ReleaseInstanceResponse.RequestId"));
+		releaseInstanceResponse.setMessage(_ctx.stringValue("ReleaseInstanceResponse.Message"));
+		releaseInstanceResponse.setCode(_ctx.stringValue("ReleaseInstanceResponse.Code"));
+		releaseInstanceResponse.setSuccess(_ctx.booleanValue("ReleaseInstanceResponse.Success"));
+
+		Data data = new Data();
+		data.setReleaseResult(_ctx.booleanValue("ReleaseInstanceResponse.Data.ReleaseResult"));
+		data.setHostId(_ctx.stringValue("ReleaseInstanceResponse.Data.HostId"));
+		releaseInstanceResponse.setData(data);
+	 
+	 	return releaseInstanceResponse;
+	}
+}
