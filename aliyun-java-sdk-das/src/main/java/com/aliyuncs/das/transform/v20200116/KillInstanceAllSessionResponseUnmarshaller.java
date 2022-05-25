24c9bd317c779f394b5165287793e5b33979ba0f
sdk-team <sdk-team@alibabacloud.com>
2022-05-25 14:06:29 +0000

    Kill Redis instance all sessions api.

---
+++ aliyun-java-sdk-das/src/main/java/com/aliyuncs/das/transform/v20200116/KillInstanceAllSessionResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.das.transform.v20200116;
+
+import com.aliyuncs.das.model.v20200116.KillInstanceAllSessionResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class KillInstanceAllSessionResponseUnmarshaller {
+
+	public static KillInstanceAllSessionResponse unmarshall(KillInstanceAllSessionResponse killInstanceAllSessionResponse, UnmarshallerContext _ctx) {
+		
+		killInstanceAllSessionResponse.setRequestId(_ctx.stringValue("KillInstanceAllSessionResponse.RequestId"));
+		killInstanceAllSessionResponse.setCode(_ctx.stringValue("KillInstanceAllSessionResponse.Code"));
+		killInstanceAllSessionResponse.setMessage(_ctx.stringValue("KillInstanceAllSessionResponse.Message"));
+		killInstanceAllSessionResponse.setData(_ctx.stringValue("KillInstanceAllSessionResponse.Data"));
+		killInstanceAllSessionResponse.setSuccess(_ctx.stringValue("KillInstanceAllSessionResponse.Success"));
+	 
+	 	return killInstanceAllSessionResponse;
+	}
+}
