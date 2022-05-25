76b3fd6a641dc0060c560f0c60fe119957a5e5d6
sdk-team <sdk-team@alibabacloud.com>
2022-05-25 09:22:12 +0000

    Test.

---
+++ aliyun-java-sdk-ververica/src/main/java/com/aliyuncs/ververica/model/v20200501/ReplaceDeploymentResponse.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ververica.model.v20200501;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ververica.transform.v20200501.ReplaceDeploymentResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ReplaceDeploymentResponse extends AcsResponse {
+
+	private Boolean success;
+
+	private String requestId;
+
+	private Integer data;
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Integer getData() {
+		return this.data;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
+	@Override
+	public ReplaceDeploymentResponse getInstance(UnmarshallerContext context) {
+		return	ReplaceDeploymentResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-ververica/src/main/java/com/aliyuncs/ververica/transform/v20200501/ReplaceDeploymentResponseUnmarshaller.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ververica.transform.v20200501;
+
+import com.aliyuncs.ververica.model.v20200501.ReplaceDeploymentResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ReplaceDeploymentResponseUnmarshaller {
+
+	public static ReplaceDeploymentResponse unmarshall(ReplaceDeploymentResponse replaceDeploymentResponse, UnmarshallerContext _ctx) {
+		
+		replaceDeploymentResponse.setSuccess(_ctx.booleanValue("ReplaceDeploymentResponse.success"));
+		replaceDeploymentResponse.setRequestId(_ctx.stringValue("ReplaceDeploymentResponse.requestId"));
+		replaceDeploymentResponse.setData(_ctx.integerValue("ReplaceDeploymentResponse.data"));
+	 
+	 	return replaceDeploymentResponse;
+	}
+}
