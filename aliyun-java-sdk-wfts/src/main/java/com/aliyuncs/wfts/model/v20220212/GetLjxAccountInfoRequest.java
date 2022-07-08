1429ca387cf6736bc233dcdd7bf31e7d2f3d97ed
sdk-team <sdk-team@alibabacloud.com>
2022-07-08 03:59:31 +0000

    Automatically generate sdk tasks.

---
+++ aliyun-java-sdk-wfts/src/main/java/com/aliyuncs/wfts/model/v20220212/GetLjxAccountInfoRequest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.wfts.model.v20220212;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetLjxAccountInfoRequest extends RoaAcsRequest<GetLjxAccountInfoResponse> {
+	   
+
+	private String ljxAccountInfoId;
+	public GetLjxAccountInfoRequest() {
+		super("Wfts", "2022-02-12", "GetLjxAccountInfo");
+		setUriPattern("/get/ljx/acc");
+		setMethod(MethodType.GET);
+	}
+
+	public String getLjxAccountInfoId() {
+		return this.ljxAccountInfoId;
+	}
+
+	public void setLjxAccountInfoId(String ljxAccountInfoId) {
+		this.ljxAccountInfoId = ljxAccountInfoId;
+		if(ljxAccountInfoId != null){
+			putQueryParameter("LjxAccountInfoId", ljxAccountInfoId);
+		}
+	}
+
+	@Override
+	public Class<GetLjxAccountInfoResponse> getResponseClass() {
+		return GetLjxAccountInfoResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-wfts/src/main/java/com/aliyuncs/wfts/model/v20220212/GetLjxAccountInfoResponse.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.wfts.model.v20220212;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.wfts.transform.v20220212.GetLjxAccountInfoResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetLjxAccountInfoResponse extends AcsResponse {
+
+	private String ljxAccountInfoId;
+
+	private String requestId;
+
+	public String getLjxAccountInfoId() {
+		return this.ljxAccountInfoId;
+	}
+
+	public void setLjxAccountInfoId(String ljxAccountInfoId) {
+		this.ljxAccountInfoId = ljxAccountInfoId;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	@Override
+	public GetLjxAccountInfoResponse getInstance(UnmarshallerContext context) {
+		return	GetLjxAccountInfoResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
