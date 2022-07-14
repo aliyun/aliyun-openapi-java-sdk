9680e1953a3fba1c200300485b781fed14174c60
sdk-team <sdk-team@alibabacloud.com>
2022-07-14 06:42:53 +0000

    Support API ARMInstance.

---
+++ aliyun-java-sdk-ens/src/main/java/com/aliyuncs/ens/model/v20171110/CreateARMServerInstancesRequest.java
@@ -0,0 +1,192 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ens.model.v20171110;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateARMServerInstancesRequest extends RpcAcsRequest<CreateARMServerInstancesResponse> {
+	   
+
+	private String keyPairName;
+
+	private String resolution;
+
+	private Integer frequency;
+
+	private String ensRegionId;
+
+	private Integer period;
+
+	private String periodUnit;
+
+	private Boolean autoRenew;
+
+	private String imageId;
+
+	private String serverType;
+
+	private String instanceType;
+
+	private Integer amount;
+
+	private String payType;
+	public CreateARMServerInstancesRequest() {
+		super("Ens", "2017-11-10", "CreateARMServerInstances", "ens");
+		setMethod(MethodType.POST);
+	}
+
+	public String getKeyPairName() {
+		return this.keyPairName;
+	}
+
+	public void setKeyPairName(String keyPairName) {
+		this.keyPairName = keyPairName;
+		if(keyPairName != null){
+			putQueryParameter("KeyPairName", keyPairName);
+		}
+	}
+
+	public String getResolution() {
+		return this.resolution;
+	}
+
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+		if(resolution != null){
+			putQueryParameter("Resolution", resolution);
+		}
+	}
+
+	public Integer getFrequency() {
+		return this.frequency;
+	}
+
+	public void setFrequency(Integer frequency) {
+		this.frequency = frequency;
+		if(frequency != null){
+			putQueryParameter("Frequency", frequency.toString());
+		}
+	}
+
+	public String getEnsRegionId() {
+		return this.ensRegionId;
+	}
+
+	public void setEnsRegionId(String ensRegionId) {
+		this.ensRegionId = ensRegionId;
+		if(ensRegionId != null){
+			putQueryParameter("EnsRegionId", ensRegionId);
+		}
+	}
+
+	public Integer getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(Integer period) {
+		this.period = period;
+		if(period != null){
+			putQueryParameter("Period", period.toString());
+		}
+	}
+
+	public String getPeriodUnit() {
+		return this.periodUnit;
+	}
+
+	public void setPeriodUnit(String periodUnit) {
+		this.periodUnit = periodUnit;
+		if(periodUnit != null){
+			putQueryParameter("PeriodUnit", periodUnit);
+		}
+	}
+
+	public Boolean getAutoRenew() {
+		return this.autoRenew;
+	}
+
+	public void setAutoRenew(Boolean autoRenew) {
+		this.autoRenew = autoRenew;
+		if(autoRenew != null){
+			putQueryParameter("AutoRenew", autoRenew.toString());
+		}
+	}
+
+	public String getImageId() {
+		return this.imageId;
+	}
+
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+		if(imageId != null){
+			putQueryParameter("ImageId", imageId);
+		}
+	}
+
+	public String getServerType() {
+		return this.serverType;
+	}
+
+	public void setServerType(String serverType) {
+		this.serverType = serverType;
+		if(serverType != null){
+			putQueryParameter("ServerType", serverType);
+		}
+	}
+
+	public String getInstanceType() {
+		return this.instanceType;
+	}
+
+	public void setInstanceType(String instanceType) {
+		this.instanceType = instanceType;
+		if(instanceType != null){
+			putQueryParameter("InstanceType", instanceType);
+		}
+	}
+
+	public Integer getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+		if(amount != null){
+			putQueryParameter("Amount", amount.toString());
+		}
+	}
+
+	public String getPayType() {
+		return this.payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+		if(payType != null){
+			putQueryParameter("PayType", payType);
+		}
+	}
+
+	@Override
+	public Class<CreateARMServerInstancesResponse> getResponseClass() {
+		return CreateARMServerInstancesResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-ens/src/main/java/com/aliyuncs/ens/model/v20171110/CreateARMServerInstancesResponse.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ens.model.v20171110;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ens.transform.v20171110.CreateARMServerInstancesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateARMServerInstancesResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String orderId;
+
+	private List<String> instanceIds;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getOrderId() {
+		return this.orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public List<String> getInstanceIds() {
+		return this.instanceIds;
+	}
+
+	public void setInstanceIds(List<String> instanceIds) {
+		this.instanceIds = instanceIds;
+	}
+
+	@Override
+	public CreateARMServerInstancesResponse getInstance(UnmarshallerContext context) {
+		return	CreateARMServerInstancesResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
