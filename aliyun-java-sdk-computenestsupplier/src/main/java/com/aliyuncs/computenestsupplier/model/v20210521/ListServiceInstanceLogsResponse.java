8064933c9752351eaf387f4b9bbe80c391461b1c
sdk-team <sdk-team@alibabacloud.com>
2022-07-21 12:07:33 +0000

    Modify tag parameters name.

---
+++ aliyun-java-sdk-computenestsupplier/src/main/java/com/aliyuncs/computenestsupplier/model/v20210521/ListServiceInstanceLogsResponse.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.computenestsupplier.model.v20210521;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.computenestsupplier.transform.v20210521.ListServiceInstanceLogsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListServiceInstanceLogsResponse extends AcsResponse {
+
+	private String nextToken;
+
+	private String requestId;
+
+	private String maxResults;
+
+	private List<ServiceInstanceLogs> serviceInstancesLogs;
+
+	public String getNextToken() {
+		return this.nextToken;
+	}
+
+	public void setNextToken(String nextToken) {
+		this.nextToken = nextToken;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getMaxResults() {
+		return this.maxResults;
+	}
+
+	public void setMaxResults(String maxResults) {
+		this.maxResults = maxResults;
+	}
+
+	public List<ServiceInstanceLogs> getServiceInstancesLogs() {
+		return this.serviceInstancesLogs;
+	}
+
+	public void setServiceInstancesLogs(List<ServiceInstanceLogs> serviceInstancesLogs) {
+		this.serviceInstancesLogs = serviceInstancesLogs;
+	}
+
+	public static class ServiceInstanceLogs {
+
+		private String timestamp;
+
+		private String serviceInstanceId;
+
+		private String source;
+
+		private String content;
+
+		private String logType;
+
+		private String resourceType;
+
+		private String resourceId;
+
+		private String status;
+
+		public String getTimestamp() {
+			return this.timestamp;
+		}
+
+		public void setTimestamp(String timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		public String getServiceInstanceId() {
+			return this.serviceInstanceId;
+		}
+
+		public void setServiceInstanceId(String serviceInstanceId) {
+			this.serviceInstanceId = serviceInstanceId;
+		}
+
+		public String getSource() {
+			return this.source;
+		}
+
+		public void setSource(String source) {
+			this.source = source;
+		}
+
+		public String getContent() {
+			return this.content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public String getLogType() {
+			return this.logType;
+		}
+
+		public void setLogType(String logType) {
+			this.logType = logType;
+		}
+
+		public String getResourceType() {
+			return this.resourceType;
+		}
+
+		public void setResourceType(String resourceType) {
+			this.resourceType = resourceType;
+		}
+
+		public String getResourceId() {
+			return this.resourceId;
+		}
+
+		public void setResourceId(String resourceId) {
+			this.resourceId = resourceId;
+		}
+
+		public String getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+	}
+
+	@Override
+	public ListServiceInstanceLogsResponse getInstance(UnmarshallerContext context) {
+		return	ListServiceInstanceLogsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-computenestsupplier/src/main/java/com/aliyuncs/computenestsupplier/transform/v20210521/ListServiceInstanceLogsResponseUnmarshaller.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.computenestsupplier.transform.v20210521;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.computenestsupplier.model.v20210521.ListServiceInstanceLogsResponse;
+import com.aliyuncs.computenestsupplier.model.v20210521.ListServiceInstanceLogsResponse.ServiceInstanceLogs;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListServiceInstanceLogsResponseUnmarshaller {
+
+	public static ListServiceInstanceLogsResponse unmarshall(ListServiceInstanceLogsResponse listServiceInstanceLogsResponse, UnmarshallerContext _ctx) {
+		
+		listServiceInstanceLogsResponse.setRequestId(_ctx.stringValue("ListServiceInstanceLogsResponse.RequestId"));
+		listServiceInstanceLogsResponse.setNextToken(_ctx.stringValue("ListServiceInstanceLogsResponse.NextToken"));
+		listServiceInstanceLogsResponse.setMaxResults(_ctx.stringValue("ListServiceInstanceLogsResponse.MaxResults"));
+
+		List<ServiceInstanceLogs> serviceInstancesLogs = new ArrayList<ServiceInstanceLogs>();
+		for (int i = 0; i < _ctx.lengthValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs.Length"); i++) {
+			ServiceInstanceLogs serviceInstanceLogs = new ServiceInstanceLogs();
+			serviceInstanceLogs.setTimestamp(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].Timestamp"));
+			serviceInstanceLogs.setServiceInstanceId(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].ServiceInstanceId"));
+			serviceInstanceLogs.setSource(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].Source"));
+			serviceInstanceLogs.setContent(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].Content"));
+			serviceInstanceLogs.setLogType(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].LogType"));
+			serviceInstanceLogs.setResourceType(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].ResourceType"));
+			serviceInstanceLogs.setResourceId(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].ResourceId"));
+			serviceInstanceLogs.setStatus(_ctx.stringValue("ListServiceInstanceLogsResponse.ServiceInstancesLogs["+ i +"].Status"));
+
+			serviceInstancesLogs.add(serviceInstanceLogs);
+		}
+		listServiceInstanceLogsResponse.setServiceInstancesLogs(serviceInstancesLogs);
+	 
+	 	return listServiceInstanceLogsResponse;
+	}
+}
