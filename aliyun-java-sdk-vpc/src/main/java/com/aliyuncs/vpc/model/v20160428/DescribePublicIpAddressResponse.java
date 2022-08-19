bfb73e959232aa063970ecf3365fa45b920d1c80
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 09:59:10 +0000

    Add DescribePublicIpAddress Interface.

---
+++ aliyun-java-sdk-vpc/src/main/java/com/aliyuncs/vpc/model/v20160428/DescribePublicIpAddressResponse.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vpc.model.v20160428;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.vpc.transform.v20160428.DescribePublicIpAddressResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribePublicIpAddressResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean success;
+
+	private String code;
+
+	private String message;
+
+	private Integer pageSize;
+
+	private Integer pageNumber;
+
+	private Integer totalCount;
+
+	private String regionId;
+
+	private String publicIpAddress;
+
+	private List<String> ipRange;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public Integer getPageNumber() {
+		return this.pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public Integer getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(Integer totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public String getRegionId() {
+		return this.regionId;
+	}
+
+	public void setRegionId(String regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getPublicIpAddress() {
+		return this.publicIpAddress;
+	}
+
+	public void setPublicIpAddress(String publicIpAddress) {
+		this.publicIpAddress = publicIpAddress;
+	}
+
+	public List<String> getIpRange() {
+		return this.ipRange;
+	}
+
+	public void setIpRange(List<String> ipRange) {
+		this.ipRange = ipRange;
+	}
+
+	@Override
+	public DescribePublicIpAddressResponse getInstance(UnmarshallerContext context) {
+		return	DescribePublicIpAddressResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-vpc/src/main/java/com/aliyuncs/vpc/transform/v20160428/DescribePublicIpAddressResponseUnmarshaller.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vpc.transform.v20160428;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.vpc.model.v20160428.DescribePublicIpAddressResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribePublicIpAddressResponseUnmarshaller {
+
+	public static DescribePublicIpAddressResponse unmarshall(DescribePublicIpAddressResponse describePublicIpAddressResponse, UnmarshallerContext _ctx) {
+		
+		describePublicIpAddressResponse.setRequestId(_ctx.stringValue("DescribePublicIpAddressResponse.RequestId"));
+		describePublicIpAddressResponse.setSuccess(_ctx.booleanValue("DescribePublicIpAddressResponse.Success"));
+		describePublicIpAddressResponse.setCode(_ctx.stringValue("DescribePublicIpAddressResponse.Code"));
+		describePublicIpAddressResponse.setMessage(_ctx.stringValue("DescribePublicIpAddressResponse.Message"));
+		describePublicIpAddressResponse.setPageSize(_ctx.integerValue("DescribePublicIpAddressResponse.PageSize"));
+		describePublicIpAddressResponse.setPageNumber(_ctx.integerValue("DescribePublicIpAddressResponse.PageNumber"));
+		describePublicIpAddressResponse.setTotalCount(_ctx.integerValue("DescribePublicIpAddressResponse.TotalCount"));
+		describePublicIpAddressResponse.setRegionId(_ctx.stringValue("DescribePublicIpAddressResponse.RegionId"));
+		describePublicIpAddressResponse.setPublicIpAddress(_ctx.stringValue("DescribePublicIpAddressResponse.PublicIpAddress"));
+
+		List<String> ipRange = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("DescribePublicIpAddressResponse.IpRange.Length"); i++) {
+			ipRange.add(_ctx.stringValue("DescribePublicIpAddressResponse.IpRange["+ i +"]"));
+		}
+		describePublicIpAddressResponse.setIpRange(ipRange);
+	 
+	 	return describePublicIpAddressResponse;
+	}
+}
