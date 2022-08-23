a7f5b434ed5f259394057400414e2f185e9ca336
sdk-team <sdk-team@alibabacloud.com>
2022-08-23 08:22:16 +0000

    QuerySmsReports.

---
+++ aliyun-java-sdk-idaas-doraemon/src/main/java/com/aliyuncs/idaas_doraemon/model/v20210520/QuerySmsReportsResponse.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.idaas_doraemon.model.v20210520;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.idaas_doraemon.transform.v20210520.QuerySmsReportsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QuerySmsReportsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Long totalElements;
+
+	private List<SmsReportsItem> smsReports;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Long getTotalElements() {
+		return this.totalElements;
+	}
+
+	public void setTotalElements(Long totalElements) {
+		this.totalElements = totalElements;
+	}
+
+	public List<SmsReportsItem> getSmsReports() {
+		return this.smsReports;
+	}
+
+	public void setSmsReports(List<SmsReportsItem> smsReports) {
+		this.smsReports = smsReports;
+	}
+
+	public static class SmsReportsItem {
+
+		private String tid;
+
+		private String mobile;
+
+		private String code;
+
+		private String stat;
+
+		private String sn;
+
+		private String tenantId;
+
+		private String appId;
+
+		private String eventId;
+
+		private String time;
+
+		public String getTid() {
+			return this.tid;
+		}
+
+		public void setTid(String tid) {
+			this.tid = tid;
+		}
+
+		public String getMobile() {
+			return this.mobile;
+		}
+
+		public void setMobile(String mobile) {
+			this.mobile = mobile;
+		}
+
+		public String getCode() {
+			return this.code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getStat() {
+			return this.stat;
+		}
+
+		public void setStat(String stat) {
+			this.stat = stat;
+		}
+
+		public String getSn() {
+			return this.sn;
+		}
+
+		public void setSn(String sn) {
+			this.sn = sn;
+		}
+
+		public String getTenantId() {
+			return this.tenantId;
+		}
+
+		public void setTenantId(String tenantId) {
+			this.tenantId = tenantId;
+		}
+
+		public String getAppId() {
+			return this.appId;
+		}
+
+		public void setAppId(String appId) {
+			this.appId = appId;
+		}
+
+		public String getEventId() {
+			return this.eventId;
+		}
+
+		public void setEventId(String eventId) {
+			this.eventId = eventId;
+		}
+
+		public String getTime() {
+			return this.time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+	}
+
+	@Override
+	public QuerySmsReportsResponse getInstance(UnmarshallerContext context) {
+		return	QuerySmsReportsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-idaas-doraemon/src/main/java/com/aliyuncs/idaas_doraemon/transform/v20210520/QuerySmsReportsResponseUnmarshaller.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.idaas_doraemon.transform.v20210520;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.idaas_doraemon.model.v20210520.QuerySmsReportsResponse;
+import com.aliyuncs.idaas_doraemon.model.v20210520.QuerySmsReportsResponse.SmsReportsItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QuerySmsReportsResponseUnmarshaller {
+
+	public static QuerySmsReportsResponse unmarshall(QuerySmsReportsResponse querySmsReportsResponse, UnmarshallerContext _ctx) {
+		
+		querySmsReportsResponse.setRequestId(_ctx.stringValue("QuerySmsReportsResponse.RequestId"));
+		querySmsReportsResponse.setTotalElements(_ctx.longValue("QuerySmsReportsResponse.TotalElements"));
+
+		List<SmsReportsItem> smsReports = new ArrayList<SmsReportsItem>();
+		for (int i = 0; i < _ctx.lengthValue("QuerySmsReportsResponse.SmsReports.Length"); i++) {
+			SmsReportsItem smsReportsItem = new SmsReportsItem();
+			smsReportsItem.setTid(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Tid"));
+			smsReportsItem.setMobile(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Mobile"));
+			smsReportsItem.setCode(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Code"));
+			smsReportsItem.setStat(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Stat"));
+			smsReportsItem.setSn(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Sn"));
+			smsReportsItem.setTenantId(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].TenantId"));
+			smsReportsItem.setAppId(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].AppId"));
+			smsReportsItem.setEventId(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].EventId"));
+			smsReportsItem.setTime(_ctx.stringValue("QuerySmsReportsResponse.SmsReports["+ i +"].Time"));
+
+			smsReports.add(smsReportsItem);
+		}
+		querySmsReportsResponse.setSmsReports(smsReports);
+	 
+	 	return querySmsReportsResponse;
+	}
+}
