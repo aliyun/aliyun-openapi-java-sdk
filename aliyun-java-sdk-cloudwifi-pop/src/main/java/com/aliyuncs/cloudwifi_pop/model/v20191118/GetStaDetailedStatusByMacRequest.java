a4db0efd7b741cdfc611cff2920ca39ab041d306
sdk-team <sdk-team@alibabacloud.com>
2022-09-01 07:20:08 +0000

    Add two APIs.

---
+++ aliyun-java-sdk-cloudwifi-pop/src/main/java/com/aliyuncs/cloudwifi_pop/model/v20191118/GetStaDetailedStatusByMacRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudwifi_pop.model.v20191118;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.cloudwifi_pop.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetStaDetailedStatusByMacRequest extends RpcAcsRequest<GetStaDetailedStatusByMacResponse> {
+	   
+
+	private String staMac;
+
+	private String appName;
+
+	private String appCode;
+	public GetStaDetailedStatusByMacRequest() {
+		super("cloudwifi-pop", "2019-11-18", "GetStaDetailedStatusByMac", "cloudap");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getStaMac() {
+		return this.staMac;
+	}
+
+	public void setStaMac(String staMac) {
+		this.staMac = staMac;
+		if(staMac != null){
+			putQueryParameter("StaMac", staMac);
+		}
+	}
+
+	public String getAppName() {
+		return this.appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+		if(appName != null){
+			putQueryParameter("AppName", appName);
+		}
+	}
+
+	public String getAppCode() {
+		return this.appCode;
+	}
+
+	public void setAppCode(String appCode) {
+		this.appCode = appCode;
+		if(appCode != null){
+			putQueryParameter("AppCode", appCode);
+		}
+	}
+
+	@Override
+	public Class<GetStaDetailedStatusByMacResponse> getResponseClass() {
+		return GetStaDetailedStatusByMacResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-cloudwifi-pop/src/main/java/com/aliyuncs/cloudwifi_pop/transform/v20191118/JudgeXingTianBusinessResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudwifi_pop.transform.v20191118;
+
+import com.aliyuncs.cloudwifi_pop.model.v20191118.JudgeXingTianBusinessResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class JudgeXingTianBusinessResponseUnmarshaller {
+
+	public static JudgeXingTianBusinessResponse unmarshall(JudgeXingTianBusinessResponse judgeXingTianBusinessResponse, UnmarshallerContext _ctx) {
+		
+		judgeXingTianBusinessResponse.setRequestId(_ctx.stringValue("JudgeXingTianBusinessResponse.RequestId"));
+		judgeXingTianBusinessResponse.setData(_ctx.booleanValue("JudgeXingTianBusinessResponse.Data"));
+		judgeXingTianBusinessResponse.setErrorMessage(_ctx.stringValue("JudgeXingTianBusinessResponse.ErrorMessage"));
+		judgeXingTianBusinessResponse.setIsSuccess(_ctx.booleanValue("JudgeXingTianBusinessResponse.IsSuccess"));
+		judgeXingTianBusinessResponse.setErrorCode(_ctx.integerValue("JudgeXingTianBusinessResponse.ErrorCode"));
+	 
+	 	return judgeXingTianBusinessResponse;
+	}
+}
