4ff7308215feb2b78729d7c6d6251a65f8f5e579
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 03:15:19 +0000

    Automatically generate sdk tasks.

---
+++ aliyun-java-sdk-dytnsapi/src/main/java/com/aliyuncs/dytnsapi/transform/v20200217/DescribePhoneTwiceTelVerifyResponseUnmarshaller.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dytnsapi.transform.v20200217;
+
+import com.aliyuncs.dytnsapi.model.v20200217.DescribePhoneTwiceTelVerifyResponse;
+import com.aliyuncs.dytnsapi.model.v20200217.DescribePhoneTwiceTelVerifyResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribePhoneTwiceTelVerifyResponseUnmarshaller {
+
+	public static DescribePhoneTwiceTelVerifyResponse unmarshall(DescribePhoneTwiceTelVerifyResponse describePhoneTwiceTelVerifyResponse, UnmarshallerContext _ctx) {
+		
+		describePhoneTwiceTelVerifyResponse.setRequestId(_ctx.stringValue("DescribePhoneTwiceTelVerifyResponse.RequestId"));
+		describePhoneTwiceTelVerifyResponse.setCode(_ctx.stringValue("DescribePhoneTwiceTelVerifyResponse.Code"));
+		describePhoneTwiceTelVerifyResponse.setMessage(_ctx.stringValue("DescribePhoneTwiceTelVerifyResponse.Message"));
+
+		Data data = new Data();
+		data.setVerifyResult(_ctx.stringValue("DescribePhoneTwiceTelVerifyResponse.Data.VerifyResult"));
+		data.setCarrier(_ctx.stringValue("DescribePhoneTwiceTelVerifyResponse.Data.Carrier"));
+		describePhoneTwiceTelVerifyResponse.setData(data);
+	 
+	 	return describePhoneTwiceTelVerifyResponse;
+	}
+}
