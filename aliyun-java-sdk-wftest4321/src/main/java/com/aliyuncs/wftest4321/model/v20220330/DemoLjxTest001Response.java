f21ecf640f1e7e2125591de4e6a81e5aeafabb68
sdk-team <sdk-team@alibabacloud.com>
2022-10-21 08:00:07 +0000

    Jeep1021tesst.

---
+++ aliyun-java-sdk-wftest4321/src/main/java/com/aliyuncs/wftest4321/model/v20220330/DemoLjxTest001Response.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.wftest4321.model.v20220330;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.wftest4321.transform.v20220330.DemoLjxTest001ResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DemoLjxTest001Response extends AcsResponse {
+
+	private String requestId;
+
+	private String code;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public DemoLjxTest001Response getInstance(UnmarshallerContext context) {
+		return	DemoLjxTest001ResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
