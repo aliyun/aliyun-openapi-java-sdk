6a8c6930c1a56b4963ba22b5e78f77b8266cd0c0
sdk-team <sdk-team@alibabacloud.com>
2022-11-02 12:13:58 +0000

    Automatically generate sdk tasks.

--- aliyun-java-sdk-ecs/src/main/java/com/aliyuncs/ecs/model/v20140526/InvokeCommandRequest.java
+++ aliyun-java-sdk-ecs/src/main/java/com/aliyuncs/ecs/model/v20140526/InvokeCommandRequest.java
@@ -44,6 +44,8 @@ public class InvokeCommandRequest extends RpcAcsRequest<InvokeCommandResponse> {
 
 	private String windowsPasswordName;
 
+	private List<Tag> tags;
+
 	private Boolean timed;
 
 	private String resourceOwnerAccount;
@@ -156,6 +158,20 @@ public class InvokeCommandRequest extends RpcAcsRequest<InvokeCommandResponse> {
 		}
 	}
 
+	public List<Tag> getTags() {
+		return this.tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;	
+		if (tags != null) {
+			for (int depth1 = 0; depth1 < tags.size(); depth1++) {
+				putQueryParameter("Tag." + (depth1 + 1) + ".Key" , tags.get(depth1).getKey());
+				putQueryParameter("Tag." + (depth1 + 1) + ".Value" , tags.get(depth1).getValue());
+			}
+		}	
+	}
+
 	public Boolean getTimed() {
 		return this.timed;
 	}
@@ -243,6 +259,29 @@ public class InvokeCommandRequest extends RpcAcsRequest<InvokeCommandResponse> {
 		this.username = username;
 		if(username != null){
 			putQueryParameter("Username", username);
+		}
+	}
+
+	public static class Tag {
+
+		private String key;
+
+		private String value;
+
+		public String getKey() {
+			return this.key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
 		}
 	}
 
