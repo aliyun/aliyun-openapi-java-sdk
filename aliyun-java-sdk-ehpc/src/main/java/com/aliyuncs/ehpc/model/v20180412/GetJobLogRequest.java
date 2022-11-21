8ecfcfb2ad594066fedf511d20e5fceea9f51a83
sdk-team <sdk-team@alibabacloud.com>
2022-11-21 07:18:24 +0000

    RunasUserPassword is not required in CreateJobFile.

---
+++ aliyun-java-sdk-ehpc/src/main/java/com/aliyuncs/ehpc/model/v20180412/GetJobLogRequest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ehpc.model.v20180412;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.ehpc.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetJobLogRequest extends RpcAcsRequest<GetJobLogResponse> {
+	   
+
+	private Long offset;
+
+	private String execHost;
+
+	private String clusterId;
+
+	private String jobId;
+
+	private Integer size;
+	public GetJobLogRequest() {
+		super("EHPC", "2018-04-12", "GetJobLog");
+		setMethod(MethodType.GET);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getOffset() {
+		return this.offset;
+	}
+
+	public void setOffset(Long offset) {
+		this.offset = offset;
+		if(offset != null){
+			putQueryParameter("Offset", offset.toString());
+		}
+	}
+
+	public String getExecHost() {
+		return this.execHost;
+	}
+
+	public void setExecHost(String execHost) {
+		this.execHost = execHost;
+		if(execHost != null){
+			putQueryParameter("ExecHost", execHost);
+		}
+	}
+
+	public String getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(String clusterId) {
+		this.clusterId = clusterId;
+		if(clusterId != null){
+			putQueryParameter("ClusterId", clusterId);
+		}
+	}
+
+	public String getJobId() {
+		return this.jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+		if(jobId != null){
+			putQueryParameter("JobId", jobId);
+		}
+	}
+
+	public Integer getSize() {
+		return this.size;
+	}
+
+	public void setSize(Integer size) {
+		this.size = size;
+		if(size != null){
+			putQueryParameter("Size", size.toString());
+		}
+	}
+
+	@Override
+	public Class<GetJobLogResponse> getResponseClass() {
+		return GetJobLogResponse.class;
+	}
+
+}
