fa2156177801a0c2f183caef531b1d212cdc0e41
sdk-team <sdk-team@alibabacloud.com>
2022-12-06 09:42:24 +0000

    Basic endpoint support NLB.

---
+++ aliyun-java-sdk-ga/src/main/java/com/aliyuncs/ga/model/v20191120/UpdateAdditionalCertificateWithListenerRequest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ga.model.v20191120;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.ga.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateAdditionalCertificateWithListenerRequest extends RpcAcsRequest<UpdateAdditionalCertificateWithListenerResponse> {
+	   
+
+	private Boolean dryRun;
+
+	private String clientToken;
+
+	private String certificateId;
+
+	private String listenerId;
+
+	private String domain;
+
+	private String acceleratorId;
+	public UpdateAdditionalCertificateWithListenerRequest() {
+		super("Ga", "2019-11-20", "UpdateAdditionalCertificateWithListener", "gaplus");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Boolean getDryRun() {
+		return this.dryRun;
+	}
+
+	public void setDryRun(Boolean dryRun) {
+		this.dryRun = dryRun;
+		if(dryRun != null){
+			putQueryParameter("DryRun", dryRun.toString());
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getCertificateId() {
+		return this.certificateId;
+	}
+
+	public void setCertificateId(String certificateId) {
+		this.certificateId = certificateId;
+		if(certificateId != null){
+			putQueryParameter("CertificateId", certificateId);
+		}
+	}
+
+	public String getListenerId() {
+		return this.listenerId;
+	}
+
+	public void setListenerId(String listenerId) {
+		this.listenerId = listenerId;
+		if(listenerId != null){
+			putQueryParameter("ListenerId", listenerId);
+		}
+	}
+
+	public String getDomain() {
+		return this.domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+		if(domain != null){
+			putQueryParameter("Domain", domain);
+		}
+	}
+
+	public String getAcceleratorId() {
+		return this.acceleratorId;
+	}
+
+	public void setAcceleratorId(String acceleratorId) {
+		this.acceleratorId = acceleratorId;
+		if(acceleratorId != null){
+			putQueryParameter("AcceleratorId", acceleratorId);
+		}
+	}
+
+	@Override
+	public Class<UpdateAdditionalCertificateWithListenerResponse> getResponseClass() {
+		return UpdateAdditionalCertificateWithListenerResponse.class;
+	}
+
+}
