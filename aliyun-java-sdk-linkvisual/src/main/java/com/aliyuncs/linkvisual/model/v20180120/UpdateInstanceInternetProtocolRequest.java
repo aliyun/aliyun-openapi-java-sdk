26e40523c3fdc3086f1847b8bb6e68d687543136
sdk-team <sdk-team@alibabacloud.com>
2022-12-26 03:05:10 +0000

    增加IP协议版本配置接口.

---
+++ aliyun-java-sdk-linkvisual/src/main/java/com/aliyuncs/linkvisual/model/v20180120/UpdateInstanceInternetProtocolRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkvisual.model.v20180120;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.linkvisual.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateInstanceInternetProtocolRequest extends RpcAcsRequest<UpdateInstanceInternetProtocolResponse> {
+	   
+
+	private String iotInstanceId;
+
+	private String ipVersion;
+	public UpdateInstanceInternetProtocolRequest() {
+		super("Linkvisual", "2018-01-20", "UpdateInstanceInternetProtocol", "Linkvisual");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getIotInstanceId() {
+		return this.iotInstanceId;
+	}
+
+	public void setIotInstanceId(String iotInstanceId) {
+		this.iotInstanceId = iotInstanceId;
+		if(iotInstanceId != null){
+			putQueryParameter("IotInstanceId", iotInstanceId);
+		}
+	}
+
+	public String getIpVersion() {
+		return this.ipVersion;
+	}
+
+	public void setIpVersion(String ipVersion) {
+		this.ipVersion = ipVersion;
+		if(ipVersion != null){
+			putQueryParameter("IpVersion", ipVersion);
+		}
+	}
+
+	@Override
+	public Class<UpdateInstanceInternetProtocolResponse> getResponseClass() {
+		return UpdateInstanceInternetProtocolResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-linkvisual/src/main/java/com/aliyuncs/linkvisual/transform/v20180120/UpdateInstanceInternetProtocolResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkvisual.transform.v20180120;
+
+import com.aliyuncs.linkvisual.model.v20180120.UpdateInstanceInternetProtocolResponse;
+import java.util.Map;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UpdateInstanceInternetProtocolResponseUnmarshaller {
+
+	public static UpdateInstanceInternetProtocolResponse unmarshall(UpdateInstanceInternetProtocolResponse updateInstanceInternetProtocolResponse, UnmarshallerContext _ctx) {
+		
+		updateInstanceInternetProtocolResponse.setRequestId(_ctx.stringValue("UpdateInstanceInternetProtocolResponse.RequestId"));
+		updateInstanceInternetProtocolResponse.setCode(_ctx.stringValue("UpdateInstanceInternetProtocolResponse.Code"));
+		updateInstanceInternetProtocolResponse.setData(_ctx.mapValue("UpdateInstanceInternetProtocolResponse.Data"));
+		updateInstanceInternetProtocolResponse.setErrorMessage(_ctx.stringValue("UpdateInstanceInternetProtocolResponse.ErrorMessage"));
+		updateInstanceInternetProtocolResponse.setSuccess(_ctx.booleanValue("UpdateInstanceInternetProtocolResponse.Success"));
+	 
+	 	return updateInstanceInternetProtocolResponse;
+	}
+}
