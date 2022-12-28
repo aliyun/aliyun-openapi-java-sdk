fb4f887a5eb409b4ff1d6985b91bcba5e0b4fa9c
sdk-team <sdk-team@alibabacloud.com>
2022-12-28 03:51:27 +0000

    Live sdk update.

---
+++ aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/model/v20161101/DescribeLiveCenterTransferResponse.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.live.model.v20161101;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.live.transform.v20161101.DescribeLiveCenterTransferResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeLiveCenterTransferResponse extends AcsResponse {
+
+	private String requestId;
+
+	private List<LiveCenterTransferInfo> liveCenterTransferInfoList;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<LiveCenterTransferInfo> getLiveCenterTransferInfoList() {
+		return this.liveCenterTransferInfoList;
+	}
+
+	public void setLiveCenterTransferInfoList(List<LiveCenterTransferInfo> liveCenterTransferInfoList) {
+		this.liveCenterTransferInfoList = liveCenterTransferInfoList;
+	}
+
+	public static class LiveCenterTransferInfo {
+
+		private String endTime;
+
+		private String appName;
+
+		private String startTime;
+
+		private String transferArgs;
+
+		private String streamName;
+
+		private String dstUrl;
+
+		private String domainName;
+
+		public String getEndTime() {
+			return this.endTime;
+		}
+
+		public void setEndTime(String endTime) {
+			this.endTime = endTime;
+		}
+
+		public String getAppName() {
+			return this.appName;
+		}
+
+		public void setAppName(String appName) {
+			this.appName = appName;
+		}
+
+		public String getStartTime() {
+			return this.startTime;
+		}
+
+		public void setStartTime(String startTime) {
+			this.startTime = startTime;
+		}
+
+		public String getTransferArgs() {
+			return this.transferArgs;
+		}
+
+		public void setTransferArgs(String transferArgs) {
+			this.transferArgs = transferArgs;
+		}
+
+		public String getStreamName() {
+			return this.streamName;
+		}
+
+		public void setStreamName(String streamName) {
+			this.streamName = streamName;
+		}
+
+		public String getDstUrl() {
+			return this.dstUrl;
+		}
+
+		public void setDstUrl(String dstUrl) {
+			this.dstUrl = dstUrl;
+		}
+
+		public String getDomainName() {
+			return this.domainName;
+		}
+
+		public void setDomainName(String domainName) {
+			this.domainName = domainName;
+		}
+	}
+
+	@Override
+	public DescribeLiveCenterTransferResponse getInstance(UnmarshallerContext context) {
+		return	DescribeLiveCenterTransferResponseUnmarshaller.unmarshall(this, context);
+	}
+}
---
+++ aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/transform/v20161101/DescribeLiveCenterTransferResponseUnmarshaller.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.live.transform.v20161101;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.live.model.v20161101.DescribeLiveCenterTransferResponse;
+import com.aliyuncs.live.model.v20161101.DescribeLiveCenterTransferResponse.LiveCenterTransferInfo;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeLiveCenterTransferResponseUnmarshaller {
+
+	public static DescribeLiveCenterTransferResponse unmarshall(DescribeLiveCenterTransferResponse describeLiveCenterTransferResponse, UnmarshallerContext _ctx) {
+		
+		describeLiveCenterTransferResponse.setRequestId(_ctx.stringValue("DescribeLiveCenterTransferResponse.RequestId"));
+
+		List<LiveCenterTransferInfo> liveCenterTransferInfoList = new ArrayList<LiveCenterTransferInfo>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList.Length"); i++) {
+			LiveCenterTransferInfo liveCenterTransferInfo = new LiveCenterTransferInfo();
+			liveCenterTransferInfo.setEndTime(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].EndTime"));
+			liveCenterTransferInfo.setAppName(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].AppName"));
+			liveCenterTransferInfo.setStartTime(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].StartTime"));
+			liveCenterTransferInfo.setTransferArgs(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].TransferArgs"));
+			liveCenterTransferInfo.setStreamName(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].StreamName"));
+			liveCenterTransferInfo.setDstUrl(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].DstUrl"));
+			liveCenterTransferInfo.setDomainName(_ctx.stringValue("DescribeLiveCenterTransferResponse.LiveCenterTransferInfoList["+ i +"].DomainName"));
+
+			liveCenterTransferInfoList.add(liveCenterTransferInfo);
+		}
+		describeLiveCenterTransferResponse.setLiveCenterTransferInfoList(liveCenterTransferInfoList);
+	 
+	 	return describeLiveCenterTransferResponse;
+	}
+}
