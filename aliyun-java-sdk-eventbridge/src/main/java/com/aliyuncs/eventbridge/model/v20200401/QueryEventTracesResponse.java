5e233c55a3a6e5a52fdc07df84013b7f394010b6
sdk-team <sdk-team@alibabacloud.com>
2023-02-27 11:43:44 +0000

    Add new interface.

---
+++ aliyun-java-sdk-eventbridge/src/main/java/com/aliyuncs/eventbridge/model/v20200401/QueryEventTracesResponse.java
@@ -0,0 +1,210 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.eventbridge.model.v20200401;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.eventbridge.transform.v20200401.QueryEventTracesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryEventTracesResponse extends AcsResponse {
+
+	private String message;
+
+	private String requestId;
+
+	private String code;
+
+	private Boolean success;
+
+	private List<DataItem> data;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public List<DataItem> getData() {
+		return this.data;
+	}
+
+	public void setData(List<DataItem> data) {
+		this.data = data;
+	}
+
+	public static class DataItem {
+
+		private String action;
+
+		private Long receivedTime;
+
+		private String ruleMatchingTime;
+
+		private Long actionTime;
+
+		private String notifyStatus;
+
+		private String ruleName;
+
+		private String eventId;
+
+		private String eventBusName;
+
+		private String endpoint;
+
+		private String notifyLatency;
+
+		private Long notifyTime;
+
+		private String eventSource;
+
+		public String getAction() {
+			return this.action;
+		}
+
+		public void setAction(String action) {
+			this.action = action;
+		}
+
+		public Long getReceivedTime() {
+			return this.receivedTime;
+		}
+
+		public void setReceivedTime(Long receivedTime) {
+			this.receivedTime = receivedTime;
+		}
+
+		public String getRuleMatchingTime() {
+			return this.ruleMatchingTime;
+		}
+
+		public void setRuleMatchingTime(String ruleMatchingTime) {
+			this.ruleMatchingTime = ruleMatchingTime;
+		}
+
+		public Long getActionTime() {
+			return this.actionTime;
+		}
+
+		public void setActionTime(Long actionTime) {
+			this.actionTime = actionTime;
+		}
+
+		public String getNotifyStatus() {
+			return this.notifyStatus;
+		}
+
+		public void setNotifyStatus(String notifyStatus) {
+			this.notifyStatus = notifyStatus;
+		}
+
+		public String getRuleName() {
+			return this.ruleName;
+		}
+
+		public void setRuleName(String ruleName) {
+			this.ruleName = ruleName;
+		}
+
+		public String getEventId() {
+			return this.eventId;
+		}
+
+		public void setEventId(String eventId) {
+			this.eventId = eventId;
+		}
+
+		public String getEventBusName() {
+			return this.eventBusName;
+		}
+
+		public void setEventBusName(String eventBusName) {
+			this.eventBusName = eventBusName;
+		}
+
+		public String getEndpoint() {
+			return this.endpoint;
+		}
+
+		public void setEndpoint(String endpoint) {
+			this.endpoint = endpoint;
+		}
+
+		public String getNotifyLatency() {
+			return this.notifyLatency;
+		}
+
+		public void setNotifyLatency(String notifyLatency) {
+			this.notifyLatency = notifyLatency;
+		}
+
+		public Long getNotifyTime() {
+			return this.notifyTime;
+		}
+
+		public void setNotifyTime(Long notifyTime) {
+			this.notifyTime = notifyTime;
+		}
+
+		public String getEventSource() {
+			return this.eventSource;
+		}
+
+		public void setEventSource(String eventSource) {
+			this.eventSource = eventSource;
+		}
+	}
+
+	@Override
+	public QueryEventTracesResponse getInstance(UnmarshallerContext context) {
+		return	QueryEventTracesResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-eventbridge/src/main/java/com/aliyuncs/eventbridge/transform/v20200401/QueryTracedEventByEventIdResponseUnmarshaller.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.eventbridge.transform.v20200401;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.eventbridge.model.v20200401.QueryTracedEventByEventIdResponse;
+import com.aliyuncs.eventbridge.model.v20200401.QueryTracedEventByEventIdResponse.DataItem;
+import com.aliyuncs.eventbridge.model.v20200401.QueryTracedEventByEventIdResponse.DataItem.EventsItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryTracedEventByEventIdResponseUnmarshaller {
+
+	public static QueryTracedEventByEventIdResponse unmarshall(QueryTracedEventByEventIdResponse queryTracedEventByEventIdResponse, UnmarshallerContext _ctx) {
+		
+		queryTracedEventByEventIdResponse.setRequestId(_ctx.stringValue("QueryTracedEventByEventIdResponse.RequestId"));
+		queryTracedEventByEventIdResponse.setMessage(_ctx.stringValue("QueryTracedEventByEventIdResponse.Message"));
+		queryTracedEventByEventIdResponse.setCode(_ctx.stringValue("QueryTracedEventByEventIdResponse.Code"));
+		queryTracedEventByEventIdResponse.setSuccess(_ctx.booleanValue("QueryTracedEventByEventIdResponse.Success"));
+
+		List<DataItem> data = new ArrayList<DataItem>();
+		for (int i = 0; i < _ctx.lengthValue("QueryTracedEventByEventIdResponse.Data.Length"); i++) {
+			DataItem dataItem = new DataItem();
+			dataItem.setNextToken(_ctx.stringValue("QueryTracedEventByEventIdResponse.Data["+ i +"].NextToken"));
+			dataItem.setTotal(_ctx.integerValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Total"));
+
+			List<EventsItem> events = new ArrayList<EventsItem>();
+			for (int j = 0; j < _ctx.lengthValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events.Length"); j++) {
+				EventsItem eventsItem = new EventsItem();
+				eventsItem.setEventId(_ctx.stringValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events["+ j +"].EventId"));
+				eventsItem.setEventBusName(_ctx.stringValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events["+ j +"].EventBusName"));
+				eventsItem.setEventReceivedTime(_ctx.longValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events["+ j +"].EventReceivedTime"));
+				eventsItem.setEventSource(_ctx.stringValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events["+ j +"].EventSource"));
+				eventsItem.setEventType(_ctx.stringValue("QueryTracedEventByEventIdResponse.Data["+ i +"].Events["+ j +"].EventType"));
+
+				events.add(eventsItem);
+			}
+			dataItem.setEvents(events);
+
+			data.add(dataItem);
+		}
+		queryTracedEventByEventIdResponse.setData(data);
+	 
+	 	return queryTracedEventByEventIdResponse;
+	}
+}
