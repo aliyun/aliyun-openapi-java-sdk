dbeb8e0228281a57a2f80aadbe97b7ce26e3c238
sdk-team <sdk-team@alibabacloud.com>
2023-03-08 08:44:22 +0000

    Support get early media recording.

---
+++ aliyun-java-sdk-ccc/src/main/java/com/aliyuncs/ccc/transform/v20200701/GetEarlyMediaRecordingResponseUnmarshaller.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ccc.transform.v20200701;
+
+import com.aliyuncs.ccc.model.v20200701.GetEarlyMediaRecordingResponse;
+import com.aliyuncs.ccc.model.v20200701.GetEarlyMediaRecordingResponse.Data;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetEarlyMediaRecordingResponseUnmarshaller {
+
+	public static GetEarlyMediaRecordingResponse unmarshall(GetEarlyMediaRecordingResponse getEarlyMediaRecordingResponse, UnmarshallerContext _ctx) {
+		
+		getEarlyMediaRecordingResponse.setRequestId(_ctx.stringValue("GetEarlyMediaRecordingResponse.RequestId"));
+		getEarlyMediaRecordingResponse.setCode(_ctx.stringValue("GetEarlyMediaRecordingResponse.Code"));
+		getEarlyMediaRecordingResponse.setHttpStatusCode(_ctx.integerValue("GetEarlyMediaRecordingResponse.HttpStatusCode"));
+		getEarlyMediaRecordingResponse.setMessage(_ctx.stringValue("GetEarlyMediaRecordingResponse.Message"));
+
+		Data data = new Data();
+		data.setFileUrl(_ctx.stringValue("GetEarlyMediaRecordingResponse.Data.FileUrl"));
+		data.setFileName(_ctx.stringValue("GetEarlyMediaRecordingResponse.Data.FileName"));
+		getEarlyMediaRecordingResponse.setData(data);
+	 
+	 	return getEarlyMediaRecordingResponse;
+	}
+}
