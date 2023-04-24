12ddb12dbf98d357547483ab1ef8ffbb301996bb
sdk-team <sdk-team@alibabacloud.com>
2023-04-24 02:37:17 +0000

    Sdk.

---
+++ aliyun-java-sdk-sofa/src/main/java/com/aliyuncs/sofa/model/v20190815/QueryRMSUnifiedAlarmHistoryRequest.java
@@ -0,0 +1,171 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sofa.model.v20190815;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.sofa.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class QueryRMSUnifiedAlarmHistoryRequest extends RpcAcsRequest<QueryRMSUnifiedAlarmHistoryResponse> {
+	   
+
+	private String alarmStackInfoJsonStr;
+
+	private String workspaceName;
+
+	private Long pageSize;
+
+	private Long end;
+
+	private Long start;
+
+	private Long currentPage;
+
+	private String alarmTargetType;
+
+	private String alarmTarget;
+
+	private Long alarmLevel;
+
+	private Long status;
+	public QueryRMSUnifiedAlarmHistoryRequest() {
+		super("SOFA", "2019-08-15", "QueryRMSUnifiedAlarmHistory");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getAlarmStackInfoJsonStr() {
+		return this.alarmStackInfoJsonStr;
+	}
+
+	public void setAlarmStackInfoJsonStr(String alarmStackInfoJsonStr) {
+		this.alarmStackInfoJsonStr = alarmStackInfoJsonStr;
+		if(alarmStackInfoJsonStr != null){
+			putBodyParameter("AlarmStackInfoJsonStr", alarmStackInfoJsonStr);
+		}
+	}
+
+	public String getWorkspaceName() {
+		return this.workspaceName;
+	}
+
+	public void setWorkspaceName(String workspaceName) {
+		this.workspaceName = workspaceName;
+		if(workspaceName != null){
+			putBodyParameter("WorkspaceName", workspaceName);
+		}
+	}
+
+	public Long getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Long pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putBodyParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public Long getEnd() {
+		return this.end;
+	}
+
+	public void setEnd(Long end) {
+		this.end = end;
+		if(end != null){
+			putBodyParameter("End", end.toString());
+		}
+	}
+
+	public Long getStart() {
+		return this.start;
+	}
+
+	public void setStart(Long start) {
+		this.start = start;
+		if(start != null){
+			putBodyParameter("Start", start.toString());
+		}
+	}
+
+	public Long getCurrentPage() {
+		return this.currentPage;
+	}
+
+	public void setCurrentPage(Long currentPage) {
+		this.currentPage = currentPage;
+		if(currentPage != null){
+			putBodyParameter("CurrentPage", currentPage.toString());
+		}
+	}
+
+	public String getAlarmTargetType() {
+		return this.alarmTargetType;
+	}
+
+	public void setAlarmTargetType(String alarmTargetType) {
+		this.alarmTargetType = alarmTargetType;
+		if(alarmTargetType != null){
+			putBodyParameter("AlarmTargetType", alarmTargetType);
+		}
+	}
+
+	public String getAlarmTarget() {
+		return this.alarmTarget;
+	}
+
+	public void setAlarmTarget(String alarmTarget) {
+		this.alarmTarget = alarmTarget;
+		if(alarmTarget != null){
+			putBodyParameter("AlarmTarget", alarmTarget);
+		}
+	}
+
+	public Long getAlarmLevel() {
+		return this.alarmLevel;
+	}
+
+	public void setAlarmLevel(Long alarmLevel) {
+		this.alarmLevel = alarmLevel;
+		if(alarmLevel != null){
+			putBodyParameter("AlarmLevel", alarmLevel.toString());
+		}
+	}
+
+	public Long getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(Long status) {
+		this.status = status;
+		if(status != null){
+			putBodyParameter("Status", status.toString());
+		}
+	}
+
+	@Override
+	public Class<QueryRMSUnifiedAlarmHistoryResponse> getResponseClass() {
+		return QueryRMSUnifiedAlarmHistoryResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-sofa/src/main/java/com/aliyuncs/sofa/transform/v20190815/QueryRMSUnifiedAlarmHistoryResponseUnmarshaller.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sofa.transform.v20190815;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.sofa.model.v20190815.QueryRMSUnifiedAlarmHistoryResponse;
+import com.aliyuncs.sofa.model.v20190815.QueryRMSUnifiedAlarmHistoryResponse.AlarmHistoriesItem;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class QueryRMSUnifiedAlarmHistoryResponseUnmarshaller {
+
+	public static QueryRMSUnifiedAlarmHistoryResponse unmarshall(QueryRMSUnifiedAlarmHistoryResponse queryRMSUnifiedAlarmHistoryResponse, UnmarshallerContext _ctx) {
+		
+		queryRMSUnifiedAlarmHistoryResponse.setRequestId(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.RequestId"));
+		queryRMSUnifiedAlarmHistoryResponse.setCurrentPage(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.CurrentPage"));
+		queryRMSUnifiedAlarmHistoryResponse.setResultMessage(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.ResultMessage"));
+		queryRMSUnifiedAlarmHistoryResponse.setResultCode(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.ResultCode"));
+		queryRMSUnifiedAlarmHistoryResponse.setPageSize(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.PageSize"));
+		queryRMSUnifiedAlarmHistoryResponse.setTotalCount(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.TotalCount"));
+
+		List<AlarmHistoriesItem> alarmHistories = new ArrayList<AlarmHistoriesItem>();
+		for (int i = 0; i < _ctx.lengthValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories.Length"); i++) {
+			AlarmHistoriesItem alarmHistoriesItem = new AlarmHistoriesItem();
+			alarmHistoriesItem.setAlarmTargetType(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmTargetType"));
+			alarmHistoriesItem.setStatus(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].Status"));
+			alarmHistoriesItem.setDataSourceName(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].DataSourceName"));
+			alarmHistoriesItem.setAlarmType(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmType"));
+			alarmHistoriesItem.setGmtModified(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].GmtModified"));
+			alarmHistoriesItem.setEventId(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].EventId"));
+			alarmHistoriesItem.setWorkspaceId(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].WorkspaceId"));
+			alarmHistoriesItem.setUniqueIdentity(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].UniqueIdentity"));
+			alarmHistoriesItem.setAlarmTime(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmTime"));
+			alarmHistoriesItem.setAlarmRuleId(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmRuleId"));
+			alarmHistoriesItem.setGmtCreate(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].GmtCreate"));
+			alarmHistoriesItem.setAlarmDetail(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmDetail"));
+			alarmHistoriesItem.setAlarmUrl(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmUrl"));
+			alarmHistoriesItem.setAlarmContent(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmContent"));
+			alarmHistoriesItem.setAlarmUrlWithDomain(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmUrlWithDomain"));
+			alarmHistoriesItem.setAlarmTargetDeleted(_ctx.booleanValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmTargetDeleted"));
+			alarmHistoriesItem.setAlarmLevel(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmLevel"));
+			alarmHistoriesItem.setId(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].Id"));
+			alarmHistoriesItem.setTenantId(_ctx.longValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].TenantId"));
+			alarmHistoriesItem.setAlarmTarget(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmTarget"));
+			alarmHistoriesItem.setAlarmRuleName(_ctx.stringValue("QueryRMSUnifiedAlarmHistoryResponse.AlarmHistories["+ i +"].AlarmRuleName"));
+
+			alarmHistories.add(alarmHistoriesItem);
+		}
+		queryRMSUnifiedAlarmHistoryResponse.setAlarmHistories(alarmHistories);
+	 
+	 	return queryRMSUnifiedAlarmHistoryResponse;
+	}
+}
