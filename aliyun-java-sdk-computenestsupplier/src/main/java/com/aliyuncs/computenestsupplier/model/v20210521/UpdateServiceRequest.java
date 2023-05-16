0b34cd48302ddbd44331980a8f156eb08b1c419d
sdk-team <sdk-team@alibabacloud.com>
2023-05-16 10:08:33 +0000

    Add UpdateService GetService DeleteService.

---
+++ aliyun-java-sdk-computenestsupplier/src/main/java/com/aliyuncs/computenestsupplier/model/v20210521/UpdateServiceRequest.java
@@ -0,0 +1,311 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.computenestsupplier.model.v20210521;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.computenestsupplier.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class UpdateServiceRequest extends RpcAcsRequest<UpdateServiceResponse> {
+	   
+
+	private String alarmMetadata;
+
+	private String clientToken;
+
+	private String policyNames;
+
+	private String licenseMetadata;
+
+	private Long duration;
+
+	private Integer trialDuration;
+
+	private String upgradeMetadata;
+
+	private String deployMetadata;
+
+	private String serviceType;
+
+	private Boolean isSupportOperated;
+
+	private String tenantType;
+
+	private String serviceVersion;
+
+	private List<ServiceInfo> serviceInfos;
+
+	private String serviceId;
+
+	private String versionName;
+
+	private String operationMetadata;
+
+	private String deployType;
+	public UpdateServiceRequest() {
+		super("ComputeNestSupplier", "2021-05-21", "UpdateService");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getAlarmMetadata() {
+		return this.alarmMetadata;
+	}
+
+	public void setAlarmMetadata(String alarmMetadata) {
+		this.alarmMetadata = alarmMetadata;
+		if(alarmMetadata != null){
+			putQueryParameter("AlarmMetadata", alarmMetadata);
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getPolicyNames() {
+		return this.policyNames;
+	}
+
+	public void setPolicyNames(String policyNames) {
+		this.policyNames = policyNames;
+		if(policyNames != null){
+			putQueryParameter("PolicyNames", policyNames);
+		}
+	}
+
+	public String getLicenseMetadata() {
+		return this.licenseMetadata;
+	}
+
+	public void setLicenseMetadata(String licenseMetadata) {
+		this.licenseMetadata = licenseMetadata;
+		if(licenseMetadata != null){
+			putQueryParameter("LicenseMetadata", licenseMetadata);
+		}
+	}
+
+	public Long getDuration() {
+		return this.duration;
+	}
+
+	public void setDuration(Long duration) {
+		this.duration = duration;
+		if(duration != null){
+			putQueryParameter("Duration", duration.toString());
+		}
+	}
+
+	public Integer getTrialDuration() {
+		return this.trialDuration;
+	}
+
+	public void setTrialDuration(Integer trialDuration) {
+		this.trialDuration = trialDuration;
+		if(trialDuration != null){
+			putQueryParameter("TrialDuration", trialDuration.toString());
+		}
+	}
+
+	public String getUpgradeMetadata() {
+		return this.upgradeMetadata;
+	}
+
+	public void setUpgradeMetadata(String upgradeMetadata) {
+		this.upgradeMetadata = upgradeMetadata;
+		if(upgradeMetadata != null){
+			putQueryParameter("UpgradeMetadata", upgradeMetadata);
+		}
+	}
+
+	public String getDeployMetadata() {
+		return this.deployMetadata;
+	}
+
+	public void setDeployMetadata(String deployMetadata) {
+		this.deployMetadata = deployMetadata;
+		if(deployMetadata != null){
+			putQueryParameter("DeployMetadata", deployMetadata);
+		}
+	}
+
+	public String getServiceType() {
+		return this.serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+		if(serviceType != null){
+			putQueryParameter("ServiceType", serviceType);
+		}
+	}
+
+	public Boolean getIsSupportOperated() {
+		return this.isSupportOperated;
+	}
+
+	public void setIsSupportOperated(Boolean isSupportOperated) {
+		this.isSupportOperated = isSupportOperated;
+		if(isSupportOperated != null){
+			putQueryParameter("IsSupportOperated", isSupportOperated.toString());
+		}
+	}
+
+	public String getTenantType() {
+		return this.tenantType;
+	}
+
+	public void setTenantType(String tenantType) {
+		this.tenantType = tenantType;
+		if(tenantType != null){
+			putQueryParameter("TenantType", tenantType);
+		}
+	}
+
+	public String getServiceVersion() {
+		return this.serviceVersion;
+	}
+
+	public void setServiceVersion(String serviceVersion) {
+		this.serviceVersion = serviceVersion;
+		if(serviceVersion != null){
+			putQueryParameter("ServiceVersion", serviceVersion);
+		}
+	}
+
+	public List<ServiceInfo> getServiceInfos() {
+		return this.serviceInfos;
+	}
+
+	public void setServiceInfos(List<ServiceInfo> serviceInfos) {
+		this.serviceInfos = serviceInfos;	
+		if (serviceInfos != null) {
+			for (int depth1 = 0; depth1 < serviceInfos.size(); depth1++) {
+				putQueryParameter("ServiceInfo." + (depth1 + 1) + ".ShortDescription" , serviceInfos.get(depth1).getShortDescription());
+				putQueryParameter("ServiceInfo." + (depth1 + 1) + ".Image" , serviceInfos.get(depth1).getImage());
+				putQueryParameter("ServiceInfo." + (depth1 + 1) + ".Name" , serviceInfos.get(depth1).getName());
+				putQueryParameter("ServiceInfo." + (depth1 + 1) + ".Locale" , serviceInfos.get(depth1).getLocale());
+			}
+		}	
+	}
+
+	public String getServiceId() {
+		return this.serviceId;
+	}
+
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+		if(serviceId != null){
+			putQueryParameter("ServiceId", serviceId);
+		}
+	}
+
+	public String getVersionName() {
+		return this.versionName;
+	}
+
+	public void setVersionName(String versionName) {
+		this.versionName = versionName;
+		if(versionName != null){
+			putQueryParameter("VersionName", versionName);
+		}
+	}
+
+	public String getOperationMetadata() {
+		return this.operationMetadata;
+	}
+
+	public void setOperationMetadata(String operationMetadata) {
+		this.operationMetadata = operationMetadata;
+		if(operationMetadata != null){
+			putQueryParameter("OperationMetadata", operationMetadata);
+		}
+	}
+
+	public String getDeployType() {
+		return this.deployType;
+	}
+
+	public void setDeployType(String deployType) {
+		this.deployType = deployType;
+		if(deployType != null){
+			putQueryParameter("DeployType", deployType);
+		}
+	}
+
+	public static class ServiceInfo {
+
+		private String shortDescription;
+
+		private String image;
+
+		private String name;
+
+		private String locale;
+
+		public String getShortDescription() {
+			return this.shortDescription;
+		}
+
+		public void setShortDescription(String shortDescription) {
+			this.shortDescription = shortDescription;
+		}
+
+		public String getImage() {
+			return this.image;
+		}
+
+		public void setImage(String image) {
+			this.image = image;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getLocale() {
+			return this.locale;
+		}
+
+		public void setLocale(String locale) {
+			this.locale = locale;
+		}
+	}
+
+	@Override
+	public Class<UpdateServiceResponse> getResponseClass() {
+		return UpdateServiceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-computenestsupplier/src/main/java/com/aliyuncs/computenestsupplier/transform/v20210521/GetServiceResponseUnmarshaller.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.computenestsupplier.transform.v20210521;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.computenestsupplier.model.v20210521.GetServiceResponse;
+import com.aliyuncs.computenestsupplier.model.v20210521.GetServiceResponse.CommodityEntitiesItem;
+import com.aliyuncs.computenestsupplier.model.v20210521.GetServiceResponse.CommoditySpecification;
+import com.aliyuncs.computenestsupplier.model.v20210521.GetServiceResponse.ServiceInfo;
+import com.aliyuncs.computenestsupplier.model.v20210521.GetServiceResponse.Tag;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetServiceResponseUnmarshaller {
+
+	public static GetServiceResponse unmarshall(GetServiceResponse getServiceResponse, UnmarshallerContext _ctx) {
+		
+		getServiceResponse.setRequestId(_ctx.stringValue("GetServiceResponse.RequestId"));
+		getServiceResponse.setStatus(_ctx.stringValue("GetServiceResponse.Status"));
+		getServiceResponse.setDeployMetadata(_ctx.stringValue("GetServiceResponse.DeployMetadata"));
+		getServiceResponse.setPublishTime(_ctx.stringValue("GetServiceResponse.PublishTime"));
+		getServiceResponse.setVersion(_ctx.stringValue("GetServiceResponse.Version"));
+		getServiceResponse.setDeployType(_ctx.stringValue("GetServiceResponse.DeployType"));
+		getServiceResponse.setServiceId(_ctx.stringValue("GetServiceResponse.ServiceId"));
+		getServiceResponse.setSupplierUrl(_ctx.stringValue("GetServiceResponse.SupplierUrl"));
+		getServiceResponse.setServiceDocUrl(_ctx.stringValue("GetServiceResponse.ServiceDocUrl"));
+		getServiceResponse.setServiceProductUrl(_ctx.stringValue("GetServiceResponse.ServiceProductUrl"));
+		getServiceResponse.setServiceType(_ctx.stringValue("GetServiceResponse.ServiceType"));
+		getServiceResponse.setSupplierName(_ctx.stringValue("GetServiceResponse.SupplierName"));
+		getServiceResponse.setCommodityCode(_ctx.stringValue("GetServiceResponse.CommodityCode"));
+		getServiceResponse.setIsSupportOperated(_ctx.booleanValue("GetServiceResponse.IsSupportOperated"));
+		getServiceResponse.setPolicyNames(_ctx.stringValue("GetServiceResponse.PolicyNames"));
+		getServiceResponse.setDuration(_ctx.longValue("GetServiceResponse.Duration"));
+		getServiceResponse.setProgress(_ctx.longValue("GetServiceResponse.Progress"));
+		getServiceResponse.setStatusDetail(_ctx.stringValue("GetServiceResponse.StatusDetail"));
+		getServiceResponse.setCreateTime(_ctx.stringValue("GetServiceResponse.CreateTime"));
+		getServiceResponse.setUpdateTime(_ctx.stringValue("GetServiceResponse.UpdateTime"));
+		getServiceResponse.setShareType(_ctx.stringValue("GetServiceResponse.ShareType"));
+		getServiceResponse.setAlarmMetadata(_ctx.stringValue("GetServiceResponse.AlarmMetadata"));
+		getServiceResponse.setUpgradeMetadata(_ctx.stringValue("GetServiceResponse.UpgradeMetadata"));
+		getServiceResponse.setVersionName(_ctx.stringValue("GetServiceResponse.VersionName"));
+		getServiceResponse.setPayFromType(_ctx.stringValue("GetServiceResponse.PayFromType"));
+		getServiceResponse.setPayType(_ctx.stringValue("GetServiceResponse.PayType"));
+		getServiceResponse.setDefaultLicenseDays(_ctx.longValue("GetServiceResponse.DefaultLicenseDays"));
+		getServiceResponse.setTrialDuration(_ctx.longValue("GetServiceResponse.TrialDuration"));
+		getServiceResponse.setTrialType(_ctx.stringValue("GetServiceResponse.TrialType"));
+		getServiceResponse.setTenantType(_ctx.stringValue("GetServiceResponse.TenantType"));
+		getServiceResponse.setLicenseMetadata(_ctx.stringValue("GetServiceResponse.LicenseMetadata"));
+		getServiceResponse.setOperationMetadata(_ctx.stringValue("GetServiceResponse.OperationMetadata"));
+		getServiceResponse.setTestStatus(_ctx.stringValue("GetServiceResponse.TestStatus"));
+		getServiceResponse.setApprovalType(_ctx.stringValue("GetServiceResponse.ApprovalType"));
+		getServiceResponse.setRegistrationId(_ctx.stringValue("GetServiceResponse.RegistrationId"));
+		getServiceResponse.setResourceGroupId(_ctx.stringValue("GetServiceResponse.ResourceGroupId"));
+		getServiceResponse.setPermission(_ctx.stringValue("GetServiceResponse.Permission"));
+
+		List<ServiceInfo> serviceInfos = new ArrayList<ServiceInfo>();
+		for (int i = 0; i < _ctx.lengthValue("GetServiceResponse.ServiceInfos.Length"); i++) {
+			ServiceInfo serviceInfo = new ServiceInfo();
+			serviceInfo.setLocale(_ctx.stringValue("GetServiceResponse.ServiceInfos["+ i +"].Locale"));
+			serviceInfo.setImage(_ctx.stringValue("GetServiceResponse.ServiceInfos["+ i +"].Image"));
+			serviceInfo.setName(_ctx.stringValue("GetServiceResponse.ServiceInfos["+ i +"].Name"));
+			serviceInfo.setShortDescription(_ctx.stringValue("GetServiceResponse.ServiceInfos["+ i +"].ShortDescription"));
+
+			serviceInfos.add(serviceInfo);
+		}
+		getServiceResponse.setServiceInfos(serviceInfos);
+
+		List<CommoditySpecification> commoditySpecifications = new ArrayList<CommoditySpecification>();
+		for (int i = 0; i < _ctx.lengthValue("GetServiceResponse.CommoditySpecifications.Length"); i++) {
+			CommoditySpecification commoditySpecification = new CommoditySpecification();
+			commoditySpecification.setSpecificationCode(_ctx.stringValue("GetServiceResponse.CommoditySpecifications["+ i +"].SpecificationCode"));
+			commoditySpecification.setTemplateName(_ctx.stringValue("GetServiceResponse.CommoditySpecifications["+ i +"].TemplateName"));
+			commoditySpecification.setPredefinedParameterName(_ctx.stringValue("GetServiceResponse.CommoditySpecifications["+ i +"].PredefinedParameterName"));
+
+			commoditySpecifications.add(commoditySpecification);
+		}
+		getServiceResponse.setCommoditySpecifications(commoditySpecifications);
+
+		List<Tag> tags = new ArrayList<Tag>();
+		for (int i = 0; i < _ctx.lengthValue("GetServiceResponse.Tags.Length"); i++) {
+			Tag tag = new Tag();
+			tag.setKey(_ctx.stringValue("GetServiceResponse.Tags["+ i +"].Key"));
+			tag.setValue(_ctx.stringValue("GetServiceResponse.Tags["+ i +"].Value"));
+
+			tags.add(tag);
+		}
+		getServiceResponse.setTags(tags);
+
+		List<CommodityEntitiesItem> commodityEntities = new ArrayList<CommodityEntitiesItem>();
+		for (int i = 0; i < _ctx.lengthValue("GetServiceResponse.CommodityEntities.Length"); i++) {
+			CommodityEntitiesItem commodityEntitiesItem = new CommodityEntitiesItem();
+			commodityEntitiesItem.setTemplateName(_ctx.stringValue("GetServiceResponse.CommodityEntities["+ i +"].TemplateName"));
+			commodityEntitiesItem.setPredefinedParameterName(_ctx.stringValue("GetServiceResponse.CommodityEntities["+ i +"].PredefinedParameterName"));
+
+			List<String> entityIds = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("GetServiceResponse.CommodityEntities["+ i +"].EntityIds.Length"); j++) {
+				entityIds.add(_ctx.stringValue("GetServiceResponse.CommodityEntities["+ i +"].EntityIds["+ j +"]"));
+			}
+			commodityEntitiesItem.setEntityIds(entityIds);
+
+			commodityEntities.add(commodityEntitiesItem);
+		}
+		getServiceResponse.setCommodityEntities(commodityEntities);
+	 
+	 	return getServiceResponse;
+	}
+}
