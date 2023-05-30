8f341804408b8f6c3f897e80e99df0a72a163f65
sdk-team <sdk-team@alibabacloud.com>
2023-05-30 07:01:00 +0000

    Update SDK.

---
+++ aliyun-java-sdk-oceanbasepro/src/main/java/com/aliyuncs/oceanbasepro/model/v20190901/DeleteTenantSecurityIpGroupRequest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.oceanbasepro.model.v20190901;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.oceanbasepro.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DeleteTenantSecurityIpGroupRequest extends RpcAcsRequest<DeleteTenantSecurityIpGroupResponse> {
+	   
+
+	private String securityIpGroupName;
+
+	private String instanceId;
+
+	private String tenantId;
+	public DeleteTenantSecurityIpGroupRequest() {
+		super("OceanBasePro", "2019-09-01", "DeleteTenantSecurityIpGroup", "oceanbase");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getSecurityIpGroupName() {
+		return this.securityIpGroupName;
+	}
+
+	public void setSecurityIpGroupName(String securityIpGroupName) {
+		this.securityIpGroupName = securityIpGroupName;
+		if(securityIpGroupName != null){
+			putBodyParameter("SecurityIpGroupName", securityIpGroupName);
+		}
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		if(instanceId != null){
+			putBodyParameter("InstanceId", instanceId);
+		}
+	}
+
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+		if(tenantId != null){
+			putBodyParameter("TenantId", tenantId);
+		}
+	}
+
+	@Override
+	public Class<DeleteTenantSecurityIpGroupResponse> getResponseClass() {
+		return DeleteTenantSecurityIpGroupResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-oceanbasepro/src/main/java/com/aliyuncs/oceanbasepro/transform/v20190901/DeleteTenantSecurityIpGroupResponseUnmarshaller.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.oceanbasepro.transform.v20190901;
+
+import com.aliyuncs.oceanbasepro.model.v20190901.DeleteTenantSecurityIpGroupResponse;
+import com.aliyuncs.oceanbasepro.model.v20190901.DeleteTenantSecurityIpGroupResponse.SecurityIpGroup;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DeleteTenantSecurityIpGroupResponseUnmarshaller {
+
+	public static DeleteTenantSecurityIpGroupResponse unmarshall(DeleteTenantSecurityIpGroupResponse deleteTenantSecurityIpGroupResponse, UnmarshallerContext _ctx) {
+		
+		deleteTenantSecurityIpGroupResponse.setRequestId(_ctx.stringValue("DeleteTenantSecurityIpGroupResponse.RequestId"));
+
+		SecurityIpGroup securityIpGroup = new SecurityIpGroup();
+		securityIpGroup.setInstanceId(_ctx.stringValue("DeleteTenantSecurityIpGroupResponse.SecurityIpGroup.InstanceId"));
+		securityIpGroup.setTenantId(_ctx.stringValue("DeleteTenantSecurityIpGroupResponse.SecurityIpGroup.TenantId"));
+		securityIpGroup.setSecurityIpGroupName(_ctx.stringValue("DeleteTenantSecurityIpGroupResponse.SecurityIpGroup.SecurityIpGroupName"));
+		deleteTenantSecurityIpGroupResponse.setSecurityIpGroup(securityIpGroup);
+	 
+	 	return deleteTenantSecurityIpGroupResponse;
+	}
+}
