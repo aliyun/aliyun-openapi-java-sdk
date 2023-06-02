3b71df0f9d9ad1142e3b44c781bd5a1fd218424e
sdk-team <sdk-team@alibabacloud.com>
2023-06-02 01:55:38 +0000

    Supported GetResourceCounts for Resourcecenter.

---
+++ aliyun-java-sdk-resourcecenter/src/main/java/com/aliyuncs/resourcecenter/model/v20221201/GetResourceCountsResponse.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.resourcecenter.model.v20221201;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.resourcecenter.transform.v20221201.GetResourceCountsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetResourceCountsResponse extends AcsResponse {
+
+	private String groupByKey;
+
+	private String requestId;
+
+	private List<Filter> filters;
+
+	private List<ResourceCount> resourceCounts;
+
+	public String getGroupByKey() {
+		return this.groupByKey;
+	}
+
+	public void setGroupByKey(String groupByKey) {
+		this.groupByKey = groupByKey;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public List<Filter> getFilters() {
+		return this.filters;
+	}
+
+	public void setFilters(List<Filter> filters) {
+		this.filters = filters;
+	}
+
+	public List<ResourceCount> getResourceCounts() {
+		return this.resourceCounts;
+	}
+
+	public void setResourceCounts(List<ResourceCount> resourceCounts) {
+		this.resourceCounts = resourceCounts;
+	}
+
+	public static class Filter {
+
+		private String key;
+
+		private List<String> values;
+
+		public String getKey() {
+			return this.key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public List<String> getValues() {
+			return this.values;
+		}
+
+		public void setValues(List<String> values) {
+			this.values = values;
+		}
+	}
+
+	public static class ResourceCount {
+
+		private String groupName;
+
+		private Long count;
+
+		public String getGroupName() {
+			return this.groupName;
+		}
+
+		public void setGroupName(String groupName) {
+			this.groupName = groupName;
+		}
+
+		public Long getCount() {
+			return this.count;
+		}
+
+		public void setCount(Long count) {
+			this.count = count;
+		}
+	}
+
+	@Override
+	public GetResourceCountsResponse getInstance(UnmarshallerContext context) {
+		return	GetResourceCountsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-resourcecenter/src/main/java/com/aliyuncs/resourcecenter/transform/v20221201/GetResourceCountsResponseUnmarshaller.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.resourcecenter.transform.v20221201;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.resourcecenter.model.v20221201.GetResourceCountsResponse;
+import com.aliyuncs.resourcecenter.model.v20221201.GetResourceCountsResponse.Filter;
+import com.aliyuncs.resourcecenter.model.v20221201.GetResourceCountsResponse.ResourceCount;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetResourceCountsResponseUnmarshaller {
+
+	public static GetResourceCountsResponse unmarshall(GetResourceCountsResponse getResourceCountsResponse, UnmarshallerContext _ctx) {
+		
+		getResourceCountsResponse.setRequestId(_ctx.stringValue("GetResourceCountsResponse.RequestId"));
+		getResourceCountsResponse.setGroupByKey(_ctx.stringValue("GetResourceCountsResponse.GroupByKey"));
+
+		List<Filter> filters = new ArrayList<Filter>();
+		for (int i = 0; i < _ctx.lengthValue("GetResourceCountsResponse.Filters.Length"); i++) {
+			Filter filter = new Filter();
+			filter.setKey(_ctx.stringValue("GetResourceCountsResponse.Filters["+ i +"].Key"));
+
+			List<String> values = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("GetResourceCountsResponse.Filters["+ i +"].Values.Length"); j++) {
+				values.add(_ctx.stringValue("GetResourceCountsResponse.Filters["+ i +"].Values["+ j +"]"));
+			}
+			filter.setValues(values);
+
+			filters.add(filter);
+		}
+		getResourceCountsResponse.setFilters(filters);
+
+		List<ResourceCount> resourceCounts = new ArrayList<ResourceCount>();
+		for (int i = 0; i < _ctx.lengthValue("GetResourceCountsResponse.ResourceCounts.Length"); i++) {
+			ResourceCount resourceCount = new ResourceCount();
+			resourceCount.setGroupName(_ctx.stringValue("GetResourceCountsResponse.ResourceCounts["+ i +"].GroupName"));
+			resourceCount.setCount(_ctx.longValue("GetResourceCountsResponse.ResourceCounts["+ i +"].Count"));
+
+			resourceCounts.add(resourceCount);
+		}
+		getResourceCountsResponse.setResourceCounts(resourceCounts);
+	 
+	 	return getResourceCountsResponse;
+	}
+}
