c82934ee06489ca3993038afb17b85514f41e55c
sdk-team <sdk-team@alibabacloud.com>
2023-06-17 16:08:05 +0000

    Supported backup record.

---
+++ aliyun-java-sdk-sas/src/main/java/com/aliyuncs/sas/model/v20181203/DescribeSecurityEventMarkMissListRequest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sas.model.v20181203;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.sas.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeSecurityEventMarkMissListRequest extends RpcAcsRequest<DescribeSecurityEventMarkMissListResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String remark;
+
+	private String eventName;
+
+	private String sourceIp;
+
+	private Integer pageSize;
+
+	private Integer currentPage;
+	public DescribeSecurityEventMarkMissListRequest() {
+		super("Sas", "2018-12-03", "DescribeSecurityEventMarkMissList", "sas");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getRemark() {
+		return this.remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+		if(remark != null){
+			putQueryParameter("Remark", remark);
+		}
+	}
+
+	public String getEventName() {
+		return this.eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+		if(eventName != null){
+			putQueryParameter("EventName", eventName);
+		}
+	}
+
+	public String getSourceIp() {
+		return this.sourceIp;
+	}
+
+	public void setSourceIp(String sourceIp) {
+		this.sourceIp = sourceIp;
+		if(sourceIp != null){
+			putQueryParameter("SourceIp", sourceIp);
+		}
+	}
+
+	public Integer getPageSize() {
+		return this.pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		if(pageSize != null){
+			putQueryParameter("PageSize", pageSize.toString());
+		}
+	}
+
+	public Integer getCurrentPage() {
+		return this.currentPage;
+	}
+
+	public void setCurrentPage(Integer currentPage) {
+		this.currentPage = currentPage;
+		if(currentPage != null){
+			putQueryParameter("CurrentPage", currentPage.toString());
+		}
+	}
+
+	@Override
+	public Class<DescribeSecurityEventMarkMissListResponse> getResponseClass() {
+		return DescribeSecurityEventMarkMissListResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-sas/src/main/java/com/aliyuncs/sas/transform/v20181203/DescribeSecurityEventMarkMissListResponseUnmarshaller.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sas.transform.v20181203;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.sas.model.v20181203.DescribeSecurityEventMarkMissListResponse;
+import com.aliyuncs.sas.model.v20181203.DescribeSecurityEventMarkMissListResponse.PageInfo;
+import com.aliyuncs.sas.model.v20181203.DescribeSecurityEventMarkMissListResponse.SecurityEventMarkMissReponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeSecurityEventMarkMissListResponseUnmarshaller {
+
+	public static DescribeSecurityEventMarkMissListResponse unmarshall(DescribeSecurityEventMarkMissListResponse describeSecurityEventMarkMissListResponse, UnmarshallerContext _ctx) {
+		
+		describeSecurityEventMarkMissListResponse.setRequestId(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.RequestId"));
+
+		PageInfo pageInfo = new PageInfo();
+		pageInfo.setCurrentPage(_ctx.integerValue("DescribeSecurityEventMarkMissListResponse.PageInfo.CurrentPage"));
+		pageInfo.setPageSize(_ctx.integerValue("DescribeSecurityEventMarkMissListResponse.PageInfo.PageSize"));
+		pageInfo.setTotalCount(_ctx.integerValue("DescribeSecurityEventMarkMissListResponse.PageInfo.TotalCount"));
+		pageInfo.setCount(_ctx.integerValue("DescribeSecurityEventMarkMissListResponse.PageInfo.Count"));
+		describeSecurityEventMarkMissListResponse.setPageInfo(pageInfo);
+
+		List<SecurityEventMarkMissReponse> list = new ArrayList<SecurityEventMarkMissReponse>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeSecurityEventMarkMissListResponse.List.Length"); i++) {
+			SecurityEventMarkMissReponse securityEventMarkMissReponse = new SecurityEventMarkMissReponse();
+			securityEventMarkMissReponse.setEventName(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].EventName"));
+			securityEventMarkMissReponse.setInternetIp(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].InternetIp"));
+			securityEventMarkMissReponse.setEventNameOriginal(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].EventNameOriginal"));
+			securityEventMarkMissReponse.setAliUid(_ctx.longValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].AliUid"));
+			securityEventMarkMissReponse.setFieldValue(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].FieldValue"));
+			securityEventMarkMissReponse.setInstanceId(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].InstanceId"));
+			securityEventMarkMissReponse.setField(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].Field"));
+			securityEventMarkMissReponse.setIntranetIp(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].IntranetIp"));
+			securityEventMarkMissReponse.setFiledAliasName(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].FiledAliasName"));
+			securityEventMarkMissReponse.setUuid(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].Uuid"));
+			securityEventMarkMissReponse.setOperate(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].Operate"));
+			securityEventMarkMissReponse.setEventTypeOriginal(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].EventTypeOriginal"));
+			securityEventMarkMissReponse.setEventType(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].EventType"));
+			securityEventMarkMissReponse.setInstanceName(_ctx.stringValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].InstanceName"));
+			securityEventMarkMissReponse.setId(_ctx.longValue("DescribeSecurityEventMarkMissListResponse.List["+ i +"].Id"));
+
+			list.add(securityEventMarkMissReponse);
+		}
+		describeSecurityEventMarkMissListResponse.setList(list);
+	 
+	 	return describeSecurityEventMarkMissListResponse;
+	}
+}
