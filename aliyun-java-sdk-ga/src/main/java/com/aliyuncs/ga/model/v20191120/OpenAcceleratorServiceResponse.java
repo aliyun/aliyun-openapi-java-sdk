b3414d4969e70d28f048c81c8d31ca3f6dfe33c5
sdk-team <sdk-team@alibabacloud.com>
2023-06-28 07:33:25 +0000

    Update EndPointGroup api.

---
+++ aliyun-java-sdk-ga/src/main/java/com/aliyuncs/ga/model/v20191120/OpenAcceleratorServiceResponse.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ga.model.v20191120;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ga.transform.v20191120.OpenAcceleratorServiceResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class OpenAcceleratorServiceResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Long orderId;
+
+	private String code;
+
+	private String success;
+
+	private String message;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Long getOrderId() {
+		return this.orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public OpenAcceleratorServiceResponse getInstance(UnmarshallerContext context) {
+		return	OpenAcceleratorServiceResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-ga/src/main/java/com/aliyuncs/ga/transform/v20191120/ListEndpointGroupIpAddressCidrBlocksResponseUnmarshaller.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ga.transform.v20191120;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.ga.model.v20191120.ListEndpointGroupIpAddressCidrBlocksResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListEndpointGroupIpAddressCidrBlocksResponseUnmarshaller {
+
+	public static ListEndpointGroupIpAddressCidrBlocksResponse unmarshall(ListEndpointGroupIpAddressCidrBlocksResponse listEndpointGroupIpAddressCidrBlocksResponse, UnmarshallerContext _ctx) {
+		
+		listEndpointGroupIpAddressCidrBlocksResponse.setRequestId(_ctx.stringValue("ListEndpointGroupIpAddressCidrBlocksResponse.RequestId"));
+		listEndpointGroupIpAddressCidrBlocksResponse.setEndpointGroupRegion(_ctx.stringValue("ListEndpointGroupIpAddressCidrBlocksResponse.EndpointGroupRegion"));
+		listEndpointGroupIpAddressCidrBlocksResponse.setResourceGroupId(_ctx.stringValue("ListEndpointGroupIpAddressCidrBlocksResponse.ResourceGroupId"));
+		listEndpointGroupIpAddressCidrBlocksResponse.setState(_ctx.stringValue("ListEndpointGroupIpAddressCidrBlocksResponse.State"));
+
+		List<String> ipAddressCidrBlocks = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("ListEndpointGroupIpAddressCidrBlocksResponse.IpAddressCidrBlocks.Length"); i++) {
+			ipAddressCidrBlocks.add(_ctx.stringValue("ListEndpointGroupIpAddressCidrBlocksResponse.IpAddressCidrBlocks["+ i +"]"));
+		}
+		listEndpointGroupIpAddressCidrBlocksResponse.setIpAddressCidrBlocks(ipAddressCidrBlocks);
+	 
+	 	return listEndpointGroupIpAddressCidrBlocksResponse;
+	}
+}
