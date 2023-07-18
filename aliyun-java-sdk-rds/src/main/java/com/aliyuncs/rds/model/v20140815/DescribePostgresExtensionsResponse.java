bcd414708aa1958c4728503bdfd126b5415ae576
sdk-team <sdk-team@alibabacloud.com>
2023-07-18 15:07:31 +0000

    Supported RDS PostgreSQL manage extensions.

---
+++ aliyun-java-sdk-rds/src/main/java/com/aliyuncs/rds/model/v20140815/DescribePostgresExtensionsResponse.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.rds.model.v20140815;
+
+import java.util.List;
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.rds.transform.v20140815.DescribePostgresExtensionsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribePostgresExtensionsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Map<Object,Object> overview;
+
+	private List<Extension> uninstalledExtensions;
+
+	private List<Extension> installedExtensions;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Map<Object,Object> getOverview() {
+		return this.overview;
+	}
+
+	public void setOverview(Map<Object,Object> overview) {
+		this.overview = overview;
+	}
+
+	public List<Extension> getUninstalledExtensions() {
+		return this.uninstalledExtensions;
+	}
+
+	public void setUninstalledExtensions(List<Extension> uninstalledExtensions) {
+		this.uninstalledExtensions = uninstalledExtensions;
+	}
+
+	public List<Extension> getInstalledExtensions() {
+		return this.installedExtensions;
+	}
+
+	public void setInstalledExtensions(List<Extension> installedExtensions) {
+		this.installedExtensions = installedExtensions;
+	}
+
+	public static class Extension {
+
+		private String name;
+
+		private String defaultVersion;
+
+		private String installedVersion;
+
+		private String comment;
+
+		private String owner;
+
+		private String priority;
+
+		private String requires;
+
+		private String category;
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDefaultVersion() {
+			return this.defaultVersion;
+		}
+
+		public void setDefaultVersion(String defaultVersion) {
+			this.defaultVersion = defaultVersion;
+		}
+
+		public String getInstalledVersion() {
+			return this.installedVersion;
+		}
+
+		public void setInstalledVersion(String installedVersion) {
+			this.installedVersion = installedVersion;
+		}
+
+		public String getComment() {
+			return this.comment;
+		}
+
+		public void setComment(String comment) {
+			this.comment = comment;
+		}
+
+		public String getOwner() {
+			return this.owner;
+		}
+
+		public void setOwner(String owner) {
+			this.owner = owner;
+		}
+
+		public String getPriority() {
+			return this.priority;
+		}
+
+		public void setPriority(String priority) {
+			this.priority = priority;
+		}
+
+		public String getRequires() {
+			return this.requires;
+		}
+
+		public void setRequires(String requires) {
+			this.requires = requires;
+		}
+
+		public String getCategory() {
+			return this.category;
+		}
+
+		public void setCategory(String category) {
+			this.category = category;
+		}
+	}
+
+	@Override
+	public DescribePostgresExtensionsResponse getInstance(UnmarshallerContext context) {
+		return	DescribePostgresExtensionsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-rds/src/main/java/com/aliyuncs/rds/transform/v20140815/DescribePostgresExtensionsResponseUnmarshaller.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.rds.transform.v20140815;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.rds.model.v20140815.DescribePostgresExtensionsResponse;
+import com.aliyuncs.rds.model.v20140815.DescribePostgresExtensionsResponse.Extension;
+import java.util.Map;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribePostgresExtensionsResponseUnmarshaller {
+
+	public static DescribePostgresExtensionsResponse unmarshall(DescribePostgresExtensionsResponse describePostgresExtensionsResponse, UnmarshallerContext _ctx) {
+		
+		describePostgresExtensionsResponse.setRequestId(_ctx.stringValue("DescribePostgresExtensionsResponse.RequestId"));
+		describePostgresExtensionsResponse.setOverview(_ctx.mapValue("DescribePostgresExtensionsResponse.Overview"));
+
+		List<Extension> uninstalledExtensions = new ArrayList<Extension>();
+		for (int i = 0; i < _ctx.lengthValue("DescribePostgresExtensionsResponse.UninstalledExtensions.Length"); i++) {
+			Extension extension = new Extension();
+			extension.setName(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Name"));
+			extension.setDefaultVersion(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].DefaultVersion"));
+			extension.setInstalledVersion(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].InstalledVersion"));
+			extension.setComment(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Comment"));
+			extension.setOwner(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Owner"));
+			extension.setPriority(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Priority"));
+			extension.setRequires(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Requires"));
+			extension.setCategory(_ctx.stringValue("DescribePostgresExtensionsResponse.UninstalledExtensions["+ i +"].Category"));
+
+			uninstalledExtensions.add(extension);
+		}
+		describePostgresExtensionsResponse.setUninstalledExtensions(uninstalledExtensions);
+
+		List<Extension> installedExtensions = new ArrayList<Extension>();
+		for (int i = 0; i < _ctx.lengthValue("DescribePostgresExtensionsResponse.InstalledExtensions.Length"); i++) {
+			Extension extension1 = new Extension();
+			extension1.setName(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Name"));
+			extension1.setDefaultVersion(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].DefaultVersion"));
+			extension1.setInstalledVersion(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].InstalledVersion"));
+			extension1.setComment(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Comment"));
+			extension1.setOwner(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Owner"));
+			extension1.setPriority(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Priority"));
+			extension1.setRequires(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Requires"));
+			extension1.setCategory(_ctx.stringValue("DescribePostgresExtensionsResponse.InstalledExtensions["+ i +"].Category"));
+
+			installedExtensions.add(extension1);
+		}
+		describePostgresExtensionsResponse.setInstalledExtensions(installedExtensions);
+	 
+	 	return describePostgresExtensionsResponse;
+	}
+}
