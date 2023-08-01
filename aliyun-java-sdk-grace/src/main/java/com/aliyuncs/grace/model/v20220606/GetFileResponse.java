ba7775c04fdeca44aa3c39ec45ffe726bf7d9b49
sdk-team <sdk-team@alibabacloud.com>
2023-08-01 06:42:20 +0000

    Initialize SDK.

---
+++ aliyun-java-sdk-grace/src/main/java/com/aliyuncs/grace/model/v20220606/GetFileResponse.java
@@ -0,0 +1,202 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.grace.model.v20220606;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.grace.transform.v20220606.GetFileResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetFileResponse extends AcsResponse {
+
+	private String type;
+
+	private Long size;
+
+	private Long creationTime;
+
+	private String displayName;
+
+	private String labels;
+
+	private Boolean shared;
+
+	private String transferState;
+
+	private String name;
+
+	private String requestId;
+
+	private AnalyzeProgress analyzeProgress;
+
+	private TransferProgress transferProgress;
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getSize() {
+		return this.size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	public Long getCreationTime() {
+		return this.creationTime;
+	}
+
+	public void setCreationTime(Long creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public String getDisplayName() {
+		return this.displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getLabels() {
+		return this.labels;
+	}
+
+	public void setLabels(String labels) {
+		this.labels = labels;
+	}
+
+	public Boolean getShared() {
+		return this.shared;
+	}
+
+	public void setShared(Boolean shared) {
+		this.shared = shared;
+	}
+
+	public String getTransferState() {
+		return this.transferState;
+	}
+
+	public void setTransferState(String transferState) {
+		this.transferState = transferState;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public AnalyzeProgress getAnalyzeProgress() {
+		return this.analyzeProgress;
+	}
+
+	public void setAnalyzeProgress(AnalyzeProgress analyzeProgress) {
+		this.analyzeProgress = analyzeProgress;
+	}
+
+	public TransferProgress getTransferProgress() {
+		return this.transferProgress;
+	}
+
+	public void setTransferProgress(TransferProgress transferProgress) {
+		this.transferProgress = transferProgress;
+	}
+
+	public static class AnalyzeProgress {
+
+		private String message;
+
+		private Double percent;
+
+		private String state;
+
+		public String getMessage() {
+			return this.message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		public Double getPercent() {
+			return this.percent;
+		}
+
+		public void setPercent(Double percent) {
+			this.percent = percent;
+		}
+
+		public String getState() {
+			return this.state;
+		}
+
+		public void setState(String state) {
+			this.state = state;
+		}
+	}
+
+	public static class TransferProgress {
+
+		private Long totalSize;
+
+		private Long transferredSize;
+
+		public Long getTotalSize() {
+			return this.totalSize;
+		}
+
+		public void setTotalSize(Long totalSize) {
+			this.totalSize = totalSize;
+		}
+
+		public Long getTransferredSize() {
+			return this.transferredSize;
+		}
+
+		public void setTransferredSize(Long transferredSize) {
+			this.transferredSize = transferredSize;
+		}
+	}
+
+	@Override
+	public GetFileResponse getInstance(UnmarshallerContext context) {
+		return	GetFileResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-grace/src/main/java/com/aliyuncs/grace/transform/v20220606/GetFileResponseUnmarshaller.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.grace.transform.v20220606;
+
+import com.aliyuncs.grace.model.v20220606.GetFileResponse;
+import com.aliyuncs.grace.model.v20220606.GetFileResponse.AnalyzeProgress;
+import com.aliyuncs.grace.model.v20220606.GetFileResponse.TransferProgress;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetFileResponseUnmarshaller {
+
+	public static GetFileResponse unmarshall(GetFileResponse getFileResponse, UnmarshallerContext _ctx) {
+		
+		getFileResponse.setType(_ctx.stringValue("GetFileResponse.type"));
+		getFileResponse.setSize(_ctx.longValue("GetFileResponse.size"));
+		getFileResponse.setCreationTime(_ctx.longValue("GetFileResponse.creationTime"));
+		getFileResponse.setDisplayName(_ctx.stringValue("GetFileResponse.displayName"));
+		getFileResponse.setLabels(_ctx.stringValue("GetFileResponse.labels"));
+		getFileResponse.setShared(_ctx.booleanValue("GetFileResponse.shared"));
+		getFileResponse.setTransferState(_ctx.stringValue("GetFileResponse.transferState"));
+		getFileResponse.setName(_ctx.stringValue("GetFileResponse.name"));
+		getFileResponse.setRequestId(_ctx.stringValue("GetFileResponse.requestId"));
+
+		AnalyzeProgress analyzeProgress = new AnalyzeProgress();
+		analyzeProgress.setMessage(_ctx.stringValue("GetFileResponse.analyzeProgress.message"));
+		analyzeProgress.setPercent(_ctx.doubleValue("GetFileResponse.analyzeProgress.percent"));
+		analyzeProgress.setState(_ctx.stringValue("GetFileResponse.analyzeProgress.state"));
+		getFileResponse.setAnalyzeProgress(analyzeProgress);
+
+		TransferProgress transferProgress = new TransferProgress();
+		transferProgress.setTotalSize(_ctx.longValue("GetFileResponse.transferProgress.totalSize"));
+		transferProgress.setTransferredSize(_ctx.longValue("GetFileResponse.transferProgress.transferredSize"));
+		getFileResponse.setTransferProgress(transferProgress);
+	 
+	 	return getFileResponse;
+	}
+}
