d20fa9b38872dc4bb12f535bad0134a4e7563963
sdk-team <sdk-team@alibabacloud.com>
2023-08-11 07:38:53 +0000

    Generated 2020-06-29 for alinlp.

---
+++ aliyun-java-sdk-alinlp/src/main/java/com/aliyuncs/alinlp/model/v20200629/PostMSConvSearchTokenGeneratedResponse.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alinlp.model.v20200629;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.alinlp.transform.v20200629.PostMSConvSearchTokenGeneratedResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class PostMSConvSearchTokenGeneratedResponse extends AcsResponse {
+
+	private Integer code;
+
+	private Boolean success;
+
+	private String msg;
+
+	private String requestId;
+
+	private String data;
+
+	private Integer httpStatusCode;
+
+	public Integer getCode() {
+		return this.code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMsg() {
+		return this.msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Integer getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(Integer httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+	}
+
+	@Override
+	public PostMSConvSearchTokenGeneratedResponse getInstance(UnmarshallerContext context) {
+		return	PostMSConvSearchTokenGeneratedResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
