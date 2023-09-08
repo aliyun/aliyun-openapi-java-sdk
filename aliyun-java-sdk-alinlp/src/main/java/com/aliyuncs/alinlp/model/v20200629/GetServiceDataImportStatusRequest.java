b93a9863308c5a3e74d1cb69ca212a337383f84e
sdk-team <sdk-team@alibabacloud.com>
2023-09-08 06:15:58 +0000

    Generated 2020-06-29 for alinlp.

---
+++ aliyun-java-sdk-alinlp/src/main/java/com/aliyuncs/alinlp/model/v20200629/GetServiceDataImportStatusRequest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alinlp.model.v20200629;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.alinlp.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetServiceDataImportStatusRequest extends RpcAcsRequest<GetServiceDataImportStatusResponse> {
+	   
+
+	@SerializedName("dataImportIds")
+	private List<Long> dataImportIds;
+	public GetServiceDataImportStatusRequest() {
+		super("alinlp", "2020-06-29", "GetServiceDataImportStatus", "alinlp");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public List<Long> getDataImportIds() {
+		return this.dataImportIds;
+	}
+
+	public void setDataImportIds(List<Long> dataImportIds) {
+		this.dataImportIds = dataImportIds;	
+		if (dataImportIds != null) {
+			putBodyParameter("DataImportIds" , new Gson().toJson(dataImportIds));
+		}	
+	}
+
+	@Override
+	public Class<GetServiceDataImportStatusResponse> getResponseClass() {
+		return GetServiceDataImportStatusResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-alinlp/src/main/java/com/aliyuncs/alinlp/model/v20200629/ImportServiceDataResponse.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.alinlp.model.v20200629;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.alinlp.transform.v20200629.ImportServiceDataResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ImportServiceDataResponse extends AcsResponse {
+
+	private Integer code;
+
+	private Long data;
+
+	private Boolean success;
+
+	private String msg;
+
+	private String requestId;
+
+	public Integer getCode() {
+		return this.code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public Long getData() {
+		return this.data;
+	}
+
+	public void setData(Long data) {
+		this.data = data;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMsg() {
+		return this.msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	@Override
+	public ImportServiceDataResponse getInstance(UnmarshallerContext context) {
+		return	ImportServiceDataResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
