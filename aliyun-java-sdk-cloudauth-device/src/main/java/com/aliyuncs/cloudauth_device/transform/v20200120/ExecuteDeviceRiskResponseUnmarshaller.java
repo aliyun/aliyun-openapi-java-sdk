c776465a05e7f9536fe01c1553d6702937f6a9eb
sdk-team <sdk-team@alibabacloud.com>
2023-11-23 07:32:06 +0000

    add riskItems field return

---
+++ aliyun-java-sdk-cloudauth-device/src/main/java/com/aliyuncs/cloudauth_device/transform/v20200120/ExecuteDeviceRiskResponseUnmarshaller.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudauth_device.transform.v20200120;
+
+import com.aliyuncs.cloudauth_device.model.v20200120.ExecuteDeviceRiskResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ExecuteDeviceRiskResponseUnmarshaller {
+
+	public static ExecuteDeviceRiskResponse unmarshall(ExecuteDeviceRiskResponse executeDeviceRiskResponse, UnmarshallerContext _ctx) {
+		
+		executeDeviceRiskResponse.setRequestId(_ctx.stringValue("ExecuteDeviceRiskResponse.RequestId"));
+		executeDeviceRiskResponse.setCode(_ctx.integerValue("ExecuteDeviceRiskResponse.Code"));
+		executeDeviceRiskResponse.setTags(_ctx.stringValue("ExecuteDeviceRiskResponse.Tags"));
+		executeDeviceRiskResponse.setSubCode(_ctx.stringValue("ExecuteDeviceRiskResponse.SubCode"));
+		executeDeviceRiskResponse.setUmid(_ctx.stringValue("ExecuteDeviceRiskResponse.Umid"));
+		executeDeviceRiskResponse.setRiskItems(_ctx.stringValue("ExecuteDeviceRiskResponse.RiskItems"));
+	 
+	 	return executeDeviceRiskResponse;
+	}
+}
---
+++ aliyun-java-sdk-cloudauth-device/src/main/java/com/aliyuncs/cloudauth_device/transform/v20200120/UploadDeviceInfosResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.cloudauth_device.transform.v20200120;
+
+import com.aliyuncs.cloudauth_device.model.v20200120.UploadDeviceInfosResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class UploadDeviceInfosResponseUnmarshaller {
+
+	public static UploadDeviceInfosResponse unmarshall(UploadDeviceInfosResponse uploadDeviceInfosResponse, UnmarshallerContext _ctx) {
+		
+		uploadDeviceInfosResponse.setRequestId(_ctx.stringValue("UploadDeviceInfosResponse.RequestId"));
+		uploadDeviceInfosResponse.setCode(_ctx.stringValue("UploadDeviceInfosResponse.Code"));
+		uploadDeviceInfosResponse.setMessage(_ctx.stringValue("UploadDeviceInfosResponse.Message"));
+		uploadDeviceInfosResponse.setResultObject(_ctx.booleanValue("UploadDeviceInfosResponse.ResultObject"));
+	 
+	 	return uploadDeviceInfosResponse;
+	}
+}
