f29d82d51b104ba7c07cedc091cea0051a2021b7
sdk-team <sdk-team@alibabacloud.com>
2024-01-08 03:32:39 +0000

    Add gb device create API.

---
+++ aliyun-java-sdk-linkvisual/src/main/java/com/aliyuncs/linkvisual/model/v20180120/CreateGbDeviceRequest.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.linkvisual.model.v20180120;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.linkvisual.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateGbDeviceRequest extends RpcAcsRequest<CreateGbDeviceResponse> {
+	   
+
+	private String subProductKey;
+
+	private String gbId;
+
+	private String description;
+
+	private Integer deviceType;
+
+	private String password;
+
+	private String iotInstanceId;
+
+	private String productKey;
+
+	private String mediaNetProtocol;
+	public CreateGbDeviceRequest() {
+		super("Linkvisual", "2018-01-20", "CreateGbDevice", "Linkvisual");
+		setMethod(MethodType.POST);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getSubProductKey() {
+		return this.subProductKey;
+	}
+
+	public void setSubProductKey(String subProductKey) {
+		this.subProductKey = subProductKey;
+		if(subProductKey != null){
+			putQueryParameter("SubProductKey", subProductKey);
+		}
+	}
+
+	public String getGbId() {
+		return this.gbId;
+	}
+
+	public void setGbId(String gbId) {
+		this.gbId = gbId;
+		if(gbId != null){
+			putQueryParameter("GbId", gbId);
+		}
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+		if(description != null){
+			putQueryParameter("Description", description);
+		}
+	}
+
+	public Integer getDeviceType() {
+		return this.deviceType;
+	}
+
+	public void setDeviceType(Integer deviceType) {
+		this.deviceType = deviceType;
+		if(deviceType != null){
+			putQueryParameter("DeviceType", deviceType.toString());
+		}
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+		if(password != null){
+			putQueryParameter("Password", password);
+		}
+	}
+
+	public String getIotInstanceId() {
+		return this.iotInstanceId;
+	}
+
+	public void setIotInstanceId(String iotInstanceId) {
+		this.iotInstanceId = iotInstanceId;
+		if(iotInstanceId != null){
+			putQueryParameter("IotInstanceId", iotInstanceId);
+		}
+	}
+
+	public String getProductKey() {
+		return this.productKey;
+	}
+
+	public void setProductKey(String productKey) {
+		this.productKey = productKey;
+		if(productKey != null){
+			putQueryParameter("ProductKey", productKey);
+		}
+	}
+
+	public String getMediaNetProtocol() {
+		return this.mediaNetProtocol;
+	}
+
+	public void setMediaNetProtocol(String mediaNetProtocol) {
+		this.mediaNetProtocol = mediaNetProtocol;
+		if(mediaNetProtocol != null){
+			putQueryParameter("MediaNetProtocol", mediaNetProtocol);
+		}
+	}
+
+	@Override
+	public Class<CreateGbDeviceResponse> getResponseClass() {
+		return CreateGbDeviceResponse.class;
+	}
+
+}
