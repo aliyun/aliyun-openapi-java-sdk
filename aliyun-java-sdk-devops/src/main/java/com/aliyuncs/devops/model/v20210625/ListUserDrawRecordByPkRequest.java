9dd7d714ba1b858a77aedb280a1535bd6cc9af5c
sdk-team <sdk-team@alibabacloud.com>
2024-02-27 08:57:52 +0000

    Fix ListUserKeys the field of title spell issue.

---
+++ aliyun-java-sdk-devops/src/main/java/com/aliyuncs/devops/model/v20210625/ListUserDrawRecordByPkRequest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.devops.model.v20210625;
+
+import com.aliyuncs.RoaAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListUserDrawRecordByPkRequest extends RoaAcsRequest<ListUserDrawRecordByPkResponse> {
+	   
+
+	private String aliyunPk;
+
+	private String drawGroup;
+
+	private String drawPoolName;
+	public ListUserDrawRecordByPkRequest() {
+		super("devops", "2021-06-25", "ListUserDrawRecordByPk");
+		setProtocol(ProtocolType.HTTPS);
+		setUriPattern("/listUserDrawRecords");
+		setMethod(MethodType.GET);
+	}
+
+	public String getAliyunPk() {
+		return this.aliyunPk;
+	}
+
+	public void setAliyunPk(String aliyunPk) {
+		this.aliyunPk = aliyunPk;
+		if(aliyunPk != null){
+			putQueryParameter("aliyunPk", aliyunPk);
+		}
+	}
+
+	public String getDrawGroup() {
+		return this.drawGroup;
+	}
+
+	public void setDrawGroup(String drawGroup) {
+		this.drawGroup = drawGroup;
+		if(drawGroup != null){
+			putQueryParameter("drawGroup", drawGroup);
+		}
+	}
+
+	public String getDrawPoolName() {
+		return this.drawPoolName;
+	}
+
+	public void setDrawPoolName(String drawPoolName) {
+		this.drawPoolName = drawPoolName;
+		if(drawPoolName != null){
+			putQueryParameter("drawPoolName", drawPoolName);
+		}
+	}
+
+	@Override
+	public Class<ListUserDrawRecordByPkResponse> getResponseClass() {
+		return ListUserDrawRecordByPkResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-devops/src/main/java/com/aliyuncs/devops/transform/v20210625/ListUserDrawRecordByPkResponseUnmarshaller.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.devops.transform.v20210625;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.devops.model.v20210625.ListUserDrawRecordByPkResponse;
+import com.aliyuncs.devops.model.v20210625.ListUserDrawRecordByPkResponse.Lingmadata;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ListUserDrawRecordByPkResponseUnmarshaller {
+
+	public static ListUserDrawRecordByPkResponse unmarshall(ListUserDrawRecordByPkResponse listUserDrawRecordByPkResponse, UnmarshallerContext _ctx) {
+		
+		listUserDrawRecordByPkResponse.setRequestId(_ctx.stringValue("ListUserDrawRecordByPkResponse.requestId"));
+		listUserDrawRecordByPkResponse.setSuccess(_ctx.booleanValue("ListUserDrawRecordByPkResponse.success"));
+		listUserDrawRecordByPkResponse.setCode(_ctx.stringValue("ListUserDrawRecordByPkResponse.code"));
+		listUserDrawRecordByPkResponse.setMessage(_ctx.stringValue("ListUserDrawRecordByPkResponse.message"));
+
+		List<Lingmadata> data = new ArrayList<Lingmadata>();
+		for (int i = 0; i < _ctx.lengthValue("ListUserDrawRecordByPkResponse.data.Length"); i++) {
+			Lingmadata lingmadata = new Lingmadata();
+			lingmadata.setUccId(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].uccId"));
+			lingmadata.setDrawGroup(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].drawGroup"));
+			lingmadata.setDrawPoolName(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].drawPoolName"));
+			lingmadata.setTaskGroupId(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].taskGroupId"));
+			lingmadata.setAliyunPk(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].aliyunPk"));
+			lingmadata.setGmtCreate(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].gmtCreate"));
+			lingmadata.setDrawResult(_ctx.stringValue("ListUserDrawRecordByPkResponse.data["+ i +"].drawResult"));
+
+			data.add(lingmadata);
+		}
+		listUserDrawRecordByPkResponse.setData(data);
+	 
+	 	return listUserDrawRecordByPkResponse;
+	}
+}
