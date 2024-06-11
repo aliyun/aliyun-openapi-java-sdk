3870ccaa48f29f89a7c87e2448060a89d4afc5fd
sdk-team <sdk-team@alibabacloud.com>
2024-06-11 02:28:46 +0000

    Generated 2023-01-20 for csas.

---
+++ aliyun-java-sdk-csas/src/main/java/com/aliyuncs/csas/model/v20230120/ExportUserDevicesResponse.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.csas.model.v20230120;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.csas.transform.v20230120.ExportUserDevicesResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ExportUserDevicesResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String signedUrl;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getSignedUrl() {
+		return this.signedUrl;
+	}
+
+	public void setSignedUrl(String signedUrl) {
+		this.signedUrl = signedUrl;
+	}
+
+	@Override
+	public ExportUserDevicesResponse getInstance(UnmarshallerContext context) {
+		return	ExportUserDevicesResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
