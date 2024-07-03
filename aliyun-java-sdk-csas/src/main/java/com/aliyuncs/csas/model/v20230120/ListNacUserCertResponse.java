a7ea2439d262e4cc0c7a13fc26c4b10c252ea405
sdk-team <sdk-team@alibabacloud.com>
2024-07-03 02:55:08 +0000

    Generated 2023-01-20 for csas.

---
+++ aliyun-java-sdk-csas/src/main/java/com/aliyuncs/csas/model/v20230120/ListNacUserCertResponse.java
@@ -0,0 +1,190 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.csas.model.v20230120;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.csas.transform.v20230120.ListNacUserCertResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListNacUserCertResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Long code;
+
+	private String message;
+
+	private Long totalNum;
+
+	private List<DataListItem> dataList;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Long getCode() {
+		return this.code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Long getTotalNum() {
+		return this.totalNum;
+	}
+
+	public void setTotalNum(Long totalNum) {
+		this.totalNum = totalNum;
+	}
+
+	public List<DataListItem> getDataList() {
+		return this.dataList;
+	}
+
+	public void setDataList(List<DataListItem> dataList) {
+		this.dataList = dataList;
+	}
+
+	public static class DataListItem {
+
+		private String username;
+
+		private String aliuid;
+
+		private String expiredTime;
+
+		private String department;
+
+		private String deviceType;
+
+		private String status;
+
+		private String userId;
+
+		private String devTag;
+
+		private String hostname;
+
+		private String mac;
+
+		public String getUsername() {
+			return this.username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getAliuid() {
+			return this.aliuid;
+		}
+
+		public void setAliuid(String aliuid) {
+			this.aliuid = aliuid;
+		}
+
+		public String getExpiredTime() {
+			return this.expiredTime;
+		}
+
+		public void setExpiredTime(String expiredTime) {
+			this.expiredTime = expiredTime;
+		}
+
+		public String getDepartment() {
+			return this.department;
+		}
+
+		public void setDepartment(String department) {
+			this.department = department;
+		}
+
+		public String getDeviceType() {
+			return this.deviceType;
+		}
+
+		public void setDeviceType(String deviceType) {
+			this.deviceType = deviceType;
+		}
+
+		public String getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public String getUserId() {
+			return this.userId;
+		}
+
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+
+		public String getDevTag() {
+			return this.devTag;
+		}
+
+		public void setDevTag(String devTag) {
+			this.devTag = devTag;
+		}
+
+		public String getHostname() {
+			return this.hostname;
+		}
+
+		public void setHostname(String hostname) {
+			this.hostname = hostname;
+		}
+
+		public String getMac() {
+			return this.mac;
+		}
+
+		public void setMac(String mac) {
+			this.mac = mac;
+		}
+	}
+
+	@Override
+	public ListNacUserCertResponse getInstance(UnmarshallerContext context) {
+		return	ListNacUserCertResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
