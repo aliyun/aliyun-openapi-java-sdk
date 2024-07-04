61678c5fdbfdd6e4c43cff36a3ae0aeb08b26658
Jackson Tian <shyvo1987@gmail.com>
2024-07-04 17:37:08 +0800

    Add test cases for OIDC credentials provider

--- java-sdk-function-test/src/test/java/com/aliyuncs/CredentialsTest.java
+++ java-sdk-function-test/src/test/java/com/aliyuncs/CredentialsTest.java
@@ -1,9 +1,11 @@
 package com.aliyuncs;
 
 import com.aliyuncs.auth.BasicCredentials;
+import com.aliyuncs.auth.OIDCCredentialsProvider;
 import com.aliyuncs.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.aliyuncs.ecs.model.v20140526.DescribeInstancesRequest;
 import com.aliyuncs.ecs.model.v20140526.DescribeInstancesResponse;
+import com.aliyuncs.sts.model.v20150401.*;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
 import org.junit.Assert;
@@ -12,13 +14,13 @@ import org.junit.Test;
 public class CredentialsTest extends BaseTest {
 
     @Test
-    public void sdkManageTokenTest() throws ClientException {
+    public void ramRoleArnProviderTest() throws ClientException {
         DefaultProfile profile = DefaultProfile.getProfile(this.regionId);
         BasicCredentials basicCredentials = new BasicCredentials(this.tokenAccesskeyId, this.tokenAccesskeySecret);
         STSAssumeRoleSessionCredentialsProvider provider = new STSAssumeRoleSessionCredentialsProvider(
-                basicCredentials,
-                this.roleArn,
-                profile
+            basicCredentials,
+            this.roleArn,
+            profile
         );
         DefaultAcsClient client = new DefaultAcsClient(profile, provider);
         DescribeInstancesRequest request = new DescribeInstancesRequest();
@@ -27,6 +29,20 @@ public class CredentialsTest extends BaseTest {
         Assert.assertTrue(0 <= response.getTotalCount());
     }
 
+    @Test
+    public void oidcProviderTest() throws ClientException {
+        DefaultProfile profile = DefaultProfile.getProfile(this.regionId);
+        String roleArn = System.getenv("ALIBABA_CLOUD_ROLE_ARN");
+        String tokenFilePath = System.getenv("ALIBABA_CLOUD_OIDC_TOKEN_FILE");
+        String providerArn = System.getenv("ALIBABA_CLOUD_OIDC_PROVIDER_ARN");
 
+        OIDCCredentialsProvider provider = new OIDCCredentialsProvider(roleArn, providerArn, tokenFilePath, "sessionname", this.regionId);
+        DefaultAcsClient client = new DefaultAcsClient(profile, provider);
+        GetCallerIdentityRequest request = new GetCallerIdentityRequest();
+        GetCallerIdentityResponse response = client.getAcsResponse(request);
+        Assert.assertNotNull(response);
+        Assert.assertEquals("AssumedRoleUser", response.getIdentityType());
+        Assert.assertTrue(response.getArn().endsWith("/sessionname"));
+    }
 
 }
