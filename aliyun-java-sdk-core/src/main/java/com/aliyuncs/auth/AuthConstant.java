c1a92218dd4b8e0a09640a7a0acff4f0850da671
Jackson Tian <shyvo1987@gmail.com>
2024-07-23 18:07:11 +0800

    refine ProfileCredentialsProvider

--- aliyun-java-sdk-core/src/main/java/com/aliyuncs/auth/AuthConstant.java
+++ aliyun-java-sdk-core/src/main/java/com/aliyuncs/auth/AuthConstant.java
@@ -12,6 +12,7 @@ public class AuthConstant {
     public static final String INI_TYPE_RAM = "ecs_ram_role";
     public static final String INI_TYPE_ARN = "ram_role_arn";
     public static final String INI_TYPE_KEY_PAIR = "rsa_key_pair";
+    public static final String INI_TYPE_ACESS_KEY = "access_key";
     public static final String INI_PUBLIC_KEY_ID = "public_key_id";
     public static final String INI_PRIVATE_KEY_FILE = "private_key_file";
     public static final String INI_PRIVATE_KEY = "private_key";
