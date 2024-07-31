5c949c7455271fd35ffd806ccd528ada5f2182a1
sdk-team <sdk-team@alibabacloud.com>
2024-07-31 01:50:08 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-java-sdk-dataworks-public/src/main/java/com/aliyuncs/dataworks_public/model/v20200518/ListClusterConfigsRequest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.dataworks_public.model.v20200518;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.dataworks_public.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ListClusterConfigsRequest extends RpcAcsRequest<ListClusterConfigsResponse> {
+	   
+
+	private String configType;
+
+	private Long clusterId;
+
+	private Long projectId;
+	public ListClusterConfigsRequest() {
+		super("dataworks-public", "2020-05-18", "ListClusterConfigs");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.GET);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getConfigType() {
+		return this.configType;
+	}
+
+	public void setConfigType(String configType) {
+		this.configType = configType;
+		if(configType != null){
+			putQueryParameter("ConfigType", configType);
+		}
+	}
+
+	public Long getClusterId() {
+		return this.clusterId;
+	}
+
+	public void setClusterId(Long clusterId) {
+		this.clusterId = clusterId;
+		if(clusterId != null){
+			putQueryParameter("ClusterId", clusterId.toString());
+		}
+	}
+
+	public Long getProjectId() {
+		return this.projectId;
+	}
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+		if(projectId != null){
+			putQueryParameter("ProjectId", projectId.toString());
+		}
+	}
+
+	@Override
+	public Class<ListClusterConfigsResponse> getResponseClass() {
+		return ListClusterConfigsResponse.class;
+	}
+
+}
