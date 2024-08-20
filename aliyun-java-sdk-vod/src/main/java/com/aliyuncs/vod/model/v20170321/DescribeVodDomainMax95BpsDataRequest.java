c9cfe672b8cef89037ca6eb6423c93a0e8ef8fe4
sdk-team <sdk-team@alibabacloud.com>
2024-08-20 03:26:28 +0000

    增加DescribeVodDomainMax95BpsData

---
+++ aliyun-java-sdk-vod/src/main/java/com/aliyuncs/vod/model/v20170321/DescribeVodDomainMax95BpsDataRequest.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.vod.model.v20170321;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.vod.Endpoint;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DescribeVodDomainMax95BpsDataRequest extends RpcAcsRequest<DescribeVodDomainMax95BpsDataResponse> {
+	   
+
+	private String startTime;
+
+	private String cycle;
+
+	private String domainName;
+
+	private String endTime;
+
+	private Long ownerId;
+
+	private String timePoint;
+	public DescribeVodDomainMax95BpsDataRequest() {
+		super("vod", "2017-03-21", "DescribeVodDomainMax95BpsData", "vod");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.GET);
+		try {
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointMap").set(this, Endpoint.endpointMap);
+			com.aliyuncs.AcsRequest.class.getDeclaredField("productEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
+	}
+
+	public String getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+		if(startTime != null){
+			putQueryParameter("StartTime", startTime);
+		}
+	}
+
+	public String getCycle() {
+		return this.cycle;
+	}
+
+	public void setCycle(String cycle) {
+		this.cycle = cycle;
+		if(cycle != null){
+			putQueryParameter("Cycle", cycle);
+		}
+	}
+
+	public String getDomainName() {
+		return this.domainName;
+	}
+
+	public void setDomainName(String domainName) {
+		this.domainName = domainName;
+		if(domainName != null){
+			putQueryParameter("DomainName", domainName);
+		}
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+		if(endTime != null){
+			putQueryParameter("EndTime", endTime);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public String getTimePoint() {
+		return this.timePoint;
+	}
+
+	public void setTimePoint(String timePoint) {
+		this.timePoint = timePoint;
+		if(timePoint != null){
+			putQueryParameter("TimePoint", timePoint);
+		}
+	}
+
+	@Override
+	public Class<DescribeVodDomainMax95BpsDataResponse> getResponseClass() {
+		return DescribeVodDomainMax95BpsDataResponse.class;
+	}
+
+}
