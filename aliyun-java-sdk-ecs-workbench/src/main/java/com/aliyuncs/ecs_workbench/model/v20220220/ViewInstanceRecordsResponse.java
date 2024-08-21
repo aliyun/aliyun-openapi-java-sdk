e72a39418733dc19d805331f0e23047cfef9a017
sdk-team <sdk-team@alibabacloud.com>
2024-08-21 09:56:30 +0000

    Support record for ecs.

---
+++ aliyun-java-sdk-ecs-workbench/src/main/java/com/aliyuncs/ecs_workbench/model/v20220220/ViewInstanceRecordsResponse.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecs_workbench.model.v20220220;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.ecs_workbench.transform.v20220220.ViewInstanceRecordsResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ViewInstanceRecordsResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Boolean success;
+
+	private String code;
+
+	private Boolean root;
+
+	private String message;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Boolean getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Boolean getRoot() {
+		return this.root;
+	}
+
+	public void setRoot(Boolean root) {
+		this.root = root;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public ViewInstanceRecordsResponse getInstance(UnmarshallerContext context) {
+		return	ViewInstanceRecordsResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-ecs-workbench/src/main/java/com/aliyuncs/ecs_workbench/transform/v20220220/ViewInstanceRecordsResponseUnmarshaller.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecs_workbench.transform.v20220220;
+
+import com.aliyuncs.ecs_workbench.model.v20220220.ViewInstanceRecordsResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class ViewInstanceRecordsResponseUnmarshaller {
+
+	public static ViewInstanceRecordsResponse unmarshall(ViewInstanceRecordsResponse viewInstanceRecordsResponse, UnmarshallerContext _ctx) {
+		
+		viewInstanceRecordsResponse.setRequestId(_ctx.stringValue("ViewInstanceRecordsResponse.RequestId"));
+		viewInstanceRecordsResponse.setSuccess(_ctx.booleanValue("ViewInstanceRecordsResponse.Success"));
+		viewInstanceRecordsResponse.setCode(_ctx.stringValue("ViewInstanceRecordsResponse.Code"));
+		viewInstanceRecordsResponse.setRoot(_ctx.booleanValue("ViewInstanceRecordsResponse.Root"));
+		viewInstanceRecordsResponse.setMessage(_ctx.stringValue("ViewInstanceRecordsResponse.Message"));
+	 
+	 	return viewInstanceRecordsResponse;
+	}
+}
