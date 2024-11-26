e57e7452617b46b3a585f3f10aba099949251a3b
sdk-team <sdk-team@alibabacloud.com>
2024-11-26 06:41:50 +0000

    Update DescribeApiMetering API.

---
+++ aliyun-java-sdk-market/src/main/java/com/aliyuncs/market/transform/v20151101/DescribeInstanceForIsvResponseUnmarshaller.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.market.transform.v20151101;
+
+import com.aliyuncs.market.model.v20151101.DescribeInstanceForIsvResponse;
+import com.aliyuncs.market.model.v20151101.DescribeInstanceForIsvResponse.RelationalData;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeInstanceForIsvResponseUnmarshaller {
+
+	public static DescribeInstanceForIsvResponse unmarshall(DescribeInstanceForIsvResponse describeInstanceForIsvResponse, UnmarshallerContext _ctx) {
+		
+		describeInstanceForIsvResponse.setRequestId(_ctx.stringValue("DescribeInstanceForIsvResponse.RequestId"));
+		describeInstanceForIsvResponse.setStatus(_ctx.stringValue("DescribeInstanceForIsvResponse.Status"));
+		describeInstanceForIsvResponse.setProductName(_ctx.stringValue("DescribeInstanceForIsvResponse.ProductName"));
+		describeInstanceForIsvResponse.setInstanceId(_ctx.longValue("DescribeInstanceForIsvResponse.InstanceId"));
+		describeInstanceForIsvResponse.setExtendJson(_ctx.stringValue("DescribeInstanceForIsvResponse.ExtendJson"));
+		describeInstanceForIsvResponse.setIsTrial(_ctx.booleanValue("DescribeInstanceForIsvResponse.IsTrial"));
+		describeInstanceForIsvResponse.setBeganOn(_ctx.longValue("DescribeInstanceForIsvResponse.BeganOn"));
+		describeInstanceForIsvResponse.setComponentJson(_ctx.stringValue("DescribeInstanceForIsvResponse.ComponentJson"));
+		describeInstanceForIsvResponse.setProductType(_ctx.stringValue("DescribeInstanceForIsvResponse.ProductType"));
+		describeInstanceForIsvResponse.setProductSkuCode(_ctx.stringValue("DescribeInstanceForIsvResponse.ProductSkuCode"));
+		describeInstanceForIsvResponse.setCreatedOn(_ctx.longValue("DescribeInstanceForIsvResponse.CreatedOn"));
+		describeInstanceForIsvResponse.setEndOn(_ctx.longValue("DescribeInstanceForIsvResponse.EndOn"));
+		describeInstanceForIsvResponse.setOrderId(_ctx.longValue("DescribeInstanceForIsvResponse.OrderId"));
+		describeInstanceForIsvResponse.setProductCode(_ctx.stringValue("DescribeInstanceForIsvResponse.ProductCode"));
+		describeInstanceForIsvResponse.setSupplierName(_ctx.stringValue("DescribeInstanceForIsvResponse.SupplierName"));
+
+		RelationalData relationalData = new RelationalData();
+		relationalData.setServiceStatus(_ctx.stringValue("DescribeInstanceForIsvResponse.RelationalData.ServiceStatus"));
+		describeInstanceForIsvResponse.setRelationalData(relationalData);
+	 
+	 	return describeInstanceForIsvResponse;
+	}
+}
---
+++ aliyun-java-sdk-market/src/main/java/com/aliyuncs/market/transform/v20151101/DescribeOrderForIsvResponseUnmarshaller.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.market.transform.v20151101;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.market.model.v20151101.DescribeOrderForIsvResponse;
+import java.util.Map;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class DescribeOrderForIsvResponseUnmarshaller {
+
+	public static DescribeOrderForIsvResponse unmarshall(DescribeOrderForIsvResponse describeOrderForIsvResponse, UnmarshallerContext _ctx) {
+		
+		describeOrderForIsvResponse.setRequestId(_ctx.stringValue("DescribeOrderForIsvResponse.RequestId"));
+		describeOrderForIsvResponse.setProductName(_ctx.stringValue("DescribeOrderForIsvResponse.ProductName"));
+		describeOrderForIsvResponse.setOriginalPrice(_ctx.floatValue("DescribeOrderForIsvResponse.OriginalPrice"));
+		describeOrderForIsvResponse.setAliUid(_ctx.longValue("DescribeOrderForIsvResponse.AliUid"));
+		describeOrderForIsvResponse.setCouponPrice(_ctx.floatValue("DescribeOrderForIsvResponse.CouponPrice"));
+		describeOrderForIsvResponse.setAccountQuantity(_ctx.longValue("DescribeOrderForIsvResponse.AccountQuantity"));
+		describeOrderForIsvResponse.setPeriodType(_ctx.stringValue("DescribeOrderForIsvResponse.PeriodType"));
+		describeOrderForIsvResponse.setPayStatus(_ctx.stringValue("DescribeOrderForIsvResponse.PayStatus"));
+		describeOrderForIsvResponse.setPaymentPrice(_ctx.floatValue("DescribeOrderForIsvResponse.PaymentPrice"));
+		describeOrderForIsvResponse.setOrderStatus(_ctx.stringValue("DescribeOrderForIsvResponse.OrderStatus"));
+		describeOrderForIsvResponse.setProductSkuCode(_ctx.stringValue("DescribeOrderForIsvResponse.ProductSkuCode"));
+		describeOrderForIsvResponse.setPaidOn(_ctx.longValue("DescribeOrderForIsvResponse.PaidOn"));
+		describeOrderForIsvResponse.setComponents(_ctx.mapValue("DescribeOrderForIsvResponse.Components"));
+		describeOrderForIsvResponse.setCreatedOn(_ctx.longValue("DescribeOrderForIsvResponse.CreatedOn"));
+		describeOrderForIsvResponse.setOrderType(_ctx.stringValue("DescribeOrderForIsvResponse.OrderType"));
+		describeOrderForIsvResponse.setTotalPrice(_ctx.floatValue("DescribeOrderForIsvResponse.TotalPrice"));
+		describeOrderForIsvResponse.setQuantity(_ctx.integerValue("DescribeOrderForIsvResponse.Quantity"));
+		describeOrderForIsvResponse.setOrderId(_ctx.longValue("DescribeOrderForIsvResponse.OrderId"));
+		describeOrderForIsvResponse.setProductCode(_ctx.stringValue("DescribeOrderForIsvResponse.ProductCode"));
+
+		List<String> instanceIds = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeOrderForIsvResponse.InstanceIds.Length"); i++) {
+			instanceIds.add(_ctx.stringValue("DescribeOrderForIsvResponse.InstanceIds["+ i +"]"));
+		}
+		describeOrderForIsvResponse.setInstanceIds(instanceIds);
+	 
+	 	return describeOrderForIsvResponse;
+	}
+}
