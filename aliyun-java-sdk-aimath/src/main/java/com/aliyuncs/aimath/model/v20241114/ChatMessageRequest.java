9d5a524e744c1d7290a8aca681b0daa8a9b57c7a
sdk-team <sdk-team@alibabacloud.com>
2025-01-24 06:59:53 +0000

    Init.

---
+++ aliyun-java-sdk-aimath/src/main/java/com/aliyuncs/aimath/model/v20241114/ChatMessageRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.aimath.model.v20241114;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class ChatMessageRequest extends RpcAcsRequest<ChatMessageResponse> {
+	   
+
+	private String conversationId;
+
+	private String userId;
+
+	private String content;
+	public ChatMessageRequest() {
+		super("AIMath", "2024-11-14", "ChatMessage");
+		setMethod(MethodType.POST);
+	}
+
+	public String getConversationId() {
+		return this.conversationId;
+	}
+
+	public void setConversationId(String conversationId) {
+		this.conversationId = conversationId;
+		if(conversationId != null){
+			putBodyParameter("ConversationId", conversationId);
+		}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putBodyParameter("UserId", userId);
+		}
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+		if(content != null){
+			putBodyParameter("Content", content);
+		}
+	}
+
+	@Override
+	public Class<ChatMessageResponse> getResponseClass() {
+		return ChatMessageResponse.class;
+	}
+
+}
