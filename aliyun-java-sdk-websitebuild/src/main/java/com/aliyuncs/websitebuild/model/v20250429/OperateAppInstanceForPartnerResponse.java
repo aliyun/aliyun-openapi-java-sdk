a48818c21ee7af0f5f03b7930cbbd3fe764f67b4
sdk-team <sdk-team@alibabacloud.com>
2025-06-12 08:40:05 +0000

    Generated 2025-04-29 for WebsiteBuild.

---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/model/v20250429/OperateAppInstanceForPartnerResponse.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.model.v20250429;
+
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.websitebuild.transform.v20250429.OperateAppInstanceForPartnerResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class OperateAppInstanceForPartnerResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String errorMsg;
+
+	private String success;
+
+	private String errorCode;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+	public String getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+
+	public String getErrorCode() {
+		return this.errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	@Override
+	public OperateAppInstanceForPartnerResponse getInstance(UnmarshallerContext context) {
+		return	OperateAppInstanceForPartnerResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/transform/v20250429/OperateAppInstanceForPartnerResponseUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.transform.v20250429;
+
+import com.aliyuncs.websitebuild.model.v20250429.OperateAppInstanceForPartnerResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class OperateAppInstanceForPartnerResponseUnmarshaller {
+
+	public static OperateAppInstanceForPartnerResponse unmarshall(OperateAppInstanceForPartnerResponse operateAppInstanceForPartnerResponse, UnmarshallerContext _ctx) {
+		
+		operateAppInstanceForPartnerResponse.setRequestId(_ctx.stringValue("OperateAppInstanceForPartnerResponse.RequestId"));
+		operateAppInstanceForPartnerResponse.setErrorMsg(_ctx.stringValue("OperateAppInstanceForPartnerResponse.ErrorMsg"));
+		operateAppInstanceForPartnerResponse.setSuccess(_ctx.stringValue("OperateAppInstanceForPartnerResponse.Success"));
+		operateAppInstanceForPartnerResponse.setErrorCode(_ctx.stringValue("OperateAppInstanceForPartnerResponse.ErrorCode"));
+	 
+	 	return operateAppInstanceForPartnerResponse;
+	}
+}
