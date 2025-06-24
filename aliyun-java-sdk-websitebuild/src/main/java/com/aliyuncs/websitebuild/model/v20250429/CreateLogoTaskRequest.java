99e078d1264ff9e6a23c76c434bd69ff07f9b1c3
sdk-team <sdk-team@alibabacloud.com>
2025-06-24 16:03:36 +0000

    Generated 2025-04-29 for WebsiteBuild.

---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/model/v20250429/CreateLogoTaskRequest.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.model.v20250429;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateLogoTaskRequest extends RpcAcsRequest<CreateLogoTaskResponse> {
+	   
+
+	private String negativePrompt;
+
+	private String version;
+
+	private String prompt;
+
+	private String parameters;
+	public CreateLogoTaskRequest() {
+		super("WebsiteBuild", "2025-04-29", "CreateLogoTask");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.POST);
+	}
+
+	public String getNegativePrompt() {
+		return this.negativePrompt;
+	}
+
+	public void setNegativePrompt(String negativePrompt) {
+		this.negativePrompt = negativePrompt;
+		if(negativePrompt != null){
+			putQueryParameter("NegativePrompt", negativePrompt);
+		}
+	}
+
+	public String getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+		if(version != null){
+			putQueryParameter("Version", version);
+		}
+	}
+
+	public String getPrompt() {
+		return this.prompt;
+	}
+
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
+		if(prompt != null){
+			putQueryParameter("Prompt", prompt);
+		}
+	}
+
+	public String getParameters() {
+		return this.parameters;
+	}
+
+	public void setParameters(String parameters) {
+		this.parameters = parameters;
+		if(parameters != null){
+			putQueryParameter("Parameters", parameters);
+		}
+	}
+
+	@Override
+	public Class<CreateLogoTaskResponse> getResponseClass() {
+		return CreateLogoTaskResponse.class;
+	}
+
+}
