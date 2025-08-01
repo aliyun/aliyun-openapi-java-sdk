31b5c5f3245a4c997453de5cbf4e15f0654f601f
sdk-team <sdk-team@alibabacloud.com>
2025-08-01 10:41:41 +0000

    Add image search api.

---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/model/v20250429/SearchImageResponse.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.model.v20250429;
+
+import java.util.List;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.websitebuild.transform.v20250429.SearchImageResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class SearchImageResponse extends AcsResponse {
+
+	private String requestId;
+
+	private String errorMsg;
+
+	private String success;
+
+	private String errorCode;
+
+	private ImageResponse imageResponse;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+	public String getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+
+	public String getErrorCode() {
+		return this.errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public ImageResponse getImageResponse() {
+		return this.imageResponse;
+	}
+
+	public void setImageResponse(ImageResponse imageResponse) {
+		this.imageResponse = imageResponse;
+	}
+
+	public static class ImageResponse {
+
+		private Integer maxResults;
+
+		private String nextToken;
+
+		private List<Image> imageList;
+
+		public Integer getMaxResults() {
+			return this.maxResults;
+		}
+
+		public void setMaxResults(Integer maxResults) {
+			this.maxResults = maxResults;
+		}
+
+		public String getNextToken() {
+			return this.nextToken;
+		}
+
+		public void setNextToken(String nextToken) {
+			this.nextToken = nextToken;
+		}
+
+		public List<Image> getImageList() {
+			return this.imageList;
+		}
+
+		public void setImageList(List<Image> imageList) {
+			this.imageList = imageList;
+		}
+
+		public static class Image {
+
+			private String imageRatio;
+
+			private String descriptiveTones;
+
+			private String ossKey;
+
+			private String imageCategory;
+
+			private Integer height;
+
+			private String quantitativePalette;
+
+			private String imageUuid;
+
+			private Integer width;
+
+			private String url;
+
+			private String tagsFromImage;
+
+			public String getImageRatio() {
+				return this.imageRatio;
+			}
+
+			public void setImageRatio(String imageRatio) {
+				this.imageRatio = imageRatio;
+			}
+
+			public String getDescriptiveTones() {
+				return this.descriptiveTones;
+			}
+
+			public void setDescriptiveTones(String descriptiveTones) {
+				this.descriptiveTones = descriptiveTones;
+			}
+
+			public String getOssKey() {
+				return this.ossKey;
+			}
+
+			public void setOssKey(String ossKey) {
+				this.ossKey = ossKey;
+			}
+
+			public String getImageCategory() {
+				return this.imageCategory;
+			}
+
+			public void setImageCategory(String imageCategory) {
+				this.imageCategory = imageCategory;
+			}
+
+			public Integer getHeight() {
+				return this.height;
+			}
+
+			public void setHeight(Integer height) {
+				this.height = height;
+			}
+
+			public String getQuantitativePalette() {
+				return this.quantitativePalette;
+			}
+
+			public void setQuantitativePalette(String quantitativePalette) {
+				this.quantitativePalette = quantitativePalette;
+			}
+
+			public String getImageUuid() {
+				return this.imageUuid;
+			}
+
+			public void setImageUuid(String imageUuid) {
+				this.imageUuid = imageUuid;
+			}
+
+			public Integer getWidth() {
+				return this.width;
+			}
+
+			public void setWidth(Integer width) {
+				this.width = width;
+			}
+
+			public String getUrl() {
+				return this.url;
+			}
+
+			public void setUrl(String url) {
+				this.url = url;
+			}
+
+			public String getTagsFromImage() {
+				return this.tagsFromImage;
+			}
+
+			public void setTagsFromImage(String tagsFromImage) {
+				this.tagsFromImage = tagsFromImage;
+			}
+		}
+	}
+
+	@Override
+	public SearchImageResponse getInstance(UnmarshallerContext context) {
+		return	SearchImageResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/transform/v20250429/SearchImageResponseUnmarshaller.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.transform.v20250429;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.websitebuild.model.v20250429.SearchImageResponse;
+import com.aliyuncs.websitebuild.model.v20250429.SearchImageResponse.ImageResponse;
+import com.aliyuncs.websitebuild.model.v20250429.SearchImageResponse.ImageResponse.Image;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class SearchImageResponseUnmarshaller {
+
+	public static SearchImageResponse unmarshall(SearchImageResponse searchImageResponse, UnmarshallerContext _ctx) {
+		
+		searchImageResponse.setRequestId(_ctx.stringValue("SearchImageResponse.RequestId"));
+		searchImageResponse.setErrorMsg(_ctx.stringValue("SearchImageResponse.ErrorMsg"));
+		searchImageResponse.setSuccess(_ctx.stringValue("SearchImageResponse.Success"));
+		searchImageResponse.setErrorCode(_ctx.stringValue("SearchImageResponse.ErrorCode"));
+
+		ImageResponse imageResponse = new ImageResponse();
+		imageResponse.setMaxResults(_ctx.integerValue("SearchImageResponse.ImageResponse.MaxResults"));
+		imageResponse.setNextToken(_ctx.stringValue("SearchImageResponse.ImageResponse.NextToken"));
+
+		List<Image> imageList = new ArrayList<Image>();
+		for (int i = 0; i < _ctx.lengthValue("SearchImageResponse.ImageResponse.ImageList.Length"); i++) {
+			Image image = new Image();
+			image.setImageRatio(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].ImageRatio"));
+			image.setDescriptiveTones(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].DescriptiveTones"));
+			image.setOssKey(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].OssKey"));
+			image.setImageCategory(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].ImageCategory"));
+			image.setHeight(_ctx.integerValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].Height"));
+			image.setQuantitativePalette(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].QuantitativePalette"));
+			image.setImageUuid(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].ImageUuid"));
+			image.setWidth(_ctx.integerValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].Width"));
+			image.setUrl(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].Url"));
+			image.setTagsFromImage(_ctx.stringValue("SearchImageResponse.ImageResponse.ImageList["+ i +"].TagsFromImage"));
+
+			imageList.add(image);
+		}
+		imageResponse.setImageList(imageList);
+		searchImageResponse.setImageResponse(imageResponse);
+	 
+	 	return searchImageResponse;
+	}
+}
