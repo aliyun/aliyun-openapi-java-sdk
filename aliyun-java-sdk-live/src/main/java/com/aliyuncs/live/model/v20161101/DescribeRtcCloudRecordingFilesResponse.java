9a63c2df8ecaa4729426bcb7e470b5139e2662f0
sdk-team <sdk-team@alibabacloud.com>
2025-08-13 02:01:41 +0000

    Generated 2016-11-01 for live.

--- aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/model/v20161101/DescribeRtcCloudRecordingFilesResponse.java
+++ aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/model/v20161101/DescribeRtcCloudRecordingFilesResponse.java
@@ -14,6 +14,7 @@
 
 package com.aliyuncs.live.model.v20161101;
 
+import java.util.List;
 import com.aliyuncs.AcsResponse;
 import com.aliyuncs.live.transform.v20161101.DescribeRtcCloudRecordingFilesResponseUnmarshaller;
 import com.aliyuncs.transform.UnmarshallerContext;
@@ -23,6 +24,135 @@ import com.aliyuncs.transform.UnmarshallerContext;
  * @version 
  */
 public class DescribeRtcCloudRecordingFilesResponse extends AcsResponse {
+
+	private String requestId;
+
+	private TaskInfo taskInfo;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public TaskInfo getTaskInfo() {
+		return this.taskInfo;
+	}
+
+	public void setTaskInfo(TaskInfo taskInfo) {
+		this.taskInfo = taskInfo;
+	}
+
+	public static class TaskInfo {
+
+		private String taskId;
+
+		private String status;
+
+		private RecordFileList recordFileList;
+
+		public String getTaskId() {
+			return this.taskId;
+		}
+
+		public void setTaskId(String taskId) {
+			this.taskId = taskId;
+		}
+
+		public String getStatus() {
+			return this.status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public RecordFileList getRecordFileList() {
+			return this.recordFileList;
+		}
+
+		public void setRecordFileList(RecordFileList recordFileList) {
+			this.recordFileList = recordFileList;
+		}
+
+		public static class RecordFileList {
+
+			private List<MediaInfo> vodMediaList;
+
+			private List<String> mp3FileList;
+
+			private List<String> mp4FileList;
+
+			private List<String> hlsFileList;
+
+			public List<MediaInfo> getVodMediaList() {
+				return this.vodMediaList;
+			}
+
+			public void setVodMediaList(List<MediaInfo> vodMediaList) {
+				this.vodMediaList = vodMediaList;
+			}
+
+			public List<String> getMp3FileList() {
+				return this.mp3FileList;
+			}
+
+			public void setMp3FileList(List<String> mp3FileList) {
+				this.mp3FileList = mp3FileList;
+			}
+
+			public List<String> getMp4FileList() {
+				return this.mp4FileList;
+			}
+
+			public void setMp4FileList(List<String> mp4FileList) {
+				this.mp4FileList = mp4FileList;
+			}
+
+			public List<String> getHlsFileList() {
+				return this.hlsFileList;
+			}
+
+			public void setHlsFileList(List<String> hlsFileList) {
+				this.hlsFileList = hlsFileList;
+			}
+
+			public static class MediaInfo {
+
+				private String stream;
+
+				private List<String> mediaIds;
+
+				private List<String> mergedIds;
+
+				public String getStream() {
+					return this.stream;
+				}
+
+				public void setStream(String stream) {
+					this.stream = stream;
+				}
+
+				public List<String> getMediaIds() {
+					return this.mediaIds;
+				}
+
+				public void setMediaIds(List<String> mediaIds) {
+					this.mediaIds = mediaIds;
+				}
+
+				public List<String> getMergedIds() {
+					return this.mergedIds;
+				}
+
+				public void setMergedIds(List<String> mergedIds) {
+					this.mergedIds = mergedIds;
+				}
+			}
+		}
+	}
 
 	@Override
 	public DescribeRtcCloudRecordingFilesResponse getInstance(UnmarshallerContext context) {
--- aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/transform/v20161101/DescribeRtcCloudRecordingFilesResponseUnmarshaller.java
+++ aliyun-java-sdk-live/src/main/java/com/aliyuncs/live/transform/v20161101/DescribeRtcCloudRecordingFilesResponseUnmarshaller.java
@@ -14,7 +14,13 @@
 
 package com.aliyuncs.live.transform.v20161101;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.aliyuncs.live.model.v20161101.DescribeRtcCloudRecordingFilesResponse;
+import com.aliyuncs.live.model.v20161101.DescribeRtcCloudRecordingFilesResponse.TaskInfo;
+import com.aliyuncs.live.model.v20161101.DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList;
+import com.aliyuncs.live.model.v20161101.DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.MediaInfo;
 import com.aliyuncs.transform.UnmarshallerContext;
 
 
@@ -22,6 +28,54 @@ public class DescribeRtcCloudRecordingFilesResponseUnmarshaller {
 
 	public static DescribeRtcCloudRecordingFilesResponse unmarshall(DescribeRtcCloudRecordingFilesResponse describeRtcCloudRecordingFilesResponse, UnmarshallerContext _ctx) {
 		
+		describeRtcCloudRecordingFilesResponse.setRequestId(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.RequestId"));
+
+		TaskInfo taskInfo = new TaskInfo();
+		taskInfo.setTaskId(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.TaskId"));
+		taskInfo.setStatus(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.Status"));
+
+		RecordFileList recordFileList = new RecordFileList();
+
+		List<String> mp3FileList = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.Mp3FileList.Length"); i++) {
+			mp3FileList.add(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.Mp3FileList["+ i +"]"));
+		}
+		recordFileList.setMp3FileList(mp3FileList);
+
+		List<String> mp4FileList = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.Mp4FileList.Length"); i++) {
+			mp4FileList.add(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.Mp4FileList["+ i +"]"));
+		}
+		recordFileList.setMp4FileList(mp4FileList);
+
+		List<String> hlsFileList = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.HlsFileList.Length"); i++) {
+			hlsFileList.add(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.HlsFileList["+ i +"]"));
+		}
+		recordFileList.setHlsFileList(hlsFileList);
+
+		List<MediaInfo> vodMediaList = new ArrayList<MediaInfo>();
+		for (int i = 0; i < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList.Length"); i++) {
+			MediaInfo mediaInfo = new MediaInfo();
+			mediaInfo.setStream(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList["+ i +"].Stream"));
+
+			List<String> mediaIds = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList["+ i +"].MediaIds.Length"); j++) {
+				mediaIds.add(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList["+ i +"].MediaIds["+ j +"]"));
+			}
+			mediaInfo.setMediaIds(mediaIds);
+
+			List<String> mergedIds = new ArrayList<String>();
+			for (int j = 0; j < _ctx.lengthValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList["+ i +"].MergedIds.Length"); j++) {
+				mergedIds.add(_ctx.stringValue("DescribeRtcCloudRecordingFilesResponse.TaskInfo.RecordFileList.VodMediaList["+ i +"].MergedIds["+ j +"]"));
+			}
+			mediaInfo.setMergedIds(mergedIds);
+
+			vodMediaList.add(mediaInfo);
+		}
+		recordFileList.setVodMediaList(vodMediaList);
+		taskInfo.setRecordFileList(recordFileList);
+		describeRtcCloudRecordingFilesResponse.setTaskInfo(taskInfo);
 	 
 	 	return describeRtcCloudRecordingFilesResponse;
 	}
