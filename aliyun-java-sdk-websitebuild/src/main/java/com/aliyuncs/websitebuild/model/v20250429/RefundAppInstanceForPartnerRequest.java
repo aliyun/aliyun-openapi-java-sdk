6287b2e8ef4fdd8b98a0ddb08f5c5bfc467e9762
sdk-team <sdk-team@alibabacloud.com>
2026-01-15 07:26:20 +0000

    Generated 2025-04-29 for WebsiteBuild.

---
+++ aliyun-java-sdk-websitebuild/src/main/java/com/aliyuncs/websitebuild/model/v20250429/RefundAppInstanceForPartnerRequest.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.websitebuild.model.v20250429;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class RefundAppInstanceForPartnerRequest extends RpcAcsRequest<RefundAppInstanceForPartnerResponse> {
+	   
+
+	private String refundReason;
+
+	private String userId;
+
+	private String bizId;
+	public RefundAppInstanceForPartnerRequest() {
+		super("WebsiteBuild", "2025-04-29", "RefundAppInstanceForPartner");
+		setProtocol(ProtocolType.HTTPS);
+		setMethod(MethodType.POST);
+	}
+
+	public String getRefundReason() {
+		return this.refundReason;
+	}
+
+	public void setRefundReason(String refundReason) {
+		this.refundReason = refundReason;
+		if(refundReason != null){
+			putQueryParameter("RefundReason", refundReason);
+		}
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+		if(userId != null){
+			putQueryParameter("UserId", userId);
+		}
+	}
+
+	public String getBizId() {
+		return this.bizId;
+	}
+
+	public void setBizId(String bizId) {
+		this.bizId = bizId;
+		if(bizId != null){
+			putQueryParameter("BizId", bizId);
+		}
+	}
+
+	@Override
+	public Class<RefundAppInstanceForPartnerResponse> getResponseClass() {
+		return RefundAppInstanceForPartnerResponse.class;
+	}
+
+}
