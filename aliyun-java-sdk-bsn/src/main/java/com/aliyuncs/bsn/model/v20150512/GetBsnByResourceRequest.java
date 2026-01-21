664d2dcdb2c23bf8a83f1de8a8eb12bfc069a844
sdk-team <sdk-team@alibabacloud.com>
2026-01-21 13:27:56 +0000

    Generated 2015-05-12 for Bsn.

---
+++ aliyun-java-sdk-bsn/src/main/java/com/aliyuncs/bsn/model/v20150512/GetBsnByResourceRequest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.bsn.model.v20150512;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class GetBsnByResourceRequest extends RpcAcsRequest<GetBsnByResourceResponse> {
+	   
+
+	private String resourceId;
+
+	private Integer resourceType;
+
+	private Long aliuid;
+	public GetBsnByResourceRequest() {
+		super("Bsn", "2015-05-12", "GetBsnByResource");
+		setMethod(MethodType.POST);
+	}
+
+	public String getResourceId() {
+		return this.resourceId;
+	}
+
+	public void setResourceId(String resourceId) {
+		this.resourceId = resourceId;
+		if(resourceId != null){
+			putQueryParameter("resourceId", resourceId);
+		}
+	}
+
+	public Integer getResourceType() {
+		return this.resourceType;
+	}
+
+	public void setResourceType(Integer resourceType) {
+		this.resourceType = resourceType;
+		if(resourceType != null){
+			putQueryParameter("resourceType", resourceType.toString());
+		}
+	}
+
+	public Long getAliuid() {
+		return this.aliuid;
+	}
+
+	public void setAliuid(Long aliuid) {
+		this.aliuid = aliuid;
+		if(aliuid != null){
+			putQueryParameter("aliuid", aliuid.toString());
+		}
+	}
+
+	@Override
+	public Class<GetBsnByResourceResponse> getResponseClass() {
+		return GetBsnByResourceResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-bsn/src/main/java/com/aliyuncs/bsn/transform/v20150512/GetBsnByResourceResponseUnmarshaller.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.bsn.transform.v20150512;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyuncs.bsn.model.v20150512.GetBsnByResourceResponse;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+
+public class GetBsnByResourceResponseUnmarshaller {
+
+	public static GetBsnByResourceResponse unmarshall(GetBsnByResourceResponse getBsnByResourceResponse, UnmarshallerContext _ctx) {
+		
+
+		List<String> datas = new ArrayList<String>();
+		for (int i = 0; i < _ctx.lengthValue("GetBsnByResourceResponse.datas.Length"); i++) {
+			datas.add(_ctx.stringValue("GetBsnByResourceResponse.datas["+ i +"]"));
+		}
+		getBsnByResourceResponse.setDatas(datas);
+	 
+	 	return getBsnByResourceResponse;
+	}
+}
